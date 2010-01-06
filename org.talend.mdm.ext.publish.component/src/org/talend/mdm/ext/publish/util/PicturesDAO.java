9fcfe42557e6e09b7e2ff7712e046d461ee2bb7c
ctoum <ctoum@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-06 17:52:12 +0000

    enterprise add-on
    git-svn-id: http://talendforge.org/svn/tom/trunk@35172 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.ext.publish.component/src/org/talend/mdm/ext/publish/util/PicturesDAO.java
+++
@@ -1,9 +0,0 @@
-package org.talend.mdm.ext.publish.util;
-
-import com.amalto.core.util.XtentisException;
-
-public interface PicturesDAO {
-	
-	public String[] getAllPKs() throws XtentisException;
-	
-}
