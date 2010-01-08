038f5bd6da684c14218c0b828829f20fd16b3d6e
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-08 10:28:19 +0000

    remove workflow exception while startup
    git-svn-id: http://talendforge.org/svn/tom/trunk@35313 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/objects/configurationinfo/assemble/StartServiceSubProc.java
+++ org.talend.mdm.core/src/com/amalto/core/objects/configurationinfo/assemble/StartServiceSubProc.java
@@ -1,5 +1,9 @@
 package com.amalto.core.objects.configurationinfo.assemble;
 
+import org.talend.mdm.commmon.util.core.MDMConfiguration;
+
+import com.amalto.core.util.Util;
+
 
 
 public class StartServiceSubProc extends AssembleSubProc{
@@ -9,17 +13,21 @@ public class StartServiceSubProc extends AssembleSubProc{
 	public void run() throws Exception {
 		
     	//workflow service
-		Object workflowService= 
-			com.amalto.core.util.Util.retrieveComponent(
-				null, 
-				"amalto/local/service/workflow"
-			);
 		
-		com.amalto.core.util.Util.getMethod(workflowService, "start").invoke(
-				workflowService,
-				new Object[] {}
-			);
-		
+		if (Util.isEnterprise()) {
+			
+			Object workflowService= 
+				com.amalto.core.util.Util.retrieveComponent(
+					null, 
+					"amalto/local/service/workflow"
+				);
+			
+			com.amalto.core.util.Util.getMethod(workflowService, "start").invoke(
+					workflowService,
+					new Object[] {}
+				);
+			
+		}
 		
 	}
 	
