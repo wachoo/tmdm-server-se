35c3ebcbf8e5eaa9bddf532694e641c37723142c
yguo <yguo@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-27 08:07:43 +0000

    code review.
    git-svn-id: http://talendforge.org/svn/tom/trunk@36431 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.xmldb/src/com/amalto/xmldb/QueryBuilder.java
+++ org.talend.mdm.xmldb/src/com/amalto/xmldb/QueryBuilder.java
@@ -357,63 +357,39 @@ public class QueryBuilder {
 				//where = XPathUtils.factor(wc.getRightValueOrPath(),pivots)+" = "+factorPivots; //Join error aiming added
 				where = "matches("+factorPivots+", \""+encoded+"\",\"i\") ";
 			} else	 if(operator.equals(WhereCondition.EQUALS)) {
-				if(isNum) {
+				if (isNum) {
 					where = "number("+factorPivots+") eq "+encoded;
-				} 
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + "= " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else{
+				} else  {
 					where = factorPivots+" eq \""+encoded+"\"";
 				}
 			} else if(operator.equals(WhereCondition.NOT_EQUALS)) {
 				if (isNum) {
 					where = "number("+factorPivots+") ne "+encoded;
-				}
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + " != " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else {
+				} else {
 					where = factorPivots+" ne \""+encoded+"\"";
 				}
 			} else	 if(operator.equals(WhereCondition.GREATER_THAN)) {
 				if (isNum) {
 					where = "number("+factorPivots+") gt "+encoded;
-				}
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + "> " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else {
+				} else {
 					where = factorPivots+" gt \""+encoded+"\"";
 				}
 			} else	if(operator.equals(WhereCondition.GREATER_THAN_OR_EQUAL)) {
 				if (isNum) {
 					where = "number("+factorPivots+") ge "+encoded;
-				}
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + " >= " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else {
+				} else {
 					where = factorPivots+" ge \""+encoded+"\"";
 				}
 			} else if(operator.equals(WhereCondition.LOWER_THAN)) {
 				if (isNum) {
 					where = "number("+factorPivots+") lt "+encoded;
-				} 
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + " < " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else {
+				} else {
 					where = factorPivots+" lt \""+encoded+"\"";
 				}
 			} else	if(operator.equals(WhereCondition.LOWER_THAN_OR_EQUAL)) {
 				if (isNum) {
 					where = "number("+factorPivots+") le "+encoded;
-				} 
-				else if(wc.getRightValueOrPath().endsWith("()")) {
-					where = factorPivots + " <= " + encoded.replaceAll("\\(\\)","(function)");
-				}
-				else {
+				} else {
 					where = factorPivots+" le \""+encoded+"\"";
 				}
 			} else	if(operator.equals(WhereCondition.NO_OPERATOR)) {
@@ -526,7 +502,6 @@ public class QueryBuilder {
 	    	query=query.replaceAll("\\(\\)","(1=1)");
 	    	//replace (1=1) and to ""
 	    	query=query.replaceAll("\\(1=1\\) and","");
-	    	query=query.replaceAll("(function)","");
 	    	System.out.println("query:\n");
 	    	System.out.println(query);
 	    	return query;
