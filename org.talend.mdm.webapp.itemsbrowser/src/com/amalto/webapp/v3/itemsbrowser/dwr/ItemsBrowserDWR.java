74ae0ff68b09c76e92bcfa4271dc7c869c2b6b31
yguo <yguo@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-03-23 09:35:50 +0000

    bug fixed.
    git-svn-id: http://talendforge.org/svn/tom/trunk@38959 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser/src/com/amalto/webapp/v3/itemsbrowser/dwr/ItemsBrowserDWR.java
+++ org.talend.mdm.webapp.itemsbrowser/src/com/amalto/webapp/v3/itemsbrowser/dwr/ItemsBrowserDWR.java
@@ -518,40 +518,42 @@ public class ItemsBrowserDWR {
 				if(maxOccurs<0 || maxOccurs>1){
 					NodeList nodeList = Util.getNodeList(d,xpath);
                
-					if(infos != null && treeNode.isRetrieveFKinfos()) {
-   					for (int i = 0; i < nodeList.getLength(); i++) {
-   						if(!treeNode.isReadOnly())
-   							nodeAutorization.add(xpath+"["+(i+1)+"]");
-   						idToXpath.put(nodeCount,xpath+"["+(i+1)+"]");
-   						TreeNode treeNodeTmp = (TreeNode) treeNode.clone();
-   						String value = StringEscapeUtils.escapeHtml(nodeList.item(i).getTextContent());
-   						String jasonData = getForeignKeyList(0, 10, value,  treeNode.getForeignKey(), keyInfos, false);
-   						treeNode.setValueInfo(jasonData);
-   						if(nodeList.item(i).getFirstChild()!=null)
-   						{
-   							treeNodeTmp.setValue(nodeList.item(i).getFirstChild().getNodeValue());
-   							if(treeNode.getValueInfo() != null)
-   							{
-   								JSONObject jason = new JSONObject(treeNode.getValueInfo());
-   								JSONArray rows = (JSONArray)jason.get("rows");
-   								for(int n = 0; n < rows.length(); n++)
-   								{
-   									JSONObject row = (JSONObject)rows.get(n);
-   									String keyValue = (String)row.get("keys");
-   									if(keyValue.equals(treeNodeTmp.getValue()))
-   									{
-   										treeNodeTmp.setValueInfo(row.getString("infos"));
-   										break;
-   									}
-   								}
-   							}
-   
-   						}
-   						treeNodeTmp.setNodeId(nodeCount);
-   						// TODO check addThisNode
-   		    			list.add(treeNodeTmp);  
-   						nodeCount++;
-   					}
+					for (int i = 0; i < nodeList.getLength(); i++) {
+						if(!treeNode.isReadOnly())
+							nodeAutorization.add(xpath+"["+(i+1)+"]");
+						idToXpath.put(nodeCount,xpath+"["+(i+1)+"]");
+						TreeNode treeNodeTmp = (TreeNode) treeNode.clone();
+						
+						if(infos != null && treeNode.isRetrieveFKinfos() && treeNode.getForeignKey() != null) {
+						   String value = StringEscapeUtils.escapeHtml(nodeList.item(i).getTextContent());
+						   String jasonData = getForeignKeyList(0, 10, value,  treeNode.getForeignKey(), keyInfos, false);
+						   treeNode.setValueInfo(jasonData);
+						}
+						
+						if(nodeList.item(i).getFirstChild()!=null)
+						{
+							treeNodeTmp.setValue(nodeList.item(i).getFirstChild().getNodeValue());
+							if(treeNode.getValueInfo() != null)
+							{
+								JSONObject jason = new JSONObject(treeNode.getValueInfo());
+								JSONArray rows = (JSONArray)jason.get("rows");
+								for(int n = 0; n < rows.length(); n++)
+								{
+									JSONObject row = (JSONObject)rows.get(n);
+									String keyValue = (String)row.get("keys");
+									if(keyValue.equals(treeNodeTmp.getValue()))
+									{
+										treeNodeTmp.setValueInfo(row.getString("infos"));
+										break;
+									}
+								}
+							}
+
+						}
+						treeNodeTmp.setNodeId(nodeCount);
+						// TODO check addThisNode
+		    			list.add(treeNodeTmp);  
+						nodeCount++;
 					}
 					if(nodeList.getLength() == 0){
 						if(!treeNode.isReadOnly())
