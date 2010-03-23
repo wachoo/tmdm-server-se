2686d27fe9495051bf888d7bbe639c3c3d011a89
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-03-23 05:59:29 +0000

    extract foreignkey widget from workflow to common
    git-svn-id: http://talendforge.org/svn/tom/trunk@38927 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.openmdm/src/com/amalto/webapp/widget/WidgetDWR.java
+++ org.talend.mdm.webapp.openmdm/src/com/amalto/webapp/widget/WidgetDWR.java
@@ -1,19 +1,39 @@
 package com.amalto.webapp.widget;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.talend.mdm.commmon.util.core.EDBType;
+import org.talend.mdm.commmon.util.core.MDMConfiguration;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import com.amalto.webapp.core.bean.ComboItemBean;
 import com.amalto.webapp.core.bean.Configuration;
 import com.amalto.webapp.core.bean.ListRange;
 import com.amalto.webapp.core.dwr.CommonDWR;
+import com.amalto.webapp.core.json.JSONArray;
+import com.amalto.webapp.core.json.JSONObject;
+import com.amalto.webapp.core.util.Util;
+import com.amalto.webapp.util.webservices.WSCount;
+import com.amalto.webapp.util.webservices.WSDataClusterPK;
+import com.amalto.webapp.util.webservices.WSStringArray;
+import com.amalto.webapp.util.webservices.WSWhereCondition;
+import com.amalto.webapp.util.webservices.WSWhereItem;
+import com.amalto.webapp.util.webservices.WSXPathsSearch;
 import com.sun.xml.xsom.XSElementDecl;
 
 
 public class WidgetDWR {
+	
+	/**********************************************
+     * where condition panel widget
+     ***********************************************/
 	
 	public ListRange getFieldList(int start, int limit,String sort,String dir,String regex) throws Exception {
         ListRange listRange = new ListRange();
@@ -52,5 +72,167 @@ public class WidgetDWR {
 
 		return listRange;
 	}
+	
+	/**********************************************
+     * foreign key widget
+     ***********************************************/
+    
+    /**
+     * Get the number of foreign key.
+     * @param xpathForeignKey
+     * @return
+     * @throws Exception
+     */
+    public String countForeignKey_filter(String xpathForeignKey) throws Exception {
+       Configuration config = Configuration.getInstance();
+       String conceptName = Util.getConceptFromPath(xpathForeignKey);
+       
+       WSWhereCondition whereCondition = Util.getConditionFromPath(xpathForeignKey);
+       WSWhereItem whereItem = null;
+       
+       if(whereCondition != null){
+          whereItem = new WSWhereItem(whereCondition, null, null);
+       }
+       
+       return Util.getPort().count(
+          new WSCount(
+             new WSDataClusterPK(config.getCluster()),
+             conceptName,
+             whereItem,//null,
+             -1
+          )
+       ).getValue();
+    }
+    
+    /**
+     * Get the list of foreign key.
+     */
+    public String getForeignKeyList(int start, int limit, String value, String xpathForeignKey, String xpathInfoForeignKey) throws RemoteException, Exception{
+       String initxpathForeignKey = "";
+       xpathInfoForeignKey = xpathInfoForeignKey == null ? "" : xpathInfoForeignKey;
+       initxpathForeignKey = Util.getForeignPathFromPath(xpathForeignKey);
+       
+       org.apache.log4j.Logger.getLogger(this.getClass()).debug("getForeignKeyList() xPath FK: '" + initxpathForeignKey + "' xPath FK Info: '" + xpathInfoForeignKey + "' value: '" + value + "'");
+             
+       WSWhereCondition whereCondition=Util.getConditionFromPath(xpathForeignKey);
+       WSWhereItem whereItem = null;
+       if(whereCondition != null){
+          whereItem= new WSWhereItem (whereCondition, null, null);
+       }
+       
+       
+       Configuration config = Configuration.getInstance();
+       //aiming modify there is bug when initxpathForeignKey when it's like 'conceptname/key'
+       //so we convert initxpathForeignKey to 'conceptname'
+       initxpathForeignKey = initxpathForeignKey.split("/")[0];
+       //end
+       
+       // foreign key set by business concept
+       if(initxpathForeignKey.split("/").length == 1){
+          String conceptName = initxpathForeignKey;
+
+          //determine if we have xPath Infos: e.g. labels to display
+          String[] xpathInfos = new String[1];
+          if(!"".equals(xpathInfoForeignKey)) 
+             xpathInfos = xpathInfoForeignKey.split(",");
+          else
+             xpathInfos[0] = conceptName;
+          //aiming add .* to value
+          value = value == null ? "" : value;
+          value = value + ".*";
+          //end
+          // build query - add a content condition on the pivot if we search for a particular value
+          String filteredConcept = conceptName;
+          if(value != null && !"".equals(value.trim())){             
+             //Value is unlikely to be in attributes
+             filteredConcept += "[matches(. , \"" + value + "\", \"i\")]";
+			if(EDBType.ORACLE.getName().equals(MDMConfiguration.getDBType().getName())) {
+				filteredConcept+="[ora:matches(. , \""+value+"\", \"i\")]";
+			}
+          }
+          
+          //add the xPath Infos Path
+          ArrayList<String> xPaths = new ArrayList<String>();
+          for (int i = 0; i < xpathInfos.length; i++) {
+             xPaths.add(xpathInfos[i].replaceFirst(conceptName, filteredConcept));
+          }
+          //add the key paths last, since there may be multiple keys
+          xPaths.add(filteredConcept + "/../../i");
+          
+          //Run the query
+          String [] results = Util.getPort().xPathsSearch(new WSXPathsSearch(
+             new WSDataClusterPK(config.getCluster()),
+             filteredConcept,
+             new WSStringArray(xPaths.toArray(new String[xPaths.size()])),
+             whereItem,
+             -1,
+             start,
+             limit,
+             null,
+             null
+          )).getStrings();
+          
+          if (results == null) results = new String[0];
+          
+          JSONObject json = new JSONObject();
+          //json.put("count", results.length);
+          
+          JSONArray rows = new JSONArray();
+          json.put("rows", rows);
+
+          //add (?i) to incasesensitive
+          //parse the results - each result contains the xPathInfo values, followed by the keys
+          //the first row is totalCount
+          for (int i = 0; i < results.length; i++) {
+             //process no infos case
+             if(!results[i].startsWith("<result>")){
+                results[i] = "<result>" + results[i] + "</result>";
+             }
+             results[i] = results[i].replaceAll("\\n", "");//replace \n
+             results[i] = results[i].replaceAll(">(\\s+)<", "><"); //replace spaces between elements
+             Element root = Util.parse(results[i]).getDocumentElement();
+             NodeList list = root.getChildNodes();
+
+             //recover keys - which are last
+             String keys = "";
+             for (int j = "".equals(xpathInfoForeignKey) ? 1 : xpathInfos.length; j<list.getLength(); j++) {
+                Node textNode = list.item(j).getFirstChild();      
+                keys += "[" + (textNode == null ? "" : textNode.getNodeValue()) + "]";
+             }
+             
+             //recover xPathInfos
+             String infos = null;
+             
+             //if no xPath Infos given, use the key values
+             if (xpathInfos.length == 0 || "".equals(xpathInfoForeignKey)) {
+                infos = keys;
+             } else {
+                //build a dash separated string of xPath Infos
+                for (int j = 0; j < xpathInfos.length; j++) {
+                   infos = (infos == null ? "" : infos+"-");
+                   Node textNode = list.item(j).getFirstChild();
+                   infos  += textNode == null ? "" : textNode.getNodeValue();
+                }
+             }
+             
+             if((keys.equals("[]") || keys.equals("")) && (infos.equals("") || infos.equals("[]"))){
+             }else{
+                JSONObject row = new JSONObject();     
+                //add by ymli. retrieve the correct results according value. fig bug:0010481              
+                if(keys.matches("(?i)" + value) || infos.matches("(?i)" + value) || keys.indexOf("[" + value) != -1 || infos.indexOf(value) != -1){
+                   row.put("keys", keys);
+                   row.put("infos", infos);
+                   rows.put(row);
+                }
+             }
+             
+             json.put("count", rows.length());
+          }
+
+          return json.toString();
+       }
+       
+       throw new Exception("this should not happen");
+    }
 
 }
---
+++ org.talend.mdm.webapp.openmdm/web/secure/js/widget/ForeignKeyField.js
@@ -0,0 +1,158 @@
+/*
+ * Ext JS Library 2.0.2
+ * Copyright(c) 2006-2008, Ext JS, LLC.
+ * licensing@extjs.com
+ * 
+ * http://extjs.com/license
+ */
+
+/**
+ * @class amalto.widget.ForeignKeyField
+ * @extends Ext.form.TriggerField
+ * Provides a ForeignKeyField input field with a foreignkey dropdown and automatic.
+* @constructor
+* Create a new ForeignKeyField
+* @param {Object} config
+ */
+/**
+ * The custom search field
+ */
+Ext.namespace('amalto.widget');
+amalto.widget.ForeignKeyField = Ext.extend(Ext.form.TwinTriggerField, {
+    initComponent : function(){
+		amalto.widget.ForeignKeyField.superclass.initComponent.call(this);
+    },
+    
+    xpathForeignKey : "",
+    xpathForeignKeyInfo : "",
+    trigger1Class : 'x-form-clear-trigger',
+    trigger2Class : 'x-form-search-trigger',
+    hideTrigger1:false,
+    hasAdd : true,
+    taskForeignKeyWindow : "",
+    foreignKeyCombo : "",
+    taskForeignKeytore : "",
+    
+    onTrigger1Click : function() {
+        this.el.dom.value = '';
+    },
+	
+    //pop up a dialog
+    onTrigger2Click : function () {
+	    var FILTER = {
+			'fr':'Saisissez un critère de recherche puis sélectionnez une valeur dans la liste déroulante',
+			'en':'Fill the box with a key-word then select a value'
+		};
+    	
+    	var TITLE_WINDOW_FK = {
+			'fr':'Choix de la clé étrangère',
+			'en':'Choose a foreign key'
+		};
+    	var MESSAGE_MULTI_SHOW = {
+			'fr':'clés possibles trouvées',
+			'en':'possible keys are found'
+		};
+    	var MESSAGE_SINGLE_SHOW = {
+			'fr':'clé possible trouvée',
+			'en':'possible key is found'
+		};
+    	
+	    var dwrpasm = [this.xpathForeignKey, this.xpathForeignKeyInfo];
+	    WidgetInterface.countForeignKey_filter(this.xpathForeignKey, function(count) {
+	    	if(this.taskForeignKeyWindow) {
+	    		this.taskForeignKeyWindow.hide();
+	    		this.taskForeignKeyWindow.destroy();
+	    	}
+	    	
+			this.taskForeignKeytore = new Ext.data.Store({
+				proxy: new Ext.ux.data.ImprovedDWRProxy({
+			        dwrFunction : WidgetInterface.getForeignKeyList,
+			        dwrAdditional : dwrpasm
+				}),
+		        reader: new Ext.data.JsonReader({
+		            root : 'rows',
+		            totalProperty : 'count',
+		            id : 'keys'
+		        }, [
+		            {name : 'keys', mapping : 'keys'},
+		            {name : 'infos', mapping : 'infos'}
+		        ])
+			});
+			
+		    // Custom rendering Template
+		    var resultTpl = new Ext.XTemplate(
+			        '<tpl for="."><div class="search-item" style="font-size: 0.8em">',
+			            '<h3>{infos}</h3>',
+			            '{keys}',
+			        '</div></tpl>'
+			    );
+		    
+		    this.foreignKeyCombo = new Ext.form.ComboBox({
+                width : 280,
+                resizable : true, 
+                fieldLabel : FILTER[language],
+                id : 'task-foreign-key-filter',
+		        store : this.taskForeignKeytore,
+		        displayField : 'title',
+		        typeAhead : false,
+		        loadingText : 'Searching...',
+		        pageSize : 100,
+		        minChars : 2,
+		        hideTrigger : true,
+		        tpl : resultTpl,
+		        listAlign : 'tl-bl',
+		        itemSelector : 'div.search-item'
+		    });
+		    
+		    this.foreignKeyCombo.on('select', function(combo, record, index) {
+		    	this.taskForeignKeyWindow.hide();
+		    	this.taskForeignKeyWindow.destroy();
+		    	var value = record.get("infos");
+		    	this.setValue(value);
+		    }.createDelegate(this));
+		    
+		    
+		    this.taskForeignKeyWindow = new Ext.Window({
+                layout : 'fit',
+                width : 300,
+                height : 150,
+                resizable : true, 
+                closeAction : 'hide',
+                plain : true,
+                title : TITLE_WINDOW_FK[language] + '<br/>(' + count + ' ' + (count > 1 ? 
+                			MESSAGE_MULTI_SHOW[language] + ')' : MESSAGE_SINGLE_SHOW[language] + ')'),
+                items : [new Ext.form.FormPanel({
+                	labelAlign : 'top',
+                    items: [new Ext.Panel({html: '', border: false}),
+                            this.foreignKeyCombo
+	                ]
+                })]
+            });
+			
+			this.taskForeignKeyWindow.on('show', function() {
+				var foreignKeyCombo = Ext.getCmp('task-foreign-key-filter');
+				foreignKeyCombo.focus(true, 100);
+				foreignKeyCombo.reset();
+				
+				if(count < 500) {
+					foreignKeyCombo.setRawValue("");
+					foreignKeyCombo.doQuery(".*", true);
+					foreignKeyCombo.focus();
+					foreignKeyCombo.expand();
+				}
+			});
+			
+			this.taskForeignKeyWindow.on('hide', function() {
+		    	this.taskForeignKeyWindow.destroy();
+			}.createDelegate(this));
+
+			this.taskForeignKeyWindow.show();
+		}.createDelegate(this)
+		);
+	},
+	
+	setValue : function(value) {
+		this.el.dom.value = value;
+	}
+});
+Ext.reg('foreignfeyfield', amalto.widget.ForeignKeyField);
