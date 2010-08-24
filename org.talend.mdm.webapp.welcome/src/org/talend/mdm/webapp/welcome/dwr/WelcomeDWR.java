2127257fb06719dc5aebb07b6d7f76c0ec701560
yguo <yguo@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-24 03:17:37 +0000

    git-svn-id: http://talendforge.org/svn/tom/trunk@47147 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.webapp.welcome/src/org/talend/mdm/webapp/welcome/dwr/WelcomeDWR.java
@@ -0,0 +1,141 @@
+package org.talend.mdm.webapp.welcome.dwr;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.ow2.bonita.facade.runtime.ActivityState;
+import org.ow2.bonita.facade.runtime.TaskInstance;
+import org.ow2.bonita.util.AccessorUtil;
+
+import com.amalto.core.enterpriseutil.EnterpriseUtil;
+import com.amalto.core.objects.license.ejb.local.LicenseCtrlLocal;
+import com.amalto.core.util.license.LicenseVO;
+import com.amalto.core.util.license.LicenseHelper;
+import com.amalto.core.util.license.LicenseUtil;
+import com.amalto.webapp.core.util.Menu;
+import com.amalto.webapp.core.util.Util;
+import com.amalto.webapp.core.util.dwr.ExtJSFormResponse;
+import com.amalto.webapp.core.util.dwr.ExtJSFormSuccessResponse;
+
+public class WelcomeDWR {
+   public WelcomeDWR() {
+      super();
+   }
+   
+   /**
+    * get license informations.
+    * @return
+    */
+   public ExtJSFormResponse getLicenseMsg(String language){
+       LicenseVO license = new LicenseVO();
+
+       try {
+           LicenseCtrlLocal ctrl = EnterpriseUtil.getLicenseCtrlLocal();
+           LicenseUtil licenseData = ctrl.getLicenseData(true);
+           
+           license.setLicense(LicenseHelper.getInstance().getLicense().getLicense());
+           license.setLicenseValid(licenseData.isLicenseDateValid());
+           license.setWarning(ctrl.getWarnings(language));
+           license.setAdminUsers(licenseData.getAdminUsers());
+           license.setNormalUsers(licenseData.getInteractiveUsers());
+           license.setViewers(licenseData.getViewers());
+           license.setNbUser(licenseData.getNbUser());
+       }
+       catch(Exception e) {
+           e.printStackTrace();
+           if(true) {
+               license.setLicense(null);
+           }
+           else if(true) {
+               license.setLicenseValid(false);
+           }
+       }
+       
+       return new ExtJSFormSuccessResponse("", license);   
+   }
+   
+   /**
+    * get label of link on welcome page.
+    * @param language
+    * @return
+    */
+   public Map<String,String> getLabels(String language) {
+       Map<String, String> labels = new HashMap<String, String>();
+       
+       try {
+           TreeMap<String, Menu> subMenus = Menu.getRootMenu().getSubMenus();
+           for(Iterator<String> iter = subMenus.keySet().iterator();iter.hasNext();) {
+               String key = iter.next();
+               Menu subMenu= subMenus.get(key);
+               
+               labels.put(subMenu.getApplication(), subMenu.getLabels().get(language));
+           }
+       }
+       catch(Exception e) {
+           e.printStackTrace();
+       }
+       
+       return labels;
+   }
+   
+   /**
+    * get workflow task informations.
+    * @return
+    */
+   public int getTaskMsg() {
+       try {
+           Collection tasks = EnterpriseUtil.getWorkflowService().getTaskList(
+               AccessorUtil.getAPIAccessor().getManagementAPI().getLoggedUser(), 
+               ActivityState.READY);
+           return tasks.size();
+       }
+       catch(Exception e) {
+           e.printStackTrace();
+       }
+       
+       return 0;
+   }
+   
+   /**
+    * check if is show license link.
+    * @return
+    */
+   public boolean isHiddenLicense() {
+       return isHiddenMenu("License");
+   }
+   
+   /**
+    * check if is show specify menu.
+    * @param menu
+    * @return
+    */
+   private boolean isHiddenMenu(String menu) {
+       try {
+           TreeMap<String, Menu> subMenus = Menu.getRootMenu().getSubMenus();
+           for(Iterator<String> iter = subMenus.keySet().iterator();iter.hasNext();) {
+               String key = iter.next();
+               Menu subMenu= subMenus.get(key);
+               
+               if(menu.equals(subMenu.getApplication())) {
+                   return false;
+               }
+           }
+       }
+       catch(Exception e) {
+           e.printStackTrace();
+       }
+       
+       return true;
+   }
+   
+   /**
+    * check if is show task link.
+    * @return
+    */
+   public boolean isHiddenTask() {
+       return isHiddenMenu("WorkflowTasks");
+   }
+}
---
+++ org.talend.mdm.webapp.welcome/web/secure/js/Welcome.js
@@ -0,0 +1,472 @@
+amalto.namespace("amalto.welcome");
+
+amalto.welcome.Welcome = function () {
+	loadResource("/welcome/secure/css/Welcome.css", "" );
+	
+	var WELCOME_TITLE = {
+		'fr':'WELCOME',
+		'en':'WELCOME'
+	}
+	
+	var WELCOME_PANEL = {
+		'fr':'Administration Center is the centralized place where you can manage:',
+		'en':'Administration Center is the centralized place where you can manage:'
+	}
+	
+	var WELCOME_DESCRIPTION = {
+		'fr':'Welcome to talend MDM.',
+		'en':'Welcome to talend MDM.'
+	}
+	
+	var BROWSE_ITEMS = {
+		'en':'Browse Records',
+		'fr':'Accès aux données'
+	}
+	
+	var JOURNAL = {
+		'en':'Journal',
+		'fr':'Journal'
+	}
+	
+	var LICENSE_SOON_MESSAGE = {
+		'fr': 'La license expirera dans {0} jours. Veuillez contacter votre commercial Talend.',
+		'en': 'The License will expire in {0} days. Please contact your Talend account manager.'
+	}
+	
+	var LICENSE_DES_NUMBERS = {
+		'fr':'You have currently subscribed to a license with {0} users.',
+		'en':'You have currently subscribed to a license with {0} users.'
+	}
+	
+	var LICENSE_DES_CON = {
+		'fr':'If you need to add users to your license, you may contact your Talend account manager.',
+		'en':'If you need to add users to your license, you may contact your Talend account manager.'
+	}
+	
+	var welcomePanel;	
+	var messageArea;
+	var startMessagePL;
+	var alertMessageLB;
+	var taskMessagePL;
+	var startPanel;
+	var alertsPanel;
+	var taskPanel;
+	var descriptionPanel;
+	var admins;
+	var normals;
+	var viewers;
+	var nbusers;
+	var hidenTaskPL = true;
+	var hidenAlertsPL = true;
+	/********************************************************************
+	 * Action show welcome
+	 ********************************************************************/
+	
+	/**
+	 * display welcome page.
+	 */
+	 function displayWelcome(){
+	  	showWelcomePanel();
+	 }
+	 
+	 /**
+	  * open the page by specify context and application.
+	  */
+	 function openPages(context, application) {
+		 //update display of menu in menu Panel
+//		 YAHOO.util.Dom.removeClass(Ext.get('menus').dom.getElementsByTagName('a'), 'selected');
+//		 YAHOO.util.Dom.addClass(a.id, 'selected');
+
+		 amalto.core.loadMainScript(context, application,
+			function() {
+				var initFunction = "amalto." + context + "." + application + ".init()";
+				setTimeout(initFunction,'50');
+			}
+		 );
+	 }
+	 
+	 /**
+	  * show welcome panel.
+	  */
+	 function showWelcomePanel() {
+		 //@temp yguo, should get label of link from menu object.
+	 	var tabPanel = amalto.core.getTabPanel();
+
+	 	if(tabPanel.getItem('welcome') == undefined) {
+	 		startMessagePL = new Ext.Panel({
+	 			id: 'startMessagePL',
+				border : false,
+				header : false,
+				split : true,
+				style : 'font-weight:bold;',
+				height : 30,
+				collapsible : false,
+				bodyborder : true,
+				bodyStyle : "background-color:#F1F1F1;",
+				colspan: 2,
+				items : [{
+					id : 'startMessageLB',
+					xtype : 'label',
+					style : 'padding-left: 20px;',
+					text : 'Here are a few links to help you get started with Talend MDM:'
+				}]
+	 		});
+	 		
+	 		var startFieldPL = new Ext.Panel({
+	 			id : 'startFieldPL',
+	 			layout : 'table',
+	 			layoutConfig: {
+			        columns: 3
+			    },
+	 			items : [
+						{
+							width : 20,
+							rowspan: 2,
+							border : false
+						},
+						new Ext.form.FieldSet({
+							 title : 'Useful Links',
+							 width : 500,
+							 colspan: 2,
+							 style : 'font-size:80%;',
+							 autoHeight : true,
+							 style : 'font-size:80%;',
+							 html : '<span id="ItemsBrowser" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'itemsbrowser\', \'ItemsBrowser\');">' + 
+								   '<IMG SRC=\"/talendmdm/secure/img/menu/browse.png\"/>' + ' ' + BROWSE_ITEMS[language] + '</span>' + '<br/>' +
+								   '<span id="Journal" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'updatereport\', \'UpdateReport\');">' + 
+								   '<IMG SRC=\"/talendmdm/secure/img/menu/updatereport.png\"/>' + ' ' + JOURNAL[language] + '</span>'
+						})
+	 			         ]
+	 		});
+	 		
+			startPanel = new Ext.Panel({
+				id: 'startPL',
+				iconCls : 'start_icon',
+				title : 'Getting Started',
+				height : 145,
+				border: true,
+				header:true,
+				split:true,
+				collapsible: true,
+				bodyborder: true,
+				items : [
+				         startMessagePL,
+				         startFieldPL
+				         ]
+			});
+			
+			hidenAlertsPL = isHiddenLicense();
+			
+			
+			alertMessageLB = new Ext.Panel({
+	 			id: 'alertMessageLB',
+				border : false,
+				header : false,
+				split : true,
+				height : 30,
+				colspan : 2,
+				collapsible : false,
+				style : 'font-weight:bold;',
+				bodyborder : true,
+				bodyStyle : "background-color:#F1F1F1;",
+				items :[
+				        {
+				        	id : 'alertsMessage',
+				        	xtype : 'label',
+				        	style : 'padding-left: 20px;',
+				        }]
+	 		});
+			
+			var alertsFieldPL = new Ext.Panel({
+				id : 'alertsFieldPL',
+				layout : 'table',
+				layoutConfig : {
+				 columns: 3
+				},
+				border : false,
+				items : [
+						{
+							width : 20,
+							rowspan: 2,
+							border : false
+						},
+						new Ext.form.FieldSet({
+							title : 'Alerts',
+							id : 'alertsFields',
+							width : 500,
+							style : 'font-size: 80%;',
+							colspan : 2,
+							autoHeight : true,
+							html : '<span id="licenseAlert" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'license\', \'License\');">' + 
+								   '<IMG SRC=\"../img/alert-icon.png\"/>License</span>'
+						})
+				         ]
+			});
+			
+			alertsPanel = new Ext.Panel({
+				id: 'alertsPL',
+				iconCls : 'alert_icon',
+				hidden : hidenAlertsPL,
+				title : 'Alerts',
+				border: true,
+				height:120,
+				header:true,
+				split:true,
+				collapsible: true,
+				bodyborder: true,
+				items : [alertMessageLB, alertsFieldPL]
+			});
+			
+			hidenTaskPL = isHiddenTask();
+			
+			taskMessagePL = new Ext.Panel({
+	 			id: 'taskMessagePL',
+				border : false,
+				header : false,
+				split : true,
+				style : 'font-weight:bold;',
+				colspan : 2,
+				height : 30,
+				collapsible : false,
+				bodyborder : true,
+				bodyStyle : "background-color:#F1F1F1;",
+				items : [{
+					id : 'taskMessageLB',
+					xtype : 'label',
+					style : 'padding-left: 20px;',
+				}]
+	 		});
+			
+			var taskFieldPL = new Ext.Panel({
+				id : 'taskFieldPL',
+				layout : 'table',
+				border : false,
+				layoutConfig : {
+				 columns: 3
+				},
+				items : [
+						{
+							width : 20,
+							rowspan: 2,
+							border : false
+						},
+						new Ext.form.FieldSet({
+							title : 'Tasks',
+							id : 'tasksField',
+							width : 500,
+							autoHeight : true,
+							style : 'font-size:80%;',
+							html : '<span id="workflowtasks" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'workflowtasks\', \'WorkflowTasks\');"></span>'
+						})
+				         ]
+			});
+			
+			taskPanel = new Ext.Panel({
+				id: 'taskPL',
+				iconCls : 'task_list_icon',
+				hidden : hidenTaskPL,
+				title : 'Tasks',
+				border: true,
+				height:120,
+				header:true,
+				split:true,
+				collapsible: true,
+				bodyborder: true,
+				items : [taskMessagePL, taskFieldPL]
+			});
+			
+			descriptionPanel = new Ext.Panel({
+				id : 'messagePL',
+				iconCls : 'description_icon',
+				title : 'Description',
+				border: true,
+				layout : 'table',
+				height:120,
+				bodyStyle : "background-color:#F1F1F1;padding:5px",
+				layoutConfig: {
+			        columns: 2
+			    },
+				header:true,
+				split:true,
+				collapsible: true,
+				bodyborder: true,
+				items :[{
+					rowspan: 4
+				},{
+					id : 'descriptionWelcome',
+					xtype : 'label',
+					style : 'font-size:80%;',
+					text : WELCOME_DESCRIPTION[language],
+					colspan: 2
+				},{
+					id : 'startDescription',
+					xtype : 'label',
+					style : 'font-size:80%;',
+					text : 'You can start with "Getting Started" panel.',
+					colspan: 2
+				},{
+					id : 'descriptionNumbers',
+					xtype : 'label',
+					style : 'font-size:80%;',
+					hidden : hidenAlertsPL,
+					colspan: 2
+				},{
+					id : 'descriptionCon',
+					xtype : 'label',
+					style : 'font-size:80%;',
+					text : LICENSE_DES_CON[language],
+					hidden : hidenAlertsPL,
+					colspan: 2
+				}]
+			});
+			
+			welcomePanel = new Ext.Panel({
+				id: 'welcome',
+				title: WELCOME_TITLE[language],
+				deferredRender: false,
+				autoScroll: false,
+				border: false,
+				bodyBorder:false,
+				closable: true,
+				items: 
+				[new Ext.Panel({
+					 id : 'welcomePl',
+					 title : WELCOME_PANEL[language],
+					 border : true,
+					 header : true,
+					 split : true,
+					 collapsible : false,
+					 bodyborder : true,
+					 items : [startPanel, alertsPanel, taskPanel]
+				 })]
+			});
+			
+//			setLabels(language);
+			if(!hidenTaskPL) {
+				applyTaskMessage();
+			}
+			
+			if(!hidenAlertsPL) {
+				applyAlertsMessage(language);
+			}
+		}		
+		
+		tabPanel.add(welcomePanel); 
+		welcomePanel.show();
+		welcomePanel.doLayout();
+		amalto.core.doLayout();
+	 }
+	 
+	 /**
+	  * set all label of links.
+	  */
+	 function setLabels(language) {
+		 WelcomeInterface.getLabels(language, function(result) {
+			 //@temp yguo, fool to set all labels.abstract id
+			 DWRUtil.setValue("workflowtasks", '<IMG SRC=\"/talendmdm/secure/img/menu/workflowtasks.png\"/>' + '  ' + result["WorkflowTasks"]);
+		 });
+	 }
+	 
+	 /**
+	  * build all license message.
+	  */
+	 function applyAlertsMessage(language) {
+		 var licenseMessage;
+		 //@temp yguo, should get alerts from server.
+		 WelcomeInterface.getLicenseMsg(language, function(result) {
+			 if(result.data.license) {
+				 applyDescriptionMessage(result.data.adminUsers + result.data.normalUsers + result.data.viewers);
+
+				 if(result.data.licenseValid && result.data.warning) {
+					 licenseMessage = result.data.warning;
+				 }
+				 else if(result.data.licenseValid) {
+					 licenseMessage = "License is available.";
+					 Ext.getCmp("alertsFields").setVisible(false);
+					 Ext.getCmp("alertsMessage").setText("No alerts.");
+					 return;
+				 }
+				 else {
+					 licenseMessage = "License expired, please update your license by license wizard.";
+				 }
+			 }
+			 else {
+				 licenseMessage = "No license available, please input your valid license by license wizard.";
+			 }
+			 
+			 var licenseAlert = Ext.getCmp("licenseAlert");
+			 DWRUtil.setValue("licenseAlert",  '<IMG SRC=\"/talendmdm/secure/img/genericUI/alert-icon.png\"/>' + ' ' + licenseMessage);
+			 Ext.getCmp("alertsMessage").setText("You can handle alerts by the following links.");
+		 });
+	 }
+	 
+	 /**
+	  * build all message for workflow task.
+	  */
+	 function applyTaskMessage() {
+		 var taskMessage;
+		 var taskMessageLB = Ext.getCmp("taskMessageLB");
+		 taskMessageLB.setText("please waiting .......");
+		 
+		 WelcomeInterface.getTaskMsg(function(result){
+			 taskMessage = "You have " + result + " workflow task ready to handle.";
+			 
+			 DWRUtil.setValue("workflowtasks", '<IMG SRC=\"/talendmdm/secure/img/genericUI/task-list-icon.png\"/>' + ' ' + taskMessage);
+			 
+			 if(result == "0") {
+				 taskMessageLB.setText("No tasks.");
+				 Ext.getCmp("tasksField").setVisible(false);
+			 }
+			 else {
+				 taskMessageLB.setText("You can handle tasks by the following links.");
+			 }
+		 });
+	 }
+	 
+	 /**
+	  * get all description.
+	  */
+	 function applyDescriptionMessage(number) {
+		 var numberDescription = LICENSE_DES_NUMBERS[language].replace("{0}", number);
+		 Ext.getCmp("descriptionNumbers").setText(numberDescription);
+	 }
+	 
+	 /**
+	  * check if is show license.
+	  */
+	 function isHiddenLicense() {
+		 var hidden = true;
+		 DWREngine.setAsync(false);
+		 
+		 WelcomeInterface.isHiddenLicense(function(result){
+			 hidden = result;
+		 });
+		 
+		 DWREngine.setAsync(true);
+		 return hidden;
+	 }
+	 
+	 /**
+	  * check if is show license.
+	  */
+	 function isHiddenTask() {
+		 var hiddenTask = true;
+		 DWREngine.setAsync(false);
+		 
+		 WelcomeInterface.isHiddenTask(function(result){
+			 hiddenTask = result;
+		 });
+		 
+		 DWREngine.setAsync(true);
+		 return hiddenTask;
+	 }
+	 
+ 	return {
+		init: function() {displayWelcome(); },
+		getTask : function(){getTask()},
+		openPages : function(context, application){openPages(context, application)},
+		getLicense : function(){getLicense();},
+		getStartItems : function(){getStartItems();}
+ 	}
+
+}();
