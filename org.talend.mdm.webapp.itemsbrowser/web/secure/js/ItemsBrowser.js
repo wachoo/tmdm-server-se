785dfdd542af433902628c560e18edf289595a96
ymli <ymli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-27 07:56:35 +0000

    fix the bug:0015824
    git-svn-id: http://talendforge.org/svn/tom/trunk@48942 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser/web/secure/js/ItemsBrowser.js
+++ org.talend.mdm.webapp.itemsbrowser/web/secure/js/ItemsBrowser.js
@@ -2413,7 +2413,7 @@ amalto.itemsbrowser.ItemsBrowser = function () {
     			    var node2 = new YAHOO.widget.HTMLNode(nameTmp, root, false, true);
     				var rootnode = root.children[0];
     				itemTree.removeNode(rootnode);
-    				ItemsBrowserInterface.setTree(dataObject, itemPK2, node2.index, false, treeIndex, true, function(result){
+    				ItemsBrowserInterface.setTree(dataObject, ids, node2.index, false, treeIndex, true, function(result){
                         node2.setDynamicLoad(fnLoadData, 1);
                         node2.expand();
                         itemTree.draw();
