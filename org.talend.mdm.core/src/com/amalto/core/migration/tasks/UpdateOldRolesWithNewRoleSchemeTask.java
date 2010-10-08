28f53adb98918c150cbb3cd3953343f2da5e5421
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-10-08 08:35:37 +0000

    feature 0015585 added: add migration support for the roles involved in workflow.
    git-svn-id: http://talendforge.org/svn/tom/trunk@49546 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/migration/tasks/UpdateOldRolesWithNewRoleSchemeTask.java
+++ org.talend.mdm.core/src/com/amalto/core/migration/tasks/UpdateOldRolesWithNewRoleSchemeTask.java
@@ -1,8 +1,16 @@
 package com.amalto.core.migration.tasks;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 
 import org.talend.mdm.commmon.util.core.ICoreConstants;
 import org.w3c.dom.NodeList;
@@ -18,6 +26,9 @@ public class UpdateOldRolesWithNewRoleSchemeTask extends AbstractMigrationTask{
 		oldRoleToNewRoleMap.put("Default_User", ICoreConstants.SYSTEM_INTERACTIVE_ROLE);
 		oldRoleToNewRoleMap.put("Default_Viewer", ICoreConstants.SYSTEM_VIEW_ROLE);
 	}
+	
+	static final String BAR_ZIP = ".bar";
+	static final String SUFFIX_BAR = "_r";
 	
 	private boolean updateRolesInProvision()
 	{
@@ -83,9 +94,115 @@ public class UpdateOldRolesWithNewRoleSchemeTask extends AbstractMigrationTask{
 		}
 		return true;
 	}
+	
+    private static boolean updateRolesInWorkFlow()
+    {
+        String barHome = Util.getBarHomeDir();
+        File barDir = new File(barHome);
+        File monitorFiles[] = barDir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name)
+            {
+                return name.endsWith(".bar");
+            }
+        });
+
+        ArrayList<String> oldBarFiles = new ArrayList<String>();
+        ArrayList<String> newBarFiles = new ArrayList<String>();
+        for (File barFile : monitorFiles)
+        {
+            String outName = barFile.getAbsolutePath().substring(0, barFile.getAbsolutePath().length() - 4) + SUFFIX_BAR + BAR_ZIP;
+            try {
+                JarInputStream jarIn = new JarInputStream(new FileInputStream(barFile));
+                JarOutputStream jarOut = new JarOutputStream(new FileOutputStream(outName));
+                JarEntry entry;
+                byte[] buf = new byte[4096];
+                while ((entry = jarIn.getNextJarEntry()) != null) {
+
+                  if ("META-INF/MANIFEST.MF".equals(entry.getName())) continue;
+                  int read;
+                  jarOut.putNextEntry(entry);
+                  if(!entry.getName().endsWith(".proc") && !entry.getName().endsWith(".xml"))
+                  {
+                      while ((read = jarIn.read(buf)) != -1) {
+                          jarOut.write(buf, 0, read);
+                      }
+                  }
+                  else
+                  {
+                      ByteArrayOutputStream outBytes = new ByteArrayOutputStream();
+                      while ((read = jarIn.read(buf, 0, 4096)) != -1) {
+                          outBytes.write(buf, 0 , read);
+                      }
+
+                      String orgSrc = new String(outBytes.toByteArray());
+                      String orgCpy = new String(orgSrc);
+                      for (Map.Entry<String, String> pair : oldRoleToNewRoleMap.entrySet())
+                      {
+                          orgSrc = orgSrc.replaceAll(pair.getKey().toString(), pair.getValue().toString());
+                      }
+
+                      if(orgSrc.equals(orgCpy))
+                      {
+                          jarOut.write(orgCpy.getBytes(), 0, orgCpy.getBytes().length);
+                      }
+                      else
+                      {
+                          jarOut.write(orgSrc.getBytes(), 0, orgSrc.getBytes().length);
+                          if(oldBarFiles.indexOf(barFile.getAbsolutePath()) == -1){
+                              oldBarFiles.add(barFile.getAbsolutePath());
+                          }
+                          if(newBarFiles.indexOf(outName) == -1){
+                              newBarFiles.add(outName);  
+                          }
+                      }
+
+                  }
+
+                  jarOut.closeEntry();
+                }
+                
+                if(newBarFiles.indexOf(outName) == -1){
+                	oldBarFiles.add(outName);  
+                }
+                jarOut.flush();
+                jarOut.close();
+                jarIn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        for (String fileName: oldBarFiles)
+        {
+            File toDelFile = new File(fileName);
+            if(toDelFile.exists())
+                toDelFile.delete();
+        }
+
+        System.gc();
+
+        boolean rename = true;
+        for (String fileName : newBarFiles)
+        {
+            File newFile = new File(fileName);
+            if(newFile.exists())
+            {
+                int sign = newFile.getAbsolutePath().indexOf(SUFFIX_BAR);
+                if(newFile.renameTo(new File(newFile.getAbsolutePath().substring(0, sign) + BAR_ZIP)) == false)
+                {
+                	rename = false;
+                }
+            }
+        }
+        
+        return rename;
+  }
+
+	
 	@Override
 	protected Boolean execute() {
 		
-		return updateRolesInProvision() && updateRolesInDataModel();
+		return updateRolesInProvision() && updateRolesInDataModel() && updateRolesInWorkFlow();
 	}
 }
