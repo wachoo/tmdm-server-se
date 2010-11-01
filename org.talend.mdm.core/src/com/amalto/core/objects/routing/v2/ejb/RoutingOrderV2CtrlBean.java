f2a0e8d8c5e60cc205eb5fdb7f105a202dc9b320
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-11-01 06:54:49 +0000

    fix bug  	 0016841: pop up error message when search any field in Event Manager
    git-svn-id: http://talendforge.org/svn/tom/trunk@50757 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/objects/routing/v2/ejb/RoutingOrderV2CtrlBean.java
+++ org.talend.mdm.core/src/com/amalto/core/objects/routing/v2/ejb/RoutingOrderV2CtrlBean.java
@@ -723,7 +723,7 @@ public class RoutingOrderV2CtrlBean implements SessionBean, TimedObject {
      	
         if((anyFieldContains!=null) && (!"*".equals(anyFieldContains))) {
         	wAnd.add(new WhereCondition(
-        			pojoName,
+        			pojoName+"/.",
         			WhereCondition.CONTAINS,
         			anyFieldContains,
         			WhereCondition.PRE_NONE,
