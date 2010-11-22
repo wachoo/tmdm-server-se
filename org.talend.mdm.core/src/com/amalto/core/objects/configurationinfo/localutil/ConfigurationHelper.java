bc85b058e29d170f58497be3233808e0274b33fe
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-11-22 09:28:21 +0000

    Refactor to use existCluster() method instead of walking through all the clusters to determine whether it exists or not
    git-svn-id: http://talendforge.org/svn/tom/trunk@51559 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/objects/configurationinfo/localutil/ConfigurationHelper.java
+++ org.talend.mdm.core/src/com/amalto/core/objects/configurationinfo/localutil/ConfigurationHelper.java
@@ -1,7 +1,5 @@
 package com.amalto.core.objects.configurationinfo.localutil;
 
-import java.util.Arrays;
-
 import javax.naming.InitialContext;
 
 import org.apache.log4j.Logger;
@@ -13,106 +11,95 @@ import com.amalto.core.util.Util;
 import com.amalto.core.util.XtentisException;
 
 public class ConfigurationHelper {
-	
-	private static final Logger logger=Logger.getLogger(ConfigurationHelper.class);
-	private static XmlServerSLWrapperLocal server = null;//Do not use this field directly
-	
-	public static XmlServerSLWrapperLocal getServer() throws XtentisException {
-		if(server==null){
-			try {
-    			server  =  ((XmlServerSLWrapperLocalHome)new InitialContext().lookup(XmlServerSLWrapperLocalHome.JNDI_NAME)).create();
-    		} catch (Exception e) {
-    			String err = "Unable to access the XML Server wrapper";
-    			org.apache.log4j.Logger.getLogger(CoreUpgrades.class).error(err,e);
-    			throw new XtentisException(err);
-    		}
-		}
-		return server;
-	}
 
+    private static final Logger logger = Logger.getLogger(ConfigurationHelper.class);
 
+    private static XmlServerSLWrapperLocal server = null;// Do not use this field directly
 
-	public static void createCluster(String revisionID, Class<? extends ObjectPOJO> objectClass) throws XtentisException{
-		createCluster(revisionID,ObjectPOJO.getCluster(objectClass));
+    public static XmlServerSLWrapperLocal getServer() throws XtentisException {
+        if (server == null) {
+            try {
+                server = ((XmlServerSLWrapperLocalHome) new InitialContext().lookup(XmlServerSLWrapperLocalHome.JNDI_NAME))
+                        .create();
+            } catch (Exception e) {
+                String err = "Unable to access the XML Server wrapper";
+                logger.error(err, e);
+                throw new XtentisException(err, e);
+            }
+        }
+        return server;
     }
-	
-	/**
-	 * @param revisionID
-	 * @param clusterName
-	 * @throws XtentisException
-	 * 
-	 * override : false
-	 */
-	public static void createCluster(String revisionID, String clusterName) throws XtentisException{
-    	
-    	try {
-    		boolean exist = false;
-    		
-    		String[] clusters = getServer().getAllClusters(revisionID);
-    		if (clusters != null) {
-    			exist = Arrays.asList(clusters).contains(clusterName);
-    		}
-    		if (!exist){
-    			getServer().createCluster(revisionID, clusterName);
-    			logger.info("Created a new datacluster "+clusterName);
-    		}
-    		
-    	} catch (Exception e) {
-    		throw new XtentisException(e);
-    	}
-    	
+
+    public static void createCluster(String revisionID, Class<? extends ObjectPOJO> objectClass) throws XtentisException {
+        createCluster(revisionID, ObjectPOJO.getCluster(objectClass));
     }
-	
-	/**
-	 * @param revisionID
-	 * @param clusterName
-	 * @throws XtentisException
-	 * 
-	 * delete if exist
-	 */
-	public static void removeCluster(String revisionID, String clusterName) throws XtentisException{
-		
-		try {
-    		boolean exist = false;
-    		
-    		String[] clusters = getServer().getAllClusters(revisionID);
-    		if (clusters != null) {
-    			exist = Arrays.asList(clusters).contains(clusterName);
-    		}
-    		if (exist){
-    			getServer().deleteCluster(revisionID, clusterName);
-    			logger.info("Deleted a datacluster "+clusterName);
-    		}
-    		
-    	} catch (Exception e) {
-    		throw new XtentisException(e);
-    	}
-	}
-	
-	/**
-	 * @param datacluster
-	 * @param xmlString
-	 * @param uniqueID
-	 * @throws XtentisException
-	 * 
-	 * override : false
-	 */
-	public static void putDomcument(String datacluster, String xmlString,String uniqueID) throws XtentisException {
-		if(Util.getXmlServerCtrlLocal().getDocumentAsString(null, datacluster, uniqueID)==null){
-			Util.getXmlServerCtrlLocal().putDocumentFromString(xmlString, uniqueID, datacluster, null);
-			logger.info("Inserted document "+uniqueID+" to datacluster "+datacluster);
-		}
-	}
-	
-	/**
-	 * @param revisionID
-	 * @param clusterName
-	 * @param uniqueID
-	 * @throws XtentisException
-	 */
-	public static void deleteDocumnet(String revisionID, String clusterName, String uniqueID) throws XtentisException{
-		if(Util.getXmlServerCtrlLocal().getDocumentAsString(null, clusterName, uniqueID)!=null){ 
-		  Util.getXmlServerCtrlLocal().deleteDocument(revisionID, clusterName, uniqueID);
-		}
-	}
+
+    /**
+     * @param revisionID
+     * @param clusterName
+     * @throws XtentisException
+     * 
+     * override : false
+     */
+    public static void createCluster(String revisionID, String clusterName) throws XtentisException {
+
+        try {
+            boolean exist = getServer().existCluster(revisionID, clusterName);
+            if (!exist) {
+                getServer().createCluster(revisionID, clusterName);
+                logger.info("Created a new datacluster " + clusterName);
+            }
+        } catch (Exception e) {
+            throw new XtentisException(e);
+        }
+
+    }
+
+    /**
+     * @param revisionID
+     * @param clusterName
+     * @throws XtentisException
+     * 
+     * delete if exist
+     */
+    public static void removeCluster(String revisionID, String clusterName) throws XtentisException {
+
+        try {
+            boolean exist = getServer().existCluster(revisionID, clusterName);
+            if (exist) {
+                getServer().deleteCluster(revisionID, clusterName);
+                logger.info("Deleted a datacluster " + clusterName);
+            }
+
+        } catch (Exception e) {
+            throw new XtentisException(e);
+        }
+    }
+
+    /**
+     * @param datacluster
+     * @param xmlString
+     * @param uniqueID
+     * @throws XtentisException
+     * 
+     * override : false
+     */
+    public static void putDomcument(String datacluster, String xmlString, String uniqueID) throws XtentisException {
+        if (Util.getXmlServerCtrlLocal().getDocumentAsString(null, datacluster, uniqueID) == null) {
+            Util.getXmlServerCtrlLocal().putDocumentFromString(xmlString, uniqueID, datacluster, null);
+            logger.info("Inserted document " + uniqueID + " to datacluster " + datacluster);
+        }
+    }
+
+    /**
+     * @param revisionID
+     * @param clusterName
+     * @param uniqueID
+     * @throws XtentisException
+     */
+    public static void deleteDocumnet(String revisionID, String clusterName, String uniqueID) throws XtentisException {
+        if (Util.getXmlServerCtrlLocal().getDocumentAsString(null, clusterName, uniqueID) != null) {
+            Util.getXmlServerCtrlLocal().deleteDocument(revisionID, clusterName, uniqueID);
+        }
+    }
 }
