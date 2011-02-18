93aeaec7848d1d730542763a864be9bb1cc5b031
lzhang <lzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-02-18 01:05:56 +0000

    feature 0018896 added: add icons which are used in search panel
    git-svn-id: http://talendforge.org/svn/tom/trunk@55360 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.webapp.itemsbrowser2/src/main/resources/org/talend/mdm/webapp/itemsbrowser2/client/search/Icons.java
@@ -0,0 +1,35 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.mdm.webapp.itemsbrowser2.client.search;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * @author sbessaies
+ */
+public interface Icons extends ClientBundle {
+
+    Icons INSTANCE = GWT.create(Icons.class);
+
+    @Source("add.png")
+    ImageResource add();
+
+    @Source("chart_organisation_add.png")
+    ImageResource chart_organisation_add();
+
+    @Source("remove.png")
+    ImageResource remove();
+}
