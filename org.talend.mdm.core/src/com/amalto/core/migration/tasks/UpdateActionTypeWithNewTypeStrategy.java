cd1efd8027c7db69797c99d3ad8dc07b07acf27a
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-08 03:25:30 +0000

    bug 0019285 fixed: add a migration task for updateReport to make sure the operationType of updateReport is updated with the "PHYSICAL_DELETE" and "LOGICAL_DELETE" is also added
    git-svn-id: http://talendforge.org/svn/tom/trunk@56299 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.core/src/com/amalto/core/migration/tasks/UpdateActionTypeWithNewTypeStrategy.java
@@ -0,0 +1,78 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.core.migration.tasks;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+
+import com.amalto.core.migration.AbstractMigrationTask;
+import com.amalto.core.objects.configurationinfo.localutil.ConfigurationHelper;
+import com.amalto.core.util.Util;
+
+/**
+ * DOC fliu class global comment. Detailled comment
+ */
+public class UpdateActionTypeWithNewTypeStrategy extends AbstractMigrationTask {
+
+    @Override
+    protected Boolean execute() {
+        final String cluster = "amaltoOBJECTSDataModel";//$NON-NLS-1$
+        final String dataModel = "UpdateReport";//$NON-NLS-1$
+        try {
+            String dataModelXml = ConfigurationHelper.getServer().getDocumentAsString(null, cluster, dataModel);
+            Document doc = Util.parse(dataModelXml);
+            NodeList nodeList = Util.getNodeList(doc, "./schema/text()"); //$NON-NLS-1$
+            Document schemaRoot = null;
+            if (nodeList.getLength() > 0) {
+                Object obj = nodeList.item(0);
+                if (obj instanceof Text) {
+                    String wholeSchema = ((Text) obj).getWholeText();
+                    schemaRoot = Util.parseXSD(wholeSchema);
+                }
+            }
+            if (schemaRoot != null) {
+                Element rootNS = Util.getRootElement("nsholder", schemaRoot.getDocumentElement().getNamespaceURI(), "xsd"); //$NON-NLS-1$
+                NodeList enumList = Util
+                        .getNodeList(
+                                schemaRoot,
+                                "//xsd:complexType[@name='Update']//xsd:restriction[@base='xsd:string']/xsd:enumeration[@value='DELETE']", //$NON-NLS-1$
+                                rootNS.getNamespaceURI(), "xsd"); //$NON-NLS-1$
+                if (enumList.getLength() > 0) {
+                    Node enumNode = enumList.item(0);
+                    Node parentNode = enumNode.getParentNode();
+                    Node actionTypeNode = enumNode.getAttributes().getNamedItem("value"); //$NON-NLS-1$
+                    actionTypeNode.setNodeValue("LOGIC_DELETE"); //$NON-NLS-1$
+                    Element newOne = schemaRoot.createElement("xsd:enumeration"); //$NON-NLS-1$
+                    newOne.setAttribute("value", "PHYSICAL_DELETE"); //$NON-NLS-1$
+                    parentNode.appendChild(newOne);
+
+                    String newSchema = "<schema>" + StringEscapeUtils.escapeXml(Util.nodeToString(schemaRoot)) + "</schema>"; //$NON-NLS-1$
+                    Node oldChild = Util.getNodeList(doc, "./schema").item(0); //$NON-NLS-1$
+                    Node elem = doc.importNode(Util.parse(newSchema).getDocumentElement(), true);
+                    doc.getDocumentElement().replaceChild(elem, oldChild);
+
+                    ConfigurationHelper.getServer().putDocumentFromString(Util.nodeToString(doc), dataModel, //$NON-NLS-1$
+                            cluster, null); //$NON-NLS-1$
+                }
+
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
