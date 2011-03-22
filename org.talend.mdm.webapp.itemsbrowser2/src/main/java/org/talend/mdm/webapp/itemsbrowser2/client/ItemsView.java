f5714e10d44fda12ce2b4a76d08d15dd9022890d
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-22 07:42:34 +0000

    add a 'FIXME'  comment
    git-svn-id: http://talendforge.org/svn/tom/trunk@57074 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/client/ItemsView.java
+++ org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/client/ItemsView.java
@@ -267,7 +267,8 @@ public class ItemsView extends View {
 
         Dispatcher.forwardEvent(ItemsEvents.InitSearchContainer);
     }
-
+    
+    //FIXME： can we refact this method to PubService
     private native void regResizeViewPort()/*-{
         var instance = this;
         $wnd.org_talend_mdm_webapp_itemsbrowser2_client_ItemsView_onResizeViewPort = function(){
