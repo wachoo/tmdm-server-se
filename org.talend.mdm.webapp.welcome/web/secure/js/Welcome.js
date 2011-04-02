db58aae765580573413b23d4879bbce80fd9d622
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-02 09:49:19 +0000

    issue 0019861: New record browsing menu entry fixed 'Browse Items' link in welcome tab does not work
    git-svn-id: http://talendforge.org/svn/tom/trunk@57884 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.welcome/web/secure/js/Welcome.js
+++ org.talend.mdm.webapp.welcome/web/secure/js/Welcome.js
@@ -253,7 +253,7 @@ amalto.welcome.Welcome = function () {
 							 style : 'font-size:80%;',
 							 autoHeight : true,
 							 style : 'font-size:80%;',
-							 html : '<span id="ItemsBrowser" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'itemsbrowser\', \'ItemsBrowser\');">' + 
+							 html : '<span id="ItemsBrowser" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'itemsbrowser2\', \'ItemsBrowser2\');">' + 
 								   '<IMG SRC=\"/talendmdm/secure/img/menu/browse.png\"/>' + ' ' + BROWSE_ITEMS[language] + '</span>' + '<br/>' +
 								   '<span id="Journal" style="padding-right:8px;cursor: pointer; width:150;" onclick="amalto.welcome.Welcome.openPages(\'updatereport\', \'UpdateReport\');">' + 
 								   '<IMG SRC=\"/talendmdm/secure/img/menu/updatereport.png\"/>' + ' ' + JOURNAL[language] + '</span>'
