1e6f4ce8e64071b72cffc383a9de4433b81a4a86
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-05 09:01:32 +0000

    bug 0019224 : time measures are now printed using log4j (in debug level) LoadServlet only returns message if debug is also enabled.
    git-svn-id: http://talendforge.org/svn/tom/trunk@57937 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/servlet/LoadServlet.java
+++ org.talend.mdm.core/src/com/amalto/core/servlet/LoadServlet.java
@@ -1,163 +1,161 @@
 package com.amalto.core.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
+import com.amalto.core.ejb.ItemPOJO;
+import com.amalto.core.ejb.local.XmlServerSLWrapperLocal;
+import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
+import com.amalto.core.objects.datamodel.ejb.DataModelPOJO;
+import com.amalto.core.objects.datamodel.ejb.DataModelPOJOPK;
+import com.amalto.core.util.TimeMeasure;
+import com.amalto.core.util.Util;
+import com.amalto.core.util.XSDKey;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.Logger;
-import org.talend.mdm.commmon.util.time.TimeMeasure;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-
-import com.amalto.core.ejb.ItemPOJO;
-import com.amalto.core.ejb.local.XmlServerSLWrapperLocal;
-import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
-import com.amalto.core.objects.datamodel.ejb.DataModelPOJO;
-import com.amalto.core.objects.datamodel.ejb.DataModelPOJOPK;
-import com.amalto.core.objects.universe.ejb.UniversePOJO;
-import com.amalto.core.util.LocalUser;
-import com.amalto.core.util.Util;
-import com.amalto.core.util.XSDKey;
-import com.amalto.core.util.XtentisException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 
 /**
  * @author starkey
- * 
+ *
  *
  */
 
 public class LoadServlet extends HttpServlet {
 
-	private static final String PARAMETER_CLUSTER = "cluster"; //$NON-NLS-1$
-	private static final String PARAMETER_CONCEPT = "concept"; //$NON-NLS-1$
-	private static final String PARAMETER_DATAMODEL = "datamodel"; //$NON-NLS-1$
-	private static final String PARAMETER_VALIDATE = "validate"; //$NON-NLS-1$
-	private static final String PARAMETER_SMARTPK = "smartpk"; //$NON-NLS-1$
-	private static final String PARAMETER_ITEMDATA = "itemdata"; //$NON-NLS-1$
-	
-	private static final Logger log = Logger.getLogger(LoadServlet.class);
+    private static final String PARAMETER_CLUSTER = "cluster"; //$NON-NLS-1$
+    private static final String PARAMETER_CONCEPT = "concept"; //$NON-NLS-1$
+    private static final String PARAMETER_DATAMODEL = "datamodel"; //$NON-NLS-1$
+    private static final String PARAMETER_VALIDATE = "validate"; //$NON-NLS-1$
+    private static final String PARAMETER_SMARTPK = "smartpk"; //$NON-NLS-1$
+    private static final String PARAMETER_ITEMDATA = "itemdata"; //$NON-NLS-1$
+
+    private static final Logger log = Logger.getLogger(LoadServlet.class);
+
     /**
      * Constructor
-     * 
      */
     public LoadServlet() {
         super();
     }
 
-    
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        
+
     }
 
     @Override
     protected void doPost(
             HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-            doGet(request,response);
-        }
-    
+        doGet(request, response);
+    }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException,
-        IOException {
+            throws ServletException,
+            IOException {
 
         req.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
-        
+
         String action = req.getParameter("action"); //$NON-NLS-1$
-        if(action==null||action.length()==0)action=getServletConfig().getInitParameter("action"); //$NON-NLS-1$
+        if (action == null || action.length() == 0)
+            action = getServletConfig().getInitParameter("action"); //$NON-NLS-1$
 
         resp.setContentType("text/html; charset=\"UTF-8\""); //$NON-NLS-1$
         resp.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
-        PrintWriter writer = resp.getWriter();
+
+        // configure writer depending on logger configuration.
+        PrintWriter writer = configureWriter(resp);
         writer.write("<html><body>"); //$NON-NLS-1$
-        
-        String timeMeasureTag=System.currentTimeMillis()+": Bulk load items";
-        ResultLogger resultLogger=new ResultLogger();
-        
-    	try {
-    		if ("load".equals(action)) { //$NON-NLS-1$
-    			writer.write(
-   					"<p><b>Load datas into MDM</b><br/>" +
-   					"Check jboss/server/default/log/server.log or the jboss console output to determine when load is completed</b></p>"
-    			);
-    			
-    			TimeMeasure.begin(timeMeasureTag);
-    			resultLogger.logTimeMeasureBegin(timeMeasureTag);
-    			
-    			
-    			String cluster = null;
-    			String concept = null;
-    			String datamodel = null;
-    		    List<String> itemdatas =new ArrayList<String>();
-    		    
-    		    boolean needAutoGenPK=false;
-    		    boolean needValidate=false;
-    			
-    			Enumeration<String> parameterNames=req.getParameterNames();
-    			for (;parameterNames.hasMoreElements();) {
-					String parameterName = parameterNames.nextElement();
-					
-					if(parameterName.equals(PARAMETER_CLUSTER)) {
-						cluster = req.getParameter(parameterName);
-						continue;
-					}
-					
-					if(parameterName.equals(PARAMETER_CONCEPT)) {
-						concept = req.getParameter(parameterName);
-						continue;
-					}
-					
-					if(parameterName.equals(PARAMETER_DATAMODEL)) {
-						datamodel = req.getParameter(parameterName);
-						continue;
-					}
-					
-					//optional
-					if(parameterName.equals(PARAMETER_VALIDATE)) {
-						String validate = req.getParameter(parameterName);
-						needValidate=(validate!=null&&validate.equals("true"))?true:false; //$NON-NLS-1$
-						continue;
-					}
-					
-					//optional
-					if(parameterName.equals(PARAMETER_SMARTPK)) {
-						String smartpk = req.getParameter(parameterName);
-						needAutoGenPK=(smartpk!=null&&smartpk.equals("true"))?true:false; //$NON-NLS-1$
-						continue;
-					}
-					
-					if(parameterName.startsWith(PARAMETER_ITEMDATA)) {
-						itemdatas.add(req.getParameter(parameterName));
-					}
-				}
-    			
-    			
-    			DataClusterPOJOPK clusterPK=new DataClusterPOJOPK(cluster);
-    			
- 
+
+        String timeMeasureTag = System.currentTimeMillis() + ": Bulk load items";
+        ResultLogger resultLogger = new ResultLogger();
+
+        try {
+            if ("load".equals(action)) { //$NON-NLS-1$
+                writer.write(
+                        "<p><b>Load datas into MDM</b><br/>" +
+                                "Check jboss/server/default/log/server.log or the jboss console output to determine when load is completed</b></p>"
+                );
+
+                TimeMeasure.begin(timeMeasureTag);
+                resultLogger.logTimeMeasureBegin(timeMeasureTag);
+
+
+                String cluster = null;
+                String concept = null;
+                String datamodel = null;
+                List<String> itemdatas = new ArrayList<String>();
+
+                boolean needAutoGenPK = false;
+                boolean needValidate = false;
+
+                Enumeration<String> parameterNames = req.getParameterNames();
+                for (; parameterNames.hasMoreElements();) {
+                    String parameterName = parameterNames.nextElement();
+
+                    if (parameterName.equals(PARAMETER_CLUSTER)) {
+                        cluster = req.getParameter(parameterName);
+                        continue;
+                    }
+
+                    if (parameterName.equals(PARAMETER_CONCEPT)) {
+                        concept = req.getParameter(parameterName);
+                        continue;
+                    }
+
+                    if (parameterName.equals(PARAMETER_DATAMODEL)) {
+                        datamodel = req.getParameter(parameterName);
+                        continue;
+                    }
+
+                    //optional
+                    if (parameterName.equals(PARAMETER_VALIDATE)) {
+                        String validate = req.getParameter(parameterName);
+                        needValidate = (validate != null && validate.equals("true")) ? true : false; //$NON-NLS-1$
+                        continue;
+                    }
+
+                    //optional
+                    if (parameterName.equals(PARAMETER_SMARTPK)) {
+                        String smartpk = req.getParameter(parameterName);
+                        needAutoGenPK = (smartpk != null && smartpk.equals("true")) ? true : false; //$NON-NLS-1$
+                        continue;
+                    }
+
+                    if (parameterName.startsWith(PARAMETER_ITEMDATA)) {
+                        itemdatas.add(req.getParameter(parameterName));
+                    }
+                }
+
+
+                DataClusterPOJOPK clusterPK = new DataClusterPOJOPK(cluster);
+
+
                 DataModelPOJO dataModel = Util.getDataModelCtrlLocal().getDataModel(new DataModelPOJOPK(datamodel));
-                String schemaString=dataModel.getSchema();
+                String schemaString = dataModel.getSchema();
                 Document schema = Util.parseXSD(schemaString);
                 XSDKey conceptKey = com.amalto.core.util.Util.getBusinessConceptKey(schema, concept);
-                
-                resultLogger.logTimeMeasureStep(timeMeasureTag, "Parse schema", TimeMeasure.step(timeMeasureTag,"Parse schema"));
+
+                resultLogger.logTimeMeasureStep(timeMeasureTag, "Parse schema", TimeMeasure.step(timeMeasureTag, "Parse schema"));
                 //each item
                 XmlServerSLWrapperLocal server = Util.getXmlServerCtrlLocal();
                 boolean transactionSupported = server.supportTransaction();
-                
+
                 try {
                     if (transactionSupported)
                         server.start();
@@ -171,7 +169,7 @@ public class LoadServlet extends HttpServlet {
                                 continue;
 
                             Element root = Util.parse(xmldata).getDocumentElement();
-                            
+
                             // get key values
                             // support UUID or auto-increase temporarily
                             String[] ids = null;
@@ -217,7 +215,7 @@ public class LoadServlet extends HttpServlet {
 
                     if (transactionSupported)
                         server.commit();
-                    
+
                 } catch (Exception e) {
                     log.error(e.getMessage(), e);
                     if (transactionSupported) {
@@ -244,65 +242,103 @@ public class LoadServlet extends HttpServlet {
                 writer.write("<p><b>Unknown action: </b>" + action + "<br/>");
 
             }
-    	
-    	} catch (Exception e) {
-    		writer.write("<h1>An error occured: "+e.getLocalizedMessage()+"</h1>");
-    		TimeMeasure.end(timeMeasureTag);
-    		log.error(e.getMessage(), e);
-    	}
+
+        } catch (Exception e) {
+            writer.write("<h1>An error occured: " + e.getLocalizedMessage() + "</h1>");
+            TimeMeasure.end(timeMeasureTag);
+            log.error(e.getMessage(), e);
+        }
         writer.write("</body></html>"); //$NON-NLS-1$
-                
     }
-    
-      
+
+
+    /**
+     * Returns a writer that does not print anything if logger hasn't DEBUG level.
+     *
+     * @param resp A servlet response output.
+     * @return The same {@link HttpServletResponse} if debug <b>is</b> enabled, or a no-op one if debug is disabled.
+     * @throws IOException Thrown by {@link javax.servlet.http.HttpServletResponse#getWriter()}.
+     */
+    private static PrintWriter configureWriter(HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        if (log.isDebugEnabled()) {
+            return writer;
+        } else {
+            return new PrintWriter(writer) {
+                @Override
+                public void write(int c) {
+                    // Nothing to do (debug isn't enabled)
+                }
+
+                @Override
+                public void write(char[] buf, int off, int len) {
+                    // Nothing to do (debug isn't enabled)
+                }
+
+                @Override
+                public void write(char[] buf) {
+                    // Nothing to do (debug isn't enabled)
+                }
+
+                @Override
+                public void write(String s, int off, int len) {
+                    // Nothing to do (debug isn't enabled)
+                }
+
+                @Override
+                public void write(String s) {
+                    // Nothing to do (debug isn't enabled)
+                }
+            };
+        }
+
+    }
+
+
     /**
      * Represents an element on the stack.
      */
     private static class ResultLogger {
 
-    	private StringBuffer resultLogger=null;
+        private StringBuffer resultLogger = null;
 
         public ResultLogger() {
             this.resultLogger = new StringBuffer();
         }
 
         public void logTimeMeasure(String msg) {
-			
-        	resultLogger.append(System.currentTimeMillis()+" [TimeMeasure]:"+msg+"</br>");
 
-		}
-        
-        
+            resultLogger.append(System.currentTimeMillis() + " [TimeMeasure]:" + msg + "</br>");
+
+        }
+
+
         private void logTimeMeasureBegin(String timerId) {
-        	
-        	logTimeMeasure("Start '" + timerId + "' ...");
 
-		}
-        
-        private void logTimeMeasureEnd(String timerId,long totalElapsedTime) {
-        	
-        	logTimeMeasure("End '" + timerId + "', total elapsed time: " + totalElapsedTime + " ms ");
+            logTimeMeasure("Start '" + timerId + "' ...");
 
-		}
-        
-        public void logTimeMeasureStep(String timerId,String stepName,long stepCount) {
-        	logTimeMeasure("&nbsp;&nbsp;&nbsp;&nbsp;-> '" + timerId + "', step name '" + stepName  + "', elapsed time since previous step: " + stepCount + " ms ");
+        }
+
+        private void logTimeMeasureEnd(String timerId, long totalElapsedTime) {
+
+            logTimeMeasure("End '" + timerId + "', total elapsed time: " + totalElapsedTime + " ms ");
+
+        }
+
+        public void logTimeMeasureStep(String timerId, String stepName, long stepCount) {
+            logTimeMeasure("&nbsp;&nbsp;&nbsp;&nbsp;-> '" + timerId + "', step name '" + stepName + "', elapsed time since previous step: " + stepCount + " ms ");
         }
 
         public void logErrorMessage(String msg) {
-        	
-        	resultLogger.append(System.currentTimeMillis()+" [ErrorMessage]:"+msg+"</br>");
-			
-		}
-        
-        private void clear() {
-        	resultLogger=null;
-		}
-        
+
+            resultLogger.append(System.currentTimeMillis() + " [ErrorMessage]:" + msg + "</br>");
+
+        }
+
         public String print() {
-			return resultLogger.toString();
-		}
+            return resultLogger.toString();
+        }
     }
- 
-    
+
+
 }
