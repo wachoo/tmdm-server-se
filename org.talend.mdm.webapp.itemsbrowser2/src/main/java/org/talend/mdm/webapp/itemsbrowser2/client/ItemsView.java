2e83e584bc81f3c86d9843b9209dad0e9e81aaa9
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-08 11:11:20 +0000

    feature 0020427 modified:  midified bug：   Remove the 'Save and Close' button
    git-svn-id: http://talendforge.org/svn/tom/trunk@58231 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/client/ItemsView.java
+++ org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/client/ItemsView.java
@@ -179,7 +179,7 @@ public class ItemsView extends View {
                     // Unit.PX);
                     itemsSearchContainer.getItemsFormPanel().getContent().getBody().dom.getStyle().setOverflow(Overflow.AUTO);
                     GetService.renderFormWindow(itemBean.getIds(), itemBean.getConcept(), false, itemsSearchContainer //$NON-NLS-1$
-                            .getItemsFormPanel().getContent().getBody().dom, true, false);
+                            .getItemsFormPanel().getContent().getBody().dom, true, false, false);
                 }
                 // TODO handle legacy form
             } else if (itemsFormTarget.equals(ItemsView.TARGET_IN_NEW_TAB)) {
@@ -219,7 +219,7 @@ public class ItemsView extends View {
                     window.setPosition(left + offset, top + offset);
                     window.setId("formWindowContainer");//$NON-NLS-1$
                     GetService.regCallback();
-                    GetService.renderFormWindow(itemBean.getIds(), itemBean.getConcept(), false, window.getBody().dom, false, true);//$NON-NLS-1$
+                    GetService.renderFormWindow(itemBean.getIds(), itemBean.getConcept(), false, window.getBody().dom, false, true, true);//$NON-NLS-1$
                 }
                 // TODO handle legacy form
             }
