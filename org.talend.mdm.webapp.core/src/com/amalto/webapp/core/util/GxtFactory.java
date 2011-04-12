ad0802e5f94596bfaa9d1d9ef93e5f838b87cee9
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-12 16:39:49 +0000

    Fix warnings
    git-svn-id: http://talendforge.org/svn/tom/trunk@58372 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.core/src/com/amalto/webapp/core/util/GxtFactory.java
+++ org.talend.mdm.webapp.core/src/com/amalto/webapp/core/util/GxtFactory.java
@@ -20,17 +20,16 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-
 /**
- * DOC HSHU  class global comment. Detailled comment
+ * DOC HSHU class global comment. Detailled comment
  */
 public class GxtFactory {
-    
+
     /** the log used by this class */
     private static Logger log = Logger.getLogger(GxtFactory.class);
 
     /** the collection of actions that we know about */
-    private Map entries = new HashMap();
+    private Map<String, String> entries = new HashMap<String, String>();
 
     /** the name of the action mapping file */
     private String gxtRegisterFileName;
@@ -39,50 +38,43 @@ public class GxtFactory {
      * Creates a new instance, using the given configuration file.
      */
     public GxtFactory(String gxtRegisterFileName) {
-      this.gxtRegisterFileName = gxtRegisterFileName;
-      init();
+        this.gxtRegisterFileName = gxtRegisterFileName;
+        init();
     }
 
     /**
-     * Initialises this component, reading in and creating the map
-     * of action names to action classes.
+     * Initialises this component, reading in and creating the map of action names to action classes.
      */
     private void init() {
-      try {
-        // load the properties file containing the name -> class name mapping
-        InputStream in = getClass().getClassLoader().getResourceAsStream(gxtRegisterFileName);
-        Properties props = new Properties();
-        props.load(in);
+        try {
+            // load the properties file containing the name -> class name mapping
+            InputStream in = getClass().getClassLoader().getResourceAsStream(gxtRegisterFileName);
+            Properties props = new Properties();
+            props.load(in);
 
-        // and store them in a HashMap
-        Enumeration e = props.propertyNames();
-        String actionName;
-        String className;
+            // and store them in a HashMap
+            Enumeration<?> e = props.propertyNames();
+            String actionName;
+            String className;
 
-        while (e.hasMoreElements()) {
-          actionName = (String)e.nextElement();
-          className = props.getProperty(actionName);
+            while (e.hasMoreElements()) {
+                actionName = (String) e.nextElement();
+                className = props.getProperty(actionName);
 
-          entries.put(actionName, className);
+                entries.put(actionName, className);
+            }
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
-
-      } catch (Exception e) {
-        log.error(e.getMessage(), e);
-        e.printStackTrace();
-      }
     }
 
- 
-    public String getGxtEntryModule(String context,String application){
-        String key=context+"."+application;
-        
-        if (entries!=null&&entries.containsKey(key)) {
-            
-            return entries.get(key)==null?null:(String)entries.get(key);
-            
+    public String getGxtEntryModule(String context, String application) {
+        String key = context + '.' + application;
+        if (entries != null && entries.containsKey(key)) {
+            return entries.get(key) == null ? null : (String) entries.get(key);
         }
         return null;
     }
-    
 
 }
