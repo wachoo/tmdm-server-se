0e975b259cbb0f5c72f3c9948daf08e706d3baf7
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-13 07:15:20 +0000

    fix bug  	 0019572: Add an action to flush the authent cache
    git-svn-id: http://talendforge.org/svn/tom/trunk@58414 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.core/src/com/amalto/core/util/AuthentCacheAgent.java
@@ -0,0 +1,78 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.core.util;
+
+import java.lang.management.ManagementFactory;
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.security.auth.Subject;
+
+import org.jboss.security.plugins.JaasSecurityManagerService;
+
+
+
+public class AuthentCacheAgent {
+	 private MBeanServer mbs = null;
+	 
+	 public AuthentCacheAgent(){
+		  // Get the platform MBeanServer
+	      mbs = ManagementFactory.getPlatformMBeanServer();
+	 }
+	 
+	 public void flushAuthentCache(){
+
+	      ObjectName name = null;
+
+	      try {
+	         // Uniquely identify the MBeans and register them with the MBeanServer 
+	         name = new ObjectName("jboss.security:service=JaasSecurityManager");
+	         
+	         try{
+	        	 MBeanInfo info = mbs.getMBeanInfo( name );
+	        	 System.out.println( "Conversion Class: " + info.getClassName() );
+	         }catch(InstanceNotFoundException e2){	        	 
+	        	 mbs.registerMBean(new JaasSecurityManagerService(), name);
+	         }
+	         
+	         //Invoke convertTo op
+	         String opName = "flushAuthenticationCache";
+	         String sig[] = { "java.lang.String","java.security.Principal" };
+	         
+	         Object user=null;
+	         Subject subject = LocalUser.getCurrentSubject();
+	            Set<Principal> set = subject.getPrincipals();
+	            for (Iterator<Principal> iter = set.iterator(); iter.hasNext();) {
+	                Principal principal = iter.next();
+	                if (principal instanceof Group) {
+	                    Group group = (Group) principal;
+	                    if ("Username".equals(group.getName())) {
+	                    	user=group;
+	                    	break;
+	                    }
+	                 }
+	         }
+	         Object opArgs[] = { "java:/jaas/xtentisSecurity",user};   
+	         //The following code throws ReflectionException
+	         Object result = mbs.invoke( name, opName, opArgs, sig );		        
+	      } catch(Exception e) {
+	         e.printStackTrace();
+	      }		 
+	 }
+}
