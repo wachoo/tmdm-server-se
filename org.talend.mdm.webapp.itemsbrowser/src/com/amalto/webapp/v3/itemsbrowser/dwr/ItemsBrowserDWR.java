3d03db1846817943521884923c836b5f19089ffc
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-05 08:37:54 +0000

    issue 0021230: Get an exception when launch a process generated by a workflow
    git-svn-id: http://talendforge.org/svn/tom/trunk@59824 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser/src/com/amalto/webapp/v3/itemsbrowser/dwr/ItemsBrowserDWR.java
+++ org.talend.mdm.webapp.itemsbrowser/src/com/amalto/webapp/v3/itemsbrowser/dwr/ItemsBrowserDWR.java
@@ -3422,7 +3422,7 @@ public class ItemsBrowserDWR {
                         .getPipeline().getPipelineItem();
                 if (entries.length > 0) {
                     WSTransformerContextPipelinePipelineItem item = entries[entries.length - 1];
-                    if (item.getVariable().equals("output")) {//$NON-NLS-1$
+                    if (item.getVariable().equals("outputUrl")) {//$NON-NLS-1$
                         byte[] bytes = item.getWsTypedContent().getWsBytes().getBytes();
                         String content = new String(bytes);
                         try {
