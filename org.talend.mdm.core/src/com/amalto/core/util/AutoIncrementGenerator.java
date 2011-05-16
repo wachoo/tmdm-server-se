7351383c7aa061d2c68cc6647ec87792069afc33
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-16 09:59:31 +0000

    bug 21360 : change log of NPE into a more meaningful message (NPE meant no configuration found in database).
    git-svn-id: http://talendforge.org/svn/tom/trunk@60551 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/util/AutoIncrementGenerator.java
+++ org.talend.mdm.core/src/com/amalto/core/util/AutoIncrementGenerator.java
@@ -1,10 +1,9 @@
 package com.amalto.core.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.talend.mdm.commmon.util.webapp.XSystemObjects;
 
 import com.amalto.core.ejb.ItemPOJO;
@@ -19,68 +18,77 @@ import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
  *
  */
 public class AutoIncrementGenerator {
-	//static volatile long  num=-1;	
+    //static volatile long  num=-1;
 
-	static File file = new File("auto_increment.conf");
-	public static String AUTO_INCREMENT="Auto_Increment";
-	private static Properties CONFIGURATION = null;
-	static DataClusterPOJOPK DC=new DataClusterPOJOPK(XSystemObjects.DC_CONF.getName());
-	static String[] IDS=new String[] {"AutoIncrement"};
-	static String CONCEPT="AutoIncrement";
-	static{
-		init();
-	}
-	public static void init(){
-		//first try Current path
-		CONFIGURATION = new Properties();
-		try {
-			ItemPOJOPK pk=new ItemPOJOPK(DC, CONCEPT, IDS);
-			ItemPOJO itempojo=ItemPOJO.load(pk);
-			String xml=itempojo.getProjectionAsString();
-			if(xml!=null && xml.trim().length()>0) {
-				CONFIGURATION=Util.convertAutoIncrement(xml);
-			}
-		} catch (Exception e) {
-			org.apache.log4j.Logger.getLogger(AutoIncrementGenerator.class).error(e.getLocalizedMessage(),e);
-		}	
-	}
-	/**
-	 * this is not a good algrithom, need to find a better way
-	 * @return
-	 */
-	public synchronized static long  generateNum(String universe,String dataCluster,String conceptName){
-		
-		String key=universe+"."+dataCluster+"."+conceptName;
-		long num=0;		
-		String n=CONFIGURATION.getProperty(key);
-		if(n==null){
-			num=0;				
-		}else{
-			num=Long.valueOf(n).longValue();
-		}
-		num++;
-			
-		CONFIGURATION.setProperty(key, String.valueOf(num));
-		saveToDB();
-		return num;
-	}
-	
-	public static  void saveToDB() {
+    private static final Logger logger = Logger.getLogger(AutoIncrementGenerator.class);
+    static File file = new File("auto_increment.conf"); // $NON-NLS-1$
+    public static String AUTO_INCREMENT = "Auto_Increment"; // $NON-NLS-1$
+    private static Properties CONFIGURATION = null;
+    static DataClusterPOJOPK DC = new DataClusterPOJOPK(XSystemObjects.DC_CONF.getName());
+    static String[] IDS = new String[]{"AutoIncrement"}; // $NON-NLS-1$
+    static String CONCEPT = "AutoIncrement"; // $NON-NLS-1$
 
-		try {
-			String xmlString=Util.convertAutoIncrement(CONFIGURATION);
-			ItemPOJO pojo = new ItemPOJO(					
-					DC,	//cluster
-					CONCEPT,								//concept name
-					IDS,
-					System.currentTimeMillis(),			//insertion time
-					xmlString												//actual data
-			);
-			pojo.setDataModelName(XSystemObjects.DM_CONF.getName());
-			pojo.store(null);
-			
-		}catch(Exception e) {
-			
-		}		
-	}
+    static {
+        init();
+    }
+
+    public static void init() {
+        //first try Current path
+        CONFIGURATION = new Properties();
+        try {
+            ItemPOJOPK pk = new ItemPOJOPK(DC, CONCEPT, IDS);
+            ItemPOJO itempojo = ItemPOJO.load(pk);
+            if (itempojo == null) {
+                logger.info("Could not load configuration from database, use default configuration.");
+            } else {
+                String xml = itempojo.getProjectionAsString();
+                if (xml != null && xml.trim().length() > 0) {
+                    CONFIGURATION = Util.convertAutoIncrement(xml);
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+    }
+
+    /**
+     * this is not a good algorithm, need to find a better way
+     *
+     * @param universe
+     * @param dataCluster
+     * @param conceptName
+     * @return
+     */
+    public synchronized static long generateNum(String universe, String dataCluster, String conceptName) {
+        String key = universe + "." + dataCluster + "." + conceptName;
+        long num;
+        String n = CONFIGURATION.getProperty(key);
+        if (n == null) {
+            num = 0;
+        } else {
+            num = Long.valueOf(n);
+        }
+        num++;
+
+        CONFIGURATION.setProperty(key, String.valueOf(num));
+        saveToDB();
+        return num;
+    }
+
+    public static void saveToDB() {
+        try {
+            String xmlString = Util.convertAutoIncrement(CONFIGURATION);
+            ItemPOJO pojo = new ItemPOJO(
+                    DC,    //cluster
+                    CONCEPT,                                //concept name
+                    IDS,
+                    System.currentTimeMillis(),            //insertion time
+                    xmlString                                                //actual data
+            );
+            pojo.setDataModelName(XSystemObjects.DM_CONF.getName());
+            pojo.store(null);
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+    }
 }
