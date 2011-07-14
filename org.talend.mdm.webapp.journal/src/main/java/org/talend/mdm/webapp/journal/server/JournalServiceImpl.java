b42087d18b3879773f7f666928dcf24707111447
lzhang <lzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-14 03:57:50 +0000

    feature	0023257 added: create project's infrastructure
    git-svn-id: http://talendforge.org/svn/tom/trunk@64323 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/server/JournalServiceImpl.java
@@ -0,0 +1,26 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.webapp.journal.server;
+
+import org.talend.mdm.webapp.journal.client.JournalService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class JournalServiceImpl extends RemoteServiceServlet implements JournalService {
+
+
+}
---
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/shared/DataChangeLog.java
@@ -0,0 +1,160 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.webapp.journal.shared;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DataChangeLog {
+	
+	
+	
+	private String source;
+	
+	private String timeInMillis;
+	
+    private String operationType;
+    
+	private String concept;
+	
+	private String key;
+	
+	private String xmlSource;
+	
+	private String ids;
+	
+	//additional fields
+	private String userName;
+	
+    private String revisionID;
+    
+    private String dataCluster;
+    
+    private String dataModel;
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+
+    private String epochTime;
+
+	public String getSource() {
+		return cleanOutput(source);
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getTimeInMillis() {
+		String showTime="";
+		if(timeInMillis!=null&&timeInMillis.length()>0&&!timeInMillis.equals("null")){
+			Calendar c= Calendar.getInstance();
+			c.setTimeInMillis(Long.parseLong(timeInMillis));
+			showTime=sdf.format(c.getTime());
+		}
+		return showTime;
+	}
+
+	public void setTimeInMillis(String timeInMillis) {
+		this.timeInMillis = timeInMillis;
+	}
+
+	public String getOperationType() {
+		return cleanOutput(operationType);
+	}
+
+	public void setOperationType(String operationType) {
+		this.operationType = operationType;
+	}
+
+	public String getConcept() {
+		return cleanOutput(concept);
+	}
+
+	public void setConcept(String concept) {
+		this.concept = concept;
+	}
+
+	public String getKey() {
+		return cleanOutput(key);
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getXmlSource() {
+		return xmlSource;
+	}
+
+	public void setXmlSource(String xmlSource) {
+		this.xmlSource = xmlSource;
+	}
+	
+	public String getIds() {
+		return ids;
+	}
+
+	public void setIds(String ids) {
+		this.ids = ids;
+	}
+	
+	public String getUserName() {
+		return cleanOutput(userName);
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getRevisionID() {
+		return cleanOutput(revisionID);
+	}
+
+	public void setRevisionID(String revisionID) {
+		this.revisionID = revisionID;
+	}
+
+	public String getDataCluster() {
+		return cleanOutput(dataCluster);
+	}
+
+	public void setDataCluster(String dataCluster) {
+		this.dataCluster = dataCluster;
+	}
+
+	public String getDataModel() {
+        if (dataModel == null || dataModel.equals("null")) //$NON-NLS-1$
+            dataModel = ""; //$NON-NLS-1$
+
+        return dataModel.trim();
+	}
+
+	public void setDataModel(String dataModel) {
+		this.dataModel = dataModel;
+	}
+
+	private String cleanOutput(String output) {
+		
+		
+		
+		return output;
+	}
+
+    public void setEpochTime(String epochTime) {
+        this.epochTime = epochTime;
+    }
+
+    public String getEpochTime() {
+        return epochTime;
+    }
+}
