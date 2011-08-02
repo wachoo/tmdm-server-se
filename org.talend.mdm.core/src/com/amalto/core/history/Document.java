28e4f54cc12e4c6340c6a73ea008d6b04d73975e
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-02 18:01:25 +0000

    recovery for r65266
    git-svn-id: http://talendforge.org/svn/tom/trunk@65379 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/history/Document.java
+++ org.talend.mdm.core/src/com/amalto/core/history/Document.java
@@ -7,7 +7,17 @@ public interface Document {
     /**
      * @return Returns the document as string (only the user's document, not the MDM specific XML header).
      */
-    String getAsString();
+    String exportToString();
+
+    /**
+     * Transforms the document into a new one. Instance passed as parameter of {@link DocumentTransformer} is <code>this</code>,
+     * so any modification done to the document in the transformer is performed (i.e. transformation isn't necessarily
+     * performed on a copy).
+     *
+     * @param transformer A {@link DocumentTransformer} implementation.
+     * @return A document transformed by the transformer.
+     */
+    Document transform(DocumentTransformer transformer);
 
     /**
      * @return true is the document has just been created, false otherwise.
@@ -27,6 +37,7 @@ public interface Document {
      * <p>
      * <b>Note:</b>The logged user must be admin to call this method.
      * </p>
+     *
      * @throws IllegalStateException If the user is not an admin user.
      */
     void restore();
