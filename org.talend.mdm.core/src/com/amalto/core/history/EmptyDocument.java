0211517d80d15e35860b6e10ce9cc6168332f140
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-02 10:48:43 +0000

    to do recovery for last revision which caused the build failure
    git-svn-id: http://talendforge.org/svn/tom/trunk@65361 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/history/EmptyDocument.java
+++ org.talend.mdm.core/src/com/amalto/core/history/EmptyDocument.java
@@ -11,45 +11,14 @@
 
 package com.amalto.core.history;
 
-import com.amalto.core.history.accessor.Accessor;
-import com.amalto.core.history.accessor.DOMAccessorFactory;
-import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang.StringUtils;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
 *
 */
 public class EmptyDocument implements MutableDocument {
-    public static final org.w3c.dom.Document EMPTY_DOCUMENT;
-
-    static {
-        try {
-            EMPTY_DOCUMENT = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public String exportToString() {
+    public String getAsString() {
         return StringUtils.EMPTY;
-    }
-
-    public Accessor createAccessor(String path) {
-        return DOMAccessorFactory.createAccessor(path, this);
-    }
-
-    public org.w3c.dom.Document asDOM() {
-        return EMPTY_DOCUMENT;
-    }
-
-    public Document transform(DocumentTransformer transformer) {
-        if (transformer == null) {
-            throw new IllegalArgumentException("Transformer argument cannot be null");
-        }
-        return transformer.transform(this);
     }
 
     public boolean isCreated() {
