dc7d44b41615cce69cfcd06fe77cd311b5e4e4d6
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-09 08:05:20 +0000

    fix bug TMDM-1722
    git-svn-id: http://talendforge.org/svn/tom/trunk@65720 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.updatereport/src/talend/webapp/v3/updatereport/servlet/UpdateReportDetailsServelt.java
+++ org.talend.mdm.webapp.updatereport/src/talend/webapp/v3/updatereport/servlet/UpdateReportDetailsServelt.java
@@ -56,144 +56,145 @@ public class UpdateReportDetailsServelt extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String ids = req.getParameter("ids");
-        String export = req.getParameter("params");
+        String ids = req.getParameter("ids"); //$NON-NLS-1$
+        String export = req.getParameter("params"); //$NON-NLS-1$
 
-        String jsonTree = "";
+        String jsonTree = ""; //$NON-NLS-1$
         JSONObject json = new JSONObject();
 
         if (ids != null && ids.length() > 0) {
             try {
                 WSDataClusterPK wsDataClusterPK = new WSDataClusterPK(XSystemObjects.DC_UPDATE_PREPORT.getName());
-                String conceptName = "Update";// Hard Code
-                String[] idss = ids.split("\\.");
+                String conceptName = "Update";// Hard Code //$NON-NLS-1$
+                String[] idss = ids.split("\\."); //$NON-NLS-1$
                 WSGetItem wsGetItem = new WSGetItem(new WSItemPK(wsDataClusterPK, conceptName, idss));
                 WSItem wsItem = Util.getPort().getItem(wsGetItem);
                 String content = wsItem.getContent();
                 if (content != null && content.length() > 0) {
                     // no recursion
                     Document doc = Util.parse(content);
-                    String userName = Util.getFirstTextNode(doc, "/Update/UserName");
-                    String source = Util.getFirstTextNode(doc, "/Update/Source");
-                    String timeInMillis = Util.getFirstTextNode(doc, "/Update/TimeInMillis");
-                    String operationType = Util.getFirstTextNode(doc, "/Update/OperationType");
-                    String revisionID = Util.getFirstTextNode(doc, "/Update/RevisionID");
-                    String dataCluster = Util.getFirstTextNode(doc, "/Update/DataCluster");
-                    String dataModel = Util.getFirstTextNode(doc, "/Update/DataModel");
-                    String concept = Util.getFirstTextNode(doc, "/Update/Concept");
-                    String key = Util.getFirstTextNode(doc, "/Update/Key");
+                    String userName = Util.getFirstTextNode(doc, "/Update/UserName"); //$NON-NLS-1$
+                    String source = Util.getFirstTextNode(doc, "/Update/Source"); //$NON-NLS-1$
+                    String timeInMillis = Util.getFirstTextNode(doc, "/Update/TimeInMillis"); //$NON-NLS-1$
+                    String operationType = Util.getFirstTextNode(doc, "/Update/OperationType"); //$NON-NLS-1$
+                    String revisionID = Util.getFirstTextNode(doc, "/Update/RevisionID"); //$NON-NLS-1$
+                    String dataCluster = Util.getFirstTextNode(doc, "/Update/DataCluster"); //$NON-NLS-1$
+                    String dataModel = Util.getFirstTextNode(doc, "/Update/DataModel"); //$NON-NLS-1$
+                    String concept = Util.getFirstTextNode(doc, "/Update/Concept"); //$NON-NLS-1$
+                    String key = Util.getFirstTextNode(doc, "/Update/Key"); //$NON-NLS-1$
 
                     ArrayList<JSONObject> rootGroup = new ArrayList<JSONObject>();
                     JSONObject userNameNode = new JSONObject();
-                    userNameNode.put("id", "userName");
-                    userNameNode.put("text", "UserName:" + cleanOutput(userName));
-                    userNameNode.put("leaf", true);
+                    userNameNode.put("id", "userName"); //$NON-NLS-1$ //$NON-NLS-2$
+                    userNameNode.put("text", "UserName:" + cleanOutput(userName)); //$NON-NLS-1$
+                    userNameNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(userNameNode);
 
                     JSONObject sourceNode = new JSONObject();
-                    sourceNode.put("id", "source");
-                    sourceNode.put("text", "Source:" + source);
-                    sourceNode.put("leaf", true);
+                    sourceNode.put("id", "source"); //$NON-NLS-1$//$NON-NLS-2$
+                    sourceNode.put("text", "Source:" + source); //$NON-NLS-1$
+                    sourceNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(sourceNode);
 
                     JSONObject timeInMillisNode = new JSONObject();
-                    timeInMillisNode.put("id", "timeInMillis");
-                    timeInMillisNode.put("text", "TimeInMillis:" + timeInMillis);
-                    timeInMillisNode.put("leaf", true);
+                    timeInMillisNode.put("id", "timeInMillis"); //$NON-NLS-1$ //$NON-NLS-2$
+                    timeInMillisNode.put("text", "TimeInMillis:" + timeInMillis); //$NON-NLS-1$
+                    timeInMillisNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(timeInMillisNode);
 
                     JSONObject operationTypeNode = new JSONObject();
-                    operationTypeNode.put("id", "operationType");
-                    operationTypeNode.put("text", "OperationType:" + operationType);
-                    operationTypeNode.put("leaf", true);
+                    operationTypeNode.put("id", "operationType"); //$NON-NLS-1$ //$NON-NLS-2$
+                    operationTypeNode.put("text", "OperationType:" + operationType); //$NON-NLS-1$ 
+                    operationTypeNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(operationTypeNode);
 
                     JSONObject conceptNode = new JSONObject();
-                    conceptNode.put("id", "concept");
-                    conceptNode.put("text", "Concept:" + concept);
-                    conceptNode.put("leaf", true);
+                    conceptNode.put("id", "concept"); //$NON-NLS-1$//$NON-NLS-2$
+                    conceptNode.put("text", "Concept:" + concept); //$NON-NLS-1$ 
+                    conceptNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(conceptNode);
 
                     JSONObject revisionIDNode = new JSONObject();
-                    revisionIDNode.put("id", "revisionID");
-                    revisionIDNode.put("text", "RevisionID:" + cleanOutput(revisionID));
-                    revisionIDNode.put("leaf", true);
+                    revisionIDNode.put("id", "revisionID"); //$NON-NLS-1$ //$NON-NLS-2$
+                    revisionIDNode.put("text", "RevisionID:" + cleanOutput(revisionID)); //$NON-NLS-1$
+                    revisionIDNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(revisionIDNode);
 
                     JSONObject dataClusterNode = new JSONObject();
-                    dataClusterNode.put("id", "dataCluster");
-                    dataClusterNode.put("text", "DataCluster:" + cleanOutput(dataCluster));
-                    dataClusterNode.put("leaf", true);
+                    dataClusterNode.put("id", "dataCluster"); //$NON-NLS-1$//$NON-NLS-2$
+                    dataClusterNode.put("text", "DataCluster:" + cleanOutput(dataCluster)); //$NON-NLS-1$
+                    dataClusterNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(dataClusterNode);
 
                     JSONObject dataModelNode = new JSONObject();
-                    dataModelNode.put("id", "dataModel");
-                    dataModelNode.put("text", "DataModel:" + cleanOutput(dataModel));
-                    dataModelNode.put("leaf", true);
+                    dataModelNode.put("id", "dataModel"); //$NON-NLS-1$//$NON-NLS-2$
+                    dataModelNode.put("text", "DataModel:" + cleanOutput(dataModel)); //$NON-NLS-1$ 
+                    dataModelNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(dataModelNode);
 
                     JSONObject keyNode = new JSONObject();
-                    keyNode.put("id", "key");
-                    keyNode.put("text", "Key:" + key);
-                    keyNode.put("leaf", true);
+                    keyNode.put("id", "key"); //$NON-NLS-1$ //$NON-NLS-2$
+                    keyNode.put("text", "Key:" + key); //$NON-NLS-2$
+                    keyNode.put("leaf", true); //$NON-NLS-1$
                     rootGroup.add(keyNode);
 
-                    NodeList ls = Util.getNodeList(doc, "/Update/Item");
+                    NodeList ls = Util.getNodeList(doc, "/Update/Item"); //$NON-NLS-1$
 
                     if (ls.getLength() > 0) {
 
                         for (int i = 0; i < ls.getLength(); i++) {
-                            String path = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/path");
-                            String elementPath = path.replaceAll("\\[\\d+\\]$", "");
+                            String path = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/path"); //$NON-NLS-1$//$NON-NLS-2$
+                            String elementPath = path.replaceAll("\\[\\d+\\]$", ""); //$NON-NLS-1$//$NON-NLS-2$
                             FKInstance fkInstance = getRetrieveConf(dataModel, concept, elementPath);
 
-                            String oldValue = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/oldValue");
+                            String oldValue = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/oldValue"); //$NON-NLS-1$//$NON-NLS-2$
 
-                            if (oldValue == null || oldValue.equals("null"))
-                                oldValue = "";
-                            String newValue = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/newValue");
+                            if (oldValue == null || oldValue.equals("null")) //$NON-NLS-1$
+                                oldValue = ""; //$NON-NLS-1$
+                            String newValue = Util.getFirstTextNode(doc, "/Update/Item[" + (i + 1) + "]/newValue"); //$NON-NLS-1$ //$NON-NLS-2$
 
-                            if (newValue == null || newValue.equals("null"))
-                                newValue = "";
+                            if (newValue == null || newValue.equals("null")) //$NON-NLS-1$
+                                newValue = ""; //$NON-NLS-1$
 
-                            if (fkInstance.retireveFKInfo && !"".equals(oldValue) && fkInstance.getFkInfo() != null) {
+                            if (fkInstance.retireveFKInfo && !"".equals(oldValue) && fkInstance.getFkInfo() != null) { //$NON-NLS-1$
                                 oldValue = getFKInfoByRetrieveConf(dataCluster, fkInstance.getFkInfo(), oldValue);
                             }
 
-                            if (fkInstance.retireveFKInfo && !"".equals(newValue) && fkInstance.getFkInfo() != null) {
+                            if (fkInstance.retireveFKInfo && !"".equals(newValue) && fkInstance.getFkInfo() != null) { //$NON-NLS-1$
                                 newValue = getFKInfoByRetrieveConf(dataCluster, fkInstance.getFkInfo(), newValue);
                             }
 
                             JSONArray array = new JSONArray();
                             JSONObject pathNode = new JSONObject();
-                            pathNode.put("id", "item" + i + "-path");
-                            pathNode.put("text", "path:" + path);
-                            pathNode.put("leaf", true);
+                            pathNode.put("id", "item" + i + "-path"); //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+                            pathNode.put("text", "path:" + path); //$NON-NLS-1$//$NON-NLS-2$
+                            pathNode.put("leaf", true); //$NON-NLS-1$
                             array.put(pathNode);
                             JSONObject oldValueNode = new JSONObject();
-                            oldValueNode.put("id", "item" + i + "-oldValue");
-                            oldValueNode.put("text", "oldValue:" + oldValue);
-                            oldValueNode.put("leaf", true);
+                            oldValueNode.put("id", "item" + i + "-oldValue"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                            oldValueNode.put("text", "oldValue:" + oldValue); //$NON-NLS-1$ //$NON-NLS-2$
+                            oldValueNode.put("leaf", true); //$NON-NLS-1$
                             array.put(oldValueNode);
                             JSONObject newValueNode = new JSONObject();
-                            newValueNode.put("id", "item" + i + "-newValue");
-                            newValueNode.put("text", "newValue:" + newValue);
-                            newValueNode.put("leaf", true);
+                            newValueNode.put("id", "item" + i + "-newValue"); //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+                            newValueNode.put("text", "newValue:" + newValue); //$NON-NLS-1$//$NON-NLS-2$
+                            newValueNode.put("leaf", true); //$NON-NLS-1$
                             array.put(newValueNode);
 
                             JSONObject itemNode = new JSONObject();
-                            itemNode.put("id", "item" + i);
-                            itemNode.put("text", "Item");
-                            itemNode.put("leaf", false);
-                            itemNode.put("children", array);
+                            itemNode.put("id", "item" + i); //$NON-NLS-1$ //$NON-NLS-2$
+                            itemNode.put("text", "Item"); //$NON-NLS-1$//$NON-NLS-2$
+                            itemNode.put("leaf", false); //$NON-NLS-1$
+                            itemNode.put("children", array); //$NON-NLS-1$
                             rootGroup.add(itemNode);
 
                         }
                     }
 
-                    json.put("head", rootGroup);
-                    jsonTree = ((JSONArray) json.get("head")).toString();
-                    resp.setCharacterEncoding("utf-8");
+                    json.put("head", rootGroup); //$NON-NLS-1$
+                    jsonTree = ((JSONArray) json.get("head")).toString(); //$NON-NLS-1$
+
+                    resp.setCharacterEncoding("utf-8"); //$NON-NLS-1$
                     PrintWriter out = resp.getWriter();
                     out.println(jsonTree);
                     out.close();
@@ -207,9 +208,9 @@ public class UpdateReportDetailsServelt extends HttpServlet {
             // export to excel
             org.apache.log4j.Logger.getLogger(this.getClass()).info("SERVLET Reporting export for excel ");
 
-            DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            DateFormat df = new SimpleDateFormat("dd-MM-yyyy"); //$NON-NLS-1$
             resp.reset();
-            resp.setContentType("application/vnd.ms-excel");
+            resp.setContentType("application/vnd.ms-excel"); //$NON-NLS-1$
             String theReportFile = "Journal_" + df.format(new Date()) + ".xls";
             resp.setHeader("Content-Disposition", "attachment; filename=\"" + theReportFile + "\"");
 
@@ -233,7 +234,7 @@ public class UpdateReportDetailsServelt extends HttpServlet {
                 cols[1] = new String[] { "Conteneur de données", "Modèle de données", "Entité", "Clé", "ID de révision", "Type d\'opération",
                         "Date d\'opération", "Source", "Utilisateur" };
                 String language = req.getParameter("language") != null ? req.getParameter("language") : "en";
-                if (language.equals("en"))
+                if (language.equals("en")) //$NON-NLS-1$
                     for (int i = 0; i < cols[0].length; i++) {
                         row.createCell((short) i).setCellValue(cols[0][i]);
                         row.getCell((short) i).setCellStyle(cs);
@@ -266,7 +267,8 @@ public class UpdateReportDetailsServelt extends HttpServlet {
             }
             // Write the output
             OutputStream out = resp.getOutputStream();
-            resp.setCharacterEncoding("utf-8");
+
+            resp.setCharacterEncoding("utf-8"); //$NON-NLS-1$
             wb.write(out);
             out.close();
         }
@@ -274,8 +276,8 @@ public class UpdateReportDetailsServelt extends HttpServlet {
 
     private String cleanOutput(String output) {
 
-        if (output == null || output.equals("null"))
-            output = "";
+        if (output == null || output.equals("null")) //$NON-NLS-1$
+            output = ""; //$NON-NLS-1$
 
         output = output.trim();
 
@@ -291,11 +293,11 @@ public class UpdateReportDetailsServelt extends HttpServlet {
      * @return
      */
     private String getFKInfoByRetrieveConf(String dataCluster, String fkInfo, String rightValueOrPath) {
-        String fkInfoValue = "";
-        String conceptName = fkInfo.substring(0, fkInfo.indexOf("/"));
+        String fkInfoValue = ""; //$NON-NLS-1$
+        String conceptName = fkInfo.substring(0, fkInfo.indexOf("/")); //$NON-NLS-1$
         String value = rightValueOrPath;
 
-        if (rightValueOrPath.indexOf("[") == 0 && rightValueOrPath.lastIndexOf("]") == rightValueOrPath.length() - 1) {
+        if (rightValueOrPath.indexOf("[") == 0 && rightValueOrPath.lastIndexOf("]") == rightValueOrPath.length() - 1) { //$NON-NLS-1$ //$NON-NLS-2$
             value = rightValueOrPath.subSequence(1, rightValueOrPath.length() - 1).toString();
         }
 
@@ -305,7 +307,7 @@ public class UpdateReportDetailsServelt extends HttpServlet {
             WSItem wsItem = Util.getPort().getItem(
                     new WSGetItem(new WSItemPK(new WSDataClusterPK(dataCluster), conceptName, ids)));
             Document document = Util.parse(wsItem.getContent());
-            NodeList list = Util.getNodeList(document, "/" + fkInfo);
+            NodeList list = Util.getNodeList(document, "/" + fkInfo); //$NON-NLS-1$
             Node it = list.item(0);
 
             if (it != null) {
@@ -350,19 +352,19 @@ public class UpdateReportDetailsServelt extends HttpServlet {
                     NodeList annotList = el.getChildNodes();
 
                     for (int k = 0; k < annotList.getLength(); k++) {
-                        if ("appinfo".equals(annotList.item(k).getLocalName())) {
-                            Node source1 = annotList.item(k).getAttributes().getNamedItem("source");
+                        if ("appinfo".equals(annotList.item(k).getLocalName())) { //$NON-NLS-1$
+                            Node source1 = annotList.item(k).getAttributes().getNamedItem("source"); //$NON-NLS-1$
 
                             if (source1 == null)
                                 continue;
-                            String appinfoSource = annotList.item(k).getAttributes().getNamedItem("source").getNodeValue();
+                            String appinfoSource = annotList.item(k).getAttributes().getNamedItem("source").getNodeValue(); //$NON-NLS-1$
 
-                            if ("X_ForeignKeyInfo".equals(appinfoSource)) {
+                            if ("X_ForeignKeyInfo".equals(appinfoSource)) { //$NON-NLS-1$
                                 fkInstance.setFkInfo(annotList.item(k).getFirstChild().getNodeValue());
                             }
 
-                            if ("X_Retrieve_FKinfos".equals(appinfoSource)) {
-                                fkInstance.setRetireveFKInfo("true".equals(annotList.item(k).getFirstChild().getNodeValue()));
+                            if ("X_Retrieve_FKinfos".equals(appinfoSource)) { //$NON-NLS-1$
+                                fkInstance.setRetireveFKInfo("true".equals(annotList.item(k).getFirstChild().getNodeValue())); //$NON-NLS-1$
                                 break;
                             }
                         }
