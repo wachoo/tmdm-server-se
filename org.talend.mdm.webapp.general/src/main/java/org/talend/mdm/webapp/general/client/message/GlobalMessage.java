ad4bee800ee7240341649d66874153592762b6b4
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-18 06:16:41 +0000

    feature TMDM-1666 modified:   redevelop the overall frame,  Global message broadcast in between webapp modules
    git-svn-id: http://talendforge.org/svn/tom/trunk@66199 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/message/GlobalMessage.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/message/GlobalMessage.java
@@ -12,30 +12,17 @@
 // ============================================================================
 package org.talend.mdm.webapp.general.client.message;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class GlobalMessage {
-	Map<String, JavaScriptFunction> messages = new HashMap<String, JavaScriptFunction>();
-	
-	private static GlobalMessage instance;
-	private GlobalMessage(){}
-	
-	public static GlobalMessage getInstance(){
-		if (instance == null){
-			instance = new GlobalMessage();
-		}
-		return instance;
-	}
-	
-	public void registerGlobalMessageHandler(String msgId, JavaScriptFunction fn){
-		messages.put(msgId, fn);
-	}
-	
-	public void notifyMessage(String msgId, Object data){
-		JavaScriptFunction fn = messages.get(msgId);
-		if (fn != null){
-			fn.executeFn(msgId, data);
-		}
-	}
+
+    public static native void registerGlobalMessageHandler(String msgId, MessageHandler handler)/*-{
+        $wnd.General.registerGlobalMessage(msgId, function(msgId, data){
+        handler.@org.talend.mdm.webapp.general.client.message.MessageHandler::handlerMessage(Ljava/lang/String;Ljava/lang/Object;)(msgId, data);
+        });
+    }-*/;
+
+    public static native void notifyMessage(String msgId, Object data)/*-{
+        $wnd.General.notifyGlobalMessage(msgId, data);
+    }-*/;
+
 }
--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/message/PublicMessageService.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/message/PublicMessageService.java
@@ -12,29 +12,36 @@
 // ============================================================================
 package org.talend.mdm.webapp.general.client.message;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gwt.core.client.JavaScriptObject;
 
 public class PublicMessageService {
 	
+    static Map<String, JavaScriptFunction> messages = new HashMap<String, JavaScriptFunction>();
+
 	public static native void registerMessageService()/*-{
-		$wnd.General = {
-			registerGlobalMessage : function(msgId, handler){//  msgConfig.id, msgConfig.handler
-				@org.talend.mdm.webapp.general.client.message.PublicMessageService::registerGlobalMessageHandler(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(msgId, handler);
-			},
-			notifyGlobalMessage : function(msgId, data){
-				@org.talend.mdm.webapp.general.client.message.PublicMessageService::notifyMessage(Ljava/lang/String;Ljava/lang/Object;)(msgId, data);
-			}
-		};
-	}-*/;
+        $wnd.General = {
+        	registerGlobalMessage : function(msgId, handler){//  msgConfig.id, msgConfig.handler
+        		@org.talend.mdm.webapp.general.client.message.PublicMessageService::registerGlobalMessageHandler(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)(msgId, handler);
+        	},
+        	notifyGlobalMessage : function(msgId, data){
+        		@org.talend.mdm.webapp.general.client.message.PublicMessageService::notifyMessage(Ljava/lang/String;Ljava/lang/Object;)(msgId, data);
+        	}
+        };
+    }-*/;
 	
 	static void registerGlobalMessageHandler(String msgId, JavaScriptObject handler){
-		
 		JavaScriptFunction fsFn = new JavaScriptFunction(handler);
-		GlobalMessage.getInstance().registerGlobalMessageHandler(msgId, fsFn);
+        messages.put(msgId, fsFn);
 	}
 	
 	static void notifyMessage(String msgId, Object data){
-		GlobalMessage.getInstance().notifyMessage(msgId, data);
+        JavaScriptFunction fn = messages.get(msgId);
+        if (fn != null) {
+            fn.executeFn(msgId, data);
+        }
 	}
 	
 }
