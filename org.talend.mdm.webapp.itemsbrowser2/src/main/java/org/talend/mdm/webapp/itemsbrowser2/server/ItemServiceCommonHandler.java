f9922d994c8bc6c61809ad4f05e50e85948a86f7
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-24 05:47:20 +0000

    add feature TMDM-2327: Make typical validation errors more understandable for business users
    git-svn-id: http://talendforge.org/svn/tom/trunk@66523 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/server/ItemServiceCommonHandler.java
+++ org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/server/ItemServiceCommonHandler.java
@@ -419,7 +419,7 @@ public class ItemServiceCommonHandler extends ItemsServiceImpl {
                 }
                 // add feature TMDM-2327 SAXException:cvc-complex-type.2.4.b message transform
                 if (e.getLocalizedMessage().indexOf("cvc-complex-type.2.4.b") != -1) { //$NON-NLS-1$
-                    err = "before saving the '" + item.getConcept() + "' item,please fill the required field's contents";//$NON-NLS-1$ //$NON-NLS-2$
+                    err = "Unable to save item,before saving the '" + item.getConcept() + "' item,please fill the required field's contents";//$NON-NLS-1$ //$NON-NLS-2$
                 }
                 result = new ItemResult(ItemResult.FAILURE, err);
             }
