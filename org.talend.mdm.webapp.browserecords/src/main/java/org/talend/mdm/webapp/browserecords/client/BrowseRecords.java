13c5378504d2f41d7261b6dc9a36cbcfc94718f7
lzhang <lzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-30 04:02:31 +0000

    feature TMDM-2396 added: move current search panel into browserecords project
    git-svn-id: http://talendforge.org/svn/tom/trunk@66866 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/BrowseRecords.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/BrowseRecords.java
@@ -12,11 +12,18 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecords.client;
 
-import org.talend.mdm.webapp.browserecords.client.layout.columns.ColumnLayoutPanel;
+import org.talend.mdm.webapp.browserecords.client.i18n.MessagesFactory;
+import org.talend.mdm.webapp.browserecords.client.mvc.BrowseRecordsController;
+import org.talend.mdm.webapp.browserecords.client.util.UserSession;
+import org.talend.mdm.webapp.browserecords.shared.AppHeader;
 
+import com.allen_sauer.gwt.log.client.Log;
+import com.extjs.gxt.ui.client.Registry;
+import com.extjs.gxt.ui.client.mvc.Dispatcher;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -26,12 +33,57 @@ public class BrowseRecords implements EntryPoint {
     /**
      * Create a remote service proxy to talk to the server-side BrowseRecords service.
      */
-    public static final String BROWSERECORDS_SERVICE = "BrowseRecordsService"; //$NON-NLS-1$   
+    public static final String BROWSERECORDS_SERVICE = "BrowseRecordsService"; //$NON-NLS-1$
 
+    public static final String USER_SESSION = "UserSession"; //$NON-NLS-1$
+
+
+    /**
+     * This is the entry point method.
+     */
     public void onModuleLoad() {
-        Window.alert("hello good");
-        ColumnLayoutPanel clp = new ColumnLayoutPanel(3);
-        clp.setSize("700", "600");
-        RootPanel.get().add(clp);
+        // log setting
+        Log.setUncaughtExceptionHandler();
+
+        Registry.register(BROWSERECORDS_SERVICE, GWT.create(BrowseRecordsService.class));
+
+        // register user session
+        Registry.register(USER_SESSION, new UserSession());
+
+        // add controller to dispatcher
+        final Dispatcher dispatcher = Dispatcher.get();
+        dispatcher.addController(new BrowseRecordsController());
+
+        // init app-header
+        getItemService().getAppHeader(new AsyncCallback<AppHeader>() {
+
+            public void onFailure(Throwable caught) {
+                Dispatcher.forwardEvent(BrowseRecordsEvents.Error, caught);
+            }
+
+            public void onSuccess(AppHeader header) {
+                if (header.getDatacluster() == null || header.getDatamodel() == null) {
+                    Window.alert(MessagesFactory.getMessages().data_model_not_specified());
+                    return;
+                }
+                getSession().put(UserSession.APP_HEADER, header);
+
+                dispatcher.dispatch(BrowseRecordsEvents.InitFrame);
+            }
+
+        });
+    }
+
+    private static BrowseRecordsServiceAsync getItemService() {
+
+        BrowseRecordsServiceAsync service = (BrowseRecordsServiceAsync) Registry.get(BrowseRecords.BROWSERECORDS_SERVICE);
+        return service;
+
+    }
+
+    public static UserSession getSession() {
+
+        return Registry.get(BrowseRecords.USER_SESSION);
+
     }
 }
