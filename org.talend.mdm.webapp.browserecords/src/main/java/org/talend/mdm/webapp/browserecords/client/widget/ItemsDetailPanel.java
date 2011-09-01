f7ce1195a82d574a8c1fcdb2599e303d631dedfa
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-01 06:53:08 +0000

    git-svn-id: http://talendforge.org/svn/tom/trunk@67071 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemsDetailPanel.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemsDetailPanel.java
@@ -12,11 +12,57 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecords.client.widget;
 
+import com.extjs.gxt.ui.client.GXT;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 
 /**
  * DOC Administrator  class global comment. Detailled comment
  */
 public class ItemsDetailPanel extends ContentPanel {
+    
+    ContentPanel productPanel; 
+    ContentPanel storesPanel; 
+    //private final ItemDetailToolBar itemDetailToolBar = new ItemDetailToolBar();
+    
+    public ItemsDetailPanel() {
+        super();
+        this.setBodyBorder(false);
+        this.setHeaderVisible(false);
+        this.setLayout(new FitLayout());
+        this.setWidth(800);
+        this.setHeight(500);
+        this.initPanel();
+    }    
+    
+    private void initPanel(){    
+        TabPanel tabPanel = new TabPanel();  
+        tabPanel.setWidth(450);  
+        tabPanel.setAutoHeight(true);  
+        
+        productPanel = new ContentPanel(); 
+        productPanel.setHeaderVisible(false);     
+        storesPanel = new ContentPanel(); 
+        storesPanel.setHeaderVisible(false);
+        productPanel.setTopComponent(new ItemDetailToolBar());
+        storesPanel.setTopComponent(new ItemDetailToolBar());
+      
+        TabItem productTab = new TabItem("Product");  
+        productTab.addStyleName("pad-text");        
+        productTab.add(productPanel);
+        tabPanel.add(productTab);  
+      
+        TabItem storesTab = new TabItem("StoresTab");  
+        storesTab.addStyleName("pad-text");  
+        storesTab.add(storesPanel); 
+        tabPanel.add(storesTab);        
+        add(tabPanel);   
+    }
 
 }
