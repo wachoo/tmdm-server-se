df96fc738fc02fac0f98db6bc35843f50818879b
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-02 05:45:26 +0000

    bug TMDM-1664 add: support trigger action to dataContainerBox and dataModelBox of ActionsPanel to make sure all items are showed up when combox is triggered
    git-svn-id: http://talendforge.org/svn/tom/trunk@67161 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/layout/ActionsPanel.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/client/layout/ActionsPanel.java
@@ -13,66 +13,72 @@ import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.ComboBox;
+import com.extjs.gxt.ui.client.widget.form.ComboBox.TriggerAction;
 import com.extjs.gxt.ui.client.widget.form.FormPanel.LabelAlign;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.FormLayout;
 
-
 public class ActionsPanel extends ContentPanel {
 
     private static ActionsPanel instance;
-    
+
     private ListStore<ComboBoxModel> containerStore = new ListStore<ComboBoxModel>();
+
     private ComboBox<ComboBoxModel> dataContainerBox = new ComboBox<ComboBoxModel>();
-    
+
     private ListStore<ComboBoxModel> dataStore = new ListStore<ComboBoxModel>();
+
     private ComboBox<ComboBoxModel> dataModelBox = new ComboBox<ComboBoxModel>();
-    
+
     private Button saveBtn = new Button(MessageFactory.getMessages().save());
-    
-    private ActionsPanel(){
+
+    private ActionsPanel() {
         super();
         this.setHeading(MessageFactory.getMessages().actions());
         FormLayout formLayout = new FormLayout();
         formLayout.setLabelAlign(LabelAlign.TOP);
         this.setLayout(formLayout);
-        
+
         dataContainerBox.setFieldLabel(MessageFactory.getMessages().data_container());
         dataContainerBox.setDisplayField("value"); //$NON-NLS-1$
         dataContainerBox.setValueField("value"); //$NON-NLS-1$
         dataContainerBox.setWidth(100);
         dataContainerBox.setStore(containerStore);
+        dataContainerBox.setTypeAhead(true);
+        dataContainerBox.setTriggerAction(TriggerAction.ALL);
         dataModelBox.setFieldLabel(MessageFactory.getMessages().data_model());
         dataModelBox.setDisplayField("value"); //$NON-NLS-1$
         dataModelBox.setValueField("value"); //$NON-NLS-1$
         dataModelBox.setWidth(100);
         dataModelBox.setStore(dataStore);
+        dataModelBox.setTypeAhead(true);
+        dataModelBox.setTriggerAction(TriggerAction.ALL);
         FormData formData = new FormData();
         formData.setMargins(new Margins(5));
         this.add(dataContainerBox, formData);
         this.add(dataModelBox, formData);
         this.add(saveBtn, formData);
-        
+
         initEvent();
     }
-    
-    public static ActionsPanel getInstance(){
-        if (instance == null){
+
+    public static ActionsPanel getInstance() {
+        if (instance == null) {
             instance = new ActionsPanel();
         }
         return instance;
     }
-    
-    private void initEvent(){
+
+    private void initEvent() {
         saveBtn.addSelectionListener(new SelectionListener<ButtonEvent>() {
+
             public void componentSelected(ButtonEvent ce) {
                 Dispatcher dispatcher = Dispatcher.get();
                 dispatcher.dispatch(GeneralEvent.SwitchClusterAndModel);
             }
         });
     }
-    
-    
+
     public void loadAction(ActionBean action) {
         containerStore.removeAll();
         dataStore.removeAll();
@@ -91,11 +97,11 @@ public class ActionsPanel extends ContentPanel {
 
     }
 
-    public String getDataCluster(){
+    public String getDataCluster() {
         return dataContainerBox.getValue().getValue();
     }
-    
-    public String getDataModel(){
+
+    public String getDataModel() {
         return dataModelBox.getValue().getValue();
     }
 }
