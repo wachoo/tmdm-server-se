9cbfde57becca9a1a7d7b4c3174cebd8b3b8e4b5
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-05 05:53:19 +0000

    git-svn-id: http://talendforge.org/svn/tom/trunk@67233 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemPanel.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemPanel.java
@@ -1,11 +1,20 @@
 package org.talend.mdm.webapp.browserecords.client.widget;
 
+import org.talend.mdm.webapp.browserecords.client.BrowseRecords;
+import org.talend.mdm.webapp.browserecords.client.BrowseRecordsServiceAsync;
 import org.talend.mdm.webapp.browserecords.client.model.ItemBean;
+import org.talend.mdm.webapp.browserecords.client.util.UserSession;
+import org.talend.mdm.webapp.browserecords.client.widget.treedetail.ForeignKeyTreeDetail;
+import org.talend.mdm.webapp.browserecords.shared.ViewBean;
 
+import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 
 
+
 public class ItemPanel extends ContentPanel {
+    
+    private final ForeignKeyTreeDetail tree = new ForeignKeyTreeDetail();
     
     private ItemDetailToolBar toolBar;
     private ItemBean item;
@@ -16,15 +25,31 @@ public class ItemPanel extends ContentPanel {
     }
     
     public ItemPanel(ItemBean item, String type){
+        
         this.item = item;
         this.toolBar = new ItemDetailToolBar(item);
-        this.type = type;
+        this.type = type;        
         this.initUI();
     }
     
+    @SuppressWarnings("static-access")
     private void initUI(){
         this.setBodyBorder(false);
         this.setHeaderVisible(false);
-        this.setTopComponent(toolBar);
+        this.setTopComponent(toolBar);     
+        tree.setViewBean((ViewBean)this.getSession().get(UserSession.CURRENT_VIEW));
+        this.add(tree);
+    }
+    
+    private static BrowseRecordsServiceAsync getItemService() {
+
+        BrowseRecordsServiceAsync service = (BrowseRecordsServiceAsync) Registry.get(BrowseRecords.BROWSERECORDS_SERVICE);
+        return service;
+
+    }
+
+    public static UserSession getSession() {
+        return Registry.get(BrowseRecords.USER_SESSION);
+
     }
 }
