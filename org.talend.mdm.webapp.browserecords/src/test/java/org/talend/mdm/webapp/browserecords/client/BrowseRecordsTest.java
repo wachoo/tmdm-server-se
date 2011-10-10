cea59c5ddde6c5ba1424e787373911ca602e70e5
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-10 01:32:01 +0000

    TMDM-2516 : change the copyright and make it comply with the current template
    git-svn-id: http://talendforge.org/svn/tom/trunk@69568 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/client/BrowseRecordsTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/client/BrowseRecordsTest.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+//
 package org.talend.mdm.webapp.browserecords.client;
 
 import com.google.gwt.junit.client.GWTTestCase;
@@ -16,7 +28,7 @@ public class BrowseRecordsTest extends GWTTestCase {
     @Override
     public String getModuleName() {
         // GWTTestCase Required
-        return "org.talend.mdm.webapp.browserecords.BrowseRecords";
+        return "org.talend.mdm.webapp.browserecords.BrowseRecords"; //$NON-NLS-1$
     }
 
 }
