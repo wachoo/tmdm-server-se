7eacacd02665f583ade46c734da11520d9bfb800
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-11-02 01:25:38 +0000

    feature TMDM-2732 modified:  fix bug status bar in search results
    git-svn-id: http://talendforge.org/svn/tom/trunk@71321 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/widget/PagingToolBarEx.java
+++ org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/widget/PagingToolBarEx.java
@@ -16,12 +16,24 @@ import org.talend.mdm.webapp.base.client.i18n.BaseMessagesFactory;
 
 import com.extjs.gxt.ui.client.core.El;
 import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.FieldEvent;
 import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.MenuEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.Component;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.button.ButtonGroup;
+import com.extjs.gxt.ui.client.widget.button.SplitButton;
 import com.extjs.gxt.ui.client.widget.form.Field;
 import com.extjs.gxt.ui.client.widget.form.NumberField;
 import com.extjs.gxt.ui.client.widget.form.Validator;
+import com.extjs.gxt.ui.client.widget.layout.ToolBarLayout;
+import com.extjs.gxt.ui.client.widget.menu.HeaderMenuItem;
+import com.extjs.gxt.ui.client.widget.menu.Menu;
+import com.extjs.gxt.ui.client.widget.menu.MenuItem;
+import com.extjs.gxt.ui.client.widget.menu.SeparatorMenuItem;
 import com.extjs.gxt.ui.client.widget.toolbar.LabelToolItem;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
 import com.extjs.gxt.ui.client.widget.toolbar.SeparatorToolItem;
@@ -34,6 +46,7 @@ public class PagingToolBarEx extends PagingToolBar {
 
     public PagingToolBarEx(int pageSize) {
         super(pageSize);
+        setLayout(new PagingToolBarExLayout());
         LabelToolItem sizeLabel = new LabelToolItem(BaseMessagesFactory.getMessages().page_size_label());
 
         final NumberField sizeField = new NumberField() {
@@ -70,6 +83,11 @@ public class PagingToolBarEx extends PagingToolBar {
         this.insert(sizeField, this.getItemCount() - 2);
     }
 
+    protected void onResize(int width, int height) {
+        super.onResize(width, height);
+        this.layout(true);
+    }
+
     private native void blur(Element el)/*-{
                                         el.blur();
                                         }-*/;
@@ -93,4 +111,89 @@ public class PagingToolBarEx extends PagingToolBar {
             return null;
         }
     };
+
+    class PagingToolBarExLayout extends ToolBarLayout {
+
+        protected void initMore() {
+            super.initMore();
+            moreMenu.setWidth(250);
+        }
+
+        protected void addComponentToMenu(Menu menu, Component c) {
+            if (c instanceof SeparatorToolItem) {
+                menu.add(new SeparatorMenuItem());
+            } else if (c instanceof SplitButton) {
+                final SplitButton sb = (SplitButton) c;
+                MenuItem item = new MenuItem(sb.getText(), sb.getIcon());
+                item.setEnabled(c.isEnabled());
+                item.setItemId(c.getItemId());
+                if (sb.getMenu() != null) {
+                    item.setSubMenu(sb.getMenu());
+                }
+                item.addSelectionListener(new SelectionListener<MenuEvent>() {
+
+                    @Override
+                    public void componentSelected(MenuEvent ce) {
+                        ButtonEvent e = new ButtonEvent(sb);
+                        e.setEvent(ce.getEvent());
+                        sb.fireEvent(Events.Select, e);
+                    }
+
+                });
+                menu.add(item);
+            } else if (c instanceof LabelToolItem) {
+                LabelToolItem l = (LabelToolItem) c;
+                MenuItem item = new MenuItem(l.getLabel());
+                menu.add(item);
+            } else if (c instanceof Button) {
+                final Button b = (Button) c;
+                String menuText = b.getText();
+                if (menuText == null || menuText.trim().length() == 0) {
+                    menuText = b.getToolTip().getToolTipConfig() == null ? "" : b.getToolTip().getToolTipConfig().getText(); //$NON-NLS-1$
+                }
+                MenuItem item = new MenuItem(menuText, b.getIcon());
+                item.setToolTip(b.getToolTip().getToolTipConfig());
+                item.setItemId(c.getItemId());
+                if (b.getMenu() != null) {
+                    item.setHideOnClick(false);
+                    item.setSubMenu(b.getMenu());
+                }
+                item.setEnabled(c.isEnabled());
+                item.addSelectionListener(new SelectionListener<MenuEvent>() {
+
+                    @Override
+                    public void componentSelected(MenuEvent ce) {
+                        ButtonEvent e = new ButtonEvent(b);
+                        e.setEvent(ce.getEvent());
+                        b.fireEvent(Events.Select, e);
+                    }
+
+                });
+                menu.add(item);
+            } else if (c instanceof ButtonGroup) {
+                ButtonGroup g = (ButtonGroup) c;
+                g.setItemId(c.getItemId());
+                menu.add(new SeparatorMenuItem());
+                String heading = g.getHeading();
+                if (heading != null && heading.length() > 0 && !heading.equals("&#160;")) {
+                    menu.add(new HeaderMenuItem(g.getHeading()));
+                }
+                for (Component c2 : g.getItems()) {
+                    addComponentToMenu(menu, c2);
+                }
+                menu.add(new SeparatorMenuItem());
+            }
+
+              if (menu.getItemCount() > 0) {
+                if (menu.getItem(0) instanceof SeparatorMenuItem) {
+                    menu.remove(menu.getItem(0));
+                }
+                if (menu.getItemCount() > 0) {
+                    if (menu.getItem(menu.getItemCount() - 1) instanceof SeparatorMenuItem) {
+                        menu.remove(menu.getItem(menu.getItemCount() - 1));
+                    }
+                }
+            }
+        }
+    }
 }
