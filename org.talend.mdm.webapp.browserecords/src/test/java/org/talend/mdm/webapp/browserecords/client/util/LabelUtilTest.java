1b14501ab8dbfd7edb1ed04d0d9849a8240fcab8
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-11-24 10:54:11 +0000

    add the missing commit for TMDM-2905
    git-svn-id: http://talendforge.org/svn/tom/trunk@73071 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/client/util/LabelUtilTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/client/util/LabelUtilTest.java
@@ -2,12 +2,10 @@ package org.talend.mdm.webapp.browserecords.client.util;
 
 import junit.framework.TestCase;
 
-import org.talend.mdm.webapp.browserecords.client.util.LabelUtil;
-
 public class LabelUtilTest extends TestCase {
 
     public void testConvertSpecialHTMLCharacter(){
-        assertEquals(LabelUtil.convertSpecialHTMLCharacter("'"), "&acute;"); //$NON-NLS-1$ //$NON-NLS-2$
+        assertEquals(LabelUtil.convertSpecialHTMLCharacter("'"), "&apos;"); //$NON-NLS-1$ //$NON-NLS-2$
         assertEquals(LabelUtil.convertSpecialHTMLCharacter("\""), "&quot;"); //$NON-NLS-1$ //$NON-NLS-2$
     }
     
