52ecf173910413dcc04a732797aeb8bdb62b25ea
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-09 08:52:58 +0000

    add for TMDM-3093: add copyright header for newly created files
    git-svn-id: http://talendforge.org/svn/tom/trunk@74208 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ForeignKey/FKPropertyEditor.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ForeignKey/FKPropertyEditor.java
@@ -1,8 +1,22 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
 package org.talend.mdm.webapp.browserecords.client.widget.ForeignKey;
 
 import java.util.Collection;
 
 import org.talend.mdm.webapp.base.client.model.ForeignKeyBean;
+
 import com.extjs.gxt.ui.client.widget.form.PropertyEditor;
 
 public class FKPropertyEditor implements PropertyEditor<ForeignKeyBean> {
