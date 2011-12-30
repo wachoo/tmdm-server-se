422a54034554987b9916a6c330d25edeba7d3fa8
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-30 07:01:19 +0000

    merge r75679 from trunk to branch 5.0 fix bug TMDM-3163: format code
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@75681 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/CommonUtilTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/CommonUtilTest.java
@@ -27,11 +27,12 @@ import org.talend.mdm.webapp.browserecords.client.util.CommonUtil;
 
 import com.extjs.gxt.ui.client.data.ModelData;
 
+@SuppressWarnings("nls")
 public class CommonUtilTest extends TestCase {
 
     public void testGetDefaultTreeModel() {
         try {
-            CommonUtil.getDefaultTreeModel(new SimpleTypeModel(), "en"); //$NON-NLS-1$
+            CommonUtil.getDefaultTreeModel(new SimpleTypeModel(), "en");
             fail();
         } catch (NullPointerException e) {
 
@@ -39,40 +40,39 @@ public class CommonUtilTest extends TestCase {
     }
 
     public void transferXpath() {
-        String xp = "a/b/c/d"; //$NON-NLS-1$
+        String xp = "a/b/c/d";
         Map<String, String> map = new HashMap<String, String>();
-        map.put("a/b", "hello"); //$NON-NLS-1$//$NON-NLS-2$
-        map.put("a/b/c", "peili"); //$NON-NLS-1$//$NON-NLS-2$
-        map.put("a/b/c/d", "liang"); //$NON-NLS-1$ //$NON-NLS-2$
+        map.put("a/b", "hello");
+        map.put("a/b/c", "peili");
+        map.put("a/b/c/d", "liang");
         StringBuffer sb = new StringBuffer();
         // a/b/c/d hello peili liang
         Stack<String> stack = new Stack<String>();
         do {
             String v = map.get(xp);
             stack.push(v);
-            xp = xp.substring(0, xp.lastIndexOf("/")); //$NON-NLS-1$
-        } while (xp.indexOf("/") != -1); //$NON-NLS-1$
+            xp = xp.substring(0, xp.lastIndexOf("/"));
+        } while (xp.indexOf("/") != -1);
         boolean flag = true;
 
         while (!stack.isEmpty()) {
             if (flag)
                 flag = false;
             else
-                sb.append("/"); //$NON-NLS-1$
+                sb.append("/");
             sb.append(stack.pop());
         }
-        Assert.assertEquals("hello/peili/liang", sb.toString()); //$NON-NLS-1$
+        Assert.assertEquals("hello/peili/liang", sb.toString());
     }
-
 
     public void testGetRealPath() throws Exception {
         ItemNodeModel nodeModel = TestData.getModel();
         // 1. getPathWithIndex
-        List<String> xpathes = TestData.getXpathes("xpathes.properties"); //$NON-NLS-1$
+        List<String> xpathes = TestData.getXpathes("xpathes.properties");
         Iterator<String> iter = xpathes.iterator();
         assertPathWithIndex(nodeModel, iter);
         // 2. getRealTypePath
-        xpathes = TestData.getXpathes("realTypePathes.properties"); //$NON-NLS-1$
+        xpathes = TestData.getXpathes("realTypePathes.properties");
         iter = xpathes.iterator();
         assertRealTypePath(nodeModel, iter);
     }
@@ -89,7 +89,7 @@ public class CommonUtilTest extends TestCase {
             }
         }
     }
-    
+
     private void assertRealTypePath(ItemNodeModel nodeModel, Iterator<String> iter) {
         String xpath = iter.next();
         String nodeRealTypePath = CommonUtil.getRealTypePath(nodeModel);
@@ -102,11 +102,11 @@ public class CommonUtilTest extends TestCase {
             }
         }
     }
-    
+
     public void test_polymorphismTypeXpathRegex() {
-        String xpath = "Person:Student/Name"; //$NON-NLS-1$
-        xpath = xpath.replaceAll(":\\w+", ""); //$NON-NLS-1$//$NON-NLS-2$
-        assertEquals("Person/Name", xpath); //$NON-NLS-1$
+        String xpath = "Person:Student/Name";
+        xpath = xpath.replaceAll(":\\w+", "");
+        assertEquals("Person/Name", xpath);
     }
 
 }
