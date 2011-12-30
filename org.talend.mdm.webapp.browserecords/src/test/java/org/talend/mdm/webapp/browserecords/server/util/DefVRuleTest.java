d8106ad100a00f325e3bffef61288e35bd2d8f17
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-30 06:53:51 +0000

    fix bug TMDM-3163: format code
    git-svn-id: http://talendforge.org/svn/tom/trunk@75679 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/DefVRuleTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/DefVRuleTest.java
@@ -17,36 +17,34 @@ import junit.framework.TestCase;
 import org.dom4j.Document;
 import org.talend.mdm.webapp.base.shared.TypeModel;
 import org.talend.mdm.webapp.browserecords.server.defaultrule.DefVRule;
-import org.talend.mdm.webapp.browserecords.server.util.action.BrowseRecordsActionTest;
 import org.talend.mdm.webapp.browserecords.shared.EntityModel;
 
 import com.amalto.webapp.core.util.XmlUtil;
 
+@SuppressWarnings("nls")
 public class DefVRuleTest extends TestCase {
 
     /**
-     * set Agency/Name's defaultValue = liang, <li>when you run the defVRule.setDefaultValue(), saxon-8.7.jar and
-     * jaxen-1.1.1.jar can not be found in the test environment, so if you want to test it, please link the two jar
-     * package. DOC Administrator Comment method "test_setDefaultValue".
+     * set Agency/Name's defaultValue = liang, <li>when you run the defVRule.setDefaultValue(), the following codes:<li>
+     * DefVRule defVRule = new DefVRule(entityModel.getMetaDataTypes()); <li>String expression = "\"liang\""; <li>String
+     * xpath = "Agency/Name"; <li>String concept = "Agency"; <li>defVRule.setDefaultValue(xpath, concept, d,
+     * expression); <li>
+     * assertEquals("liang", d.selectSingleNode(xpath).getText()); <li>saxon-8.7.jar and jaxen-1.1.1.jar can not be
+     * found in the test environment, so if you want to test it, please link the two jar package. DOC Administrator
+     * Comment method "test_setDefaultValue".
      * 
      * @throws Exception
      */
     public void test_setDefaultValue() throws Exception {
-        EntityModel entityModel = BrowseRecordsActionTest.getEntityModel();
+        EntityModel entityModel = TestData.getEntityModel();
         Document d = getDocument(entityModel);
         assertNotNull(d.asXML());
-        // DefVRule defVRule = new DefVRule(entityModel.getMetaDataTypes());
-        // String expression = "\"liang\""; //$NON-NLS-1$
-        // String xpath = "Agency/Name"; //$NON-NLS-1$
-        // String concept = "Agency"; //$NON-NLS-1$
-        // defVRule.setDefaultValue(xpath, concept, d, expression);
-        // assertEquals("liang", d.selectSingleNode(xpath).getText()); //$NON-NLS-1$
     }
 
     private Document getDocument(EntityModel entityModel) throws Exception {
         DefVRule defVRule = new DefVRule(entityModel.getMetaDataTypes());
-        String language = "en"; //$NON-NLS-1$
-        TypeModel typeModel = entityModel.getMetaDataTypes().get("Agency"); //$NON-NLS-1$
+        String language = "en";
+        TypeModel typeModel = entityModel.getMetaDataTypes().get("Agency");
         org.w3c.dom.Document doc = defVRule.getSubXML(typeModel, null, language);
         return XmlUtil.parseDocument(doc);
     }
--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/action/BrowseRecordsActionTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/action/BrowseRecordsActionTest.java
@@ -12,68 +12,62 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecords.server.util.action;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import junit.framework.TestCase;
 
-import org.talend.mdm.webapp.base.client.model.DataTypeConstants;
-import org.talend.mdm.webapp.base.client.model.DataTypeCustomized;
-import org.talend.mdm.webapp.base.shared.SimpleTypeModel;
-import org.talend.mdm.webapp.base.shared.TypeModel;
 import org.talend.mdm.webapp.browserecords.client.model.ItemBean;
 import org.talend.mdm.webapp.browserecords.client.model.ItemNodeModel;
 import org.talend.mdm.webapp.browserecords.server.actions.BrowseRecordsAction;
-import org.talend.mdm.webapp.browserecords.shared.ComplexTypeModel;
-import org.talend.mdm.webapp.browserecords.shared.EntityModel;
+import org.talend.mdm.webapp.browserecords.server.util.TestData;
 
 import com.extjs.gxt.ui.client.data.ModelData;
 
+@SuppressWarnings("nls")
 public class BrowseRecordsActionTest extends TestCase {
 
     private BrowseRecordsAction action = new BrowseRecordsAction();
 
-    private String xml = "<Agency><Name>Newark</Name><Name>Newark1</Name><City>Newark</City><State>NJ</State><Zip>07107</Zip><Region>EAST</Region><MoreInfo>Map@@http://maps.google.com/maps?q=40.760667,-74.1879&amp;ll=40.760667,-74.1879&amp;z=9</MoreInfo><Id>NJ01</Id></Agency>"; //$NON-NLS-1$
+    private String xml = "<Agency><Name>Newark</Name><Name>Newark1</Name><City>Newark</City><State>NJ</State><Zip>07107</Zip><Region>EAST</Region><MoreInfo>Map@@http://maps.google.com/maps?q=40.760667,-74.1879&amp;ll=40.760667,-74.1879&amp;z=9</MoreInfo><Id>NJ01</Id></Agency>";
 
     public void testMultiOccurenceNode() throws Exception {
-        String language = "en"; //$NON-NLS-1$
-        ItemNodeModel model = action.getItemNodeModel(getItemBean(), getEntityModel(), language);
+        String language = "en";
+        ItemNodeModel model = action.getItemNodeModel(getItemBean(), TestData.getEntityModel(), language);
         List<ModelData> child = model.getChildren();
 
         for (int i = 0; i < child.size(); i++) {
             String value = ((ItemNodeModel) child.get(i)).getObjectValue().toString();
             switch (i) {
             case 0: {
-                assertEquals("NJ01", value); //$NON-NLS-1$
+                assertEquals("NJ01", value);
                 break;
             }
             case 1: {
-                assertEquals("Newark", value); //$NON-NLS-1$
+                assertEquals("Newark", value);
                 break;
             }
             case 2: {
-                assertEquals("Newark1", value); //$NON-NLS-1$
+                assertEquals("Newark1", value);
                 break;
             }
             case 3: {
-                assertEquals("Newark", value); //$NON-NLS-1$
+                assertEquals("Newark", value);
                 break;
             }
             case 4: {
-                assertEquals("NJ", value); //$NON-NLS-1$
+                assertEquals("NJ", value);
                 break;
             }
             case 5: {
-                assertEquals("07107", value); //$NON-NLS-1$
+                assertEquals("07107", value);
                 break;
             }
             case 6: {
-                assertEquals("EAST", value); //$NON-NLS-1$
+                assertEquals("EAST", value);
                 break;
             }
             case 7: {
-                assertEquals("Map@@http://maps.google.com/maps?q=40.760667,-74.1879&ll=40.760667,-74.1879&z=9", value); //$NON-NLS-1$
+                assertEquals("Map@@http://maps.google.com/maps?q=40.760667,-74.1879&ll=40.760667,-74.1879&z=9", value);
                 break;
             }
             default: {
@@ -84,83 +78,7 @@ public class BrowseRecordsActionTest extends TestCase {
     }
 
     private ItemBean getItemBean() {
-        ItemBean item = new ItemBean("Agency", "NJ01", xml); //$NON-NLS-1$//$NON-NLS-2$
+        ItemBean item = new ItemBean("Agency", "NJ01", xml);
         return item;
-    }
-
-    public static EntityModel getEntityModel() {
-        Map<String, TypeModel> metadata = new LinkedHashMap<String, TypeModel>();
-        DataTypeCustomized agency = new DataTypeCustomized("AgencyType", "anyType"); //$NON-NLS-1$//$NON-NLS-2$
-        ComplexTypeModel complexModel = new ComplexTypeModel("Agency", agency); //$NON-NLS-1$
-        complexModel.getLabelMap().put("en", "D* Agence"); //$NON-NLS-1$ //$NON-NLS-2$
-        complexModel.getLabelMap().put("fr", "Agency"); //$NON-NLS-1$//$NON-NLS-2$
-        complexModel.setXpath("Agency"); //$NON-NLS-1$
-        metadata.put("Agency", complexModel); //$NON-NLS-1$
-
-        SimpleTypeModel typeModel = new SimpleTypeModel("Id", DataTypeConstants.STRING); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-        typeModel.getLabelMap().put("fr", "Identifiant"); //$NON-NLS-1$ //$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "Identifier"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.setXpath("Agency/Id"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/Id"); //$NON-NLS-1$
-        metadata.put("Agency/Id", typeModel); //$NON-NLS-1$
-
-        typeModel = new SimpleTypeModel("Name", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "Nom"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "Name"); //$NON-NLS-1$ //$NON-NLS-2$
-        typeModel.setMaxOccurs(-1);
-        typeModel.setMinOccurs(1);
-        metadata.put("Agency/Name", typeModel); //$NON-NLS-1$
-        typeModel.setXpath("Agency/Name"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/Name"); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        typeModel = new SimpleTypeModel("City", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "City"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "City"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.setXpath("Agency/City"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/City"); //$NON-NLS-1$
-        metadata.put("Agency/City", typeModel); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        typeModel = new SimpleTypeModel("State", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "State"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "State"); //$NON-NLS-1$ //$NON-NLS-2$
-        typeModel.setXpath("Agency/State"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/State"); //$NON-NLS-1$
-        metadata.put("Agency/State", typeModel); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        typeModel = new SimpleTypeModel("Zip", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "Zip"); //$NON-NLS-1$ //$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "Zip"); //$NON-NLS-1$ //$NON-NLS-2$
-        typeModel.setXpath("Agency/Zip"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/Zip"); //$NON-NLS-1$
-        metadata.put("Agency/Zip", typeModel); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        typeModel = new SimpleTypeModel("Region", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "Region"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "Region"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.setXpath("Agency/Region"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/Region"); //$NON-NLS-1$
-        metadata.put("Agency/Region", typeModel); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        typeModel = new SimpleTypeModel("MoreInfo", DataTypeConstants.STRING); //$NON-NLS-1$
-        typeModel.getLabelMap().put("fr", "MoreInfo"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.getLabelMap().put("en", "MoreInfo"); //$NON-NLS-1$//$NON-NLS-2$
-        typeModel.setXpath("Agency/MoreInfo"); //$NON-NLS-1$
-        typeModel.setTypePath("Agency/MoreInfo"); //$NON-NLS-1$
-        metadata.put("Agency/MoreInfo", typeModel); //$NON-NLS-1$
-        complexModel.addSubType(typeModel);
-
-        EntityModel entity = new EntityModel();
-        entity.setMetaDataTypes(metadata);
-        entity.setConceptName("Agency"); //$NON-NLS-1$
-        String[] keys = { "Agency/Id" }; //$NON-NLS-1$
-        entity.setKeys(keys);
-
-        return entity;
     }
 }
