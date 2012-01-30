ce35ba895c7fbdda823e2372d147811ca907ee7d
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-01-30 07:12:09 +0000

    merge r77322 from trunk to branch 5.0 feature TMDM-2327 modified:  modify ugly exception
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@77326 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.core/src/com/amalto/core/util/CVCException.java
@@ -0,0 +1,50 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.core.util;
+
+
+public class CVCException extends Exception {
+
+    /**
+     * 
+     */
+    public CVCException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public CVCException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public CVCException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public CVCException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+}
