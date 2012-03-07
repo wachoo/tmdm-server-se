839cbd3dd667b2b48d67e10852ccd04ab18695fd
hcai <hcai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-07 10:12:14 +0000

    feature TMDM-1768:Journal GXT Refactor -- Search
    git-svn-id: http://talendforge.org/svn/tom/trunk@79455 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/Journal.java
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/Journal.java
@@ -21,6 +21,7 @@ import com.extjs.gxt.ui.client.core.XDOM;
 import com.extjs.gxt.ui.client.mvc.Dispatcher;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -38,6 +39,7 @@ public class Journal implements EntryPoint {
     public void onModuleLoad() {
         if (GWT.isScript()) {
             XDOM.setAutoIdPrefix(GWT.getModuleName() + "-" + XDOM.getAutoIdPrefix()); //$NON-NLS-1$
+            registerPubService();
             Log.setUncaughtExceptionHandler();
             Registry.register(JOURNAL_SERVICE, GWT.create(JournalService.class));
             Registry.register(SEARCH_CRITERIA, new JournalSearchCriteria());
@@ -58,7 +60,74 @@ public class Journal implements EntryPoint {
             RootPanel.get().add(GenerateContainer.getContentPanel());
         }
     }
+    
+    private native void registerPubService()/*-{
+        var instance = this;
+        $wnd.amalto.journal = {};
+        $wnd.amalto.journal.Journal = function(){
 
+        function initUI(){        
+        instance.@org.talend.mdm.webapp.journal.client.Journal::initUI()();
+        }
+
+        return {
+        init : function(){initUI();}
+        }
+        }();
+    }-*/;
+    
+    private native void _initUI()/*-{
+        var tabPanel = $wnd.amalto.core.getTabPanel();
+        var panel = tabPanel.getItem("Journal"); 
+        if (panel == undefined){
+        @org.talend.mdm.webapp.journal.client.GenerateContainer::generateContentPanel()();
+        panel = this.@org.talend.mdm.webapp.journal.client.Journal::createPanel()();
+        tabPanel.add(panel);
+        }
+        tabPanel.setSelection(panel.getItemId());
+    }-*/;
+
+    native JavaScriptObject createPanel()/*-{
+        var instance = this;
+        var panel = {
+        render : function(el){
+        instance.@org.talend.mdm.webapp.journal.client.Journal::renderContent(Ljava/lang/String;)(el.id);
+        },
+        setSize : function(width, height){
+        var cp = @org.talend.mdm.webapp.journal.client.GenerateContainer::getContentPanel()();
+        cp.@com.extjs.gxt.ui.client.widget.ContentPanel::setSize(II)(width, height);
+        },
+        getItemId : function(){
+        var cp = @org.talend.mdm.webapp.journal.client.GenerateContainer::getContentPanel()();
+        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getItemId()();
+        },
+        getEl : function(){
+        var cp = @org.talend.mdm.webapp.journal.client.GenerateContainer::getContentPanel()();
+        var el = cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getElement()();
+        return {dom : el};
+        },
+        doLayout : function(){
+        var cp = @org.talend.mdm.webapp.journal.client.GenerateContainer::getContentPanel()();
+        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::doLayout()();
+        },
+        title : function(){
+        var cp = @org.talend.mdm.webapp.journal.client.GenerateContainer::getContentPanel()();
+        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getHeading()();
+        }
+        };
+        return panel;
+    }-*/;
+
+    public void renderContent(final String contentId) {
+        onModuleRender();
+        RootPanel panel = RootPanel.get(contentId);
+        panel.add(GenerateContainer.getContentPanel());
+    }
+
+    public void initUI() {
+        _initUI();
+    }
+    
     private void onModuleRender() {
         Dispatcher dispatcher = Dispatcher.get();
         dispatcher.dispatch(JournalEvents.InitFrame);
