434270ef63ed7386d84e56499fcab22ea04f1235
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-20 01:48:32 +0000

    TMDM-661:add missing methods
    git-svn-id: http://talendforge.org/svn/tom/trunk@80061 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core.open/src/com/amalto/core/delegator/impl/DefaultItemCtrlDelegator.java
+++ org.talend.mdm.core.open/src/com/amalto/core/delegator/impl/DefaultItemCtrlDelegator.java
@@ -1,9 +1,33 @@
 package com.amalto.core.delegator.impl;
 
+import java.util.ArrayList;
+
 import com.amalto.core.delegator.IItemCtrlDelegator;
+import com.amalto.core.objects.view.ejb.ViewPOJOPK;
+import com.amalto.xmlserver.interfaces.IWhereItem;
 
 
 public class DefaultItemCtrlDelegator extends IItemCtrlDelegator {
+
+	/* (non-Javadoc)
+	 * @see com.amalto.core.delegator.IItemCtrlDelegator#getViewWCFromRole(com.amalto.core.objects.view.ejb.ViewPOJOPK)
+	 */
+	@Override
+	protected ArrayList<IWhereItem> getViewWCFromRole(ViewPOJOPK viewPOJOPK)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.amalto.core.delegator.IItemCtrlDelegator#getRecordsSecurityFromRole(java.lang.String)
+	 */
+	@Override
+	protected ArrayList<IWhereItem> getRecordsSecurityFromRole(
+			String mainPivotName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 	
