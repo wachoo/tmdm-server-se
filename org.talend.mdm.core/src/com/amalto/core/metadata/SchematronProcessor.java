ac58e5fc0577e6e60c740f50b59fef677e9d896f
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-10 09:50:35 +0000

    TMDM-3716: Fix incorrect parsing of schematron annotation.
    git-svn-id: http://talendforge.org/svn/tom/trunk@81343 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/metadata/SchematronProcessor.java
+++ org.talend.mdm.core/src/com/amalto/core/metadata/SchematronProcessor.java
@@ -15,17 +15,34 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.ws.commons.schema.XmlSchemaAnnotation;
 import org.apache.ws.commons.schema.XmlSchemaAppInfo;
 
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 import java.util.Iterator;
 
 public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
+
+    private static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
     public void process(MetadataRepository repository, ComplexTypeMetadata type, XmlSchemaAnnotation annotation, XmlSchemaAnnotationProcessorState state) {
         if (annotation != null) {
             Iterator annotations = annotation.getItems().getIterator();
             while (annotations.hasNext()) {
                 XmlSchemaAppInfo appInfo = (XmlSchemaAppInfo) annotations.next();
-                if ("X_Schematron".equals(appInfo.getSource())) {
-                    String rawTextContent = appInfo.getMarkup().item(0).getTextContent();
-                    state.setSchematron("<schema>" + StringEscapeUtils.unescapeXml(rawTextContent) + "</schema>");
+                if ("X_Schematron".equals(appInfo.getSource())) { //$NON-NLS-1$
+                    try {
+                        // TODO This is not really efficient but doing it nicely would require to rewrite a StringEscapeUtils.unescapeXml()
+                        StringWriter sw = new StringWriter();
+                        Transformer transformer = transformerFactory.newTransformer();
+                        transformer.setOutputProperty("omit-xml-declaration", "yes"); //$NON-NLS-1$ //$NON-NLS-2$
+                        transformer.transform(new DOMSource(appInfo.getMarkup().item(0)), new StreamResult(sw));
+                        state.setSchematron("<schema>" + StringEscapeUtils.unescapeXml(sw.toString()) + "</schema>"); //$NON-NLS-1$ //$NON-NLS-2$
+                    } catch (TransformerException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
