a408f28fbb49c1a1375b1158f3a8173c25fa3552
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-19 03:49:17 +0000

    TMDM-3739: add missing merge for r82000
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@82002 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/metadata/MetadataExtensible.java
+++ org.talend.mdm.core/src/com/amalto/core/metadata/MetadataExtensible.java
@@ -1,26 +1,27 @@
-// ============================================================================
-//
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
+/*
+ * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
 package com.amalto.core.metadata;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * DOC Starkey  class global comment. Detailled comment
+ * DOC Starkey  class global comment. Detailed comment
  */
-public abstract class MetadataExtensible {
+public class MetadataExtensible {
 
     private Map<String, Object> dataMap;
+
+    protected MetadataExtensible() {
+    }
 
     /**
      * Sets the defined property with the given name.
