d6188b0c8aeff46159e0b3d037544c953387584a
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-07 08:10:19 +0000

    merge r82923 from trunk to branch 5.0 TMDM-3813: Unable to select entity with the customer database
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@82929 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/inputfield/ComboBoxField.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/inputfield/ComboBoxField.java
@@ -1,16 +1,17 @@
 package org.talend.mdm.webapp.browserecords.client.widget.inputfield;
 
+import org.talend.mdm.webapp.base.client.widget.ComboBoxEx;
+
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.event.ResizeEvent;
 import com.extjs.gxt.ui.client.event.ResizeListener;
 import com.extjs.gxt.ui.client.fx.Resizable;
 import com.extjs.gxt.ui.client.widget.BoxComponent;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.form.ComboBox;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.google.gwt.user.client.Element;
 
-public class ComboBoxField<D extends ModelData> extends ComboBox<D> {
+public class ComboBoxField<D extends ModelData> extends ComboBoxEx<D> {
 
     class Resize extends Resizable {
 
