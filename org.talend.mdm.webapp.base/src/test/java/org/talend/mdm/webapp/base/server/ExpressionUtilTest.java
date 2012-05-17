d459330c4a4c7d24a7b4cd8defcd12824d8f4567
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-17 08:19:54 +0000

    TMDM-3980 :  modify ExpressionUtil logic, support single xpath expression
    git-svn-id: http://talendforge.org/svn/tom/trunk@83761 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.base/src/test/java/org/talend/mdm/webapp/base/server/ExpressionUtilTest.java
+++ org.talend.mdm.webapp.base/src/test/java/org/talend/mdm/webapp/base/server/ExpressionUtilTest.java
@@ -22,17 +22,96 @@ public class ExpressionUtilTest extends TestCase {
 
     public void test_getDepTypes() {
         // 1. xpath = "/Test/subelement"
-        String expression = "fn:starts-with(" + "/Test/subelement,liang" + ")"; //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+        String expression = "fn:starts-with(/Test/subelement,liang)"; //$NON-NLS-1$
         ExpressionUtil expressionUtil = new ExpressionUtil(expression);
         List<String> typePathes = expressionUtil.getDepTypes();
         assertEquals(1, typePathes.size());
         assertEquals("Test/subelement", typePathes.get(0)); //$NON-NLS-1$
         // 2. xpath = "Test/subelement"
-        expression = "fn:starts-with(" + "Test/subelement,liang" + ")"; //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+        expression = "fn:starts-with(Test/subelement,liang)"; //$NON-NLS-1$
         expressionUtil = new ExpressionUtil(expression);
         typePathes = expressionUtil.getDepTypes();
         assertEquals(1, typePathes.size());
         assertEquals("subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "fn:starts-with(/Test/subelement,../subelement)"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(2, typePathes.size());
+        assertEquals("Test/subelement", typePathes.get(0)); //$NON-NLS-1$
+        assertEquals("../subelement", typePathes.get(1)); //$NON-NLS-1$
+
+        expression = "/Test/subelement"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("Test/subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "../subelement"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("../subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "./subelement"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("./subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "1 + ./subelement"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("./subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "./subelement + 1"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("./subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "./Test/cp[1]/subelement = 'zhang'"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(1, typePathes.size());
+        assertEquals("./Test/cp/subelement", typePathes.get(0)); //$NON-NLS-1$
+
+        expression = "./Test/cp[position()=count(/Test/nodes)]/subelement[5] = 'zhang' and /Test/cp[2]/name[4] = /Test/subelement[6]"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(3, typePathes.size());
+        assertEquals("./Test/cp/subelement", typePathes.get(0)); //$NON-NLS-1$
+        assertEquals("Test/cp/name", typePathes.get(1)); //$NON-NLS-1$
+        assertEquals("Test/subelement", typePathes.get(2)); //$NON-NLS-1$
+
+        expression = "./Test/cp[1]/subelement = 'zhang' or ./Test/subelement = \"wang\""; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(2, typePathes.size());
+        assertEquals("./Test/cp/subelement", typePathes.get(0)); //$NON-NLS-1$
+        assertEquals("./Test/subelement", typePathes.get(1)); //$NON-NLS-1$
+
+        expression = "'zhang'"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(0, typePathes.size());
+
+        expression = "    'zhang'"; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(0, typePathes.size());
+
+        expression = "\"zhang\""; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(0, typePathes.size());
+
+        expression = "    \"zhang\""; //$NON-NLS-1$
+        expressionUtil = new ExpressionUtil(expression);
+        typePathes = expressionUtil.getDepTypes();
+        assertEquals(0, typePathes.size());
+
     }
 
     public void test_regex() {
