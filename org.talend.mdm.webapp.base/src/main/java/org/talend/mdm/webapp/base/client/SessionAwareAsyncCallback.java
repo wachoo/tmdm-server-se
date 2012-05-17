e04b2721f8f90255d22c3beb814944df54fc66e1
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-17 06:09:19 +0000

    TMDM-3891: Fix compliance issue on branch 5
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@83733 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/SessionAwareAsyncCallback.java
+++ org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/SessionAwareAsyncCallback.java
@@ -1,41 +1,3 @@
-<<<<<<< .working
-// ============================================================================
-//
-// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
-package org.talend.mdm.webapp.base.client;
-
-import org.talend.mdm.webapp.base.client.exception.ServiceExceptionHandler;
-import org.talend.mdm.webapp.base.client.util.WaitBox;
-
-import com.allen_sauer.gwt.log.client.Log;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public abstract class SessionAwareAsyncCallback<T> implements AsyncCallback<T> {
-
-    public final void onFailure(Throwable caught) {
-        WaitBox.hide();
-        if (Log.isErrorEnabled())
-            Log.error(caught.toString());
-
-        ServiceExceptionHandler serviceExceptionHandler = new ServiceExceptionHandler(caught);
-        if (!serviceExceptionHandler.work())
-            doOnFailure(caught);
-    }
-
-    protected void doOnFailure(Throwable caught) {
-        ServiceExceptionHandler.doOnFailure(caught);
-    }
-}
-=======
 // ============================================================================
 //
 // Copyright (C) 2006-2012 Talend Inc. - www.talend.com
@@ -95,4 +57,3 @@ public abstract class SessionAwareAsyncCallback<T> implements AsyncCallback<T> {
         MessageBox.alert(BaseMessagesFactory.getMessages().error_title(), errorMsg, null);
     }
 }
->>>>>>> .merge-right.r83727
--- org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/exception/ServiceException.java
+++ org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/exception/ServiceException.java
@@ -1,64 +1,3 @@
-<<<<<<< .working
-// ============================================================================
-//
-// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
-package org.talend.mdm.webapp.base.client.exception;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public class ServiceException extends Exception implements IsSerializable {
-
-    private static final long serialVersionUID = 1L;
-
-    private Map<String, Object> parameterMap;
-
-    public ServiceException() {
-        super();
-    }
-
-    public ServiceException(String message) {
-        super(message);
-    }
-
-    public ServiceException(Throwable cause) {
-        super(cause);
-    }
-
-    public ServiceException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-
-    public ServiceException(String parameterKey, Object parameterValue) {
-        super();
-        setParameter(parameterKey, parameterValue);
-    }
-
-
-    private void setParameter(String key, Object parameter) {
-        if (parameterMap == null)
-            parameterMap = new HashMap<String, Object>();
-        parameterMap.put(key, parameter);
-    }
-
-    protected <X> X getParameter(String key) {
-        if (parameterMap == null)
-            return null;
-        return (X) parameterMap.get(key);
-    }
-}
-=======
 // ============================================================================
 //
 // Copyright (C) 2006-2012 Talend Inc. - www.talend.com
@@ -87,4 +26,3 @@ public class ServiceException extends Exception implements IsSerializable {
         super(message);
     }
 }
->>>>>>> .merge-right.r83727
