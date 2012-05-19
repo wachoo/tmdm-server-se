18a37177cc87f75e278ee1e639c7ef7c065bc3a3
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-19 12:22:58 +0000

    TMDM-3871: Manually backport r83854 to branch 5.0
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@83894 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/NoAccessCheckUtilTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/NoAccessCheckUtilTest.java
@@ -1,14 +1,11 @@
 package org.talend.mdm.webapp.browserecords.server.util;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
 
 import junit.framework.TestCase;
 
@@ -17,7 +14,7 @@ public class NoAccessCheckUtilTest extends TestCase {
     
     
     private static final List<String> ROLES = Arrays.asList(new String[] { "System_Admin" }); //$NON-NLS-1$
-    private static final String XSD = "<xsd:schema xmlns:xsd='http://www.w3.org/2001/XMLSchema'><xsd:import namespace='http://www.w3.org/2001/XMLSchema' /><xsd:element name='M28_E01'><xsd:complexType><xsd:all><xsd:element name='subelement' type='xsd:string' /></xsd:all></xsd:complexType><xsd:unique name='M28_E01'><xsd:selector xpath='.' /><xsd:field xpath='subelement' /></xsd:unique></xsd:element><xsd:element name='M28_E02'><xsd:annotation><xsd:appinfo source='X_Hide'>System_Admin</xsd:appinfo></xsd:annotation><xsd:complexType><xsd:all><xsd:element name='subelement' type='xsd:string'><xsd:annotation><xsd:appinfo source='X_Write'>System_Admin</xsd:appinfo></xsd:annotation></xsd:element></xsd:all></xsd:complexType><xsd:unique name='M28_E02'><xsd:selector xpath='.' /><xsd:field xpath='subelement' /></xsd:unique></xsd:element></xsd:schema>"; //$NON-NLS-1$
+
 
     public void testCheckNoAccessHelper() throws Exception {
         String modelXSD = getXSDModel("BrowseRecordsActionTest.xsd"); //$NON-NLS-1$
@@ -25,12 +22,17 @@ public class NoAccessCheckUtilTest extends TestCase {
         assertTrue(NoAccessCheckUtil.checkNoAccessHelper(modelXSD, "M28_E02", ROLES)); //$NON-NLS-1$
     }
     
-    private String getXSDModel(String filename) throws Exception {
-        InputStream is = new ByteArrayInputStream(XSD.getBytes("UTF-8")); //$NON-NLS-1$
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(is);
-        String XSDModel = com.amalto.core.util.Util.nodeToString(doc);
-        return XSDModel;
+    private String getXSDModel(String filename) throws IOException {
+
+        InputStream stream = getClass().getResourceAsStream(filename);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+        StringBuffer buffer = new StringBuffer();
+        String line = ""; //$NON-NLS-1$
+        while ((line = in.readLine()) != null) {
+            buffer.append(line);
+        }
+        return buffer.toString();
+
     }
 }
