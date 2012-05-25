5b2f7704dcd034738aaa67cc7a9290133da9e8bf
hcai <hcai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-25 05:35:01 +0000

    TMDM-4043: move the custom layout case out of ViewHelperTest
    git-svn-id: http://talendforge.org/svn/tom/trunk@84134 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/bizhelpers/ViewHelperTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/bizhelpers/ViewHelperTest.java
@@ -12,10 +12,6 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecords.server.bizhelpers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,13 +20,9 @@ import junit.framework.TestCase;
 import org.talend.mdm.webapp.base.client.model.DataTypeCustomized;
 import org.talend.mdm.webapp.base.shared.SimpleTypeModel;
 import org.talend.mdm.webapp.base.shared.TypeModel;
-import org.talend.mdm.webapp.browserecords.client.model.ColumnTreeLayoutModel;
 import org.talend.mdm.webapp.browserecords.shared.ComplexTypeModel;
 import org.talend.mdm.webapp.browserecords.shared.EntityModel;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import com.amalto.webapp.core.util.Util;
 import com.amalto.webapp.util.webservices.WSView;
 
 @SuppressWarnings("nls")
@@ -45,93 +37,6 @@ public class ViewHelperTest extends TestCase {
     
     public ViewHelperTest(){
         initData();
-    }
-
-    public void testBuilderLayout() throws Exception {
-        String xml = inputStream2String(this.getClass().getResourceAsStream("temp_ColumnTreeLayout.xml"));
-        Document doc = Util.parse(xml);
-        Element root = doc.getDocumentElement();
-        ColumnTreeLayoutModel result = ViewHelper.builderLayout(root);
-        assertNotNull(result);
-        assertEquals(2, result.getColumnTreeModels().size());
-        assertTrue(result.getColumnTreeModels().get(0).getStyle().equals(""));
-        assertTrue(result.getColumnTreeModels().get(1).getStyle().equals(""));
-        assertTrue(result.getColumnTreeModels().get(0).getStyle().equals(""));
-        assertNotNull(result.getColumnTreeModels().get(0).getColumnElements());
-        assertNotNull(result.getColumnTreeModels().get(1).getColumnElements());
-        assertEquals(3, result.getColumnTreeModels().get(0).getColumnElements().size());
-        assertEquals(2, result.getColumnTreeModels().get(1).getColumnElements().size());
-        assertEquals("/1", result.getColumnTreeModels().get(0).getColumnElements().get(0).getParent());
-        assertNull(result.getColumnTreeModels().get(0).getColumnElements().get(0).getChildren());
-        assertEquals("/Cusomer/Name", result.getColumnTreeModels().get(0).getColumnElements().get(1).getxPath());
-        assertEquals("Name", result.getColumnTreeModels().get(0).getColumnElements().get(1).getLabel());
-        assertEquals("/1", result.getColumnTreeModels().get(0).getColumnElements().get(2).getParent());
-        assertNotNull(result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren());
-        assertEquals(2, result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().size());
-        assertEquals("/1/@children.2", result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(0).getParent());
-        assertEquals("/Customer/Address/zipCode", result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(0).getxPath());
-        assertEquals("zipCode", result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(0).getLabel());
-        assertTrue(result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(1).getHtmlSnippet().equals(""));
-        assertTrue(result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(1).getLabelStyle().equals(""));
-        assertTrue(result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(1).getStyle().equals(""));
-        assertTrue(result.getColumnTreeModels().get(0).getColumnElements().get(2).getChildren().get(1).getValueStyle().equals(""));
-        assertEquals(2, result.getColumnTreeModels().get(1).getColumnElements().size());
-        assertEquals("/2", result.getColumnTreeModels().get(1).getColumnElements().get(0).getParent());
-        assertEquals("/Customer/picture", result.getColumnTreeModels().get(1).getColumnElements().get(0).getxPath());
-        assertEquals("/2", result.getColumnTreeModels().get(1).getColumnElements().get(1).getParent());
-        assertEquals("/Customer/country", result.getColumnTreeModels().get(1).getColumnElements().get(1).getxPath());
-        
-        xml = inputStream2String(this.getClass().getResourceAsStream("productform.xml"));
-        doc = Util.parse(xml);
-        root = doc.getDocumentElement();
-        result = ViewHelper.builderLayout(root);
-        assertNotNull(result);
-        assertEquals(2, result.getColumnTreeModels().size());
-        assertEquals(8, result.getColumnTreeModels().get(0).getColumnElements().size());
-        assertEquals(3, result.getColumnTreeModels().get(1).getColumnElements().size());
-        assertEquals("_4DuYgGKFEeG_l-qIWXZr4g", result.getColumnTreeModels().get(0).getColumnElements().get(0).getParent());
-        assertNotNull(result.getColumnTreeModels().get(0).getColumnElements().get(3).getChildren());
-        assertEquals("/Product/Features", result.getColumnTreeModels().get(0).getColumnElements().get(3).getxPath());
-        assertEquals("a", result.getColumnTreeModels().get(1).getColumnElements().get(0).getLabelStyle());
-        assertEquals("b", result.getColumnTreeModels().get(1).getColumnElements().get(0).getValueStyle());
-        assertEquals("d", result.getColumnTreeModels().get(1).getColumnElements().get(1).getStyle());
-        assertEquals("_4EnJUmKFEeG_l-qIWXZr4g", result.getColumnTreeModels().get(1).getColumnElements().get(1).getParent());
-        assertNotNull(result.getColumnTreeModels().get(1).getColumnElements().get(2).getHtmlSnippet());
-        assertNotSame(0, result.getColumnTreeModels().get(1).getColumnElements().get(2).getHtmlSnippet().trim().length());
-        
-        xml = inputStream2String(this.getClass().getResourceAsStream("Store.xml"));
-        doc = Util.parse(xml);
-        root = doc.getDocumentElement();
-        result = ViewHelper.builderLayout(root);
-        assertNotNull(result);
-        assertEquals(2, result.getColumnTreeModels().size());
-        assertEquals(5, result.getColumnTreeModels().get(0).getColumnElements().size());
-        assertNull(result.getColumnTreeModels().get(1).getColumnElements());
-        
-        xml = inputStream2String(this.getClass().getResourceAsStream("MyTest.xml"));
-        doc = Util.parse(xml);
-        root = doc.getDocumentElement();
-        result = ViewHelper.builderLayout(root);
-        assertNotNull(result);
-        assertEquals(3, result.getColumnTreeModels().size());
-        assertNotNull(result.getColumnTreeModels().get(0).getColumnElements());
-        assertNull(result.getColumnTreeModels().get(1).getColumnElements());
-        assertNull(result.getColumnTreeModels().get(2).getColumnElements());
-
-    }
-    
-    private String inputStream2String(InputStream is) {
-        BufferedReader in = new BufferedReader(new InputStreamReader(is));
-        StringBuffer buffer = new StringBuffer();
-        String line = "";
-        try {
-            while ((line = in.readLine()) != null) {
-                buffer.append(line);
-            }
-        } catch (IOException e) {
-            fail();
-        }
-        return buffer.toString();
     }
     
     public void testGetConceptFromDefaultViewName(){
