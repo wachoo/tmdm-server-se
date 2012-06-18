c0e5daf5ece289edde12562235d82091f0500113
hcai <hcai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-18 10:27:56 +0000

    TMDM-4211: Manually backport r82543 from trunk to 5.0
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@85816 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/inputfield/PictureField.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/inputfield/PictureField.java
@@ -55,7 +55,7 @@ public class PictureField extends TextField<String> {
 
     private static final String CONTEXT_PATH = GWT.getModuleBaseURL().replaceFirst(GWT.getModuleName() + "/", ""); //$NON-NLS-1$ //$NON-NLS-2$
 
-    private static final String DefaultImage = CONTEXT_PATH + "images/icons/no_image.gif"; //$NON-NLS-1$
+    private static final String DefaultImage = CONTEXT_PATH + "resources/images/talend/no_image.png"; //$NON-NLS-1$
 
     protected El wrap = new El(DOM.createSpan());
 
@@ -185,6 +185,10 @@ public class PictureField extends TextField<String> {
 
     @Override
     public void setValue(String value) {
+    	if (value.equalsIgnoreCase("http://")) { //$NON-NLS-1$ 
+            return; 
+        } 
+    	
         String oldValue = this.value;
         this.value = value;
 
