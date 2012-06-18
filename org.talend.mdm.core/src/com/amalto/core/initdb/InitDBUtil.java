b6dedec4600f316f9ef9dcfdd2cf098452dd44bb
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-18 11:37:38 +0000

    TMDM-3724: Missing import statement in InitDBUtil
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@85839 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/initdb/InitDBUtil.java
+++ org.talend.mdm.core/src/com/amalto/core/initdb/InitDBUtil.java
@@ -24,6 +24,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.amalto.core.ejb.local.XmlServerSLWrapperLocal;
 import com.amalto.core.objects.configurationinfo.localutil.ConfigurationHelper;
 import com.amalto.core.util.Util;
 import com.amalto.core.util.XtentisException;
