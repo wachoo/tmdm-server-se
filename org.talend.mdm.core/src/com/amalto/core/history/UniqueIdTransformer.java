3933630ff5ca7b4019790e9d34ddc247db1b02ae
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-18 10:42:45 +0000

    TMDM-4199: Fix NPE that happens when trying to add unique IDs on an empty document.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@85827 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/history/UniqueIdTransformer.java
+++ org.talend.mdm.core/src/com/amalto/core/history/UniqueIdTransformer.java
@@ -69,13 +69,16 @@ public class UniqueIdTransformer implements DocumentTransformer {
         Stack<Integer> levels = new Stack<Integer>();
         levels.push(0);
         {
-            _addIds(document, document.getDocumentElement(), levels);
+            Element documentElement = document.getDocumentElement();
+            if (documentElement != null) {
+                _addIds(document, documentElement, levels);
+            }
         }
         levels.pop();
     }
 
-    private void _addIds(org.w3c.dom.Document document, Node node, Stack<Integer> levels) {
-        NamedNodeMap attributes = node.getAttributes();
+    private void _addIds(org.w3c.dom.Document document, Element element, Stack<Integer> levels) {
+        NamedNodeMap attributes = element.getAttributes();
         Attr id = document.createAttribute(ID_ATTRIBUTE_NAME);
 
         int thisElementId = levels.pop() + 1;
@@ -87,18 +90,16 @@ public class UniqueIdTransformer implements DocumentTransformer {
             }
         }
         String prefix = builder.toString().isEmpty() ? StringUtils.EMPTY : builder.toString() + '-';
-        id.setValue(prefix + node.getNodeName() + '-' + thisElementId);
+        id.setValue(prefix + element.getNodeName() + '-' + thisElementId);
         attributes.setNamedItem(id);
 
         levels.push(thisElementId);
         {
             levels.push(0);
-            NodeList children = node.getChildNodes();
+            NodeList children = element.getElementsByTagName("*"); //$NON-NLS-1$
             for (int i = 0; i < children.getLength(); i++) {
-                Node child = children.item(i);
-                if (child instanceof Element) {
-                    _addIds(document, child, levels);
-                }
+                Element child = (Element) children.item(i);
+                _addIds(document, child, levels);
             }
             levels.pop();
         }
