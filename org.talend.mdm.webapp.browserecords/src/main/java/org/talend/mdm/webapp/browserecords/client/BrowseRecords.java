00fef5b97555713901c15d6d3b70cf1b389eb862
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-20 07:06:50 +0000

    TMDM-3641 : safely create object "amalto.itemsbrowser.ItemsBrowser" to compatible original extjs project
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@86024 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/BrowseRecords.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/BrowseRecords.java
@@ -55,22 +55,25 @@ public class BrowseRecords implements EntryPoint {
     private RootPanel panel;
 
     public native void regItemDetails()/*-{
-        $wnd.amalto.itemsbrowser.ItemsBrowser.editItemDetails = function(fromWhichApp, ids, entity, callback){
-            var checkArgs = true;
-            checkArgs = checkArgs && (arguments.length >= 3);
-            checkArgs = checkArgs && (typeof fromWhichApp === "string");
-            checkArgs = checkArgs && (ids.length >= 1);
-            checkArgs = checkArgs && (typeof entity === "string");
-            if (!checkArgs){
-                throw {message: "argument format error!"};
-            }
+            $wnd.amalto = $wnd.amalto || {};
+            $wnd.amalto.itemsbrowser = $wnd.amalto.itemsbrowser || {};
+            $wnd.amalto.itemsbrowser.ItemsBrowser = $wnd.amalto.itemsbrowser.ItemsBrowser || {};
+            $wnd.amalto.itemsbrowser.ItemsBrowser.editItemDetails = function(fromWhichApp, ids, entity, callback){
+                var checkArgs = true;
+                checkArgs = checkArgs && (arguments.length >= 3);
+                checkArgs = checkArgs && (typeof fromWhichApp === "string");
+                checkArgs = checkArgs && (ids.length >= 1);
+                checkArgs = checkArgs && (typeof entity === "string");
+                if (!checkArgs){
+                    throw {message: "argument format error!"};
+                }
             
-            var idstr;
-            if(ids.length == 1){
-                idstr = ids[0];
-            }
-            @org.talend.mdm.webapp.browserecords.client.widget.treedetail.TreeDetailUtil::initItemsDetailPanelById(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Boolean;Ljava/lang/Boolean;)(fromWhichApp, idstr, entity, new Boolean(false), new Boolean(false));
-        };
+                var idstr;
+                if(ids.length == 1){
+                    idstr = ids[0];
+                }
+                @org.talend.mdm.webapp.browserecords.client.widget.treedetail.TreeDetailUtil::initItemsDetailPanelById(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Boolean;Ljava/lang/Boolean;)(fromWhichApp, idstr, entity, new Boolean(false), new Boolean(false));
+            };
     }-*/;
 
     /**
