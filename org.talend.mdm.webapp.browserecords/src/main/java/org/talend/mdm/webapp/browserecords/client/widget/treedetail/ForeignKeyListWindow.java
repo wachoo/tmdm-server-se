aa8f927b5bc7123b4eb106f63e215e92ac270a72
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-06 06:47:00 +0000

    TMDM-4092：add quotes for search value of foreign key to search it as String
    git-svn-id: http://talendforge.org/svn/tom/trunk@86919 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/treedetail/ForeignKeyListWindow.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/treedetail/ForeignKeyListWindow.java
@@ -166,6 +166,8 @@ public class ForeignKeyListWindow extends Window {
         String value = filter.getRawValue();
         if (value == null || value.trim().length() == 0) {
             value = ".*"; //$NON-NLS-1$
+        }else{
+            value = "'" + filter.getRawValue() + "'"; //$NON-NLS-1$ //$NON-NLS-2$
         }
         return value;
     }
@@ -294,9 +296,7 @@ public class ForeignKeyListWindow extends Window {
                 if (be.getKeyCode() == KeyCodes.KEY_LEFT || be.getKeyCode() == KeyCodes.KEY_RIGHT) {
                     return;
                 }
-                if (CommonUtil.validateSearchValue(entityModel.getMetaDataTypes(), getFilterValue())) {
-                    loader.load(0, pageSize);
-                }
+                loader.load(0, pageSize);
             }
         });
         filter.setWidth(WINDOW_WIDTH - 80);
