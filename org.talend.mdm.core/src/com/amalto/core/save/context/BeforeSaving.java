46a8747b4840d4e53930dbaf3cdf10137d727094
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-30 10:25:35 +0000

    merge r88290 from trunk to branch 5.1 TMDM-4386: Fix incorrect way to get result from beforeSaving process execution
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_1@88291 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/save/context/BeforeSaving.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/BeforeSaving.java
@@ -14,12 +14,13 @@ package com.amalto.core.save.context;
 import com.amalto.core.history.MutableDocument;
 import com.amalto.core.save.DOMDocument;
 import com.amalto.core.save.DocumentSaverContext;
-import com.amalto.core.save.ReportDocumentSaverContext;
 import com.amalto.core.save.SaverSession;
+import com.amalto.core.save.UserAction;
 import com.amalto.core.util.OutputReport;
 import com.amalto.core.util.Util;
 import com.sun.org.apache.xpath.internal.XPathAPI;
 import org.apache.commons.lang.StringUtils;
+import org.jboss.logging.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -28,11 +29,13 @@ import org.w3c.dom.NodeList;
 public class BeforeSaving implements DocumentSaver {
 
     public static final String BEFORE_SAVING_VALIDATION_MESSAGE_PREFIX = "BeforeSaving Validation Error --> "; //$NON-NLS-1$
-    
+
+    private static final Logger LOGGER = Logger.getLogger(BeforeSaving.class);
+
     private DocumentSaver next;
 
     private String message = StringUtils.EMPTY;
-    
+
     BeforeSaving(DocumentSaver next) {
         this.next = next;
     }
@@ -47,30 +50,50 @@ public class BeforeSaving implements DocumentSaver {
 
         if (outputreport != null) { // when a process was found
             String errorCode;
-            message = outputreport.getMessage();
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Output report: message present (" + String.valueOf(outputreport.getMessage() != null) + ") ");
+                LOGGER.debug("Output report: item present (" + String.valueOf(outputreport.getItem() != null) + ") ");
+            }
+
             try {
-                Document doc = Util.parse(message);
-                // handle output_report
-                String xpath = "//report/message"; //$NON-NLS-1$
-                Node errorNode = XPathAPI.selectSingleNode(doc, xpath);
-                errorCode = null;
-                if (errorNode instanceof Element) {
-                    Element errorElement = (Element) errorNode;
-                    errorCode = errorElement.getAttribute("type"); //$NON-NLS-1$
-                    Node child = errorElement.getFirstChild();
-                    if (child instanceof org.w3c.dom.Text) {
-                        message = child.getTextContent();
-                    }
+                if (outputreport.getMessage() == null && outputreport.getItem() == null) {
+                    throw new RuntimeException(BEFORE_SAVING_VALIDATION_MESSAGE_PREFIX + " No message");
                 }
 
-                if (!"info".equals(errorCode)) { //$NON-NLS-1$
-                    throw new RuntimeException(BEFORE_SAVING_VALIDATION_MESSAGE_PREFIX + message);
+                if (outputreport.getMessage() != null) {
+                    message = outputreport.getMessage();
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("Output report found from before saving process");
+                        LOGGER.debug(message);
+                    }
+                    Document doc = Util.parse(message);
+                    // handle output_report
+                    String xpath = "//report/message"; //$NON-NLS-1$
+                    Node errorNode = XPathAPI.selectSingleNode(doc, xpath);
+                    if (errorNode instanceof Element) {
+                        Element errorElement = (Element) errorNode;
+                        errorCode = errorElement.getAttribute("type"); //$NON-NLS-1$
+                        Node child = errorElement.getFirstChild();
+                        if (child instanceof org.w3c.dom.Text) {
+                            message = child.getTextContent();
+                        }
+                        if (!"info".equals(errorCode)) { //$NON-NLS-1$
+                            throw new RuntimeException(BEFORE_SAVING_VALIDATION_MESSAGE_PREFIX + message);
+                        }
+                    }
+                } else {
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("No output report from before saving process");
+                    }
                 }
 
                 // handle output_item
                 if (outputreport.getItem() != null) {
-                    xpath = "//exchange/item"; //$NON-NLS-1$
-                    doc = Util.parse(outputreport.getItem());
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("Found modified item from before saving process");
+                    }
+                    String xpath = "//exchange/item"; //$NON-NLS-1$
+                    Document doc = Util.parse(outputreport.getItem());
                     Node item = XPathAPI.selectSingleNode(doc, xpath);
                     if (item != null && item instanceof Element) {
                         NodeList list = item.getChildNodes();
@@ -83,10 +106,21 @@ public class BeforeSaving implements DocumentSaver {
                         }
                         if (node != null) {
                             // set back the modified item by the process
+                            if (!node.getNodeName().equals(context.getType().getName())) {
+                                message = StringUtils.EMPTY;
+                                throw new IllegalArgumentException("Before saving returned a document typed '" + node.getNodeName() + "' but expected '" + context.getType().getName() + "'");
+                            }
                             context.setUserDocument(new DOMDocument(node));
+                            if (context.getUserAction() == UserAction.CREATE) {
+                                context.setId(new String[0]); // Will re-read id from document.
+                            }
                             // Redo a set of actions and security checks.
                             next = new GenerateActions(new Security(next));
                         }
+                    }
+                } else {
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("No modified item from before saving process");
                     }
                 }
             } catch (Exception e) {
