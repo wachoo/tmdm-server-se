64887c35e253880602500f4be9bf8e834fe92de5
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-08-01 11:33:24 +0000

    TMDM-4403: remove useless generated files
    git-svn-id: http://talendforge.org/svn/tom/trunk@88446 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.stagingarea/target/.generated/org/talend/mdm/webapp/stagingarea/client/i18n/StagingareaMessages_.java
+++
@@ -1,8 +0,0 @@
-package org.talend.mdm.webapp.stagingarea.client.i18n;
-
-public class StagingareaMessages_ implements org.talend.mdm.webapp.stagingarea.client.i18n.StagingareaMessages {
-  
-  public java.lang.String stagingarea_title() {
-    return "stagingarea";
-  }
-}
