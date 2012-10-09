00540836c5639908c77dc51e74b355972fd20c12
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-09 09:43:17 +0000

    TMDM-4711: ForeignKey display issue when foreignKeyInfo's value is empty
    git-svn-id: http://talendforge.org/svn/tom/trunk@91772 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ForeignKey/FKPropertyEditor.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ForeignKey/FKPropertyEditor.java
@@ -13,8 +13,6 @@
 
 package org.talend.mdm.webapp.browserecords.client.widget.ForeignKey;
 
-import java.util.Collection;
-
 import org.talend.mdm.webapp.base.client.model.ForeignKeyBean;
 
 import com.extjs.gxt.ui.client.widget.form.PropertyEditor;
@@ -36,20 +34,7 @@ public class FKPropertyEditor implements PropertyEditor<ForeignKeyBean> {
 
     public String getStringValue(ForeignKeyBean foreignKeyBean) {
         this.foreignKeyBean = foreignKeyBean;
-        if (foreignKeyBean.getForeignKeyInfo() != null && foreignKeyBean.getForeignKeyInfo().size() > 0) {
-            StringBuilder sb = new StringBuilder();
-            Collection<String> fkInfoValues = foreignKeyBean.getForeignKeyInfo().values();
-            int i = 0;
-            for (String fkInfoValue : fkInfoValues) {
-                if (i > 0)
-                    sb.append('-');
-                sb.append(fkInfoValue);
-                i++;
-            }
-            return sb.toString();
-        } else {
-            return foreignKeyBean.getId();
-        }
+        return foreignKeyBean.toString();
     }
 
 }
