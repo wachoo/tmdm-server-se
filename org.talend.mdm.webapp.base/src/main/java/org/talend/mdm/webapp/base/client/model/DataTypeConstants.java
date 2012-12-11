64f6b3749f371721f995480d282fd3187b6754bb
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-11 06:25:23 +0000

    TMDM-4469 : perfect baseTypeName method
    git-svn-id: http://talendforge.org/svn/tom/trunk@95067 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/model/DataTypeConstants.java
+++ org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/model/DataTypeConstants.java
@@ -73,6 +73,8 @@ public enum DataTypeConstants implements DataType {
 
     String typeName;
 
+    String baseTypeName;
+
     Object defaultValue;
 
     DataTypeConstants(String typeName) {
@@ -98,7 +100,13 @@ public enum DataTypeConstants implements DataType {
      * @see org.talend.mdm.webapp.itemsbrowser2.client.model.DataType#getBaseTypeName()
      */
     public String getBaseTypeName() {
-        return getTypeName();
+        if (baseTypeName == null) {
+            return getTypeName();
+        }
+        return baseTypeName;
     }
 
+    public void setBaseTypeName(String baseTypeName) {
+        this.baseTypeName = baseTypeName;
+    }
 }
