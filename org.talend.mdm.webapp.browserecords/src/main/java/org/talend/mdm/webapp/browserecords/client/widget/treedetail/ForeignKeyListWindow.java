38c0afc8c9354c5649ee1ad91c254c3e543d05c6
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-21 08:00:45 +0000

    TMDM-3866:order by alphabetical when it don't have foreign key info
    git-svn-id: http://talendforge.org/svn/tom/trunk@95800 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/treedetail/ForeignKeyListWindow.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/treedetail/ForeignKeyListWindow.java
@@ -40,6 +40,7 @@ import org.talend.mdm.webapp.browserecords.client.widget.inputfield.ComboBoxFiel
 import org.talend.mdm.webapp.browserecords.shared.EntityModel;
 
 import com.extjs.gxt.ui.client.Registry;
+import com.extjs.gxt.ui.client.Style.SortDir;
 import com.extjs.gxt.ui.client.data.BaseListLoadResult;
 import com.extjs.gxt.ui.client.data.BaseListLoader;
 import com.extjs.gxt.ui.client.data.BaseModel;
@@ -192,6 +193,7 @@ public class ForeignKeyListWindow extends Window {
     @Override
     protected void onRender(Element parent, int pos) {
         super.onRender(parent, pos);
+        final PagingLoadConfig config = new BasePagingLoadConfig();
         final TypeModel typeModel = buildTypeModel();
 
         final boolean hasForeignKeyFilter = this.foreignKeyFilter != null && this.foreignKeyFilter.trim().length() > 0 ? true
@@ -409,6 +411,10 @@ public class ForeignKeyListWindow extends Window {
 
                 }
             }
+            if (columns.size() > 0) {
+                config.setSortField(columns.get(0).getHeader());
+                config.setSortDir(SortDir.ASC);
+            }
         }
         if (columns.size() == 0) {
             columns.add(new ColumnConfig("id", CommonUtil.getElementFromXpath(typeModel.getXpath()), COLUMN_WIDTH)); //$NON-NLS-1$
@@ -450,7 +456,6 @@ public class ForeignKeyListWindow extends Window {
         relatedRecordGrid.addListener(Events.Attach, new Listener<GridEvent<ForeignKeyBean>>() {
 
             public void handleEvent(GridEvent<ForeignKeyBean> be) {
-                PagingLoadConfig config = new BasePagingLoadConfig();
                 config.setOffset(0);
                 config.setLimit(pageSize);
                 loader.load(config);
