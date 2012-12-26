5932c18a7a15ab95a095bfe0dde05d1b987d52a5
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-26 08:18:36 +0000

    TMDM-4818 : use standard RPC api serialization
    git-svn-id: http://talendforge.org/svn/tom/trunk@95958 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/gwt/ProxyGWTServiceImpl.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/gwt/ProxyGWTServiceImpl.java
@@ -14,93 +14,43 @@ package org.talend.mdm.webapp.general.gwt;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.ParseException;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 
 import org.talend.mdm.webapp.base.server.AbstractService;
 
 import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
 import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.server.rpc.RPC;
 import com.google.gwt.user.server.rpc.RPCRequest;
-import com.google.gwt.user.server.rpc.SerializationPolicy;
-import com.google.gwt.user.server.rpc.SerializationPolicyLoader;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
 
 public class ProxyGWTServiceImpl extends AbstractService {
 
     private static final long serialVersionUID = -3043769203139170342L;
 
+    private Properties properties;
+
     private Map<String, Object> actions = new HashMap<String, Object>();
 
     @Override
-    protected SerializationPolicy doGetSerializationPolicy(HttpServletRequest request, String moduleBaseURL, String strongName) {
-        String modulePath = null;
-        String hostAddress = null;
-        SerializationPolicy serializationPolicy = null;
-        try {
-            URL url = new URL(moduleBaseURL);
-            modulePath = url.getPath();
-
-            hostAddress = url.getProtocol() + "://" + url.getHost() + ":" + url.getPort(); //$NON-NLS-1$ //$NON-NLS-2$
-
-            String serializationPolicyFilePath = SerializationPolicyLoader
-                    .getSerializationPolicyFileName(modulePath + strongName);
-
-            InputStream is = new URL(hostAddress + serializationPolicyFilePath).openStream();
-
-            try {
-                if (is != null) {
-                    try {
-                        serializationPolicy = SerializationPolicyLoader.loadFromStream(is, null);
-                    } catch (ParseException e) {
-                        this.log("ERROR: Failed to parse the policy file '" + serializationPolicyFilePath + "'", e); 
-                    } catch (IOException e) {
-                        this.log("ERROR: Could not read the policy file '" + serializationPolicyFilePath + "'", e); 
-                    }
-                } else {
-                    String message = "ERROR: The serialization policy file '" + serializationPolicyFilePath 
-                            + "' was not found; did you forget to include it in this deployment?"; 
-                    this.log(message);
-                }
-            } finally {
-                if (is != null) {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        // Ignore this error
-                    }
-                }
-            }
-
-        } catch (MalformedURLException ex) {
-            this.log("Malformed moduleBaseURL: " + moduleBaseURL, ex); 
-        } catch (IOException ex) {
-            // Ignore this error
-        }
-        return serializationPolicy;
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        loadActions(config.getServletContext());
     }
 
-
-    private void loadActions() {
+    private void loadActions(ServletContext servletContext) {
         InputStream in = null;
         try {
-            in = this.getServletContext().getResourceAsStream("/WEB-INF/actions.properties"); //$NON-NLS-1$
-            Properties properties = new Properties();
+            in = servletContext.getResourceAsStream("/WEB-INF/actions.properties"); //$NON-NLS-1$
+            properties = new Properties();
             properties.load(in);
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            Enumeration<Object> services = properties.keys();
-            while (services.hasMoreElements()) {
-                String service = (String) services.nextElement();
-                Object action = Class.forName(properties.getProperty(service), false, classLoader).newInstance();
-                actions.put(service, action);
-            }
         } catch (Exception e) {
             // Ignore this error
         } finally {
@@ -117,44 +67,49 @@ public class ProxyGWTServiceImpl extends AbstractService {
     private synchronized Object getAction(String serviceIntfName) {
         Object action = actions.get(serviceIntfName);
         if (action == null){
-            loadActions();
+            try {
+                action = Class.forName(properties.getProperty(serviceIntfName), false,
+                        Thread.currentThread().getContextClassLoader()).newInstance();
+                actions.put(serviceIntfName, action);
+            } catch (Exception e) {
+                // Ignore this error
+            }
         }
-        return actions.get(serviceIntfName);
+        return action;
+    }
+
+    private String getServiceIntfName(String encodedRequest) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        ServerSerializationStreamReader streamReader = new ServerSerializationStreamReader(classLoader, this);
+        try {
+            streamReader.prepareToRead(encodedRequest);
+            return streamReader.readString();
+        } catch (SerializationException ex) {
+            throw new IncompatibleRemoteServiceException(ex.getMessage(), ex);
+        }
     }
 
     @Override
     protected String doProcessCall(String payload) throws SerializationException {
         try {
-            GWTRPCRequest gwtRpc = GWTRPC.decodeRequest(payload, this);
-            RPCRequest rpcRequest = gwtRpc.getRpcRequest();
-            onAfterRequestDeserialized(rpcRequest);
-
-            Object action = getAction(gwtRpc.getServiceIntfName());
+            String serviceIntfName = getServiceIntfName(payload);
+            Object action = getAction(serviceIntfName);
             if (action == null) {
-                throw new IncompatibleRemoteServiceException(gwtRpc.getServiceIntfName()
-                        + " undefined in actions.properties\nPlease check"); 
+                throw new IncompatibleRemoteServiceException(serviceIntfName + " undefined in actions.properties\nPlease check"); //$NON-NLS-1$
             }
-            Class<?> type = action.getClass();
-            if (!GWTRPC.implementsInterface(type, gwtRpc.getServiceIntfName())) {
-                // The service does not implement the requested interface
-                throw new IncompatibleRemoteServiceException(
-                        "Blocked attempt to access interface '" + gwtRpc.getServiceIntfName()
-                                + "', which is not implemented by '" + GWTRPC.printTypeName(type)
-                                + "'; this is either misconfiguration or a hack attempt");
-            }
-            if (("org.talend.mdm.webapp.general.client.GeneralService").equals(gwtRpc.getServiceIntfName())) { //$NON-NLS-1$
+
+            RPCRequest rpcRequest = RPC.decodeRequest(payload, action.getClass(), this);
+            onAfterRequestDeserialized(rpcRequest);
+            if (("org.talend.mdm.webapp.general.client.GeneralService").equals(serviceIntfName)) { //$NON-NLS-1$
                 HttpSession session = this.getThreadLocalRequest().getSession();
                 this.getServletContext().setAttribute(GwtWebContext.GENERALCONTEXT + session.getId(), session);
             }
             GwtWebContextFactory.set(new GwtWebContext(this.getThreadLocalRequest(), this.getThreadLocalResponse(), this));
-            return GWTRPC.invokeAndEncodeResponse(action, rpcRequest.getMethod(), rpcRequest.getParameters(), rpcRequest
-                    .getSerializationPolicy(), rpcRequest.getFlags());
-
+            return RPC.invokeAndEncodeResponse(action, rpcRequest.getMethod(), rpcRequest.getParameters(),
+                    rpcRequest.getSerializationPolicy(), rpcRequest.getFlags());
         } catch (IncompatibleRemoteServiceException ex) {
-            log("An IncompatibleRemoteServiceException was thrown while processing this call.", ex);
-            return GWTRPC.encodeResponseForFailure(null, ex);
+            log("An IncompatibleRemoteServiceException was thrown while processing this call.", ex); //$NON-NLS-1$
+            return RPC.encodeResponseForFailure(null, ex);
         }
     }
-
-
 }
