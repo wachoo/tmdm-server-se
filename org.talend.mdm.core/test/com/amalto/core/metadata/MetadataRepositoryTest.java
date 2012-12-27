ca56149f7f2658beb940f605ff5ef5646e1e94c6
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-27 07:29:26 +0000

    TMDM-3721:use MetadataRepository to parse xsd to fix the complex element name is same as pk bug
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_1@96005 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/test/com/amalto/core/metadata/MetadataRepositoryTest.java
+++ org.talend.mdm.core/test/com/amalto/core/metadata/MetadataRepositoryTest.java
@@ -14,6 +14,7 @@ package com.amalto.core.metadata;
 import junit.framework.TestCase;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Schema parsing <br>
@@ -174,5 +175,17 @@ public class MetadataRepositoryTest extends TestCase {
         TypeMetadata simpleType = repository.getNonInstantiableType("", "A");
         assertNotNull(simpleType);
         assertNotSame(entityType, simpleType);
+    }
+    
+    public void test18() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream stream = getClass().getResourceAsStream("schema18.xsd");
+        repository.load(stream);
+        
+        ComplexTypeMetadata type = repository.getComplexType("T18");
+        List<FieldMetadata> keyFields = type.getKeyFields();
+        assertEquals(1, keyFields.size());
+        assertEquals("subelement", keyFields.get(0).getName());
+
     }
 }
