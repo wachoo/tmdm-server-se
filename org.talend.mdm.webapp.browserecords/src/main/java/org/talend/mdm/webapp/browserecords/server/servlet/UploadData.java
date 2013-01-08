wachoo/tmdm-server-se

commit 78fa6ba98a0bcc1fb98c3755eb6f1611b9379dab

TMDM-3219：add return message and put item in transaction
git-svn-id: http://talendforge.org/svn/tom/trunk@81850 f6f1c999-d317-4740-80b0-e6d1abc6f99e

org.talend.mdm.webapp.browserecords/src/main/resources/org/talend/mdm/webapp/browserecords/client/i18n/BrowseRecordsMessages_fr.properties
@@ -156,7 +156,7 @@ label_field_encoding=Encodage
 label_button_submit=Valider
 error_incompatible_file_type=Le fichier de t\u00e9l\u00e9chargement est incompatible avec le type de fichier s\u00e9lectionn\u00e9.
 error_column_width=Please make sure your uploading file has the same column size with the view you selected,expect: {0} columns and {1} columns in the file at line {2}.
-error_column_header=Please make sure your uploading file has the same header with the view you selected,expect: {0} headers and match {1} headers in the file.
+error_column_header=La colonne ''{0}'' est inattendue, ce n''est pas un champ de l''entit\u00e8 ''{1}''.
 error_missing_mandatory_field=Des champs obligatoires sont manquants dans les donn\u00e9es t\u00e9l\u00e9charg\u00e9es
 import_progress_bar_title=Progression
 import_progress_bar_message=T\u00e9l\u00e9chargement du fichier, veuillez patienter...

org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/i18n/BrowseRecordsMessages.java
@@ -392,4 +392,6 @@ public interface BrowseRecordsMessages extends Messages {
     String fkinfo_display_lable();
     
     String fkinfo_display_type_lable();
+    
+    String error_column_header(String columnName,String concept);
 }

org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/CSVReader.java
@@ -151,7 +151,7 @@ public class CSVReader {
      */
     private String[] parseLine(String nextLine) throws IOException {
 
-        if (nextLine == null) {
+        if (nextLine == null || "".equals(nextLine)) { //$NON-NLS-1$
             return null;
         }
 

org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/UploadUtil.java
@@ -1,0 +1,75 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.webapp.browserecords.server.util;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class UploadUtil {
+    
+    public static Map<String,Boolean> getVisibleMap(String headerString){
+        Map<String,Boolean> visbleMap = new HashMap<String,Boolean>();
+        String fields[] = headerString.split("@"); //$NON-NLS-1$
+        for (int i=0;i<fields.length;i++){
+            visbleMap.put(getFieldName(fields[i]), getFieldVisible(fields[i]));
+        }
+        return visbleMap;       
+    }
+    
+    public static Set<String> chechMandatoryField(String mandatoryField, Set<String> fields) {
+
+        String[] mandatoryFields = mandatoryField.split("@"); //$NON-NLS-1$
+        Set<String> mandatorySet = new HashSet<String>();
+        for (String field : mandatoryFields)
+            mandatorySet.add(field);
+
+        for (String fieldValue : fields) {
+            String fieldName = getFieldName(fieldValue); //$NON-NLS-1$
+            if (mandatorySet.contains(fieldName))
+                mandatorySet.remove(fieldName);
+        }
+
+        return mandatorySet;
+    }
+    
+    public static String getFieldName(String fieldValue){
+        return fieldValue.split(":")[0]; //$NON-NLS-1$
+    }
+    
+    public static boolean getFieldVisible(String fieldValue){
+        return Boolean.valueOf(fieldValue.split(":")[1]); //$NON-NLS-1$
+    }
+    
+    public static String[] getDefaultHeader(String headerString){
+        List<String> headers = new LinkedList<String>();  
+        String fields[] = headerString.split("@"); //$NON-NLS-1$
+        for (int i=0;i<fields.length;i++){
+            headers.add(getFieldName(fields[i]));  
+        }
+        return headers.toArray(new String[headers.size()]);
+    }
+    
+    public static String getRootCause(Throwable throwable) {
+        String message = ""; //$NON-NLS-1$
+        Throwable currentCause = throwable;
+        while (currentCause != null) {           
+            message = currentCause.getMessage();
+            currentCause = currentCause.getCause();
+        }
+        return message;
+    }
+}

org.talend.mdm.webapp.browserecords/src/main/resources/org/talend/mdm/webapp/browserecords/client/i18n/BrowseRecordsMessages.properties
@@ -156,7 +156,7 @@ label_field_encoding=Encoding
 label_button_submit=Submit
 error_incompatible_file_type=The uploading file is incompatible with the file type you selected
 error_column_width=Please make sure your uploading file has the same column size with the view you selected,expect: {0} columns and {1} columns in the file at line {2}.
-error_column_header=Please make sure your uploading file has the same header with the view you selected,expect: {0} headers and match {1} headers in the file.
+error_column_header=Column ''{0}'' is unexpected, it is not a field of entity ''{1}''.
 error_missing_mandatory_field=Missing mandatory fields from uploaded data
 import_progress_bar_title=Progress
 import_progress_bar_message=Uploading your file, please wait...

org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/UploadUtilTest.java
@@ -1,0 +1,75 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.webapp.browserecords.server.util;
+
+import java.rmi.RemoteException;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+
+import junit.framework.TestCase;
+
+import org.talend.mdm.webapp.browserecords.server.util.UploadUtil;
+
+
+public class UploadUtilTest  extends TestCase {
+    
+    String header = "Id:true@Name:false:@Age:true"; //$NON-NLS-1$
+    String mandatoryField = "Id@Name@Age"; //$NON-NLS-1$
+    
+    public void testGetVisibleMap(){
+        Map<String,Boolean> headerMap = UploadUtil.getVisibleMap(header);
+        Boolean visible1 = headerMap.get("Id"); //$NON-NLS-1$
+        assertNotNull(visible1);
+        assertTrue(visible1);
+        Boolean visible2 = headerMap.get("Name"); //$NON-NLS-1$
+        assertNotNull(visible2);
+        assertFalse(visible2);
+        Boolean visible3 = headerMap.get("Age"); //$NON-NLS-1$
+        assertNotNull(visible3);
+        assertTrue(visible3);
+    }
+    
+    public void testChechMandatoryField(){
+        Set<String>  mandatorySet = UploadUtil.chechMandatoryField(mandatoryField, UploadUtil.getVisibleMap(header).keySet());
+        assertEquals(mandatorySet.size(), 0);
+    }
+    
+    public void testGetFieldName(){
+        ;
+        assertEquals(UploadUtil.getFieldName("Name:true"), "Name"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+  
+    public void testGetFieldVisible(){
+        assertTrue(UploadUtil.getFieldVisible("Name:true")); //$NON-NLS-1$
+    }    
+    
+    public void testGetDefaultHeader(String header){
+        UploadUtil.getDefaultHeader(header);
+        String[] headerArray = UploadUtil.getDefaultHeader(header);        
+        assertEquals(headerArray.length, 3);
+        assertEquals(headerArray[0], "Id"); //$NON-NLS-1$
+        assertEquals(headerArray[1], "Name"); //$NON-NLS-1$
+        assertEquals(headerArray[2], "Age"); //$NON-NLS-1$
+    }
+    
+    public void testGetRootCause(){
+        RemoteException romoteException = new RemoteException("RemoteException Cause"); //$NON-NLS-1$
+        ServletException servletException = new ServletException("ServletException Cause",romoteException); //$NON-NLS-1$
+        Exception exception = new Exception("Exception Cause",servletException); //$NON-NLS-1$
+        assertEquals(UploadUtil.getRootCause(exception),"RemoteException Cause"); //$NON-NLS-1$
+    }
+    
+
+}

commit 673d46a01cb5f5564b270414d2f969bf0e165108

merge r96488 from trunk to branch 5.1 TMDM-4539: Fix encoding issue introduced by r81850 from TMDM-3219
git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_1@96490 f6f1c999-d317-4740-80b0-e6d1abc6f99e

org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/servlet/UploadData.java
@@ -62,10 +62,6 @@ public class UploadData extends HttpServlet {
     private static final Messages MESSAGES = MessagesFactory.getMessages(
             "org.talend.mdm.webapp.browserecords.client.i18n.BrowseRecordsMessages", UploadData.class.getClassLoader()); //$NON-NLS-1$
 
-    private String language = "en"; // default//$NON-NLS-1$
-
-    private Locale locale = new Locale(language);
-
     private boolean cusExceptionFlag = false;
 
     public UploadData() {
@@ -92,12 +88,14 @@ public class UploadData extends HttpServlet {
         String header = ""; //$NON-NLS-1$
         String mandatoryField = ""; //$NON-NLS-1$
         String viewableXpath = ""; //$NON-NLS-1$
+        String language = "en"; //$NON-NLS-1$
 
         boolean headersOnFirstLine = false;
         int rowNumber = 0;
 
+        request.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
+        response.setCharacterEncoding("UTF-8"); //$NON-NLS-1$
         PrintWriter writer = response.getWriter();
-        request.setCharacterEncoding("UTF-8");//$NON-NLS-1$
 
         try {
             if (!FileUploadBase.isMultipartContent(request)) {
@@ -163,6 +161,7 @@ public class UploadData extends HttpServlet {
 
             concept = ViewHelper.getConceptFromDefaultViewName(viewPK);
 
+            Locale locale = new Locale(language);
             if (!UploadUtil.isViewableXpathValid(viewableXpath, concept)) {
                 throw new ServletException(MESSAGES.getMessage(locale, "error_invaild_field", concept)); //$NON-NLS-1$
             }
@@ -200,21 +199,21 @@ public class UploadData extends HttpServlet {
                     rowNumber++;
                     Row row = it.next();
                     if (rowNumber == 1 && headersOnFirstLine) {
-                        importHeader = getHeader(row, header, concept);
+                        importHeader = getHeader(row, header, concept, locale);
                         continue;
                     }
                     StringBuffer xml = new StringBuffer();
 
                     boolean allCellsEmpty = true;
-                    recordMap = new HashMap();                   
+                    recordMap = new HashMap();
                     xml.append("<" + concept + ">");//$NON-NLS-1$//$NON-NLS-2$
-                    
+
                     for (int i = 0; i < importHeader.length; i++) {
                         Cell tmpCell = row.getCell(i);
                         field = new StringBuffer();
-                        if (tmpCell != null) {                            
+                        if (tmpCell != null) {
                             field.append("<" + importHeader[i] + ">");//$NON-NLS-1$//$NON-NLS-2$
-                            
+
                             int cellType = tmpCell.getCellType();
                             switch (cellType) {
                             case Cell.CELL_TYPE_NUMERIC: {
@@ -265,21 +264,20 @@ public class UploadData extends HttpServlet {
                         } else {
                             field.append("<" + importHeader[i] + "/>"); //$NON-NLS-1$ //$NON-NLS-2$
                         }
-                        if (headersOnFirstLine){
-                            recordMap.put(importHeader[i], field.toString());  
-                        }else{
+                        if (headersOnFirstLine) {
+                            recordMap.put(importHeader[i], field.toString());
+                        } else {
                             xml.append(field.toString());
-                        }                                      
-                    }
-                    if (headersOnFirstLine)
-                    {
-                        for (int i=0;i<modelHeader.length;i++){
-                            if (recordMap.get(modelHeader[i]) != null){
-                                xml.append(recordMap.get(modelHeader[i]));
-                            }                            
                         }
-                    } 
-                    
+                    }
+                    if (headersOnFirstLine) {
+                        for (String element : modelHeader) {
+                            if (recordMap.get(element) != null) {
+                                xml.append(recordMap.get(element));
+                            }
+                        }
+                    }
+
                     xml.append("</" + concept + ">");//$NON-NLS-1$//$NON-NLS-2$
                     if (!allCellsEmpty) {
                         wSPutItemWithReportList.add(getWSPutItemWithReport(xml.toString(), language, concept,
@@ -297,7 +295,7 @@ public class UploadData extends HttpServlet {
                     rowNumber++;
                     String[] record = records.get(i);
                     if (rowNumber == 1 && headersOnFirstLine) {
-                        importHeader = getHeader(record, separator, header, concept);
+                        importHeader = getHeader(record, separator, header, concept, locale);
                         continue;
                     }
 
@@ -306,7 +304,7 @@ public class UploadData extends HttpServlet {
 
                     // build xml
                     recordMap = new HashMap();
-                    if (record.length > 0) {                              
+                    if (record.length > 0) {
                         for (int j = 0; j < importHeader.length; j++) {
                             field = new StringBuffer();
                             field.append("<" + importHeader[j] + ">");//$NON-NLS-1$//$NON-NLS-2$
@@ -314,21 +312,20 @@ public class UploadData extends HttpServlet {
                                 field.append(StringEscapeUtils.escapeXml(record[j]));
                             }
                             field.append("</" + importHeader[j] + ">");//$NON-NLS-1$//$NON-NLS-2$
-                            if (headersOnFirstLine){
-                                recordMap.put(importHeader[j], field.toString());  
-                            }else{
+                            if (headersOnFirstLine) {
+                                recordMap.put(importHeader[j], field.toString());
+                            } else {
                                 xml.append(field.toString());
-                            } 
+                            }
                         }
                     }
-                    if (headersOnFirstLine)
-                    {
-                        for (int k=0;k<modelHeader.length;k++){
-                            if (recordMap.get(modelHeader[k]) != null){
-                                xml.append(recordMap.get(modelHeader[k]));
-                            } 
+                    if (headersOnFirstLine) {
+                        for (String element : modelHeader) {
+                            if (recordMap.get(element) != null) {
+                                xml.append(recordMap.get(element));
+                            }
                         }
-                    } 
+                    }
                     xml.append("</" + concept + ">");//$NON-NLS-1$//$NON-NLS-2$                 
                     LOG.debug("Added line " + rowNumber + 1);//$NON-NLS-1$
                     LOG.trace("--val:\n" + xml);//$NON-NLS-1$
@@ -426,7 +423,7 @@ public class UploadData extends HttpServlet {
         return result;
     }
 
-    private String[] getHeader(Row headerRow, String headerString, String concept) throws ServletException {
+    private String[] getHeader(Row headerRow, String headerString, String concept, Locale locale) throws ServletException {
         List<String> headers = new LinkedList<String>();
         Iterator<Cell> iter = headerRow.cellIterator();
         while (iter.hasNext()) {
@@ -444,7 +441,7 @@ public class UploadData extends HttpServlet {
         return headers.toArray(new String[headers.size()]);
     }
 
-    private String[] getHeader(String[] headerRecord, char separator, String headerString, String concept)
+    private String[] getHeader(String[] headerRecord, char separator, String headerString, String concept, Locale locale)
             throws ServletException {
         for (String element : headerRecord) {
             String fieldName = element;

org.talend.mdm.webapp.browserecords/src/main/resources/org/talend/mdm/webapp/browserecords/client/i18n/BrowseRecordsMessages_fr.properties
@@ -165,7 +165,7 @@ label_field_encoding=Encodage
 label_button_submit=Valider
 error_incompatible_file_type=Le fichier de t\u00e9l\u00e9chargement est incompatible avec le type de fichier s\u00e9lectionn\u00e9.
 error_column_width=Please make sure your uploading file has the same column size with the view you selected,expect: {0} columns and {1} columns in the file at line {2}.
-error_column_header=La colonne ''{0}'' est inattendue, ce n''est pas un champ de l''entit\u00e8 ''{1}''.
+error_column_header=La colonne ''{0}'' est inattendue, ce n''est pas un champ de l''entit\u00e9 ''{1}''.
 error_missing_mandatory_field=Des champs obligatoires sont manquants dans les donn\u00e9es t\u00e9l\u00e9charg\u00e9es
 error_invaild_field=Certaines colonnes n''appartiennent pas \u00e0 l'entit\u00e9 ''{0}''
 import_progress_bar_title=Progression