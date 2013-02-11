2beb3990eddbc61010e602c3653c6118f394086c
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-11 03:05:00 +0000

    TMDM-5171: TOM mavenization + clean
    git-svn-id: http://talendforge.org/svn/tom/trunk@97987 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/NoAccessCheckUtil.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/NoAccessCheckUtil.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.mdm.webapp.browserecords.server.util;
 
 import java.io.ByteArrayInputStream;
@@ -5,11 +17,15 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.amalto.core.metadata.ComplexTypeMetadata;
 import com.amalto.core.metadata.MetadataRepository;
 
-
 public class NoAccessCheckUtil {
+
+    private static final Logger LOG = Logger.getLogger(NoAccessCheckUtil.class);
+
     public static boolean checkNoAccess(String modelXSD, String conceptName) {
         boolean result = false;
 
@@ -18,6 +34,9 @@ public class NoAccessCheckUtil {
             List<String> roleList = Arrays.asList(roles.split(",")); //$NON-NLS-1$
             result = checkNoAccessHelper(modelXSD, conceptName, roleList);
         } catch (Exception e) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(e.getMessage(), e);
+            }
         }
 
         return result;
@@ -39,6 +58,9 @@ public class NoAccessCheckUtil {
                 result = !noAccessRoles.isEmpty();
             }
         } catch (Exception e) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(e.getMessage(), e);
+            }
         }
 
         return result;
