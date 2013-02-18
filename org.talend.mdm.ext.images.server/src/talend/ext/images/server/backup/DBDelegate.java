85d2dba72ec83e1eb73dbe322bcda9da2212e961
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-18 09:42:06 +0000

    TMDM-5310: MDM Images storage uses XQuery Removes unused method.
    git-svn-id: http://talendforge.org/svn/tom/trunk@98161 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.ext.images.server/src/talend/ext/images/server/backup/DBDelegate.java
+++ org.talend.mdm.ext.images.server/src/talend/ext/images/server/backup/DBDelegate.java
@@ -1,17 +1,11 @@
 package talend.ext.images.server.backup;
 
-import com.amalto.core.util.XtentisException;
-
 public interface DBDelegate {
-	
-	//public boolean putResource(ResourcePK resourcePK,byte[] content) throws DBDelegateException;
 	
 	public boolean putResource(ResourcePK resourcePK,String fileName);
 	
 	public byte[] getResource(ResourcePK resourcePK);
 	
 	public boolean deleteResource(ResourcePK resourcePK);
-	
-    public String findResourceURI(String imageId) throws XtentisException;
 
 }
