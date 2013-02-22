09c80d34a7d353add3d00d65e5baef47b293d992
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-22 10:01:55 +0000

    merge r98557 from trunk to branch 5.2 TMDM-5365: Fix incorrect "force pivot" detection.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@98558 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/util/WhereConditionForcePivotFilter.java
+++ org.talend.mdm.core/src/com/amalto/core/util/WhereConditionForcePivotFilter.java
@@ -4,34 +4,38 @@ import java.util.Map;
 
 import com.amalto.xmlserver.interfaces.WhereCondition;
 
-public class WhereConditionForcePivotFilter extends WhereConditionFilter{
-	
-	
-	public static final String FORCE_PIVOT="FORCE_PIVOT";
+public class WhereConditionForcePivotFilter extends WhereConditionFilter {
 
-	public WhereConditionForcePivotFilter(Map context) {
-		super(context);
-	}
+    public static final String FORCE_PIVOT = "FORCE_PIVOT"; //$NON-NLS-1$
 
-	@Override
-	public void doFilter(WhereCondition wc) {
-		if(super.context.get(FORCE_PIVOT)==null)return;
-		if(wc.getLeftPath()==null||wc.getLeftPath().length()==0)return;
-		
-		String forcePivot= (String) super.context.get(FORCE_PIVOT);
-		String leftPath = wc.getLeftPath();
-		
-		if(!forcePivot.equals("/")&&!forcePivot.equals("//")){
-			if(forcePivot.startsWith("/"))forcePivot=forcePivot.substring(1);
-			if(forcePivot.startsWith("/"))forcePivot=forcePivot.substring(1);
-		}
-		
-		if(!leftPath.startsWith(forcePivot)){
-			//it is partial
-			leftPath=forcePivot+"/"+leftPath;
-			wc.setLeftPath(leftPath);
-		}
-		
-	}
+    public WhereConditionForcePivotFilter(Map context) {
+        super(context);
+    }
+
+    @Override
+    public void doFilter(WhereCondition wc) {
+        if (super.context.get(FORCE_PIVOT) == null) {
+            return;
+        }
+        if (wc.getLeftPath() == null || wc.getLeftPath().length() == 0) {
+            return;
+        }
+        String forcePivot = (String) context.get(FORCE_PIVOT);
+        String leftPath = wc.getLeftPath();
+        if (!forcePivot.equals("/") && !forcePivot.equals("//")) { //$NON-NLS-1$ //$NON-NLS-2$
+            if (forcePivot.startsWith("/")) { //$NON-NLS-1$
+                forcePivot = forcePivot.substring(1);
+            }
+            if (forcePivot.startsWith("/")) { //$NON-NLS-1$
+                forcePivot = forcePivot.substring(1);
+            }
+        }
+        // TMDM-5365 fix: ensure "EntityField" gets transformed into "Entity/EntityField" when entity name is in field name's start.
+        if (!leftPath.startsWith(forcePivot + "/")) { //$NON-NLS-1$
+            //it is partial
+            leftPath = forcePivot + "/" + leftPath; //$NON-NLS-1$
+            wc.setLeftPath(leftPath);
+        }
+    }
 
 }
