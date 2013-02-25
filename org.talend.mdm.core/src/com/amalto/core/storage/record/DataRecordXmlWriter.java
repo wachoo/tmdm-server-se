fa8f380562689b9277564b4274d838755eb202a0
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-25 05:48:40 +0000

    merge r98630 from trunk to branch 5.2 TMDM-5209 : auto-increment pk record should not be replaced from staging to master
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@98631 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/storage/record/DataRecordXmlWriter.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/record/DataRecordXmlWriter.java
@@ -28,6 +28,8 @@ public class DataRecordXmlWriter implements DataRecordWriter {
 
     private ComplexTypeMetadata type;
 
+    private OverrideValue override;
+
     public DataRecordXmlWriter() {
         rootElementName = null;
     }
@@ -41,13 +43,18 @@ public class DataRecordXmlWriter implements DataRecordWriter {
         this.rootElementName = type.getName();
     }
 
+    public DataRecordXmlWriter(OverrideValue override) {
+        this();
+        this.override = override;
+    }
+
     public void write(DataRecord record, OutputStream output) throws IOException {
         Writer out = new BufferedWriter(new OutputStreamWriter(output, "UTF-8")); //$NON-NLS-1$
         write(record, out);
     }
 
     public void write(DataRecord record, Writer writer) throws IOException {
-        DefaultMetadataVisitor<Void> fieldPrinter = new FieldPrinter(record, writer);
+        DefaultMetadataVisitor<Void> fieldPrinter = new FieldPrinter(record, writer, override);
         List<FieldMetadata> fields = type == null ? record.getType().getFields() : type.getFields();
         writer.write("<" + getRootElementName(record) + ">"); //$NON-NLS-1$ //$NON-NLS-2$
         for (FieldMetadata field : fields) {
@@ -61,15 +68,27 @@ public class DataRecordXmlWriter implements DataRecordWriter {
         return rootElementName == null ? record.getType().getName() : rootElementName;
     }
 
+    public interface OverrideValue {
+
+        public Object overrideValue(DataRecord record, SimpleTypeFieldMetadata simpleField, Object originalValue);
+    }
+
     private static class FieldPrinter extends DefaultMetadataVisitor<Void> {
 
         private final DataRecord record;
 
         private final Writer out;
 
+        private OverrideValue override;
+
         public FieldPrinter(DataRecord record, Writer out) {
             this.record = record;
             this.out = out;
+        }
+
+        public FieldPrinter(DataRecord record, Writer out, OverrideValue override) {
+            this(record, out);
+            this.override = override;
         }
 
         @Override
@@ -153,6 +172,9 @@ public class DataRecordXmlWriter implements DataRecordWriter {
         public Void visit(SimpleTypeFieldMetadata simpleField) {
             try {
                 Object value = record.get(simpleField);
+                if (override != null) {
+                    value = override.overrideValue(record, simpleField, value);
+                }
                 if (value != null) {
                     if (!simpleField.isMany()) {
                         out.write("<" + simpleField.getName() + ">"); //$NON-NLS-1$ //$NON-NLS-2$
