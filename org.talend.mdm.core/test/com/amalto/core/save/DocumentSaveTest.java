b7fe438c49eaf582b37d14108ef0be6be994012a
pliang <pliang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-27 10:41:15 +0000

    TMDM-5383: ForeignKey value can not be changed.
    git-svn-id: http://talendforge.org/svn/tom/trunk@98828 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/test/com/amalto/core/save/DocumentSaveTest.java
+++ org.talend.mdm.core/test/com/amalto/core/save/DocumentSaveTest.java
@@ -1957,6 +1957,28 @@ public class DocumentSaveTest extends TestCase {
         assertEquals("", evaluate(committedElement, "/Product/Features/Colors/Color[2]"));
     }
     
+    public void testProductFamilyUpdate() throws Exception {
+        final MetadataRepository repository = new MetadataRepository();
+        repository.load(DocumentSaveTest.class.getResourceAsStream("metadata1.xsd"));
+
+        SaverSource source = new TestSaverSource(repository, true, "test54_original.xml", "metadata1.xsd");
+
+        SaverSession session = SaverSession.newSession(source);
+        InputStream recordXml = DocumentSaveTest.class.getResourceAsStream("test54.xml");
+        DocumentSaverContext context = session.getContextFactory().create("MDM", "DStar", "Source", recordXml, false, true, true,
+                false);
+        DocumentSaver saver = context.createSaver();
+        saver.save(session, context);
+        MockCommitter committer = new MockCommitter();
+        session.end(committer);
+
+        assertTrue(committer.hasSaved());
+        Element committedElement = committer.getCommittedElement();
+        assertEquals("Description", evaluate(committedElement, "/Product/Description"));
+        assertEquals("60", evaluate(committedElement, "/Product/Price"));
+        assertEquals("[2]", evaluate(committedElement, "/Product/Family"));
+    }
+    
     private static class MockCommitter implements SaverSession.Committer {
 
         private Element committedElement;
