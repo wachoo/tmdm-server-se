3e861faf54aa6982a5e2ee6c7371818b894aeb60
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-12 04:56:08 +0000

    merge r99334 from trunk to branch 5.2 TMDM-5409 : clean cache when delete items by conditions
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@99385 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/ejb/ItemCtrl2Bean.java
+++ org.talend.mdm.core/src/com/amalto/core/ejb/ItemCtrl2Bean.java
@@ -368,7 +368,11 @@ public class ItemCtrl2Bean implements SessionBean {
 
         XmlServerSLWrapperLocal server = Util.getXmlServerCtrlLocal();
         try {
-            return server.deleteItems(patternsToRevisionID, patternsToClusterName, conceptName, search);
+            int count = server.deleteItems(patternsToRevisionID, patternsToClusterName, conceptName, search);
+            if (count > 0){
+                ItemPOJO.clearCache();
+            }
+            return count;
         } catch (XtentisException e) {
             throw (e);
         } catch (Exception e) {
