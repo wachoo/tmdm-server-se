f597302d5cda1e2bef72ea75cd5d4ccccb4e8270
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-14 14:21:12 +0000

    TMDM-5109: Fixes implementation validation rules (incorrect line numbers, missing isMany() check for PK info).  Adds first unit test for models validation.  Cleaner separation validation and post XSD-parsing resolution of dangling references.
    git-svn-id: http://talendforge.org/svn/tom/trunk@99606 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.core/test/com/amalto/core/metadata/validation/MetadataValidationTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package com.amalto.core.metadata.validation;
+
+import junit.framework.TestCase;
+import org.talend.mdm.commmon.metadata.DefaultValidationHandler;
+import org.talend.mdm.commmon.metadata.MetadataRepository;
+
+import java.io.InputStream;
+
+public class MetadataValidationTest extends TestCase {
+
+    public void testFK1() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FK_0.1.xsd");
+        DefaultValidationHandler handler = new DefaultValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(2, handler.getMessages().size());
+    }
+
+    public void testPK1() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("PK1_0.1.xsd");
+        DefaultValidationHandler handler = new DefaultValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getMessages().size());
+    }
+
+    public void testPK2() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("PK2_0.1.xsd");
+        DefaultValidationHandler handler = new DefaultValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getMessages().size());
+    }
+
+    public void testPK3() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("PK3_0.1.xsd");
+        DefaultValidationHandler handler = new DefaultValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getMessages().size());
+    }
+
+    public void testPK4() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("PK4_0.1.xsd");
+        DefaultValidationHandler handler = new DefaultValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getMessages().size());
+    }
+}
