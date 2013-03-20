3ae7445a2585cd7b8fce1bfe008febe0b21e816f
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-20 12:07:51 +0000

    Ease GWT debugging by supporting multiple modules in same debug session
    git-svn-id: http://talendforge.org/svn/tom/trunk@99875 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.recyclebin/src/main/java/org/talend/mdm/webapp/recyclebin/client/RecycleBin.java
+++ org.talend.mdm.webapp.recyclebin/src/main/java/org/talend/mdm/webapp/recyclebin/client/RecycleBin.java
@@ -18,10 +18,14 @@ import com.allen_sauer.gwt.log.client.Log;
 import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.core.XDOM;
 import com.extjs.gxt.ui.client.mvc.Dispatcher;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -32,12 +36,13 @@ public class RecycleBin implements EntryPoint {
      * Create a remote service proxy to talk to the server-side RecycleBin service.
      */
     public static final String RECYCLEBIN_SERVICE = "RecycleBinService"; //$NON-NLS-1$  
-    
+
     public static final String RECYCLEBIN_ID = "Recycle Bin"; //$NON-NLS-1$  
 
     /**
      * This is the entry point method.
      */
+    @Override
     public void onModuleLoad() {
         XDOM.setAutoIdPrefix(GWT.getModuleName() + "-" + XDOM.getAutoIdPrefix()); //$NON-NLS-1$
         registerPubService();
@@ -54,25 +59,27 @@ public class RecycleBin implements EntryPoint {
     private native void registerPubService()/*-{
         var instance = this;
         $wnd.amalto.recyclebin = {};
-        $wnd.amalto.recyclebin.RecycleBin = function(){
+        $wnd.amalto.recyclebin.RecycleBin = function() {
 
-        function initUI(){
-        instance.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::initUI()();
-        }
+            function initUI() {
+                instance.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::initUI()();
+            }
 
-        return {
-        init : function(){initUI();}
-        }
+            return {
+                init : function() {
+                    initUI();
+                }
+            }
         }();
     }-*/;
 
     private native void _initUI()/*-{
         var tabPanel = $wnd.amalto.core.getTabPanel();
-        var panel = tabPanel.getItem("Recycle Bin"); 
-        if (panel == undefined){
-        @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::generateContentPanel()();
-        panel = this.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::createPanel()();
-        tabPanel.add(panel);
+        var panel = tabPanel.getItem("Recycle Bin");
+        if (panel == undefined) {
+            @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::generateContentPanel()();
+            panel = this.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::createPanel()();
+            tabPanel.add(panel);
         }
         tabPanel.setSelection(panel.getItemId());
     }-*/;
@@ -80,38 +87,58 @@ public class RecycleBin implements EntryPoint {
     native JavaScriptObject createPanel()/*-{
         var instance = this;
         var panel = {
-        render : function(el){
-        instance.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::renderContent(Ljava/lang/String;)(el.id);
-        },
-        setSize : function(width, height){
-        var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
-        cp.@com.extjs.gxt.ui.client.widget.ContentPanel::setSize(II)(width, height);
-        },
-        getItemId : function(){
-        var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
-        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getItemId()();
-        },
-        getEl : function(){
-        var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
-        var el = cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getElement()();
-        return {dom : el};
-        },
-        doLayout : function(){
-        var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
-        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::doLayout()();
-        },
-        title : function(){
-        var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
-        return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getHeading()();
-        }
+            render : function(el) {
+                instance.@org.talend.mdm.webapp.recyclebin.client.RecycleBin::renderContent(Ljava/lang/String;)(el.id);
+            },
+            setSize : function(width, height) {
+                var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
+                cp.@com.extjs.gxt.ui.client.widget.ContentPanel::setSize(II)(width, height);
+            },
+            getItemId : function() {
+                var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
+                return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getItemId()();
+            },
+            getEl : function() {
+                var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
+                var el = cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getElement()();
+                return {
+                    dom : el
+                };
+            },
+            doLayout : function() {
+                var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
+                return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::doLayout()();
+            },
+            title : function() {
+                var cp = @org.talend.mdm.webapp.recyclebin.client.GenerateContainer::getContentPanel()();
+                return cp.@com.extjs.gxt.ui.client.widget.ContentPanel::getHeading()();
+            }
         };
         return panel;
     }-*/;
 
     public void renderContent(final String contentId) {
         onModuleRender();
-        RootPanel panel = RootPanel.get(contentId);
-        panel.add(GenerateContainer.getContentPanel());
+
+        final ContentPanel content = GenerateContainer.getContentPanel();
+
+        if (GWT.isScript()) {
+            RootPanel panel = RootPanel.get(contentId);
+            panel.add(content);
+        } else {
+            final Element element = DOM.getElementById(contentId);
+            SimplePanel panel = new SimplePanel() {
+
+                @Override
+                protected void setElement(Element elem) {
+                    super.setElement(element);
+                }
+            };
+            RootPanel rootPanel = RootPanel.get();
+            rootPanel.clear();
+            rootPanel.add(panel);
+            panel.add(content);
+        }
     }
 
     public void initUI() {
