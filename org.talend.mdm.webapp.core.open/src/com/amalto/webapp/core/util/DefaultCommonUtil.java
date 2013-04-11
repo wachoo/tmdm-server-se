28517acde48a93d51020652dfa618ede8913f792
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-11 09:39:08 +0000

    TMDM-5560 : Reflect real license for login page
    git-svn-id: http://talendforge.org/svn/tom/trunk@101017 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.core.open/src/com/amalto/webapp/core/util/DefaultCommonUtil.java
+++ org.talend.mdm.webapp.core.open/src/com/amalto/webapp/core/util/DefaultCommonUtil.java
@@ -1,9 +1,12 @@
 package com.amalto.webapp.core.util;
 
 public class DefaultCommonUtil {
-	
-	public static String getEditionTitle() {
-		return "Community Edition";
-	}
 
+    public static String getEditionTitle() {
+        return "Community Edition";
+    }
+
+    public static String getProductInfo() {
+        return "Community Edition";
+    }
 }
