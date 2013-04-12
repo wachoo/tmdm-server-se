5a5b7c4eea366b94072eb857bbb0aefe1bce7c16
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-12 01:31:28 +0000

    TMDM-5559: Use consistent buttons icons (up/down instead of right/left). Use of a lighter green color.
    git-svn-id: http://talendforge.org/svn/tom/trunk@101051 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/resources/icon/Icons.java
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/resources/icon/Icons.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -17,33 +17,33 @@ import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
 /**
- * DOC Administrator  class global comment. Detailled comment
+ * DOC Administrator class global comment. Detailled comment
  */
 public interface Icons extends ClientBundle {
-    
+
     Icons INSTANCE = GWT.create(Icons.class);
 
     @Source("table.png")
     ImageResource table();
-    
+
     @Source("time.png")
     ImageResource time();
-    
+
     @Source("browse.png")
     ImageResource browse();
-    
+
     @Source("leaf.gif")
     ImageResource leaf();
-    
+
     @Source("restore.png")
     ImageResource restore();
-    
+
     @Source("view.png")
     ImageResource view();
-    
-    @Source("prev.gif")
-    ImageResource prev();
-    
-    @Source("next.gif")
-    ImageResource next();
+
+    @Source("up.png")
+    ImageResource up();
+
+    @Source("down.png")
+    ImageResource down();
 }
--- org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/widget/JournalComparisonPanel.java
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/widget/JournalComparisonPanel.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -49,20 +49,20 @@ import com.extjs.gxt.ui.client.widget.treepanel.TreePanelView.TreeViewRenderMode
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 
 /**
- * DOC Administrator  class global comment. Detailled comment
+ * DOC Administrator class global comment. Detailled comment
  */
 public class JournalComparisonPanel extends ContentPanel {
 
     private JournalServiceAsync service = Registry.get(Journal.JOURNAL_SERVICE);
-    
+
     private ToolBar toolbar;
-    
+
     private TreePanel<JournalTreeModel> tree;
-    
+
     private TreePanelView<JournalTreeModel> view;
-    
+
     private JournalTreeModel root;
-    
+
     private JournalComparisonPanel otherPanel;
 
     private Map<String, JournalTreeModel> modelMap = new HashMap<String, JournalTreeModel>();
@@ -73,41 +73,43 @@ public class JournalComparisonPanel extends ContentPanel {
 
     private Button nextChangeButton;
 
-   public JournalComparisonPanel(String title, final JournalParameters parameter,final JournalGridModel journalGridModel,final boolean isBeforePanel) {
+    public JournalComparisonPanel(String title, final JournalParameters parameter, final JournalGridModel journalGridModel,
+            final boolean isBeforePanel) {
         this.setFrame(false);
         this.setHeading(title);
         this.setLayout(new FitLayout());
         this.setBodyBorder(false);
-        
-        service.getComparisionTree(parameter,UrlUtil.getLanguage(), new SessionAwareAsyncCallback<JournalTreeModel>() {
-            
+
+        service.getComparisionTree(parameter, UrlUtil.getLanguage(), new SessionAwareAsyncCallback<JournalTreeModel>() {
+
+            @Override
             public void onSuccess(JournalTreeModel root) {
                 JournalComparisonPanel.this.root = root;
                 TreeStore<JournalTreeModel> store = new TreeStore<JournalTreeModel>();
                 store.add(root, true);
 
                 view = new TreePanelView<JournalTreeModel>() {
-                    
+
                     @Override
-                    public String getTemplate(ModelData m, String id, String text, AbstractImagePrototype icon, boolean checkable,
-                            boolean checked, Joint joint, int level, TreeViewRenderMode renderMode) {
+                    public String getTemplate(ModelData m, String id, String text, AbstractImagePrototype icon,
+                            boolean checkable, boolean checked, Joint joint, int level, TreeViewRenderMode renderMode) {
                         // see TMDM-5438
-                        if ("Document".equals(text) && level == 0 && ((JournalTreeModel)m).getChildCount() == 0) { //$NON-NLS-1$
-                            return super.getTemplate(m, id, text, GXT.IMAGES.tree_folder(), checkable, checked, joint, level, renderMode);
+                        if ("Document".equals(text) && level == 0 && ((JournalTreeModel) m).getChildCount() == 0) { //$NON-NLS-1$
+                            return super.getTemplate(m, id, text, GXT.IMAGES.tree_folder(), checkable, checked, joint, level,
+                                    renderMode);
                         } else {
                             return super.getTemplate(m, id, text, icon, checkable, checked, joint, level, renderMode);
-                        }                        
-                    }                                      
+                        }
+                    }
                 };
-                
-                
-                tree = new TreePanel<JournalTreeModel>(store){
+
+                tree = new TreePanel<JournalTreeModel>(store) {
 
                     @Override
                     protected String renderChild(JournalTreeModel parent, JournalTreeModel child, int depth,
                             TreeViewRenderMode renderMode) {
                         String nodeStr = super.renderChild(parent, child, depth, renderMode);
-                        if(child.get("cls") != null){ //$NON-NLS-1$
+                        if (child.get("cls") != null) { //$NON-NLS-1$
                             nodeStr = nodeStr.replaceFirst("x-tree3-node", "x-tree3-node " + child.get("cls")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
                         }
                         return nodeStr;
@@ -119,31 +121,33 @@ public class JournalComparisonPanel extends ContentPanel {
 
                 if (modelMap.size() == 0) {
                     List<JournalTreeModel> list = store.getAllItems();
-                    for(JournalTreeModel model : list) {
+                    for (JournalTreeModel model : list) {
                         modelMap.put(model.getPath(), model);
                     }
                 }
 
                 tree.addListener(Events.Expand, new Listener<TreePanelEvent<JournalTreeModel>>() {
 
+                    @Override
                     public void handleEvent(TreePanelEvent<JournalTreeModel> be) {
-                        if(otherPanel.getModelMap().size() > 0){
+                        if (otherPanel.getModelMap().size() > 0) {
                             JournalTreeModel model = otherPanel.getModelMap().get(be.getItem().getPath());
                             otherPanel.getTree().setExpanded(model, true);
-                        }                       
+                        }
                     }
                 });
 
                 tree.addListener(Events.Collapse, new Listener<TreePanelEvent<JournalTreeModel>>() {
 
+                    @Override
                     public void handleEvent(TreePanelEvent<JournalTreeModel> be) {
-                        if(otherPanel.getModelMap().size() > 0){
+                        if (otherPanel.getModelMap().size() > 0) {
                             JournalTreeModel model = otherPanel.getModelMap().get(be.getItem().getPath());
                             otherPanel.getTree().setExpanded(model, false);
-                        } 
+                        }
                     }
                 });
-       
+
                 JournalComparisonPanel.this.add(tree);
                 JournalComparisonPanel.this.layout(true);
                 JournalComparisonPanel.this.expandRoot();
@@ -154,96 +158,102 @@ public class JournalComparisonPanel extends ContentPanel {
         });
 
         if (UpdateReportPOJO.OPERATION_TYPE_UPDATE.equals(journalGridModel.getOperationType())) {
-            
+
             this.changeNodeList = journalGridModel.getChangeNodeList();
-            
+
             final int count[] = new int[1];
             count[0] = -1;
-            
+
             toolbar = new ToolBar();
-            
+
             if (isBeforePanel) {
                 previousChangeButton = new Button(MessagesFactory.getMessages().previous_change_button());
-                previousChangeButton.setIcon(AbstractImagePrototype.create(Icons.INSTANCE.prev()));            
+                previousChangeButton.setIcon(AbstractImagePrototype.create(Icons.INSTANCE.up()));
                 previousChangeButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+
+                    @Override
                     public void componentSelected(ButtonEvent ce) {
-                        if (count[0] > 0){
+                        if (count[0] > 0) {
                             count[0] = count[0] - 1;
-                        }                    
-                        buttonStatus(count[0]);
-                        selectTreeNodeByPath(changeNodeList.get(count[0]));
-                        otherPanel.selectTreeNodeByPath(changeNodeList.get(count[0]));
-                    }           
-                });
-                previousChangeButton.setEnabled(false);                
-                                     
-                nextChangeButton = new Button(MessagesFactory.getMessages().next_change_button());
-                nextChangeButton.setIcon(AbstractImagePrototype.create(Icons.INSTANCE.next()));
-                nextChangeButton.setIconAlign(IconAlign.RIGHT);
-                nextChangeButton.addSelectionListener(new SelectionListener<ButtonEvent>() {                
-                    public void componentSelected(ButtonEvent ce) {
-                        if (count[0] < changeNodeList.size()-1){
-                            count[0] = count[0] + 1;
-                        }                   
+                        }
                         buttonStatus(count[0]);
                         selectTreeNodeByPath(changeNodeList.get(count[0]));
                         otherPanel.selectTreeNodeByPath(changeNodeList.get(count[0]));
                     }
-                });            
-                nextChangeButton.setEnabled(true);                
+                });
+                previousChangeButton.setEnabled(false);
+
+                nextChangeButton = new Button(MessagesFactory.getMessages().next_change_button());
+                nextChangeButton.setIcon(AbstractImagePrototype.create(Icons.INSTANCE.down()));
+                nextChangeButton.setIconAlign(IconAlign.RIGHT);
+                nextChangeButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+
+                    @Override
+                    public void componentSelected(ButtonEvent ce) {
+                        if (count[0] < changeNodeList.size() - 1) {
+                            count[0] = count[0] + 1;
+                        }
+                        buttonStatus(count[0]);
+                        selectTreeNodeByPath(changeNodeList.get(count[0]));
+                        otherPanel.selectTreeNodeByPath(changeNodeList.get(count[0]));
+                    }
+                });
+                nextChangeButton.setEnabled(true);
             } else {
                 previousChangeButton = new Button();
                 previousChangeButton.setEnabled(false);
                 nextChangeButton = new Button();
                 nextChangeButton.setEnabled(false);
             }
-            
+
             toolbar.add(previousChangeButton);
             toolbar.add(nextChangeButton);
             toolbar.add(new FillToolItem());
-            
+
             service.isAdmin(new SessionAwareAsyncCallback<Boolean>() {
 
+                @Override
                 public void onSuccess(Boolean isAdmin) {
-                    if (isAdmin){
+                    if (isAdmin) {
                         Button restoreButton = new Button(MessagesFactory.getMessages().restore_button());
                         restoreButton.setIcon(AbstractImagePrototype.create(Icons.INSTANCE.restore()));
                         restoreButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
-                            
+
                             @Override
                             public void componentSelected(ButtonEvent ce) {
-                                service.restoreRecord(parameter,UrlUtil.getLanguage(), new SessionAwareAsyncCallback<Boolean>() {
-                                    
+                                service.restoreRecord(parameter, UrlUtil.getLanguage(), new SessionAwareAsyncCallback<Boolean>() {
+
+                                    @Override
                                     public void onSuccess(Boolean success) {
-                                        if(success) {
+                                        if (success) {
                                             JournalComparisonPanel.this.closeTabPanel();
                                         }
                                     }
                                 });
                             };
                         });
-                        restoreButton.setEnabled(parameter.isAuth());                            
+                        restoreButton.setEnabled(parameter.isAuth());
                         toolbar.add(restoreButton);
                     }
                 }
             });
-            
+
             this.setTopComponent(toolbar);
-        }                     
+        }
     }
-   
-   private void buttonStatus(int index) {
-       if (index == 0) {
-           previousChangeButton.setEnabled(false);
-       } else {
-           previousChangeButton.setEnabled(true);
-       } 
-       if (index == changeNodeList.size()-1) {
-           nextChangeButton.setEnabled(false);
-       } else {
-           nextChangeButton.setEnabled(true);
-       }  
-   }
+
+    private void buttonStatus(int index) {
+        if (index == 0) {
+            previousChangeButton.setEnabled(false);
+        } else {
+            previousChangeButton.setEnabled(true);
+        }
+        if (index == changeNodeList.size() - 1) {
+            nextChangeButton.setEnabled(false);
+        } else {
+            nextChangeButton.setEnabled(true);
+        }
+    }
 
     public void expandRoot() {
         if (root.getChildren().size() > 0) {
@@ -252,13 +262,13 @@ public class JournalComparisonPanel extends ContentPanel {
             tree.setExpanded(model, true);
         }
     }
-    
+
     public void selectTreeNodeByPath(String path) {
         JournalTreeModel model = modelMap.get(path);
         if (path.endsWith("[1]") && model == null) { //$NON-NLS-1$
             model = modelMap.get(path.replace("[1]", "")); //$NON-NLS-1$ //$NON-NLS-2$
-        }        
-        tree.getSelectionModel().select(false, model);            
+        }
+        tree.getSelectionModel().select(false, model);
     }
 
     public TreePanel<JournalTreeModel> getTree() {
