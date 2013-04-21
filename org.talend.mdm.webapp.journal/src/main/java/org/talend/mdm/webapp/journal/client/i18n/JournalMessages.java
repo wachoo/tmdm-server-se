8a2e637e2592ba71846e442ed2b1ab71b9a4782d
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-21 04:16:21 +0000

    TMDM-4058: Fix compilation error
    git-svn-id: http://talendforge.org/svn/tom/trunk@101628 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/i18n/JournalMessages.java
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/i18n/JournalMessages.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -15,77 +15,75 @@ package org.talend.mdm.webapp.journal.client.i18n;
 import com.google.gwt.i18n.client.Messages;
 
 /**
- * DOC Administrator  class global comment. Detailled comment
+ * DOC Administrator class global comment. Detailled comment
  */
 public interface JournalMessages extends Messages {
 
     public String journal_title();
-    
+
     public String search_panel_title();
-    
+
     public String entity_label();
-    
+
     public String key_label();
-    
+
     public String source_label();
-    
+
     public String operation_type_label();
-    
+
     public String start_date_label();
-    
+
     public String end_date_label();
-    
+
     public String reset_button();
-    
+
     public String search_button();
-    
+
     public String exprot_excel_button();
-    
+
     public String results_tab();
-    
+
     public String timeline_tab();
-    
+
     public String data_container_label();
-    
+
     public String data_model_label();
-    
+
     public String revision_id_label();
-    
+
     public String operation_time_label();
-    
+
     public String user_name_label();
-    
+
     public String update_report_detail_label();
-    
+
     public String open_record_button();
-    
+
     public String before_label();
-    
+
     public String after_label();
-    
+
     public String restore_button();
-    
+
     public String data_change_viewer();
-    
+
     public String journal_label();
-    
+
     public String error_level();
-    
+
     public String select_contain_model_msg();
-    
-    public String record_not_found_msg();    
-    
+
     public String view_updatereport_button();
-    
+
     public String updatereport_label();
-    
+
     public String change_properties();
-    
+
     public String previous_change_button();
-    
+
     public String next_change_button();
-    
+
     public String warning_title();
-    
+
     public String search_date_error_message();
 }
