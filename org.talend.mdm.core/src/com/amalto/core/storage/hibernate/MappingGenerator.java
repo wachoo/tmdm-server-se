197549cbac7904ceccb9d4fc8e80b25dd6481f7b
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-29 13:52:42 +0000

    TMDM-5677: Fixes typo issue relative to MULTI_LINGUAL (missing "_").
    git-svn-id: http://talendforge.org/svn/tom/trunk@102065 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/storage/hibernate/MappingGenerator.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/hibernate/MappingGenerator.java
@@ -664,7 +664,7 @@ public class MappingGenerator extends DefaultMetadataVisitor<Element> {
             Object sqlType = field.getData("SQL_TYPE"); //$NON-NLS-1$
             if (sqlType != null) {
                 elementTypeName = String.valueOf(sqlType);
-            } else if("MULTILINGUAL".equalsIgnoreCase(fieldType.getName())) { //$NON-NLS-1$
+            } else if("MULTI_LINGUAL".equalsIgnoreCase(fieldType.getName())) { //$NON-NLS-1$
                 elementTypeName = TEXT_TYPE_NAME;
             } else if (field.getType().getData(MetadataRepository.DATA_MAX_LENGTH) != null) {
                 Object maxLength = field.getType().getData(MetadataRepository.DATA_MAX_LENGTH);
