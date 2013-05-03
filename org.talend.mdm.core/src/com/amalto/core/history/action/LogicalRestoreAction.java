269720a1911a0518307c994810de91e8c709097e
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-03 07:52:47 +0000

    merge r102171 from trunk to branch 5.2 TMDM-3497: Fix support for RESTORE operation in document's history.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@102206 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.core/src/com/amalto/core/history/action/LogicalRestoreAction.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package com.amalto.core.history.action;
+
+import com.amalto.core.history.DeleteType;
+import com.amalto.core.history.MutableDocument;
+import com.amalto.core.metadata.ComplexTypeMetadata;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ */
+public class LogicalRestoreAction extends AbstractAction {
+
+    private final ComplexTypeMetadata deletedType;
+
+    public LogicalRestoreAction(Date date, String source, String userName, ComplexTypeMetadata deletedType) {
+        super(date, source, userName);
+        this.deletedType = deletedType;
+    }
+
+    public MutableDocument perform(MutableDocument document) {
+        return document.recover(DeleteType.LOGICAL);
+    }
+
+    public MutableDocument undo(MutableDocument document) {
+        return document.delete(DeleteType.LOGICAL);
+    }
+
+    public MutableDocument addModificationMark(MutableDocument document) {
+        return document;
+    }
+
+    public MutableDocument removeModificationMark(MutableDocument document) {
+        return document;
+    }
+
+    public boolean isAllowed(Set<String> roles) {
+        List<String> denyDelete = deletedType.getDenyDelete(ComplexTypeMetadata.DeleteType.LOGICAL);
+        boolean isAllowed = true;
+        for (String role : roles) {
+            if (denyDelete.contains(role)) {
+                isAllowed = false;
+            }
+        }
+        return isAllowed;
+    }
+
+    public String getDetails() {
+        return "restore from logical delete of type '" + deletedType.getName() + "'";
+    }
+}
