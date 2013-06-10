da604b820251a416c30170c96dee47c79fe1699d
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-10 01:49:38 +0000

    TMDM-5897: Refine users's counting per their roles
    git-svn-id: http://talendforge.org/svn/tom/trunk@103882 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/util/UserManageImpl.java
+++ org.talend.mdm.core/src/com/amalto/core/util/UserManageImpl.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -17,101 +17,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.talend.mdm.commmon.util.webapp.XSystemObjects;
+import org.apache.log4j.Logger;
 
 import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
 
-/**
- * DOC hshu class global comment. Detailed comment
- */
 public class UserManageImpl extends UserManage {
 
+    private static final Logger log = Logger.getLogger(UserManageImpl.class);
+
     private static List<User> users;
-
-    /**
-     * list all users.
-     * 
-     * @return
-     */
-    protected List<User> listUsers() {
-        String dataclusterPK = XSystemObjects.DC_PROVISIONING.getName();
-        List<String> results = new ArrayList<String>();
-        users = new ArrayList<User>();
-
-        try {
-            results = Util.getItemCtrl2Local().getItems(new DataClusterPOJOPK(dataclusterPK), "User", null, 0, 0,
-                    Integer.MAX_VALUE, false);
-
-            for (String userXML : results) {
-                User user = User.parse(userXML);
-                users.add(user);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return users;
-    }
-
-    /**
-     * get web users
-     * 
-     * @return
-     */
-    public int getWebUsers() {
-        List<User> viewers = new ArrayList<User>();
-
-        for (User user : getUsers()) {
-            if (user.enabled && user.getRoleNames().contains(XSystemObjects.ROLE_DEFAULT_WEB.getName())) {
-                viewers.add(user);
-            }
-        }
-
-        return viewers.size();
-    }
-
-    /**
-     * DOC hshu Comment method "getUsers".
-     * @return
-     */
-    private List<User> getUsers() {
-        if (users == null)
-            listUsers();
-        return users;
-    }
-
-    /**
-     * get viewer users.
-     * 
-     * @return
-     */
-    public int getViewerUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_WEB.getName());
-    }
-
-    /**
-     * get the number of normal users.
-     * 
-     * @return
-     */
-    public int getNormalUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_USER.getName());
-    }
-
-    /**
-     * Get the number of admin users.
-     * 
-     * @return
-     */
-    public int getNBAdminUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_ADMIN.getName());
-    }
 
     /**
      * Get the number of active users.
      * 
      * @return
      */
+    @Override
     public int getActiveUsers() {
         List<User> activeUsers = new ArrayList<User>();
 
@@ -130,6 +51,7 @@ public class UserManageImpl extends UserManage {
      * @param user
      * @return
      */
+    @Override
     public boolean isExistUser(User user) {
         boolean result = false;
 
@@ -143,37 +65,12 @@ public class UserManageImpl extends UserManage {
     }
 
     /**
-     * Check if update cluster or model of specify user.
+     * Check if update active attribute of specify user.
      * 
      * @param user
      * @return
      */
-    public boolean isUpdateDCDM(User user) {
-        boolean result = false;
-
-        for (User existUser : getUsers()) {
-            if (user.getUserName().equals(existUser.getUserName())) {
-                String cluster = user.getDynamic().get("cluster");
-                String model = user.getDynamic().get("model");
-
-                if (cluster == null && model == null) {
-                    return false;
-                } else if (cluster != null && cluster.equals(existUser.getDynamic().get("cluster")) || model != null
-                        && model.equals(existUser.getDynamic().get("model"))) {
-                    return true;
-                }
-            }
-        }
-
-        return result;
-    }
-
-
-    /**
-     * Check if update active attribute of specify user.
-     * @param user
-     * @return
-     */
+    @Override
     public boolean isActiveUser(User user) {
         boolean result = false;
 
@@ -195,6 +92,7 @@ public class UserManageImpl extends UserManage {
      * @param user
      * @return
      */
+    @Override
     public Set<String> getOriginalRole(User user) {
         Set<String> result = new HashSet<String>();
 
@@ -208,21 +106,44 @@ public class UserManageImpl extends UserManage {
         return result;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.amalto.core.util.UserManage#getUserCount(java.lang.String)
-     */
     @Override
-    protected int getUserCount(String role) {
-        List<User> userList = new ArrayList<User>();
-
+    protected int getUserCount(String matchRole) {
+        int userCount = 0;
         for (User user : getUsers()) {
-            if (user.enabled && user.getRoleNames().contains(role)) {
-                userList.add(user);
+            if (user.enabled) {
+                Set<String> userRoles = user.getRoleNames();
+                if (userRoles.contains(matchRole)) {
+                    userCount++;
+                    break;
+                }
             }
         }
-        return userList.size();
+        return userCount;
     }
 
+    private List<User> getUsers() {
+        if (users == null) {
+            listUsers();
+        }
+        return users;
+    }
+
+    List<User> listUsers() {
+        List<String> results = new ArrayList<String>();
+        users = new ArrayList<User>();
+
+        try {
+            results = Util.getItemCtrl2Local().getItems(new DataClusterPOJOPK(PROVISIONING_CLUSTER), USER_CONCEPT, null, 0, 0,
+                    Integer.MAX_VALUE, false);
+
+            for (String userXML : results) {
+                User user = User.parse(userXML);
+                users.add(user);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return users;
+    }
 }
--- org.talend.mdm.core/src/com/amalto/core/util/UserManageOptimizedImpl.java
+++ org.talend.mdm.core/src/com/amalto/core/util/UserManageOptimizedImpl.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -12,9 +12,9 @@
 // ============================================================================
 package com.amalto.core.util;
 
-import java.util.*;
-
-import org.talend.mdm.commmon.util.webapp.XSystemObjects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 
 import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
 import com.amalto.xmlserver.interfaces.IWhereItem;
@@ -23,45 +23,17 @@ import com.amalto.xmlserver.interfaces.WhereCondition;
 
 public class UserManageOptimizedImpl extends UserManage {
 
-    private static final String PROVISIONING_CLUSTER = "PROVISIONING"; //$NON-NLS-1$
-
-    private static final String PROVISIONING_CONCEPT = "User"; //$NON-NLS-1$
-
-    @Override
-    public int getWebUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_WEB.getName());
-    }
-
-    @Override
-    public int getViewerUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_VIEWER.getName());
-    }
-
-    @Override
-    public int getNormalUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_USER.getName());
-    }
-
-    @Override
-    public int getNBAdminUsers() {
-        return getUserCount(XSystemObjects.ROLE_DEFAULT_ADMIN.getName());
-    }
-
     @Override
     public int getActiveUsers() {
         try {
             ArrayList<IWhereItem> conditions = new ArrayList<IWhereItem>();
             conditions.add(new WhereCondition("User/enabled", //$NON-NLS-1$
-                    WhereCondition.EQUALS,
-                    "yes", //$NON-NLS-1$
+                    WhereCondition.EQUALS, "yes", //$NON-NLS-1$
                     "NONE")); //$NON-NLS-1$
             IWhereItem whereItem = new WhereAnd(conditions);
-            return (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER),
-                    PROVISIONING_CONCEPT,
-                    whereItem,
-                    -1);
+            return (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER), USER_CONCEPT, whereItem, -1);
         } catch (Exception e) {
-            throw new RuntimeException("Could not retrieve active user number", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -73,34 +45,14 @@ public class UserManageOptimizedImpl extends UserManage {
         try {
             ArrayList<IWhereItem> conditions = new ArrayList<IWhereItem>();
             conditions.add(new WhereCondition("User/username", //$NON-NLS-1$
-                    WhereCondition.EQUALS,
-                    user.getUserName(),
-                    "NONE")); //$NON-NLS-1$
+                    WhereCondition.EQUALS, user.getUserName(), "NONE")); //$NON-NLS-1$
             IWhereItem whereItem = new WhereAnd(conditions);
-            int number = (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER),
-                    PROVISIONING_CONCEPT,
-                    whereItem,
-                    -1);
+            int number = (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER), USER_CONCEPT,
+                    whereItem, -1);
             return number > 0;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public boolean isUpdateDCDM(User user) {
-        User existUser = findUserByName(user);
-        if (existUser != null) {
-            String cluster = user.getDynamic().get("cluster"); //$NON-NLS-1$
-            String model = user.getDynamic().get("model"); //$NON-NLS-1$
-            if (cluster == null && model == null) {
-                return false;
-            } else if (cluster != null && cluster.equals(existUser.getDynamic().get("cluster")) || model != null //$NON-NLS-1$
-                    && model.equals(existUser.getDynamic().get("model"))) { //$NON-NLS-1$
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
@@ -128,19 +80,12 @@ public class UserManageOptimizedImpl extends UserManage {
         try {
             ArrayList<IWhereItem> conditions = new ArrayList<IWhereItem>();
             conditions.add(new WhereCondition("User/username", //$NON-NLS-1$
-                    WhereCondition.EQUALS,
-                    user.getUserName(),
-                    "NONE")); //$NON-NLS-1$
+                    WhereCondition.EQUALS, user.getUserName(), "NONE")); //$NON-NLS-1$
             IWhereItem whereItem = new WhereAnd(conditions);
-            ArrayList items = Util.getItemCtrl2Local().getItems(new DataClusterPOJOPK(PROVISIONING_CLUSTER),
-                    PROVISIONING_CONCEPT,
-                    whereItem,
-                    -1,
-                    0,
-                    1,
-                    false);
+            ArrayList<String> items = Util.getItemCtrl2Local().getItems(new DataClusterPOJOPK(PROVISIONING_CLUSTER),
+                    USER_CONCEPT, whereItem, -1, 0, 1, false);
             if (items != null && items.size() > 0) {
-                String userXML = (String) items.get(0);
+                String userXML = items.get(0);
                 return User.parse(userXML);
             }
         } catch (Exception e) {
@@ -151,24 +96,18 @@ public class UserManageOptimizedImpl extends UserManage {
     }
 
     @Override
-    protected int getUserCount(String role) {
+    protected int getUserCount(String matchRole) {
         try {
             ArrayList<IWhereItem> conditions = new ArrayList<IWhereItem>();
             conditions.add(new WhereCondition("User/enabled", //$NON-NLS-1$
-                    WhereCondition.EQUALS,
-                    "yes", //$NON-NLS-1$
+                    WhereCondition.EQUALS, "yes", //$NON-NLS-1$
                     "NONE")); //$NON-NLS-1$
             conditions.add(new WhereCondition("User/roles/role", //$NON-NLS-1$
-                    WhereCondition.EQUALS,
-                    role,
-                    "NONE")); //$NON-NLS-1$
+                    WhereCondition.EQUALS, matchRole, "NONE")); //$NON-NLS-1$
             IWhereItem whereItem = new WhereAnd(conditions);
-            return (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER),
-                    PROVISIONING_CONCEPT,
-                    whereItem,
-                    -1);
+            return (int) Util.getItemCtrl2Local().count(new DataClusterPOJOPK(PROVISIONING_CLUSTER), USER_CONCEPT, whereItem, -1);
         } catch (Exception e) {
-            throw new RuntimeException("Could not retrieve active user number", e);
+            throw new RuntimeException(e);
         }
     }
 }
