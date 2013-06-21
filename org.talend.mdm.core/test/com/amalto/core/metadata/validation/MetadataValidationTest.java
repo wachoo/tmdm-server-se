477f32745378a37fe7a424f6da4cf4df881769ac
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-21 09:20:08 +0000

    merge r104350 from trunk to branch 5.3 TMDM-5927: Adds fixes for reported issues. Ports all manual QA tests into unit tests for better regression testing. Adds FOREIGN_KEY_INFO_NOT_REFERENCING_FK_TYPE error type (missing checks from specs).
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_3@104355 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/test/com/amalto/core/metadata/validation/MetadataValidationTest.java
+++ org.talend.mdm.core/test/com/amalto/core/metadata/validation/MetadataValidationTest.java
@@ -21,6 +21,7 @@ import org.w3c.dom.Element;
 
 public class MetadataValidationTest extends TestCase {
 
+    // See TMDMQA-6270 / 6330
     public void testFK1() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("FK_0.1.xsd");
@@ -35,6 +36,7 @@ public class MetadataValidationTest extends TestCase {
         assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_EXIST));
     }
 
+    // See TMDMQA-6363
     public void testFK2() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("FK2_0.1.xsd");
@@ -55,8 +57,7 @@ public class MetadataValidationTest extends TestCase {
         TestValidationHandler handler = new TestValidationHandler();
         repository.load(resourceAsStream, handler);
         assertEquals(0, handler.getErrorCount());
-        assertEquals(0, handler.getWarningField());
-        assertEquals(0, handler.getWarningType());
+        assertEquals(0, handler.getWarningCount());
     }
 
     public void testFK4() throws Exception {
@@ -65,9 +66,37 @@ public class MetadataValidationTest extends TestCase {
         TestValidationHandler handler = new TestValidationHandler();
         repository.load(resourceAsStream, handler);
         assertEquals(0, handler.getErrorCount());
-        assertEquals(1, handler.getWarningField());
-        assertEquals(0, handler.getWarningType());
+        assertEquals(1, handler.getWarningCount());
         assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_USES_MAX_LENGTH));
+    }
+
+    // See TMDMQA-6364
+    public void testFK5() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FK5_0.1.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getErrorCount());
+        assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_OWN_FIELD));
+    }
+
+    public void testFK6() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FK6_0.1.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getErrorCount());
+        assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_EXIST));
     }
 
     public void testFKPointToNonPK() throws Exception {
@@ -75,10 +104,11 @@ public class MetadataValidationTest extends TestCase {
         InputStream resourceAsStream = this.getClass().getResourceAsStream("FKCheck.xsd");
         TestValidationHandler handler = new TestValidationHandler();
         repository.load(resourceAsStream, handler);
-        assertEquals(1, handler.getWarningField());
+        assertEquals(1, handler.getWarningCount());
         assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_SHOULD_POINT_TO_PRIMARY_KEY));
     }
 
+    // See TMDMQA-6366
     public void testFKINFO() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("FKINFO.xsd");
@@ -89,10 +119,45 @@ public class MetadataValidationTest extends TestCase {
         } catch (Exception e) {
             // Expected
         }
-        assertEquals(1, handler.getWarningField());
+        assertEquals(1, handler.getWarningCount());
         assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_INFO_NOT_PRIMITIVE_XSD_TYPED));
-        assertEquals(2, handler.getErrorCount());
+        assertEquals(4, handler.getErrorCount());
         assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_OWN_FIELD));
+        assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_EXIST));
+    }
+
+    public void testFKInfo2() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FKINFO2.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        repository.load(resourceAsStream, handler);
+        assertEquals(1, handler.getWarningCount());
+        assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_INFO_NOT_PRIMITIVE_XSD_TYPED));
+    }
+
+    // See TMDMQA-6365
+    public void testFKInfo3() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FKINFO3.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getErrorCount());
+        assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_INFO_NOT_REFERENCING_FK_TYPE));
+    }
+
+    // See TMDMQA-6332
+    public void testFKInfo4() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("FKINFO4.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        repository.load(resourceAsStream, handler);
+        assertEquals(1, handler.getWarningCount());
+        assertTrue(handler.getMessages().contains(ValidationError.FOREIGN_KEY_INFO_REPEATABLE));
     }
 
     public void testPKINFO_manyType() throws Exception {
@@ -123,6 +188,7 @@ public class MetadataValidationTest extends TestCase {
         assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_OWN_FIELD));
     }
 
+    // See TMDMQA-6327 / 6328
     public void testPK2() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("PK2_0.1.xsd");
@@ -137,6 +203,7 @@ public class MetadataValidationTest extends TestCase {
         assertTrue(handler.getMessages().contains(ValidationError.PRIMARY_KEY_INFO_CANNOT_BE_REPEATABLE));
     }
 
+    // See TMDMQA-6329
     public void testPK3() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("PK3_0.1.xsd");
@@ -166,6 +233,7 @@ public class MetadataValidationTest extends TestCase {
         assertTrue(handler.getMessages().contains(ValidationError.PRIMARY_KEY_INFO_NOT_IN_ENTITY));
     }
 
+    // See TMDMQA-6326
     public void testPK4() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("PK4_0.1.xsd");
@@ -237,6 +305,21 @@ public class MetadataValidationTest extends TestCase {
         assertTrue(handler.getMessages().contains(ValidationError.TYPE_DOES_NOT_OWN_FIELD));
     }
 
+    // See TMDMQA-6333
+    public void testPKInfo2() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("PKINFO2_0.1.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(1, handler.getWarningCount());
+        assertTrue(handler.getMessages().contains(ValidationError.PRIMARY_KEY_INFO_TYPE_NOT_PRIMITIVE));
+    }
+
+    // See TMDMQA-6317
     public void testLookUpField1() throws Exception {
         MetadataRepository repository = new MetadataRepository();
         InputStream resourceAsStream = this.getClass().getResourceAsStream("LookUpField1_0.1.xsd");
@@ -249,6 +332,36 @@ public class MetadataValidationTest extends TestCase {
         }
         assertEquals(1, handler.getErrorCount());
         assertTrue(handler.getMessages().contains(ValidationError.LOOKUP_FIELD_CANNOT_BE_KEY));
+    }
+
+    // See TMDMQA-6313
+    public void testLookUpField2() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("LookUpField2_0.1.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(1, handler.getErrorCount());
+        assertTrue(handler.getMessages().contains(ValidationError.LOOKUP_FIELD_NOT_IN_ENTITY));
+    }
+
+    // See TMDMQA-6357 / 6367
+    public void testInheritance() throws Exception {
+        MetadataRepository repository = new MetadataRepository();
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("Inheritance.xsd");
+        TestValidationHandler handler = new TestValidationHandler();
+        try {
+            repository.load(resourceAsStream, handler);
+            fail("Should fail validation.");
+        } catch (Exception e) {
+            // Expected
+        }
+        assertEquals(4, handler.getErrorCount()); // TODO Check other errors in data model in assert
+        assertTrue(handler.getMessages().contains(ValidationError.TYPE_CANNOT_OVERRIDE_SUPER_TYPE_KEY));
     }
 
     private static class TestValidationHandler implements ValidationHandler {
@@ -268,37 +381,37 @@ public class MetadataValidationTest extends TestCase {
         private Set<ValidationError> errors = new HashSet<ValidationError>();
 
         @Override
-        public void fatal(FieldMetadata field, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void fatal(FieldMetadata field, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             fatalField++;
         }
 
         @Override
-        public void error(FieldMetadata field, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void error(FieldMetadata field, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             errorField++;
         }
 
         @Override
-        public void warning(FieldMetadata field, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void warning(FieldMetadata field, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             warningField++;
         }
 
         @Override
-        public void fatal(TypeMetadata type, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void fatal(TypeMetadata type, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             fatalType++;
         }
 
         @Override
-        public void error(TypeMetadata type, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void error(TypeMetadata type, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             errorType++;
         }
 
         @Override
-        public void warning(TypeMetadata type, String message, Element element, int lineNumber, int columnNumber, ValidationError error) {
+        public void warning(TypeMetadata type, String message, Element element, Integer lineNumber, Integer columnNumber, ValidationError error) {
             errors.add(error);
             warningType++;
         }
@@ -315,12 +428,8 @@ public class MetadataValidationTest extends TestCase {
             return errorField + errorType;
         }
 
-        public int getWarningField() {
-            return warningField;
-        }
-
-        public int getWarningType() {
-            return warningType;
+        public int getWarningCount() {
+            return warningField + warningType;
         }
 
         private Set<ValidationError> getMessages() {
