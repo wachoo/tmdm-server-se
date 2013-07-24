fa8ff18e77ead4d2a35e551947e0e662dbb89c43
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-24 06:00:30 +0000

    Fix r105575 compilation + code cleanup
    git-svn-id: http://talendforge.org/svn/tom/trunk@105700 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/delegator/BeanDelegatorContainer.java
+++ org.talend.mdm.core/src/com/amalto/core/delegator/BeanDelegatorContainer.java
@@ -1,14 +1,22 @@
-/**
- * 
- */
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package com.amalto.core.delegator;
-
-import org.apache.log4j.Logger;
 
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 
 public class BeanDelegatorContainer {
 
@@ -31,8 +39,8 @@ public class BeanDelegatorContainer {
     }
 
     /**
-     * Get the unique instance of this class.
-     * In order to improve the performance, removed synchronized, using pseudo singleton mode
+     * Get the unique instance of this class. In order to improve the performance, removed synchronized, using pseudo
+     * singleton mode
      */
     public static synchronized BeanDelegatorContainer getInstance() {
         if (instance == null) {
@@ -46,13 +54,13 @@ public class BeanDelegatorContainer {
             Map<String, String> beanImplNamesMap = BeanDelegatorConfigReader.readConfiguration();
             for (Map.Entry<String, String> currentBean : beanImplNamesMap.entrySet()) {
                 try {
-                    Class clazz = Class.forName(currentBean.getValue());
-                    Constructor cons = clazz.getConstructor();
+                    Class<?> clazz = Class.forName(currentBean.getValue());
+                    Constructor<?> cons = clazz.getConstructor();
                     IBeanDelegator beanDelegator = (IBeanDelegator) cons.newInstance();
                     delegatorInstancePool.put(currentBean.getKey(), beanDelegator);
-                    LOGGER.info("Init instance:" + currentBean.getValue());
+                    LOGGER.info("Init instance:" + currentBean.getValue()); //$NON-NLS-1$
                 } catch (Exception e) {
-                    LOGGER.error("Init exception for '" + currentBean.getValue() + "'.", e);
+                    LOGGER.error("Init exception for '" + currentBean.getValue() + "'.", e); //$NON-NLS-1$ //$NON-NLS-2$
                 }
             }
         }
