99e5fafe28a5cddeb7a56371754f665569a45c34
wdzhang <wdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-04 09:56:41 +0000

    TMDM-6090: revert back to previous version and include imageserver.war in tem.ear/tom.ear for deployment
    git-svn-id: http://talendforge.org/svn/tom/trunk@107529 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.ext.images.server/src/talend/ext/images/server/backup/DBDelegateExistImpl.java
+++ org.talend.mdm.ext.images.server/src/talend/ext/images/server/backup/DBDelegateExistImpl.java
@@ -1,53 +1,35 @@
 package talend.ext.images.server.backup;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import com.amalto.core.util.Util;
 import org.apache.log4j.Logger;
 
-import com.amalto.core.ejb.remote.XmlServerSLWrapper;
+import com.amalto.core.ejb.local.XmlServerSLWrapperLocal;
+import com.amalto.core.util.XtentisException;
 
 public class DBDelegateExistImpl implements DBDelegate {
 
-    private static final String FROM_HOST = "from.host";//$NON-NLS-1$
-
-    private static final String FROM_JNDI_PORT = "from.jndi.port";//$NON-NLS-1$
-    
     private static final String CLUSTER_NAME = "MDMItemImages"; //$NON-NLS-1$
 
     private static final Logger logger = Logger.getLogger(DBDelegateExistImpl.class);
 
-    private XmlServerSLWrapper server;
+    private XmlServerSLWrapperLocal server;
 
     public DBDelegateExistImpl() {
-        InputStream is = null;
         try {
-            is = DBDelegateExistImpl.class.getResourceAsStream("ejb.remote.jndi.lookup.properties"); //$NON-NLS-1$
-            Properties properties = new Properties();
-            properties.load(is);
-            server = Util.getXmlServerCtrlHome(properties.getProperty(FROM_HOST), properties.getProperty(FROM_JNDI_PORT));
+            server = Util.getXmlServerCtrlLocal();
         } catch (Exception e) {
             String err = "Unable to access the XML Server wrapper";
             logger.error(err, e);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                 // do nothing
-                }
-            }
         }
     }
 
     public byte[] getResource(ResourcePK resourcePK) {
         try {
             return server.getDocumentBytes(null, CLUSTER_NAME, resourcePK.toString(), "BINARY"); //$NON-NLS-1$
-        } catch (Exception e) {
+        } catch (XtentisException e) {
             logger.error("Error during getResource call.", e);
             return null;
-        } 
+        }
     }
 
     public boolean putResource(ResourcePK resourcePK, String fileName) {
@@ -64,7 +46,7 @@ public class DBDelegateExistImpl implements DBDelegate {
         try {
             long rtnStatus = server.deleteDocument(null, CLUSTER_NAME, resourcePK.toString(), "BINARY"); //$NON-NLS-1$
             return rtnStatus != -1;
-        } catch (Exception e) {
+        } catch (XtentisException e) {
             logger.error("Error during deleteResource call.", e);
             return false;
         }
