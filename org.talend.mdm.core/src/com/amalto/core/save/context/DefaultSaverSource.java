b12d8f3b5dad2f9f7f8515000cfdd98e8be8d1ea
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-11 09:03:56 +0000

    TMDM-6446 ： fixed element of auto increment can not be saved and generate auto increment id from super type
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_3@109152 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/save/context/DefaultSaverSource.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/DefaultSaverSource.java
@@ -1,21 +1,28 @@
 /*
  * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.save.context;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.MetadataRepository;
+import org.talend.mdm.commmon.metadata.TypeMetadata;
+
 import com.amalto.core.ejb.ItemPOJOPK;
 import com.amalto.core.history.MutableDocument;
 import com.amalto.core.metadata.MetadataUtils;
-import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
-import org.talend.mdm.commmon.metadata.MetadataRepository;
 import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
 import com.amalto.core.objects.datamodel.ejb.DataModelPOJOPK;
 import com.amalto.core.objects.routing.v2.ejb.local.RoutingEngineV2CtrlLocal;
@@ -26,14 +33,11 @@ import com.amalto.core.server.MetadataRepositoryAdmin;
 import com.amalto.core.server.ServerContext;
 import com.amalto.core.server.XmlServer;
 import com.amalto.core.servlet.LoadServlet;
-import com.amalto.core.util.*;
-import org.talend.mdm.commmon.metadata.TypeMetadata;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import com.amalto.core.util.AutoIncrementGenerator;
+import com.amalto.core.util.LocalUser;
+import com.amalto.core.util.OutputReport;
+import com.amalto.core.util.Util;
+import com.amalto.core.util.XtentisException;
 
 public class DefaultSaverSource implements SaverSource {
 
@@ -64,6 +68,7 @@ public class DefaultSaverSource implements SaverSource {
         this.userName = userName;
     }
 
+    @Override
     public InputStream get(String dataClusterName, String typeName, String revisionId, String[] key) {
         try {
             StringBuilder builder = new StringBuilder();
@@ -87,10 +92,12 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public boolean exist(String dataCluster, String typeName, String revisionId, String[] key) {
         return get(dataCluster, typeName, revisionId, key) != null;
     }
 
+    @Override
     public MetadataRepository getMetadataRepository(String dataModelName) {
         try {
             MetadataRepositoryAdmin admin = ServerContext.INSTANCE.get().getMetadataRepositoryAdmin();
@@ -100,6 +107,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public InputStream getSchema(String dataModelName) {
         try {
             synchronized (schemasAsString) {
@@ -114,6 +122,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public String getUniverse() {
         try {
             return LocalUser.getLocalUser().getUniverse().getName();
@@ -122,16 +131,17 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public OutputReport invokeBeforeSaving(DocumentSaverContext context, MutableDocument updateReportDocument) {
         try {
-            return Util.beforeSaving(context.getType().getName(),
-                    context.getDatabaseDocument().exportToString(),
+            return Util.beforeSaving(context.getType().getName(), context.getDatabaseDocument().exportToString(),
                     updateReportDocument.exportToString());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
     public Set<String> getCurrentUserRoles() {
         try {
             // get user roles from current user.
@@ -141,6 +151,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public String getUserName() {
         try {
             return LocalUser.getLocalUser().getUsername();
@@ -149,6 +160,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public boolean existCluster(String revisionID, String dataClusterName) {
         try {
             return database.existCluster(revisionID, dataClusterName);
@@ -157,6 +169,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public String getConceptRevisionID(String typeName) {
         try {
             return LocalUser.getLocalUser().getUniverse().getConceptRevisionID(typeName);
@@ -165,6 +178,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public void resetLocalUsers() {
         try {
             LocalUser.resetLocalUsers();
@@ -173,10 +187,12 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public void initAutoIncrement() {
         AutoIncrementGenerator.init();
     }
 
+    @Override
     public void routeItem(String dataCluster, String typeName, String[] id) {
         try {
             RoutingEngineV2CtrlLocal ctrl = Util.getRoutingEngineV2CtrlLocal();
@@ -187,6 +203,7 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public void invalidateTypeCache(String dataModelName) {
         XmlSchemaValidator.invalidateCache(dataModelName);
         synchronized (schemasAsString) {
@@ -197,17 +214,39 @@ public class DefaultSaverSource implements SaverSource {
         }
     }
 
+    @Override
     public void saveAutoIncrement() {
         AutoIncrementGenerator.saveToDB();
     }
 
+    @Override
     public String nextAutoIncrementId(String universe, String dataCluster, String conceptName) {
+        long autoIncrementId = -1;
+        String concept = null;
+        String field = null;
+        if (conceptName.contains(".")) { //$NON-NLS-1$
+            String[] conceptArray = conceptName.split("\\."); //$NON-NLS-1$
+            concept = conceptArray[0];
+            field = conceptArray[1];
+        } else {
+            concept = conceptName;
+        }
         MetadataRepository metadataRepository = getMetadataRepository(dataCluster);
-        ComplexTypeMetadata complexType = metadataRepository.getComplexType(conceptName);
-        TypeMetadata superType = MetadataUtils.getSuperConcreteType(complexType);
-        return String.valueOf(AutoIncrementGenerator.generateNum(universe, dataCluster, superType.getName()));
+        if (metadataRepository != null) {
+            ComplexTypeMetadata complexType = metadataRepository.getComplexType(concept);
+            if (complexType != null) {
+                TypeMetadata superType = MetadataUtils.getSuperConcreteType(complexType);
+                if (superType != null) {
+                    concept = superType.getName();
+                }
+                String autoIncrementFiledName = field != null ? concept + "." + field : concept; //$NON-NLS-1$
+                autoIncrementId = AutoIncrementGenerator.generateNum(universe, dataCluster, autoIncrementFiledName);
+            }
+        }
+        return String.valueOf(autoIncrementId);
     }
 
+    @Override
     public String getLegitimateUser() {
         // web service caller
         if (userName != null) {
