1f26f0ea387f05e3544e792ed1e91f468047b86d
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-12-19 12:16:20 +0000

    TMDM-5995 : fixed upload polymorphism element issue and add junit test
    git-svn-id: http://talendforge.org/svn/tom/trunk@112331 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecordsinstaging/server/servlet/UploadData4Staging.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecordsinstaging/server/servlet/UploadData4Staging.java
@@ -12,75 +12,29 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecordsinstaging.server.servlet;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletException;
-
-import org.apache.poi.ss.usermodel.Row;
-import org.dom4j.Document;
-import org.dom4j.Element;
-import org.dom4j.Namespace;
+import org.talend.mdm.webapp.browserecords.server.service.UploadService;
 import org.talend.mdm.webapp.browserecords.server.servlet.UploadData;
-import org.talend.mdm.webapp.browserecordsinstaging.shared.StagingConstants;
-
-import com.amalto.webapp.util.webservices.WSPutItemWithReport;
-
+import org.talend.mdm.webapp.browserecordsinstaging.server.service.UploadService4Staging;
 
 /**
- * created by yjli on 2013-10-24
- * Detailled comment
- *
+ * created by yjli on 2013-10-24 Detailled comment
+ * 
  */
 public class UploadData4Staging extends UploadData {
-    
-    private static final long serialVersionUID = -1278619413670813163L;
-    private Map<String,String> stagingValueMap;
-    
-    @Override
-    protected String[] readHeader(Row headerRow, String[] headerRecord) throws ServletException {
-        stagingValueMap = new HashMap<String, String>();
-        return super.readHeader(headerRow, headerRecord);
-    }
 
-    protected String handleHeader(String headerName, int index) throws ServletException {
-        if (StagingConstants.TITLE_STAGING_TASKID.equals(headerName) || StagingConstants.TITLE_STAGING_STATUS.equals(headerName) || StagingConstants.TITLE_STAGING_SOURCE.equals(headerName) || StagingConstants.TITLE_STAGING_ERROR.equals(headerName)) {
-            return headerName;
-        } else {
-            return super.handleHeader(headerName, index);
-        }
-    }
+    private static final long serialVersionUID = -4815767737453028749L;
 
     @Override
-    protected WSPutItemWithReport buildWSPutItemWithReport(Document document, String model) {
-        Namespace namespace = new Namespace(StagingConstants.METADATA_NAME,StagingConstants.METADATA_URI);
-        Element rootElement = document.getRootElement();
-        rootElement.add(namespace);
-        Element taskidElement = rootElement.addElement(StagingConstants.METADATA_NAME + ":" + StagingConstants.TASKID_ELEMENT_NAME); //$NON-NLS-1$
-        taskidElement.setText(stagingValueMap.get(StagingConstants.TASKID_ELEMENT_NAME) != null ? stagingValueMap.get(StagingConstants.TASKID_ELEMENT_NAME) : ""); //$NON-NLS-1$
-        Element statusElement = rootElement.addElement(StagingConstants.METADATA_NAME + ":" + StagingConstants.STATUS_ELEMENT_NAME); //$NON-NLS-1$
-        statusElement.setText(stagingValueMap.get(StagingConstants.STATUS_ELEMENT_NAME) != null ? stagingValueMap.get(StagingConstants.STATUS_ELEMENT_NAME) : ""); //$NON-NLS-1$
-        Element resourceElement = rootElement.addElement(StagingConstants.METADATA_NAME + ":" + StagingConstants.SOURCE_ELEMENT_NAME); //$NON-NLS-1$
-        resourceElement.setText(stagingValueMap.get(StagingConstants.SOURCE_ELEMENT_NAME) != null ? stagingValueMap.get(StagingConstants.SOURCE_ELEMENT_NAME) : ""); //$NON-NLS-1$
-        Element errorElement = rootElement.addElement(StagingConstants.METADATA_NAME + ":" + StagingConstants.ERROR_ELEMENT_NAME); //$NON-NLS-1$
-        errorElement.setText(stagingValueMap.get(StagingConstants.ERROR_ELEMENT_NAME) != null ? stagingValueMap.get(StagingConstants.ERROR_ELEMENT_NAME) : ""); //$NON-NLS-1$
-        stagingValueMap.clear();
-        return super.buildWSPutItemWithReport(document, model);
-    }
-
-    @Override
-    protected void fillFieldValue(Element currentElement, String fieldPath, String fieldValue, Row row, String[] record)
+    protected UploadService generateUploadService(String concept, String fileType, boolean headersOnFirstLine,
+            Map<String, Boolean> headerVisibleMap, List<String> inheritanceNodePathList, String multipleValueSeparator,
+            String seperator, String encoding, char textDelimiter, String clusterName, String dataModelName, String language)
             throws Exception {
-        if (StagingConstants.TITLE_STAGING_TASKID.equals(fieldPath)) {
-            stagingValueMap.put(StagingConstants.TASKID_ELEMENT_NAME, fieldValue);
-        } else if (StagingConstants.TITLE_STAGING_STATUS.equals(fieldPath)) {
-            stagingValueMap.put(StagingConstants.STATUS_ELEMENT_NAME, fieldValue);
-        } else if (StagingConstants.TITLE_STAGING_SOURCE.equals(fieldPath)) {
-            stagingValueMap.put(StagingConstants.SOURCE_ELEMENT_NAME, fieldValue);
-        } else if (StagingConstants.TITLE_STAGING_ERROR.equals(fieldPath)) {
-            stagingValueMap.put(StagingConstants.ERROR_ELEMENT_NAME, fieldValue);
-        } else {
-            super.fillFieldValue(currentElement, fieldPath, fieldValue, row, record);
-        }        
-    }    
+        return new UploadService4Staging(getEntityModel(concept), fileType, headersOnFirstLine, headerVisibleMap,
+                inheritanceNodePathList, multipleValueSeparator, seperator, encoding, textDelimiter, clusterName, dataModelName,
+                language);
+    }
+
 }
