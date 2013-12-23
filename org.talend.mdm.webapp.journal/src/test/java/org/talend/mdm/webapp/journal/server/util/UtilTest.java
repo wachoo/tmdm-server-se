df7cca5a1aeecdac4ba797cbe53256b399b42322
wdzhang <wdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-12-23 11:19:09 +0000

    TMDM-6517, TMDM-6777: corrected the wrong package naming for org.talend.mdm.webapp.journal.sever
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_4@112466 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.journal/src/test/java/org/talend/mdm/webapp/journal/server/util/UtilTest.java
+++ org.talend.mdm.webapp.journal/src/test/java/org/talend/mdm/webapp/journal/server/util/UtilTest.java
@@ -10,7 +10,7 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.mdm.webapp.journal.sever.util;
+package org.talend.mdm.webapp.journal.server.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
