5cf1caf47051a62fe5b8d6f4d6ed22437ec3816d
qcui <qcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-10 08:23:12 +0000

    TMDM-6756 : Searching for records in RecycleBin get nothing with server errors
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@113266 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/ejb/DroppedItemPOJOPK.java
+++ org.talend.mdm.core/src/com/amalto/core/ejb/DroppedItemPOJOPK.java
@@ -1,7 +1,6 @@
 package com.amalto.core.ejb;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -12,104 +11,109 @@ import com.amalto.core.objects.datacluster.ejb.DataClusterPOJOPK;
 import com.amalto.core.schema.manage.SchemaCoreAgent;
 
 public class DroppedItemPOJOPK implements Serializable {
-	
-	private ItemPOJOPK refItemPOJOPK;
-	
-	private String partPath;
-	
-	private String revisionId;
 
-	public DroppedItemPOJOPK(String revisionId,ItemPOJOPK refItemPOJOPK,String partPath) {
-		super();
-		this.revisionId = (revisionId==null?"":(revisionId));
-		this.refItemPOJOPK = refItemPOJOPK;
-		this.partPath = partPath;
-	}
+    private ItemPOJOPK refItemPOJOPK;
 
+    private String partPath;
 
-	public String getUniquePK() {
-		if(revisionId==null||revisionId.equals(""))revisionId="head";
-    	return revisionId+"."+refItemPOJOPK.getUniqueID()+convertItemPartPath(partPath);
-	}
-	
-	
-	public String getRevisionId() {
-		if(revisionId!=null&&revisionId.toLowerCase().equals("head"))revisionId=null;
-		return revisionId;
-	}
+    private String revisionId;
 
+    public DroppedItemPOJOPK(String revisionId, ItemPOJOPK refItemPOJOPK, String partPath) {
+        super();
+        this.revisionId = (revisionId == null ? "" : (revisionId));
+        this.refItemPOJOPK = refItemPOJOPK;
+        this.partPath = partPath;
+    }
 
-	public ItemPOJOPK getRefItemPOJOPK() {
-		return refItemPOJOPK;
-	}
-	
-	public String getPartPath() {
-		return partPath;
-	}
+    public String getUniquePK() {
+        if (revisionId == null || revisionId.equals("")) {
+            revisionId = "head";
+        }
+        return revisionId + "." + refItemPOJOPK.getUniqueID() + convertItemPartPath(partPath);
+    }
 
+    public String getRevisionId() {
+        if (revisionId != null && revisionId.toLowerCase().equals("head")) {
+            revisionId = null;
+        }
+        return revisionId;
+    }
 
-	private static String convertItemPartPath(String partPath) {
-		
-		if(partPath!=null&&partPath.length()>0){
-			partPath=partPath.replaceAll("/", "-");
-		}
-		
-		return partPath;
-		
-	}
-	
-	
-	/**
-	 * @param input "."&&"-" are reserved words
-	 * @return DroppedItemPOJOPK
-	 * @throws Exception 
-	 */
-	public static DroppedItemPOJOPK buildUid2POJOPK(String input, Map<String, BusinessConcept> conceptMap) throws Exception {
-		//TODO need regular expression to validate input
-		
-		int pos=input.lastIndexOf("-"); // input = "head.Product.Product.1-" or "head.RTE-RAP.Contrat.1-"
-		String part1=input.substring(0, pos);
-		String part2=input.substring(pos);
-		
-		String[] part1s=part1.split("\\.");
-		if(part1s.length<3)return null;//validate
-		String revision=part1s[0];
-		String clusterName=part1s[1];
-		String conceptName=part1s[2];
-		// check xsd key's length
-        String idPrefix = clusterName + "." + conceptName + ".";  //$NON-NLS-1$//$NON-NLS-2$
-        String[] ids=new String[part1s.length-3];
+    public ItemPOJOPK getRefItemPOJOPK() {
+        return refItemPOJOPK;
+    }
+
+    public String getPartPath() {
+        return partPath;
+    }
+
+    private static String convertItemPartPath(String partPath) {
+
+        if (partPath != null && partPath.length() > 0) {
+            partPath = partPath.replaceAll("/", "-");
+        }
+
+        return partPath;
+
+    }
+
+    /**
+     * @param input "."&&"-" are reserved words
+     * @return DroppedItemPOJOPK
+     * @throws Exception
+     */
+    public static DroppedItemPOJOPK buildUid2POJOPK(String input, Map<String, BusinessConcept> conceptMap) throws Exception {
+        // TODO need regular expression to validate input
+
+        int pos = input.lastIndexOf("-"); // input = "head.Product.Product.1-" or "head.RTE-RAP.Contrat.1-"
+        String part1 = input.substring(0, pos);
+        String part2 = input.substring(pos);
+
+        String[] part1s = part1.split("\\.");
+        if (part1s.length < 3) {
+            return null;// validate
+        }
+        String revision = part1s[0];
+        String clusterName = part1s[1];
+        String conceptName = part1s[2];
+        // check xsd key's length
+        String idPrefix = clusterName + "." + conceptName + "."; //$NON-NLS-1$//$NON-NLS-2$
+        String[] ids = new String[part1s.length - 3];
         for (int i = 0; i < ids.length; i++) {
-            ids[i]=part1s[i+3];
+            ids[i] = part1s[i + 3];
         }
         if (!conceptMap.containsKey(idPrefix)) {
-            BusinessConcept businessConcept = SchemaCoreAgent.getInstance().getBusinessConcept(conceptName, new DataModelID(clusterName, revision));
+            BusinessConcept businessConcept = SchemaCoreAgent.getInstance().getBusinessConcept(conceptName,
+                    new DataModelID(clusterName, null));
             businessConcept.load();
             conceptMap.put(idPrefix, businessConcept);
         }
         if (conceptMap.get(idPrefix) != null && conceptMap.get(idPrefix).getKeyFieldPaths().size() == 1) {
-            ids = new String[] {StringUtils.removeStart(part1, idPrefix)};
+            ids = new String[] { StringUtils.removeStart(part1, revision + "." + idPrefix) };
         }
-		
-		
-		String partPath=part2.replaceAll("-", "/");
-		
-		ItemPOJOPK refItemPOJOPK = new ItemPOJOPK(new DataClusterPOJOPK(clusterName), conceptName, ids);
-		DroppedItemPOJOPK droppedItemPOJOPK=new DroppedItemPOJOPK(revision,refItemPOJOPK,partPath);
-		return droppedItemPOJOPK;
-	}
-	
-	@Override
-	public String toString() {
-		return getUniquePK();
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) return false;
-	    if (! (obj instanceof DroppedItemPOJOPK)) return false;
-	    DroppedItemPOJOPK other = (DroppedItemPOJOPK) obj;
-	    return other.getUniquePK().equals(this.getUniquePK());
-	}	
-	
+        String partPath = part2.replaceAll("-", "/");
+
+        ItemPOJOPK refItemPOJOPK = new ItemPOJOPK(new DataClusterPOJOPK(clusterName), conceptName, ids);
+        DroppedItemPOJOPK droppedItemPOJOPK = new DroppedItemPOJOPK(revision, refItemPOJOPK, partPath);
+        return droppedItemPOJOPK;
+    }
+
+    @Override
+    public String toString() {
+        return getUniquePK();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof DroppedItemPOJOPK)) {
+            return false;
+        }
+        DroppedItemPOJOPK other = (DroppedItemPOJOPK) obj;
+        return other.getUniquePK().equals(this.getUniquePK());
+    }
+
 }
