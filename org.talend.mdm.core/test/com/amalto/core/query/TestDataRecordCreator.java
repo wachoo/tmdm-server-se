f52259487e82618c1efcf3407d4c656352c8abc0
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-10 04:18:57 +0000

    Minor improvements for data record generation (support of UUID, use of constants).
    git-svn-id: http://talendforge.org/svn/tom/trunk@113917 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/test/com/amalto/core/query/TestDataRecordCreator.java
+++ org.talend.mdm.core/test/com/amalto/core/query/TestDataRecordCreator.java
@@ -21,6 +21,7 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.Random;
 import java.util.Stack;
+import java.util.UUID;
 
 class TestDataRecordCreator extends DefaultMetadataVisitor<DataRecord> {
 
@@ -70,15 +71,15 @@ class TestDataRecordCreator extends DefaultMetadataVisitor<DataRecord> {
     private Object createSimpleValue(FieldMetadata field) {
         if (field.isKey()) {
             TypeMetadata type = MetadataUtils.getSuperConcreteType(field.getType());
-            if ("string".equals(type.getName())) {
+            if (Types.STRING.equals(type.getName())) {
                 return "1";
-            } else if ("int".equals(type.getName())
-                    || "integer".equals(type.getName())
-                    || "unsignedInt".equals(type.getName())) {
+            } else if (Types.INT.equals(type.getName())
+                    || Types.INTEGER.equals(type.getName())
+                    || Types.UNSIGNED_INT.equals(type.getName())) {
                 return 1;
-            } else if ("unsignedShort".equals(type.getName())) {
+            } else if (Types.UNSIGNED_SHORT.equals(type.getName())) {
                 return ((short) currentId++);
-            } else if ("boolean".equals(type.getName())) {
+            } else if (Types.BOOLEAN.equals(type.getName())) {
                 return false;
             } else {
                 throw new NotImplementedException("Support for key with type " + type.getName());
@@ -90,7 +91,7 @@ class TestDataRecordCreator extends DefaultMetadataVisitor<DataRecord> {
         TypeMetadata type = MetadataUtils.getSuperConcreteType(field.getType());
         Random random = new Random(System.currentTimeMillis());
         if (!(field instanceof ContainedTypeFieldMetadata)) { // Don't set contained (anonymous types) values
-            if ("string".equals(type.getName())) {
+            if (Types.STRING.equals(type.getName())) {
                 Object maxLength = field.getType().getData(MetadataRepository.DATA_MAX_LENGTH);
                 if (maxLength != null) {
                     int i = Integer.parseInt(maxLength.toString());
@@ -101,43 +102,44 @@ class TestDataRecordCreator extends DefaultMetadataVisitor<DataRecord> {
                         return s;
                     }
                 } else {
-                    return "" + random.nextLong();
+                    return UUID.randomUUID().toString();
                 }
-            } else if ("integer".equals(type.getName())
-                    || "positiveInteger".equals(type.getName())
-                    || "negativeInteger".equals(type.getName())
-                    || "nonNegativeInteger".equals(type.getName())
-                    || "nonPositiveInteger".equals(type.getName())
-                    || "int".equals(type.getName())
-                    || "unsignedInt".equals(type.getName())) {
+            } else if (Types.INTEGER.equals(type.getName())
+                    || Types.POSITIVE_INTEGER.equals(type.getName())
+                    || Types.NEGATIVE_INTEGER.equals(type.getName())
+                    || Types.NON_NEGATIVE_INTEGER.equals(type.getName())
+                    || Types.NON_POSITIVE_INTEGER.equals(type.getName())
+                    || Types.INTEGER.equals(type.getName())
+                    || Types.UNSIGNED_INT.equals(type.getName())
+                    || Types.INT.equals(type.getName())) {
                 return random.nextInt();
-            } else if ("date".equals(type.getName())) {
+            } else if (Types.DATE.equals(type.getName())) {
                 return new Timestamp(random.nextInt());
-            } else if ("dateTime".equals(type.getName())) {
+            } else if (Types.DATETIME.equals(type.getName())) {
                 return new Timestamp(random.nextInt());
-            } else if ("boolean".equals(type.getName())) {
+            } else if (Types.BOOLEAN.equals(type.getName())) {
                 return random.nextBoolean();
-            } else if ("decimal".equals(type.getName())) {
+            } else if (Types.DECIMAL.equals(type.getName())) {
                 return new BigDecimal(random.nextInt());
-            } else if ("float".equals(type.getName())) {
+            } else if (Types.FLOAT.equals(type.getName())) {
                 return random.nextFloat();
-            } else if ("long".equals(type.getName()) || "unsignedLong".equals(type.getName())) {
+            } else if (Types.LONG.equals(type.getName()) || Types.UNSIGNED_LONG.equals(type.getName())) {
                 return random.nextLong();
-            } else if ("anyURI".equals(type.getName())) {
+            } else if (Types.ANY_URI.equals(type.getName())) {
                 return "http://www.talend.com";
-            } else if ("short".equals(type.getName()) || "unsignedShort".equals(type.getName())) {
+            } else if (Types.SHORT.equals(type.getName()) || Types.UNSIGNED_SHORT.equals(type.getName())) {
                 return new Short(String.valueOf(random.nextInt() % 255));
-            } else if ("QName".equals(type.getName())) {
+            } else if (Types.QNAME.equals(type.getName())) {
                 return "Qname";
-            } else if ("base64Binary".equals(type.getName())) {
+            } else if (Types.BASE64_BINARY.equals(type.getName())) {
                 return "EF56AE";
-            } else if ("hexBinary".equals(type.getName())) {
+            } else if (Types.HEX_BINARY.equals(type.getName())) {
                 return "EF56AE";
-            } else if ("byte".equals(type.getName()) || "unsignedByte".equals(type.getName())) {
+            } else if (Types.BYTE.equals(type.getName()) || Types.UNSIGNED_BYTE.equals(type.getName())) {
                 return (byte) (random.nextInt() % 2);
-            } else if ("double".equals(type.getName()) || "unsignedDouble".equals(type.getName())) {
+            } else if (Types.DOUBLE.equals(type.getName()) || Types.UNSIGNED_DOUBLE.equals(type.getName())) {
                 return random.nextDouble();
-            } else if ("duration".equals(type.getName()) || "time".equals(type.getName())) {
+            } else if (Types.DURATION.equals(type.getName()) || Types.TIME.equals(type.getName())) {
                 return new Timestamp(random.nextInt());
             } else {
                 throw new NotImplementedException("No support for type '" + type.getName() + "'");
