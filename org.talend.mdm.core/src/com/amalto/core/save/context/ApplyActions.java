acfc96fea3d9e1e4428ac6cf21f4b2af1ffa434b
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-11 15:30:20 +0000

    merge r114894 from trunk to branch 5.4 Incorrect log in trace for save actions.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_4@114895 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/save/context/ApplyActions.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/ApplyActions.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.save.context;
+
+import org.apache.log4j.Logger;
 
 import com.amalto.core.history.Action;
 import com.amalto.core.history.MutableDocument;
 import com.amalto.core.save.DocumentSaverContext;
 import com.amalto.core.save.SaverSession;
-import org.apache.log4j.Logger;
 
 class ApplyActions implements DocumentSaver {
 
@@ -29,16 +29,18 @@ class ApplyActions implements DocumentSaver {
 
     public void save(SaverSession session, DocumentSaverContext context) {
         MutableDocument databaseDocument = context.getDatabaseDocument();
-        for (Action action : context.getActions()) {
-            if (LOGGER.isTraceEnabled()) {
-                StringBuilder builder = new StringBuilder();
-                String[] ids = context.getId();
-                for (String id : ids) {
-                    builder.append(' ').append(id);
-                }
-                LOGGER.trace("Actions for record of type '" + context.getDatabaseDocument().getType() + "' (id:" + builder + ")");
+        if (LOGGER.isTraceEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            String[] ids = context.getId();
+            for (String id : ids) {
+                builder.append(' ').append(id);
+            }
+            LOGGER.trace("Actions for record of type '" + context.getDatabaseDocument().getType() + "' (id:" + builder + ")");
+            for (Action action : context.getActions()) {
                 LOGGER.trace("   " + action);
             }
+        }
+        for (Action action : context.getActions()) {
             action.perform(databaseDocument);
         }
         databaseDocument.clean();
