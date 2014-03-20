82bafdc5bbfa7cde56e052ab26e7520ca305da36
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-20 13:13:26 +0000

    TMDM-6078: Client code no longer needs to deploy new version of data model (update data model in system db upon successful adapt()).
    git-svn-id: http://talendforge.org/svn/tom/trunk@115269 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
@@ -10,66 +10,137 @@
 
 package com.amalto.core.storage.services;
 
+import static com.amalto.core.query.user.UserQueryBuilder.eq;
+import static com.amalto.core.query.user.UserQueryBuilder.from;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
+import com.amalto.core.server.MetadataRepositoryAdmin;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
 import org.talend.mdm.commmon.metadata.compare.Change;
 import org.talend.mdm.commmon.metadata.compare.Compare;
 import org.talend.mdm.commmon.metadata.compare.ImpactAnalyzer;
 
+import com.amalto.core.query.user.UserQueryBuilder;
 import com.amalto.core.server.ServerContext;
 import com.amalto.core.server.StorageAdmin;
 import com.amalto.core.storage.Storage;
+import com.amalto.core.storage.StorageResults;
 import com.amalto.core.storage.StorageType;
+import com.amalto.core.storage.record.DataRecord;
 
-@Path("/system/models") //$NON-NLS-1$
+@Path("/system/models")//$NON-NLS-1$
 public class SystemModels {
 
     private static final Logger LOGGER = Logger.getLogger(SystemModels.class);
 
+    private final Storage systemStorage;
+
+    private final ComplexTypeMetadata dataModelType;
+
+    public SystemModels() {
+        StorageAdmin storageAdmin = ServerContext.INSTANCE.get().getStorageAdmin();
+        systemStorage = storageAdmin.get(StorageAdmin.SYSTEM_STORAGE, StorageType.SYSTEM, null);
+        MetadataRepository repository = systemStorage.getMetadataRepository();
+        dataModelType = repository.getComplexType("data-model-pOJO"); //$NON-NLS-1$
+    }
+
     @GET
-    @Path("{model}") //$NON-NLS-1$
-    public String getSchema(@PathParam("model") //$NON-NLS-1$
-    String modelName) {
-        throw new UnsupportedOperationException("Get a data model content isn't currently supported.");
+    @Path("{model}")//$NON-NLS-1$
+    public String getSchema(@PathParam("model")//$NON-NLS-1$
+            String modelName) {
+        UserQueryBuilder qb = from(dataModelType).where(eq(dataModelType.getField("name"), modelName)); //$NON-NLS-1$
+        systemStorage.begin();
+        try {
+            StorageResults results = systemStorage.fetch(qb.getSelect());
+            Iterator<DataRecord> iterator = results.iterator();
+            String modelContent = StringUtils.EMPTY;
+            if (iterator.hasNext()) {
+                DataRecord model = iterator.next();
+                if (iterator.hasNext()) {
+                    throw new IllegalStateException("Found multiple data models for '" + modelName + "'.");
+                }
+                modelContent = String.valueOf(model.get("schema")); //$NON-NLS-1$
+                return modelContent;
+            }
+            systemStorage.commit();
+            return modelContent;
+        } catch (Exception e) {
+            systemStorage.rollback();
+            throw new RuntimeException("Could not update data model.", e);
+        }
     }
 
     @PUT
-    @Path("{model}") //$NON-NLS-1$
-    public void updateModel(@PathParam("model") //$NON-NLS-1$
-    String modelName, @QueryParam("force")
-    boolean force, InputStream dataModel) {
+    @Path("{model}")//$NON-NLS-1$
+    public void updateModel(@PathParam("model")//$NON-NLS-1$
+            String modelName, @QueryParam("force")//$NON-NLS-1$
+            boolean force, InputStream dataModel) {
         StorageAdmin storageAdmin = ServerContext.INSTANCE.get().getStorageAdmin();
+        MetadataRepositoryAdmin metadataRepositoryAdmin = ServerContext.INSTANCE.get().getMetadataRepositoryAdmin();
         Storage storage = storageAdmin.get(modelName, StorageType.MASTER, null);
         if (storage == null) {
             throw new IllegalArgumentException("Container '" + modelName + "' does not exist.");
         }
+        String content;
+        try {
+            content = IOUtils.toString(dataModel, "UTF-8"); //$NON-NLS-1$
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read data model from body.", e);
+        }
         // Compare new data model with existing data model
+        MetadataRepository previousRepository = storage.getMetadataRepository();
         MetadataRepository newRepository = new MetadataRepository();
-        newRepository.load(dataModel);
+        newRepository.load(new ByteArrayInputStream(content.getBytes()));
         // Ask the storage to adapt its structure following the changes
         storage.adapt(newRepository, force);
+        // Update the system storage with the new data model (if there was any change).
+        Compare.DiffResults diffResults = Compare.compare(previousRepository, newRepository);
+        if (!diffResults.getActions().isEmpty()) {
+            UserQueryBuilder qb = from(dataModelType).where(eq(dataModelType.getField("name"), modelName)); //$NON-NLS-1$
+            systemStorage.begin();
+            try {
+                StorageResults results = systemStorage.fetch(qb.getSelect());
+                Iterator<DataRecord> iterator = results.iterator();
+                if (iterator.hasNext()) {
+                    // Updates the data model in system db
+                    DataRecord model = iterator.next();
+                    model.set(dataModelType.getField("schema"), content); //$NON-NLS-1$
+                    if (iterator.hasNext()) {
+                        throw new IllegalStateException("Found multiple data models for '" + modelName + "'.");
+                    }
+                    systemStorage.update(model);
+                    // Forces update in metadata repository admin
+                    metadataRepositoryAdmin.remove(modelName);
+                    metadataRepositoryAdmin.get(modelName);
+                }
+                systemStorage.commit();
+            } catch (Exception e) {
+                systemStorage.rollback();
+                throw new RuntimeException("Could not update data model.", e);
+            }
+        }
     }
 
     @POST
-    @Path("{model}") //$NON-NLS-1$
-    public String analyzeModelChange(@PathParam("model") //$NON-NLS-1$
-    String modelName, InputStream dataModel) {
+    @Path("{model}")//$NON-NLS-1$
+    public String analyzeModelChange(@PathParam("model")//$NON-NLS-1$
+            String modelName, InputStream dataModel) {
         StorageAdmin storageAdmin = ServerContext.INSTANCE.get().getStorageAdmin();
         Storage storage = storageAdmin.get(modelName, StorageType.MASTER, null);
         if (storage == null) {
