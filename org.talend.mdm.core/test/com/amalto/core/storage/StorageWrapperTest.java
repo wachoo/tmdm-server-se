97249a34962366e73693e65ea6901ac3fdd3019f
yjli <yjli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-05-06 09:56:18 +0000

    TMDM-7208 : Fixed multiple occurrence complex type element update issue.
    git-svn-id: http://talendforge.org/svn/tom/trunk@117146 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/test/com/amalto/core/storage/StorageWrapperTest.java
+++ org.talend.mdm.core/test/com/amalto/core/storage/StorageWrapperTest.java
@@ -76,34 +76,58 @@ public class StorageWrapperTest extends TestCase {
             }
         };
         String xml = "<ii><c>Product</c><n>Product</n><dmn>Product</dmn><i>333</i><t>1372654669313</t><taskId></taskId><p> <Product><Id>333</Id><Name>333</Name><Description>333</Description><Price>333</Price></Product></p></ii>"; //$NON-NLS-1$
-        wrapper.start("Product");
+        wrapper.start("Product"); //$NON-NLS-1$
         {
             wrapper.putDocumentFromString(xml, "Product.Product.333", "Product", null); //$NON-NLS-1$ //$NON-NLS-2$
         }
-        wrapper.commit("Product");
+        wrapper.commit("Product"); //$NON-NLS-1$
         String item = wrapper.getDocumentAsString(null, "Product", "Product.Product.333"); //$NON-NLS-1$ //$NON-NLS-2$
         assertNotNull(item);
         assertTrue(item.contains("<i>333</i>")); //$NON-NLS-1$
 
         xml = "<ii><c>Product</c><n>Product</n><dmn>Product</dmn><i>33&amp;44</i><t>1372654669313</t><taskId></taskId><p> <Product><Id>33&amp;44</Id><Name>333</Name><Description>333</Description><Price>333</Price></Product></p></ii>"; //$NON-NLS-1$
-        wrapper.start("Product");
+        wrapper.start("Product"); //$NON-NLS-1$
         {
             wrapper.putDocumentFromString(xml, "Product.Product.33&44", "Product", null); //$NON-NLS-1$ //$NON-NLS-2$
         }
-        wrapper.commit("Product");
+        wrapper.commit("Product"); //$NON-NLS-1$
         item = wrapper.getDocumentAsString(null, "Product", "Product.Product.33&44"); //$NON-NLS-1$ //$NON-NLS-2$
         assertNotNull(item);
         assertTrue(item.contains("<i>33&amp;44</i>")); //$NON-NLS-1$
 
         xml = "<ii><c>Product</c><n>Product</n><dmn>Product</dmn><i>&quot;555&lt;666&gt;444&quot;</i><t>1372654669313</t><taskId></taskId><p> <Product><Id>&quot;555&lt;666&gt;444&quot;</Id><Name>333</Name><Description>333</Description><Price>333</Price></Product></p></ii>"; //$NON-NLS-1$
-        wrapper.start("Product");
+        wrapper.start("Product"); //$NON-NLS-1$
         {
             wrapper.putDocumentFromString(xml, "Product.Product.\"555<666>444\"", "Product", null); //$NON-NLS-1$ //$NON-NLS-2$
         }
-        wrapper.commit("Product");
+        wrapper.commit("Product"); //$NON-NLS-1$
         item = wrapper.getDocumentAsString(null, "Product", "Product.Product.\"555<666>444\""); //$NON-NLS-1$ //$NON-NLS-2$
         assertNotNull(item);
         assertTrue(item.contains("<i>&quot;555&lt;666&gt;444&quot;</i>")); //$NON-NLS-1$
+    }
+
+    public void testMultipleOccurrenceComplex() throws IOException {
+        final MetadataRepository repository = prepareMetadata("MultipleOccurrenceComplex.xsd"); //$NON-NLS-1$
+        final Storage storage = prepareStorage(repository);
+        DataRecordReader<String> factory = new XmlStringDataRecordReader();
+        String recordXml = "<C><subelement>1</subelement><A xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"B\"><a>7</a><B>6</B></A><A xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"A\"><a>5</a></A><A xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"B\"><a>4</a><B>3</B></A><Aa xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"B\"><a>2</a><B>1</B></Aa></C>"; //$NON-NLS-1$
+        List<DataRecord> records = new LinkedList<DataRecord>();
+        records.add(factory.read("1", repository, repository.getComplexType("C"), recordXml)); //$NON-NLS-1$ //$NON-NLS-2$
+        storage.begin();
+        storage.update(records);
+        storage.commit();
+
+        storage.begin();
+        ComplexTypeMetadata c = repository.getComplexType("C"); //$NON-NLS-1$
+        UserQueryBuilder qb = from(c);
+        qb.start(0);
+        qb.limit(1);
+        StorageResults results = storage.fetch(qb.getSelect());
+        DataRecord result = results.iterator().next();
+        DataRecordXmlWriter writer = new DataRecordXmlWriter(c);
+        ResettableStringWriter stringWriter = new ResettableStringWriter();
+        writer.write(result, stringWriter);
+        assertEquals(recordXml, stringWriter.toString());
     }
 
     public void testDateTimeAsBaseType() throws XmlServerException, ParserConfigurationException, SAXException, IOException,
@@ -113,8 +137,8 @@ public class StorageWrapperTest extends TestCase {
         DataRecordReader<String> factory = new XmlStringDataRecordReader();
 
         List<DataRecord> records = new LinkedList<DataRecord>();
-        records.add(factory.read("1", repository, repository.getComplexType("Employee"),
-                "<Employee><Id>22</Id><Holiday>2014-04-17T12:00:00</Holiday><birthday>2014-04-16T12:00:00</birthday></Employee>"));
+        records.add(factory.read("1", repository, repository.getComplexType("Employee"), //$NON-NLS-1$ //$NON-NLS-2$
+                "<Employee><Id>22</Id><Holiday>2014-04-17T12:00:00</Holiday><birthday>2014-04-16T12:00:00</birthday></Employee>")); //$NON-NLS-1$
         storage.begin();
         storage.update(records);
         storage.commit();
@@ -145,7 +169,7 @@ public class StorageWrapperTest extends TestCase {
         for (int j = 0; j < holiday.getLength(); j++) {
             datetimeStrValues.add(holiday.item(j).getFirstChild() == null ? "" : holiday.item(j).getFirstChild().getNodeValue()); //$NON-NLS-1$
         }
-        assertTrue(datetimeStrValues.contains("2014-04-17T12:00:00"));
+        assertTrue(datetimeStrValues.contains("2014-04-17T12:00:00")); //$NON-NLS-1$
         stringWriter.reset();
 
         DataRecordWriter viewWriter = new ViewSearchResultsWriter();
@@ -155,7 +179,7 @@ public class StorageWrapperTest extends TestCase {
         } catch (IOException e) {
             throw new XmlServerException(e);
         }
-        String resultsAsString = new String(output.toByteArray(), Charset.forName("UTF-8"));
+        String resultsAsString = new String(output.toByteArray(), Charset.forName("UTF-8")); //$NON-NLS-1$
         output.reset();
 
         xpath = XPathFactory.newInstance().newXPath();
@@ -166,7 +190,7 @@ public class StorageWrapperTest extends TestCase {
             datetimeStrValues
                     .add(birthday.item(j).getFirstChild() == null ? "" : birthday.item(j).getFirstChild().getNodeValue()); //$NON-NLS-1$
         }
-        assertTrue(datetimeStrValues.contains("2014-04-16T12:00:00"));
+        assertTrue(datetimeStrValues.contains("2014-04-16T12:00:00")); //$NON-NLS-1$
 
         storage.commit();
 
@@ -176,8 +200,8 @@ public class StorageWrapperTest extends TestCase {
         factory = new XmlStringDataRecordReader();
 
         records.clear();
-        records.add(factory.read("1", repository2, repository2.getComplexType("CustomDate"),
-                "<CustomDate><Id>22</Id><MyDate>2014-04-17</MyDate></CustomDate>"));
+        records.add(factory.read("1", repository2, repository2.getComplexType("CustomDate"), //$NON-NLS-1$ //$NON-NLS-2$
+                "<CustomDate><Id>22</Id><MyDate>2014-04-17</MyDate></CustomDate>")); //$NON-NLS-1$
         storage2.begin();
         storage2.update(records);
         storage2.commit();
@@ -206,7 +230,7 @@ public class StorageWrapperTest extends TestCase {
         for (int j = 0; j < holiday.getLength(); j++) {
             dateStrValues.add(holiday.item(j).getFirstChild() == null ? "" : holiday.item(j).getFirstChild().getNodeValue()); //$NON-NLS-1$
         }
-        assertTrue(dateStrValues.contains("2014-04-17"));
+        assertTrue(dateStrValues.contains("2014-04-17")); //$NON-NLS-1$
         stringWriter.reset();
 
         storage2.commit();
