67b03538f04412c559727c806e995cb11fa18b7d
glzhou <glzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-06-11 04:20:36 +0000

    merge r118449 from branch 5.5 to branch 5.4 merge r118414 from trunk to branch 5.5 TMDM-7367: Journal-before and after value in wrong side. https://jira.talendforge.org/browse/TMDM-7367
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_4@118450 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/save/context/ChangeTypeAction.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/ChangeTypeAction.java
@@ -3,33 +3,42 @@
  *
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.save.context;
 
-import com.amalto.core.history.MutableDocument;
-import com.amalto.core.history.accessor.Accessor;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.XMLConstants;
+
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.FieldMetadata;
 import org.w3c.dom.Document;
 
-import javax.xml.XMLConstants;
-import java.util.Date;
+import com.amalto.core.history.Action;
+import com.amalto.core.history.MutableDocument;
+import com.amalto.core.history.accessor.Accessor;
 
 class ChangeTypeAction extends AbstractChangeTypeAction {
 
-    public ChangeTypeAction(Date date,
-                            String source,
-                            String userName,
-                            String path,
-                            ComplexTypeMetadata previousType,
-                            ComplexTypeMetadata newType,
-                            FieldMetadata field) {
-        super(date, source, userName, path, previousType, newType, field);
+    private ChangeTypeAction(MutableDocument document, Date date, String source, String userName, String path, ComplexTypeMetadata previousType,
+            ComplexTypeMetadata newType, FieldMetadata field) {
+        super(document, date, source, userName, path, previousType, newType, field);
+    }
+
+    public static List<Action> create(MutableDocument document, Date date, String source, String userName, String path,
+            ComplexTypeMetadata previousType, ComplexTypeMetadata newType, FieldMetadata field) {
+        // Create action
+        List<Action> actions = new LinkedList<Action>();
+        ChangeTypeAction typeAction = new ChangeTypeAction(document, date, source, userName, path, previousType, newType, field);
+        actions.addAll(typeAction.getImpliedActions());
+        actions.add(typeAction);
+        return actions;
     }
 
     public MutableDocument perform(MutableDocument document) {
@@ -46,14 +55,6 @@ class ChangeTypeAction extends AbstractChangeTypeAction {
 
         Accessor typeAccessor = document.createAccessor(path + "/@xsi:type"); //$NON-NLS-1$
         String typeName = newType.getName();
-        if (typeAccessor.exist() && !typeName.equals(typeAccessor.get())) {
-            for (String currentPathToDelete : pathToClean) {
-                Accessor accessor = document.createAccessor(path + '/' + currentPathToDelete);
-                if (accessor.exist()) {
-                    accessor.delete();
-                }
-            }
-        }
         typeAccessor.createAndSet(typeName);
         return document;
     }
@@ -64,7 +65,9 @@ class ChangeTypeAction extends AbstractChangeTypeAction {
             return document;
         }
         Accessor accessor = document.createAccessor(path + "/@xsi:type"); //$NON-NLS-1$
-        accessor.delete();
+        if (previousType != null) {
+            accessor.set(previousType.getName());
+        }
         return document;
     }
 
