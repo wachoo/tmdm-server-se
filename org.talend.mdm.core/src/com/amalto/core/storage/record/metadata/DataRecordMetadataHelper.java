bc3599c8d2147e7b6b6fbe10f5c4995314d909dd
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-06-16 09:22:42 +0000

    TMDM-7422: Group id in results now contains the already-set group id or nothing (in case of a new group) // https://jira.talendforge.org/browse/TMDM-7422
    git-svn-id: http://talendforge.org/svn/tom/trunk@118566 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/storage/record/metadata/DataRecordMetadataHelper.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/record/metadata/DataRecordMetadataHelper.java
@@ -6,6 +6,7 @@ import com.amalto.core.query.user.metadata.StagingSource;
 import com.amalto.core.query.user.metadata.StagingStatus;
 import com.amalto.core.storage.Storage;
 import com.amalto.core.storage.record.DataRecordReader;
+import org.apache.log4j.Logger;
 
 import java.util.Map;
 
@@ -14,23 +15,25 @@ import java.util.Map;
  */
 public class DataRecordMetadataHelper {
 
+    private static final Logger LOGGER = Logger.getLogger(DataRecordMetadataHelper.class);
+
     private DataRecordMetadataHelper() {
     }
 
     public static void setMetadataValue(DataRecordMetadata metadata, String metadataProperty, String value) {
         Map<String, String> properties = metadata.getRecordProperties();
-        if(StagingError.STAGING_ERROR_ALIAS.equals(metadataProperty)) {
+        if (StagingError.STAGING_ERROR_ALIAS.equals(metadataProperty)) {
             properties.put(Storage.METADATA_STAGING_ERROR, value);
-        } else if(StagingSource.STAGING_SOURCE_ALIAS.equals(metadataProperty)) {
+        } else if (StagingSource.STAGING_SOURCE_ALIAS.equals(metadataProperty)) {
             properties.put(Storage.METADATA_STAGING_SOURCE, value);
-        } else if(StagingStatus.STAGING_STATUS_ALIAS.equals(metadataProperty)) {
+        } else if (StagingStatus.STAGING_STATUS_ALIAS.equals(metadataProperty)) {
             properties.put(Storage.METADATA_STAGING_STATUS, value);
         } else if (StagingBlockKey.STAGING_BLOCK_ALIAS.equals(metadataProperty)) {
             properties.put(Storage.METADATA_STAGING_BLOCK_KEY, value);
         } else if (DataRecordReader.TASK_ID.equals(metadataProperty)) {
             metadata.setTaskId(value);
-        } else {
-            throw new UnsupportedOperationException("Metadata parameter '" + metadataProperty + "' is not supported.");
+        } else if(LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Metadata parameter '" + metadataProperty + "' is not supported.");
         }
     }
 }
