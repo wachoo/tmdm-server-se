5994bd6961d6fbb61ee58e7663961cbf4ecee599
Sébastien Gandon <sgandon@talend.com>
2014-06-22 15:25:37 +0200

    [GIT Migration] Removed unnecessary root files on branch branch-5_2 from [tom]

--- org.talend.mdm.webapp.itemsbrowser2/src/main/java/org/talend/mdm/webapp/itemsbrowser2/server/bizhelpers/ItemHelper.java
+++
@@ -1,8 +0,0 @@
-package org.talend.mdm.webapp.itemsbrowser2.server.bizhelpers;
-
-
-public class ItemHelper {
-    public static final String SEARCH_DIRECTION_ASC = "ascending"; //$NON-NLS-1$
-    
-    public static final String SEARCH_DIRECTION_DESC = "descending"; //$NON-NLS-1$
-}
