bf1c74dd72bda422af79afb8e467d0a73327b74a
fhuaulme <fhuaulme@talend.com>
2014-07-08 11:12:44 +0200

    TMDM-7463: "Distinct operator is not supported on long text field on SQL Server"
    Code clean up (typo in constants).
    Missing NLS.

--- org.talend.mdm.core/src/com/amalto/core/storage/services/EventStatistics.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/EventStatistics.java
@@ -16,10 +16,6 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -49,18 +45,18 @@ import com.amalto.core.storage.StorageResults;
 import com.amalto.core.storage.StorageType;
 import com.amalto.core.storage.record.DataRecord;
 
-@Path("/system/stats/events")
+@Path("/system/stats/events") //$NON-NLS-1$
 public class EventStatistics {
 
     private static final Logger LOGGER = Logger.getLogger(EventStatistics.class);
 
-    private final String CALLJOB_SERVICE_JNDI_NAME = "amalto/local/service/callJob"; //$NON-NLS-1$
+    private static final String CALL_JOB_JNDI_NAME = "amalto/local/service/callJob"; //$NON-NLS-1$
 
-    private final String CALLPROCESS_SERVICE_JNDI_NAME = "amalto/local/service/callprocess"; //$NON-NLS-1$
+    private static final String CALL_PROCESS_JNDI_NAME = "amalto/local/service/callprocess"; //$NON-NLS-1$
 
-    private final String WORKFLOW_SERVICE_JNDI_NAME = "amalto/local/service/workflow"; //$NON-NLS-1$
+    private static final String WORKFLOW_JNDI_NAME = "amalto/local/service/workflow"; //$NON-NLS-1$
 
-    private final String WORKFLOWCONTEXT_SERVICE_JNDI_NAME = "amalto/local/service/workflowcontext"; //$NON-NLS-1$
+    private static final String WORKFLOW_CONTEXT_JNDI_NAME = "amalto/local/service/workflowcontext"; //$NON-NLS-1$
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -68,7 +64,7 @@ public class EventStatistics {
         StorageAdmin storageAdmin = ServerContext.INSTANCE.get().getStorageAdmin();
         Storage system = storageAdmin.get(StorageAdmin.SYSTEM_STORAGE, StorageType.SYSTEM, null);
         if (system == null) {
-            LOGGER.debug("Could not find system storage. Statistics is not supported for XMLDB"); //$NON-NLS-1$
+            LOGGER.debug("Could not find system storage. Statistics is not supported for XML database"); //$NON-NLS-1$
             return Response.status(Response.Status.NO_CONTENT).build();
         }
         // Build statistics
@@ -104,72 +100,57 @@ public class EventStatistics {
             throws JSONException {
         FieldMetadata parameters = routingOrderType.getField("service-parameters"); //$NON-NLS-1$
         FieldMetadata jndiNameField = routingOrderType.getField("service-jNDI"); //$NON-NLS-1$
-        // TMDM-7463: Can't use distinct with SQL Server, work around this
-        Expression routingNames = from(routingOrderType).select(parameters).select(jndiNameField).cache().getExpression();
-        Map<String, List<String>> distinctRoutingNames = new HashMap<String, List<String>>();
-        StorageResults routingNameResults = system.fetch(routingNames);
-        try {
-            for (DataRecord result : routingNameResults) {
-                String parameterString = String.valueOf(result.get(parameters));
-                List<String> names = distinctRoutingNames.get(parameterString);
-                if (names == null) {
-                    names = new LinkedList<String>();
-                    distinctRoutingNames.put(parameterString, names);
-                }
-                String jndiNameFieldString = String.valueOf(result.get(jndiNameField));
-                names.add(jndiNameFieldString);
-            }
-        } finally {
-            routingNameResults.close();
-        }
-        // Transforms distinct events to JSON format. 
+        Expression routingNames = from(routingOrderType).select(alias(distinct(parameters), parameters.getName()))
+                .select(jndiNameField).cache().getExpression();
         writer.object().key(categoryName);
         {
             writer.array();
             {
+                StorageResults routingNameResults = system.fetch(routingNames);
                 try {
                     XMLReader reader = XMLReaderFactory.createXMLReader();
-                    for (Map.Entry<String, List<String>> entry : distinctRoutingNames.entrySet()) {
-                        String parameter = entry.getKey();
-                        for (String jndiName : entry.getValue()) {
-                            String key = null;
-                            // TMDM-7324: handle different possible values.
-                            if (jndiName.equals(CALLJOB_SERVICE_JNDI_NAME)) {
-                                ParameterReader handler = new ParameterReader("url"); //$NON-NLS-1$
-                                reader.setContentHandler(handler);
-                                reader.parse(new InputSource(new StringReader(parameter)));
-                                String url = handler.getParameterValue();
-                                try {
-                                    key = (new URI(url)).getHost();
-                                } catch (URISyntaxException e) {
-                                    LOGGER.warn("Could not get information from '" + url + "'", e);
-                                    key = url; // As fallback, put the whole parameter content.
-                                }
-                            } else if (jndiName.equals(CALLPROCESS_SERVICE_JNDI_NAME)) {
-                                key = parameter.replace("process=", StringUtils.EMPTY); //$NON-NLS-1$
-                            } else if (jndiName.equals(WORKFLOW_SERVICE_JNDI_NAME)
-                                    || jndiName.equals(WORKFLOWCONTEXT_SERVICE_JNDI_NAME)) {
-                                ParameterReader handler = new ParameterReader("processId"); //$NON-NLS-1$
-                                reader.setContentHandler(handler);
-                                reader.parse(new InputSource(new StringReader(parameter)));
-                                key = handler.getParameterValue();
+                    for (DataRecord routingNameResult : routingNameResults) {
+                        // Get the URL called by event
+                        String parameter = String.valueOf(routingNameResult.get(parameters));
+                        String jndiName = String.valueOf(routingNameResult.get(jndiNameField));
+                        String key = null;
+                        // TMDM-7324: handle different possible values.
+                        if (jndiName.equals(CALL_JOB_JNDI_NAME)) {
+                            ParameterReader handler = new ParameterReader("url"); //$NON-NLS-1$
+                            reader.setContentHandler(handler);
+                            reader.parse(new InputSource(new StringReader(parameter)));
+                            String url = handler.getParameterValue();
+                            try {
+                                key = (new URI(url)).getHost();
+                            } catch (URISyntaxException e) {
+                                LOGGER.warn("Could not get information from '" + url + "'", e);
+                                key = url; // As fallback, put the whole parameter content.
                             }
-                            if (key != null && !key.isEmpty()) {
-                                // Count the number of similar events
-                                Expression routingNameCount = from(routingOrderType).select(alias(count(), "count")) //$NON-NLS-1$
-                                        .where(eq(parameters, parameter)).limit(1).cache().getExpression();
-                                StorageResults countResult = system.fetch(routingNameCount);
-                                try {
-                                    // ... and write count to result
-                                    writer.object().key(key).value(countResult.iterator().next().get("count")).endObject(); //$NON-NLS-1$
-                                } finally {
-                                    countResult.close();
-                                }
+                        } else if (jndiName.equals(CALL_PROCESS_JNDI_NAME)) {
+                            key = parameter.replace("process=", StringUtils.EMPTY); //$NON-NLS-1$
+                        } else if (jndiName.equals(WORKFLOW_JNDI_NAME) || jndiName.equals(WORKFLOW_CONTEXT_JNDI_NAME)) {
+                            ParameterReader handler = new ParameterReader("processId"); //$NON-NLS-1$
+                            reader.setContentHandler(handler);
+                            reader.parse(new InputSource(new StringReader(parameter)));
+                            key = handler.getParameterValue();
+                        }
+                        if (key != null && !key.isEmpty()) {
+                            // Count the number of similar events
+                            Expression routingNameCount = from(routingOrderType).select(alias(count(), "count")) //$NON-NLS-1$
+                                    .where(eq(parameters, parameter)).limit(1).cache().getExpression();
+                            StorageResults failedCountResult = system.fetch(routingNameCount);
+                            try {
+                                // ... and write count to result
+                                writer.object().key(key).value(failedCountResult.iterator().next().get("count")).endObject(); //$NON-NLS-1$
+                            } finally {
+                                failedCountResult.close();
                             }
                         }
                     }
                 } catch (Exception e) {
                     throw new RuntimeException("Could not build event statistics for '" + categoryName + "' events", e);
+                } finally {
+                    routingNameResults.close();
                 }
             }
             writer.endArray();
