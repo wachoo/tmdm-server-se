fe11106fd2a5dd7177e51f46cdf695241715a78d
fhuaulme <fhuaulme@talend.com>
2014-07-22 11:56:53 +0200

    TMDM-7496: "'contains the word(s)' on complex type field doesn't work when using full text "
    Includes non-instantiable types (reusable type) in full text query.
    
    (cherry picked from commit ab608d81f829752a4ab6543c13ef424ac1d3bd18)
    
    Conflicts:
    	org.talend.mdm.core/src/com/amalto/core/storage/hibernate/LuceneQueryGenerator.java

--- org.talend.mdm.core/src/com/amalto/core/storage/hibernate/LuceneQueryGenerator.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/hibernate/LuceneQueryGenerator.java
@@ -1,31 +1,73 @@
 /*
  * Copyright (C) 2006-2014 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.storage.hibernate;
 
-import com.amalto.core.query.user.*;
-import com.amalto.core.query.user.metadata.*;
-import com.amalto.core.storage.Storage;
-import com.amalto.core.storage.datasource.RDBMSDataSource;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.KeywordAnalyzer;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.search.*;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.NumericRangeQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.util.Version;
-import org.talend.mdm.commmon.metadata.*;
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.ContainedComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.DefaultMetadataVisitor;
+import org.talend.mdm.commmon.metadata.EnumerationFieldMetadata;
+import org.talend.mdm.commmon.metadata.ReferenceFieldMetadata;
+import org.talend.mdm.commmon.metadata.SimpleTypeFieldMetadata;
 
-import java.util.*;
+import com.amalto.core.query.user.Alias;
+import com.amalto.core.query.user.BigDecimalConstant;
+import com.amalto.core.query.user.BinaryLogicOperator;
+import com.amalto.core.query.user.BooleanConstant;
+import com.amalto.core.query.user.ByteConstant;
+import com.amalto.core.query.user.Compare;
+import com.amalto.core.query.user.DateConstant;
+import com.amalto.core.query.user.DateTimeConstant;
+import com.amalto.core.query.user.DoubleConstant;
+import com.amalto.core.query.user.Expression;
+import com.amalto.core.query.user.Field;
+import com.amalto.core.query.user.FieldFullText;
+import com.amalto.core.query.user.FloatConstant;
+import com.amalto.core.query.user.FullText;
+import com.amalto.core.query.user.IntegerConstant;
+import com.amalto.core.query.user.LongConstant;
+import com.amalto.core.query.user.Predicate;
+import com.amalto.core.query.user.Range;
+import com.amalto.core.query.user.ShortConstant;
+import com.amalto.core.query.user.StringConstant;
+import com.amalto.core.query.user.TimeConstant;
+import com.amalto.core.query.user.UnaryLogicOperator;
+import com.amalto.core.query.user.VisitorAdapter;
+import com.amalto.core.query.user.metadata.MetadataField;
+import com.amalto.core.query.user.metadata.StagingBlockKey;
+import com.amalto.core.query.user.metadata.StagingError;
+import com.amalto.core.query.user.metadata.StagingSource;
+import com.amalto.core.query.user.metadata.StagingStatus;
+import com.amalto.core.query.user.metadata.TaskId;
+import com.amalto.core.query.user.metadata.Timestamp;
+import com.amalto.core.storage.Storage;
+import com.amalto.core.storage.datasource.RDBMSDataSource;
 
 class LuceneQueryGenerator extends VisitorAdapter<Query> {
 
@@ -39,7 +81,7 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
 
     private boolean isBuildingNot;
 
-    LuceneQueryGenerator(List<ComplexTypeMetadata> types, RDBMSDataSource dataSource) {
+    LuceneQueryGenerator(Collection<ComplexTypeMetadata> types, RDBMSDataSource dataSource) {
         this.types = types;
         this.dataSource = dataSource;
     }
@@ -49,8 +91,7 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
         condition.getLeft().accept(this);
         Expression right = condition.getRight();
         right.accept(this);
-        if (condition.getPredicate() == Predicate.EQUALS
-                || condition.getPredicate() == Predicate.CONTAINS
+        if (condition.getPredicate() == Predicate.EQUALS || condition.getPredicate() == Predicate.CONTAINS
                 || condition.getPredicate() == Predicate.STARTS_WITH) {
             StringTokenizer tokenizer = new StringTokenizer(String.valueOf(currentValue));
             BooleanQuery termQuery = new BooleanQuery();
@@ -64,11 +105,10 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
             return termQuery;
         } else if (condition.getPredicate() == Predicate.GREATER_THAN
                 || condition.getPredicate() == Predicate.GREATER_THAN_OR_EQUALS
-                || condition.getPredicate() == Predicate.LOWER_THAN
-                || condition.getPredicate() == Predicate.LOWER_THAN_OR_EQUALS) {
-            throw new RuntimeException("Greater than, less than are not supported in full text searches.");
+                || condition.getPredicate() == Predicate.LOWER_THAN || condition.getPredicate() == Predicate.LOWER_THAN_OR_EQUALS) {
+            throw new RuntimeException("Greater than, less than are not supported in full text searches."); //$NON-NLS-1$
         } else {
-            throw new NotImplementedException("No support for predicate '" + condition.getPredicate() + "'");
+            throw new NotImplementedException("No support for predicate '" + condition.getPredicate() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
         }
     }
 
@@ -84,7 +124,7 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
             query.add(left, isNotQuery(left) ? BooleanClause.Occur.SHOULD : BooleanClause.Occur.MUST);
             query.add(right, isNotQuery(right) ? BooleanClause.Occur.SHOULD : BooleanClause.Occur.MUST);
         } else {
-            throw new NotImplementedException("No support for '" + condition.getPredicate() + "'.");
+            throw new NotImplementedException("No support for '" + condition.getPredicate() + "'."); //$NON-NLS-1$ //$NON-NLS-2$
         }
         return query;
     }
@@ -108,21 +148,32 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
             isBuildingNot = false;
             return query;
         } else {
-            throw new NotImplementedException("No support for predicate '" + condition.getPredicate() + "'.");
+            throw new NotImplementedException("No support for predicate '" + condition.getPredicate() + "'."); //$NON-NLS-1$//$NON-NLS-2$
         }
     }
 
     @Override
     public Query visit(Range range) {
+        if (range.getExpression() instanceof MetadataField) {
+            if (range.getExpression() instanceof Timestamp) {
+                Timestamp field = (Timestamp) range.getExpression();
+                field.accept(this);
+            } else {
+                MetadataField field = (MetadataField) range.getExpression();
+                field.getProjectionExpression().accept(this);
+            }
+        }
         range.getStart().accept(this);
-        Integer currentRangeStart = ((Integer) currentValue) == Integer.MIN_VALUE ? null : (Integer) currentValue;
+        Long currentRangeStart = ((Long) currentValue) == Long.MIN_VALUE ? null : (Long) currentValue;
         range.getEnd().accept(this);
-        Integer currentRangeEnd = ((Integer) currentValue) == Integer.MAX_VALUE ? null : (Integer) currentValue;
-        return NumericRangeQuery.newIntRange(currentFieldName,
-                currentRangeStart,
-                currentRangeEnd,
-                true,
-                true);
+        Long currentRangeEnd = ((Long) currentValue) == Long.MAX_VALUE ? null : (Long) currentValue;
+        return NumericRangeQuery.newLongRange(currentFieldName, currentRangeStart, currentRangeEnd, true, true);
+    }
+
+    @Override
+    public Query visit(Timestamp timestamp) {
+        currentFieldName = Storage.METADATA_TIMESTAMP;
+        return null;
     }
 
     @Override
@@ -163,6 +214,7 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
 
     @Override
     public Query visit(Alias alias) {
+        currentFieldName = alias.getAliasName();
         return null;
     }
 
@@ -244,6 +296,7 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
         final Set<String> fields = new HashSet<String>();
         for (final ComplexTypeMetadata type : types) {
             type.accept(new DefaultMetadataVisitor<Void>() {
+
                 @Override
                 public Void visit(ContainedComplexTypeMetadata containedType) {
                     super.visit(containedType);
@@ -264,7 +317,8 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
 
                 @Override
                 public Void visit(SimpleTypeFieldMetadata simpleField) {
-                    if (!Storage.METADATA_TIMESTAMP.equals(simpleField.getName()) && !Storage.METADATA_TASK_ID.equals(simpleField.getName())) {
+                    if (!Storage.METADATA_TIMESTAMP.equals(simpleField.getName())
+                            && !Storage.METADATA_TASK_ID.equals(simpleField.getName())) {
                         fields.add(simpleField.getName());
                     }
                     return null;
@@ -296,20 +350,21 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
     @Override
     public Query visit(FieldFullText fieldFullText) {
         String fieldName = fieldFullText.getField().getFieldMetadata().getName();
-        String[] fieldsAsArray = new String[]{fieldName};
+        String[] fieldsAsArray = new String[] { fieldName };
         boolean caseSensitiveSearch = dataSource.isCaseSensitiveSearch();
         String fullTextQuery = fieldName + ':' + getValue(fieldFullText, caseSensitiveSearch);
         return parseQuery(fieldsAsArray, fullTextQuery, caseSensitiveSearch);
     }
 
     private Query parseQuery(String[] fieldsAsArray, String fullTextQuery, boolean caseSensitiveSearch) {
-        MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_29, fieldsAsArray, getAnalyzer(caseSensitiveSearch));
+        MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_29, fieldsAsArray,
+                getAnalyzer(caseSensitiveSearch));
         // Very important! Lucene does an implicit lower case for "expanded terms" (which is something used).
         parser.setLowercaseExpandedTerms(!caseSensitiveSearch);
         try {
             return parser.parse(fullTextQuery);
         } catch (ParseException e) {
-            throw new RuntimeException("Invalid generated Lucene query", e);
+            throw new RuntimeException("Invalid generated Lucene query", e); //$NON-NLS-1$
         }
     }
 
@@ -335,11 +390,12 @@ class LuceneQueryGenerator extends VisitorAdapter<Query> {
         if (index > 0) {
             value = value.substring(index);
         }
-        char[] removes = new char[]{'[', ']', '+', '!', '(', ')', '^', '\"', '~', ':', '\\'}; // Removes reserved characters
+        char[] removes = new char[] { '[', ']', '+', '!', '(', ')', '^', '\"', '~', ':', '\\' }; // Removes reserved
+                                                                                                 // characters
         for (char remove : removes) {
             value = value.replace(remove, ' ');
         }
-        if (!value.endsWith("*")) {
+        if (!value.endsWith("*")) { //$NON-NLS-1$
             value += '*';
         }
         return value;
