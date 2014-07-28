f2c8f69f0d4eba2687f62122b31d683d4deb02b6
fhuaulme <fhuaulme@talend.com>
2014-07-28 15:47:44 +0200

    TMDM-7529: "Matching statistics service does not run on postgres"
    Fix incorrect types analyzed for matching service (now only takes into account types eligible for matching -> no composite key and string typed key field).
    https://jira.talendforge.org/browse/TMDM-7529

--- org.talend.mdm.core/src/com/amalto/core/storage/services/MatchingStatistics.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/MatchingStatistics.java
@@ -24,9 +24,7 @@ import com.amalto.core.storage.StagingStorage;
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONWriter;
-import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
-import org.talend.mdm.commmon.metadata.FieldMetadata;
-import org.talend.mdm.commmon.metadata.MetadataRepository;
+import org.talend.mdm.commmon.metadata.*;
 
 import com.amalto.core.query.user.Expression;
 import com.amalto.core.server.ServerContext;
@@ -69,9 +67,20 @@ public class MatchingStatistics {
                         if (StagingStorage.EXECUTION_LOG_TYPE.equals(type.getName())) {
                             continue;
                         }
+                        /*
+                         * Eligible types for matching are:
+                         *  - single key types.
+                         *  - key field type must be xsd:string.
+                         */
+                        if (type.getKeyFields().size() > 1) {
+                            continue; // not eligible
+                        }
+                        FieldMetadata keyField = type.getKeyFields().iterator().next();
+                        if (!Types.STRING.equals(MetadataUtils.getSuperConcreteType(keyField.getType()).getName())) {
+                            continue; // not eligible
+                        }
                         writer.object();
                         {
-                            FieldMetadata keyField = type.getKeyFields().iterator().next();
                             Expression count = from(type).select(alias(count(), "count")).where(gt(groupSize(), "2")) //$NON-NLS-1$ //$NON-NLS-2$
                                     .where(eq(keyField, taskId())).cache().getExpression();
                             StorageResults typeCount = dataStorage.fetch(count);
