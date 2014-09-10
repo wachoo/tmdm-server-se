14f350d2f78a532586e6c02c3daf6e09ccde6f98
qcui <qcui@talend.com>
2014-09-10 18:58:00 +0800

    TMDM-7561 : Add multiple stores for product in Hierarchy always show wrong info.
    TMDM-7561 : At first modify creating duplicate FK tab in detail panel.
    
    Merge remote-tracking branch 'origin/maintenance/5.6' into bugfix/TMDM-7561

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemDetailToolBar.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemDetailToolBar.java
@@ -1052,9 +1052,9 @@ public class ItemDetailToolBar extends ToolBar {
     }
 
     private native boolean initDSC(String taskId)/*-{
-                                                 $wnd.amalto.datastewardship.Datastewardship.taskItem(taskId);
-                                                 return true;
-                                                 }-*/;
+		$wnd.amalto.datastewardship.Datastewardship.taskItem(taskId);
+		return true;
+    }-*/;
 
     protected void initSmartViewToolBar() {
         addGeneratedViewButton();
@@ -1188,27 +1188,32 @@ public class ItemDetailToolBar extends ToolBar {
     }
 
     private native boolean initJournal(String ids, String concept)/*-{
-                                                                  $wnd.amalto.journal.Journal.browseJournalWithCriteria(ids, concept,
-                                                                  true);
-                                                                  return true;
-                                                                  }-*/;
+		$wnd.amalto.journal.Journal.browseJournalWithCriteria(ids, concept,
+				true);
+		return true;
+    }-*/;
 
     // Please note that this method is duplicated in
     // org.talend.mdm.webapp.browserecords.client.widget.integrity.SingletonDeleteStrategy.initSearchEntityPanel()
     private native boolean initSearchEntityPanel(String arrStr, String ids, String dataObject)/*-{
-                                                                                              var lineageEntities = arrStr.split(",");
-                                                                                              $wnd.amalto.itemsbrowser.ItemsBrowser.lineageItem(lineageEntities, ids,
-                                                                                              dataObject);
-                                                                                              return true;
-                                                                                              }-*/;
+		var lineageEntities = arrStr.split(",");
+		$wnd.amalto.itemsbrowser.ItemsBrowser.lineageItem(lineageEntities, ids,
+				dataObject);
+		return true;
+    }-*/;
 
     public void saveItemAndClose(final boolean isClose) {
-        if (itemBean.getIds().trim().equals("")) { //$NON-NLS-1$
+        ItemBean bean = itemBean;
+        Widget widget = itemsDetailPanel.getPrimaryKeyTabWidget();
+        if (widget instanceof ItemPanel) {// save primary key
+            ItemPanel itemPanel = (ItemPanel) widget;
+            bean = itemPanel.getItem();
+        }
+        if (bean.getIds().trim().equals("")) { //$NON-NLS-1$
             saveItemWithIdCheck(isClose);
             return;
         }
-
-        getBrowseRecordsService().isItemModifiedByOthers(itemBean, new SessionAwareAsyncCallback<Boolean>() {
+        getBrowseRecordsService().isItemModifiedByOthers(bean, new SessionAwareAsyncCallback<Boolean>() {
 
             @Override
             public void onSuccess(Boolean result) {
@@ -1383,14 +1388,14 @@ public class ItemDetailToolBar extends ToolBar {
     }
 
     public native void closeOutTabPanel()/*-{
-                                         var tabPanel = $wnd.amalto.core.getTabPanel();
-                                         tabPanel.closeCurrentTab();
-                                         }-*/;
+		var tabPanel = $wnd.amalto.core.getTabPanel();
+		tabPanel.closeCurrentTab();
+    }-*/;
 
     public native void updateOutTabPanel(String tabText)/*-{
-                                                        var tabPanel = $wnd.amalto.core.getTabPanel();
-                                                        tabPanel.updateCurrentTabText(tabText);
-                                                        }-*/;
+		var tabPanel = $wnd.amalto.core.getTabPanel();
+		tabPanel.updateCurrentTabText(tabText);
+    }-*/;
 
     class MenuEx extends Menu {
 
@@ -1451,8 +1456,8 @@ public class ItemDetailToolBar extends ToolBar {
         }
 
         private native El getExtrasTr()/*-{
-                                       return this.@com.extjs.gxt.ui.client.widget.layout.ToolBarLayout::extrasTr;
-                                       }-*/;
+			return this.@com.extjs.gxt.ui.client.widget.layout.ToolBarLayout::extrasTr;
+        }-*/;
 
         @Override
         @SuppressWarnings("unchecked")
@@ -1575,8 +1580,8 @@ public class ItemDetailToolBar extends ToolBar {
     }
 
     private native void openWindow(String url)/*-{
-                                              window.open(url);
-                                              }-*/;
+		window.open(url);
+    }-*/;
 
     protected void openDebugLineagePanel(String ids, LineagePanel panel) {
         Window window = new Window();
@@ -1589,47 +1594,47 @@ public class ItemDetailToolBar extends ToolBar {
     }
 
     protected native void openLineagePanel(String ids, LineagePanel lineagePanel)/*-{
-                                                                                 var tabPanel = $wnd.amalto.core.getTabPanel();
-                                                                                 var browseStagingRecordsPanel = tabPanel.getItem(ids);
-                                                                                 if (browseStagingRecordsPanel == undefined) {
-                                                                                 var panel = @org.talend.mdm.webapp.browserecords.client.widget.ItemDetailToolBar::convertLineagePanel(Lorg/talend/mdm/webapp/browserecords/client/widget/LineagePanel;)(lineagePanel);
-                                                                                 tabPanel.add(panel);
-                                                                                 }
-                                                                                 tabPanel.setSelection(ids);
-                                                                                 }-*/;
+		var tabPanel = $wnd.amalto.core.getTabPanel();
+		var browseStagingRecordsPanel = tabPanel.getItem(ids);
+		if (browseStagingRecordsPanel == undefined) {
+			var panel = @org.talend.mdm.webapp.browserecords.client.widget.ItemDetailToolBar::convertLineagePanel(Lorg/talend/mdm/webapp/browserecords/client/widget/LineagePanel;)(lineagePanel);
+			tabPanel.add(panel);
+		}
+		tabPanel.setSelection(ids);
+    }-*/;
 
     private native static JavaScriptObject convertLineagePanel(LineagePanel lineagePanel)/*-{
-                                                                                         var panel = {
-                                                                                         // imitate extjs's render method, really call gxt code.
-                                                                                         render : function(el) {
-                                                                                         var rootPanel = @com.google.gwt.user.client.ui.RootPanel::get(Ljava/lang/String;)(el.id);
-                                                                                         rootPanel.@com.google.gwt.user.client.ui.RootPanel::add(Lcom/google/gwt/user/client/ui/Widget;)(lineagePanel);
-                                                                                         },
-                                                                                         // imitate extjs's setSize method, really call gxt code.
-                                                                                         setSize : function(width, height) {
-                                                                                         lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::setSize(II)(width, height);
-                                                                                         },
-                                                                                         // imitate extjs's getItemId, really return itemId of ContentPanel of GXT.
-                                                                                         getItemId : function() {
-                                                                                         return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getItemId()();
-                                                                                         },
-                                                                                         // imitate El object of extjs
-                                                                                         getEl : function() {
-                                                                                         var el = lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getElement()();
-                                                                                         return {
-                                                                                         dom : el
-                                                                                         };
-                                                                                         },
-                                                                                         // imitate extjs's doLayout method, really call gxt code.
-                                                                                         doLayout : function() {
-                                                                                         return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::doLayout()();
-                                                                                         },
-                                                                                         title : function() {
-                                                                                         return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getHeading()();
-                                                                                         }
-                                                                                         };
-                                                                                         return panel;
-                                                                                         }-*/;
+		var panel = {
+			// imitate extjs's render method, really call gxt code.
+			render : function(el) {
+				var rootPanel = @com.google.gwt.user.client.ui.RootPanel::get(Ljava/lang/String;)(el.id);
+				rootPanel.@com.google.gwt.user.client.ui.RootPanel::add(Lcom/google/gwt/user/client/ui/Widget;)(lineagePanel);
+			},
+			// imitate extjs's setSize method, really call gxt code.
+			setSize : function(width, height) {
+				lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::setSize(II)(width, height);
+			},
+			// imitate extjs's getItemId, really return itemId of ContentPanel of GXT.
+			getItemId : function() {
+				return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getItemId()();
+			},
+			// imitate El object of extjs
+			getEl : function() {
+				var el = lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getElement()();
+				return {
+					dom : el
+				};
+			},
+			// imitate extjs's doLayout method, really call gxt code.
+			doLayout : function() {
+				return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::doLayout()();
+			},
+			title : function() {
+				return lineagePanel.@org.talend.mdm.webapp.browserecords.client.widget.LineagePanel::getHeading()();
+			}
+		};
+		return panel;
+    }-*/;
 
     public boolean isFkToolBar() {
         return isFkToolBar;
