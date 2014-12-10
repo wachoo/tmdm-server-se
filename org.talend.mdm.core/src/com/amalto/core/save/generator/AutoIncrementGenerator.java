e3d9c507975f0deee2fe5081908f2e4fe2a6bde9
fhuaulme <fhuaulme@talend.com>
2014-12-10 12:18:03 +0100

    TMDM-8011: "Sequence of autoincrement value is broken when multiple MDM servers share same database"
    Sync key name from in-memory implementation and SQL implementation.
    Adds ability to configure clustering mode in mdm.conf.
    Ensure in-memory auto increment state is correctly saved during server shutdown.
    (https://jira.talendforge.org/browse/TMDM-8011)

--- org.talend.mdm.core/src/com/amalto/core/save/generator/AutoIncrementGenerator.java
+++ org.talend.mdm.core/src/com/amalto/core/save/generator/AutoIncrementGenerator.java
@@ -1,12 +1,50 @@
 package com.amalto.core.save.generator;
 
+import com.amalto.core.server.Server;
+import com.amalto.core.server.ServerContext;
+import com.amalto.core.server.StorageAdmin;
+import com.amalto.core.storage.StorageType;
+import org.apache.log4j.Logger;
+import org.talend.mdm.commmon.util.core.MDMConfiguration;
+
+import java.util.Properties;
+
 public class AutoIncrementGenerator {
 
-    private static final boolean                        ENABLE_SAFE_CONCURRENT_INCREMENT = false;
+    private static final Logger                         LOGGER                         = Logger.getLogger(AutoIncrementGenerator.class);
 
-    private static final InMemoryAutoIncrementGenerator inMemoryAutoIncrementGenerator   = new InMemoryAutoIncrementGenerator();
+    private static final boolean                        ENABLE_SAFE_CONCURRENT_INCREMENT;
 
-    private static final StorageAutoIncrementGenerator  storageAutoIncrementGenerator    = new StorageAutoIncrementGenerator();
+    private static final InMemoryAutoIncrementGenerator inMemoryAutoIncrementGenerator = new InMemoryAutoIncrementGenerator();
+
+    private static final StorageAutoIncrementGenerator  storageAutoIncrementGenerator;
+
+    static {
+        // Initialize Storage auto increment generator (only if system SQL is available).
+        StorageAdmin admin = ServerContext.INSTANCE.get().getStorageAdmin();
+        if (admin.exist(StorageAdmin.SYSTEM_STORAGE, StorageType.SYSTEM, null)) {
+            storageAutoIncrementGenerator = new StorageAutoIncrementGenerator();
+        } else {
+            storageAutoIncrementGenerator = null;
+        }
+        // Enable concurrent auto increment generator
+        Properties properties = MDMConfiguration.getConfiguration();
+        boolean enableConcurrentIncrement = Boolean.parseBoolean(properties.getProperty(Server.SYSTEM_CLUSTER, Boolean.FALSE.toString()));
+        if (enableConcurrentIncrement) {
+            if (MDMConfiguration.isSqlDataBase()) {
+                if (storageAutoIncrementGenerator == null) {
+                    LOGGER.error("Unable to enable concurrent access support for auto increment generator (system database must use SQL).");
+                    enableConcurrentIncrement = false;
+                } else {
+                    LOGGER.info("Enable concurrent access support for auto increment generator.");
+                }
+            } else {
+                LOGGER.error("Unable to enable concurrent access support for auto increment generator (must use SQL databases).");
+                enableConcurrentIncrement = false;
+            }
+        }
+        ENABLE_SAFE_CONCURRENT_INCREMENT = enableConcurrentIncrement;
+    } 
 
     public static AutoIdGenerator get() {
         if (ENABLE_SAFE_CONCURRENT_INCREMENT) {
