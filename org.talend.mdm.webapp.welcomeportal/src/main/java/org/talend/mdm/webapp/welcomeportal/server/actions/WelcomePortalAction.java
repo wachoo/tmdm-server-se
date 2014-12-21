6327e4029ea001bef076bf7b42c3c1e5475cda8b
pbabin <pbabin@talend.com>
2014-12-22 00:25:26 +0100

    TMDM-8055: Fix deprecated API usage

--- org.talend.mdm.webapp.welcomeportal/src/main/java/org/talend/mdm/webapp/welcomeportal/server/actions/WelcomePortalAction.java
+++ org.talend.mdm.webapp.welcomeportal/src/main/java/org/talend/mdm/webapp/welcomeportal/server/actions/WelcomePortalAction.java
@@ -12,7 +12,11 @@
 // ============================================================================
 package org.talend.mdm.webapp.welcomeportal.server.actions;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 import org.talend.mdm.webapp.base.client.exception.ServiceException;
@@ -32,13 +36,12 @@ import com.amalto.core.webservice.WSByteArray;
 import com.amalto.core.webservice.WSDataClusterPK;
 import com.amalto.core.webservice.WSDataModelPK;
 import com.amalto.core.webservice.WSExecuteTransformerV2;
-import com.amalto.core.webservice.WSGetTransformer;
-import com.amalto.core.webservice.WSGetTransformerPKs;
+import com.amalto.core.webservice.WSGetTransformerV2;
+import com.amalto.core.webservice.WSGetTransformerV2PKs;
 import com.amalto.core.webservice.WSPutItem;
-import com.amalto.core.webservice.WSTransformer;
 import com.amalto.core.webservice.WSTransformerContext;
 import com.amalto.core.webservice.WSTransformerContextPipelinePipelineItem;
-import com.amalto.core.webservice.WSTransformerPK;
+import com.amalto.core.webservice.WSTransformerV2;
 import com.amalto.core.webservice.WSTransformerV2PK;
 import com.amalto.core.webservice.WSTypedContent;
 import com.amalto.webapp.core.bean.Configuration;
@@ -185,11 +188,11 @@ public class WelcomePortalAction implements WelcomePortalService {
         try {
             List<String> process = new ArrayList<String>();
 
-            WSTransformerPK[] wst = Util.getPort().getTransformerPKs(new WSGetTransformerPKs("*")).getWsTransformerPK(); //$NON-NLS-1$
+            WSTransformerV2PK[] wst = Util.getPort().getTransformerV2PKs(new WSGetTransformerV2PKs("*")).getWsTransformerV2PK(); //$NON-NLS-1$
 
-            for (WSTransformerPK wstransformerpk : wst) {
+            for (WSTransformerV2PK wstransformerpk : wst) {
                 if (isStandaloneProcess(wstransformerpk.getPk())) {
-                    WSTransformer wsTransformer = Util.getPort().getTransformer(new WSGetTransformer(wstransformerpk));
+                	WSTransformerV2 wsTransformer = Util.getPort().getTransformerV2(new WSGetTransformerV2(wstransformerpk));
                     // add transformer pk, and then add its desc
                     process.add(wstransformerpk.getPk());
                     String desc = getDescriptionByLau(language, wsTransformer.getDescription());
