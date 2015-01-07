ad017d2a1d9a94d591553fcf5acef33beca55d07
pbabin <pbabin@talend.com>
2015-01-07 01:42:09 +0100

    TMDM-8055: Chore cleaning

--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/server/servlet/MDMContextListener.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/server/servlet/MDMContextListener.java
@@ -25,28 +25,28 @@ import org.springframework.web.util.WebUtils;
 
 public class MDMContextListener implements ServletContextListener {
 
-    public static final String ROOT_LOCATION_PARAM = "mdmRootLocation";
+    public static final String ROOT_LOCATION_PARAM = "mdmRootLocation"; //$NON-NLS-1$
 
-    public static final String ROOT_LOCATION_KEY = "mdm.root";
+    public static final String ROOT_LOCATION_KEY = "mdm.root"; //$NON-NLS-1$
 
     @Override
     public void contextInitialized(ServletContextEvent event) {
         ServletContext servletContext = event.getServletContext();
         String location = servletContext.getInitParameter(ROOT_LOCATION_PARAM);
         String resolvedLocation = ServletContextPropertyUtils.resolvePlaceholders(location, servletContext);
-        servletContext.log("Initializing MDM root folder from [" + resolvedLocation + "]");
+        servletContext.log("Initializing MDM root folder from [" + resolvedLocation + "]"); //$NON-NLS-1$ //$NON-NLS-2$
         try {
             if (!ResourceUtils.isUrl(resolvedLocation)) {
                 resolvedLocation = WebUtils.getRealPath(servletContext, resolvedLocation);
             }
             File file = ResourceUtils.getFile(resolvedLocation);
             if (!file.exists()) {
-                throw new FileNotFoundException("MDM Root folder [" + resolvedLocation + "] not found");
+                throw new FileNotFoundException("MDM Root folder [" + resolvedLocation + "] not found"); //$NON-NLS-1$ //$NON-NLS-2$
             }
             System.setProperty(ROOT_LOCATION_KEY, file.getAbsolutePath());
-            servletContext.log("Set MDM root system property: '" + ROOT_LOCATION_KEY + "' = [" + file.getAbsolutePath() + "]");
+            servletContext.log("Set MDM root system property: '" + ROOT_LOCATION_KEY + "' = [" + file.getAbsolutePath() + "]"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("Invalid '" + ROOT_LOCATION_PARAM + "' parameter", e);
+            throw new IllegalArgumentException("Invalid '" + ROOT_LOCATION_PARAM + "' parameter", e); //$NON-NLS-1$ //$NON-NLS-2$
         }
     }
 
