e3a2b02814998fb769f933546c79b4c55b340bda
pbabin <pbabin@talend.com>
2015-02-12 09:22:08 +0100

    Fix merge commit c8367219c43fccae35e8a124d2f347a02fa52026. Recover files from deletion

---
+++ org.talend.mdm.core/src/com/amalto/core/delegator/impl/DefaultItemCtrlDelegator.java
@@ -0,0 +1,17 @@
+package com.amalto.core.delegator.impl;
+
+import java.util.ArrayList;
+
+import com.amalto.core.delegator.IItemCtrlDelegator;
+import com.amalto.core.objects.view.ViewPOJOPK;
+import com.amalto.xmlserver.interfaces.IWhereItem;
+
+
+public class DefaultItemCtrlDelegator extends IItemCtrlDelegator {
+
+	@Override
+	protected ArrayList<IWhereItem> getViewWCFromRole(ViewPOJOPK viewPOJOPK)
+			throws Exception {
+		return null;
+	}	
+}
