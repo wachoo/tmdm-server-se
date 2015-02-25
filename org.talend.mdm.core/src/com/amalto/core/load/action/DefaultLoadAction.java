ebde91d94294e8f2dc3ca9007d434d11ec5c441a
yjli-talend <yjli@talend.com>
2015-02-25 18:31:17 +0800

    TMDM-8177 Add encode format UTF-8 in InputStreamReader.

--- org.talend.mdm.core/src/com/amalto/core/load/action/DefaultLoadAction.java
+++ org.talend.mdm.core/src/com/amalto/core/load/action/DefaultLoadAction.java
@@ -1,15 +1,21 @@
 /*
  * Copyright (C) 2006-2014 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.load.action;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.talend.mdm.commmon.util.webapp.XSystemObjects;
 
 import com.amalto.core.load.io.XMLStreamTokenizer;
 import com.amalto.core.save.DocumentSaverContext;
@@ -17,12 +23,6 @@ import com.amalto.core.save.SaverSession;
 import com.amalto.core.save.context.SaverContextFactory;
 import com.amalto.core.server.XmlServer;
 import com.amalto.core.util.XSDKey;
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-import org.talend.mdm.commmon.util.webapp.XSystemObjects;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 /**
  *
@@ -43,28 +43,33 @@ public class DefaultLoadAction implements LoadAction {
         this.needValidate = needValidate;
     }
 
+    @Override
     public boolean supportValidation() {
         return true;
     }
 
+    @Override
     public void load(InputStream stream, XSDKey keyMetadata, XmlServer server, SaverSession session) {
         try {
             SaverContextFactory contextFactory = session.getContextFactory();
             // If you wish to debug content sent to server evaluate 'IOUtils.toString(request.getInputStream())'
-            XMLStreamTokenizer xmlStreamTokenizer = new XMLStreamTokenizer(stream);
+            XMLStreamTokenizer xmlStreamTokenizer = new XMLStreamTokenizer(stream, "UTF-8"); //$NON-NLS-1$
             while (xmlStreamTokenizer.hasMoreElements()) {
                 String xmlData = xmlStreamTokenizer.nextElement();
                 if (xmlData != null && xmlData.trim().length() > 0) {
-                    // Note: in case you wish to change the "replace" behavior, also check com.amalto.core.save.context.BulkLoadContext.isReplace()
-                    DocumentSaverContext context = contextFactory.create(dataClusterName,
-                            dataModelName,
-                            StringUtils.EMPTY,
+                    // Note: in case you wish to change the "replace" behavior, also check
+                    // com.amalto.core.save.context.BulkLoadContext.isReplace()
+                    DocumentSaverContext context = contextFactory.create(dataClusterName, dataModelName, StringUtils.EMPTY,
                             new ByteArrayInputStream(xmlData.getBytes("UTF-8")), //$NON-NLS-1$
                             true, // Always replace in this case (bulk load).
-                            needValidate,
-                            false,
-                            false,
-                            XSystemObjects.DC_PROVISIONING.getName().equals(dataClusterName)); // Enforce auto commit for users (for license checks).
+                            needValidate, false, false, XSystemObjects.DC_PROVISIONING.getName().equals(dataClusterName)); // Enforce
+                                                                                                                           // auto
+                                                                                                                           // commit
+                                                                                                                           // for
+                                                                                                                           // users
+                                                                                                                           // (for
+                                                                                                                           // license
+                                                                                                                           // checks).
                     context.createSaver().save(session, context);
                 }
             }
@@ -78,6 +83,7 @@ public class DefaultLoadAction implements LoadAction {
         }
     }
 
+    @Override
     public void endLoad(XmlServer server) {
         // Nothing to do
     }
