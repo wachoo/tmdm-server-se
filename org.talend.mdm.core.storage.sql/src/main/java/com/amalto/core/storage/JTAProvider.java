6c234c0f7ed467e7e1a8b76e80831db25ed6ff71
fhuaulme <fhuaulme@talend.com>
2015-03-20 13:56:25 +0100

    TMDM-8262: "EE server starts with transaction error and can not login from web server."
    "TM looks always greener on the other side": ensures MDM doesn't believe a TM is available due to a Bonita dependency in Tomcat libs.
    (https://jira.talendforge.org/browse/TMDM-8262)

---
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/JTAProvider.java
@@ -0,0 +1,37 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package com.amalto.core.storage;
+
+import org.apache.log4j.Logger;
+import org.hibernate.engine.transaction.jta.platform.internal.NoJtaPlatform;
+import org.hibernate.engine.transaction.jta.platform.spi.JtaPlatform;
+import org.hibernate.engine.transaction.jta.platform.spi.JtaPlatformProvider;
+
+// Do not remove, dynamically called by Hibernate (see META-INF/services/).
+public class JTAProvider implements JtaPlatformProvider {
+
+    public static final Logger LOGGER = Logger.getLogger(JTAProvider.class);
+
+    @Override
+    public JtaPlatform getProvidedJtaPlatform() {
+        /*
+         * MDM does not provide/use any TM, however endorsed libraries (for Bonita) may wrongly indicate to Hibernate
+         * that a TM is available. Code below ensures JTA is always disabled.
+         */
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Disabling JTA support.");
+        }
+        return NoJtaPlatform.INSTANCE;
+    }
+}
