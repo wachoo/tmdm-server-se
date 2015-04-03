bcd7c08b238f8b8605d42403bb1df89d2fa05b3f
fhuaulme <fhuaulme@talend.com>
2015-04-03 18:47:06 +0200

    TMDM-8330: "Auto-Increment discrepancies between Staging and Master"
    Initial commit for fix.
    Adds action generation that doesn't override id values with MDM generated values.
    Adds task for AutoIncrement update once staging validation is completed for a type.
    (https://jira.talendforge.org/browse/TMDM-8330)
    
    (cherry picked from commit 4577400db63257beb4dddd8bea67e45f9185a505)
    
    Conflicts:
    	org.talend.mdm.core/src/com/amalto/core/save/context/GenerateActions.java
    	org.talend.mdm.core/src/com/amalto/core/save/context/ID.java

---
+++ org.talend.mdm.core/src/com/amalto/core/save/context/CreateWithProvidedIdActions.java
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package com.amalto.core.save.context;
+
+import com.amalto.core.history.MutableDocument;
+import com.amalto.core.history.accessor.Accessor;
+import com.amalto.core.history.action.FieldUpdateAction;
+import org.apache.commons.lang.StringUtils;
+import org.talend.mdm.commmon.metadata.FieldMetadata;
+
+import java.util.Date;
+
+public class CreateWithProvidedIdActions extends CreateActions {
+
+    CreateWithProvidedIdActions(MutableDocument document, Date date, String source, String userName, String dataCluster, String dataModel, String universe, SaverSource saverSource) {
+        super(document, date, source, userName, dataCluster, dataModel, universe, saverSource);
+    }
+
+    @Override
+    protected void handleField(FieldMetadata field, boolean doCreate, String currentPath) {
+        if (field.isKey()) {
+            Accessor accessor = document.createAccessor(currentPath);
+            if (accessor.exist()) {
+                actions.add(new FieldUpdateAction(date, source, userName, currentPath, StringUtils.EMPTY, accessor.get(), field));
+                return;
+            }
+        }
+        super.handleField(field, doCreate, currentPath);
+    }
+}
