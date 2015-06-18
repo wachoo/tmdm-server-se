1545b091d2c866d631125e8b8a0ce2650895635c
yjli-talend <yjli@talend.com>
2015-06-18 19:06:45 +0800

    TMDM-8570 Remove space character in parameter of js function 'window.open'.

--- org.talend.mdm.webapp.core/src/main/webapp/secure/js/RestApiDoc.js
+++ org.talend.mdm.webapp.core/src/main/webapp/secure/js/RestApiDoc.js
@@ -4,7 +4,7 @@ amalto.apidoc.RestApiDoc = function() {
 
 	function initUIAndData() {
 		var baseCurrentUrl = stripQueryStringAndHashFromPath(this.location.toString());
-		var theWindow = window.open(baseCurrentUrl + "/../api/rest/", "Talend MDM REST API documentation");
+		var theWindow = window.open(baseCurrentUrl + "/../api/rest/", "TalendMDMRESTAPIdocumentation");
 		theWindow.focus();
 	};
 	
