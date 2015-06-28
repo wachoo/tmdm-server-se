8efbc31fb96164d4eb8456d87c9ed40f0fa576ac
pbabin <pbabin@talend.com>
2015-06-28 20:30:03 +0200

    TMDM-8484; Fix Grid alignment

--- org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/widget/ColumnAlignGrid.java
+++ org.talend.mdm.webapp.base/src/main/java/org/talend/mdm/webapp/base/client/widget/ColumnAlignGrid.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.mdm.webapp.base.client.widget;
 
 import com.extjs.gxt.ui.client.data.ModelData;
@@ -9,11 +21,5 @@ public class ColumnAlignGrid<M extends ModelData> extends Grid<M> {
 
     public ColumnAlignGrid(ListStore<M> store, ColumnModel cm) {
         super(store, cm);
-        clearGridViewBorderWidth();
     }
-    
-    private native void clearGridViewBorderWidth()/*-{
-        var view = this.@com.extjs.gxt.ui.client.widget.grid.Grid::view;
-        view.@com.extjs.gxt.ui.client.widget.grid.GridView::borderWidth = 0; 
-    }-*/;
 }
