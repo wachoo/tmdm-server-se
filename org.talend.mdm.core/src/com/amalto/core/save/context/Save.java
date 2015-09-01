93a9598d8136051ce0b27c784e3f2111894d820e
Pingwen Lin <pwlin@talend.com>
2015-09-01 11:57:20 +0800

    TMDM-8876: TalendMDMContextConnector doesn't work in workflow (don't need to parse time or save update record if there is no change)

--- org.talend.mdm.core/src/com/amalto/core/save/context/Save.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/Save.java
@@ -65,7 +65,11 @@ class Save implements DocumentSaver {
                 throw new RuntimeException("Unexpected state: update report does not have value for key '" + keyFieldName + "'.");
             }
             if ("TimeInMillis".equals(keyFieldName)) { //$NON-NLS-1$
-                updateReportTime = Long.parseLong(keyAccessor.get());
+                if (StringUtils.EMPTY.equals(keyAccessor.get())) { // No update report need to save.
+                    return;
+                } else {
+                    updateReportTime = Long.parseLong(keyAccessor.get());
+                }
             }
         }
         if (updateReportTime < 1) { // This is unexpected (would mean update report "TimeInMillis" is not present).
