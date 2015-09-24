22f5f3c0670f31ed3d6c17d1079d330451032145
jlame <jlame@talend.com>
2015-09-24 18:06:41 +0200

    TMDM-8888 : Adding a new servlet filter to set no cache in the response header

---
+++ org.talend.mdm.core/src/com/amalto/core/servlet/RestWebServiceFilter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+ * 
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
+ */
+
+package com.amalto.core.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RestWebServiceFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        
+        //positionning http response cache control to no
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Cache-Control", "no-cache"); //$NON-NLS-1$//$NON-NLS-2$
+        response.setDateHeader("Expires", 0); //$NON-NLS-1$
+        response.setHeader("Pragma", "No-cache"); //$NON-NLS-1$ //$NON-NLS-2$
+
+        //continue to next filter
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // Destroy of the transaction manager is moved to Server#close();
+    }
+}
