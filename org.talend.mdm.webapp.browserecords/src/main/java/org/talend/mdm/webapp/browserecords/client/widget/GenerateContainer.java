199628f24e1bd3f23b0ad73181f942102e673453
Haiting Yin <htyin@talend.com>
2015-10-08 13:40:53 +0800

    TMDM-8984 Error messge window pop up when click pie chart entity after change container

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/GenerateContainer.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/GenerateContainer.java
@@ -12,13 +12,20 @@
 // ============================================================================
 package org.talend.mdm.webapp.browserecords.client.widget;
 
+import org.talend.mdm.webapp.base.client.SessionAwareAsyncCallback;
 import org.talend.mdm.webapp.base.client.widget.PortletConstants;
+import org.talend.mdm.webapp.browserecords.client.BrowseRecords;
 import org.talend.mdm.webapp.browserecords.client.BrowseRecordsEvents;
+import org.talend.mdm.webapp.browserecords.client.BrowseRecordsServiceAsync;
 import org.talend.mdm.webapp.browserecords.client.i18n.MessagesFactory;
+import org.talend.mdm.webapp.browserecords.client.util.UserSession;
+import org.talend.mdm.webapp.browserecords.shared.AppHeader;
 
 import com.extjs.gxt.ui.client.GXT;
+import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.mvc.Dispatcher;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.Window;
 
@@ -55,8 +62,21 @@ public class GenerateContainer {
         Cookies.removeCookie(PortletConstants.PARAMETER_ENTITY);
         setDefaultViewPk(parameter == null ? "" : parameter); //$NON-NLS-1$
         if (parameter != null) {
-            Dispatcher dispatcher = Dispatcher.get();
-            dispatcher.dispatch(BrowseRecordsEvents.DefaultView);
+            BrowseRecordsServiceAsync service = (BrowseRecordsServiceAsync) Registry.get(BrowseRecords.BROWSERECORDS_SERVICE);
+            service.getAppHeader(new SessionAwareAsyncCallback<AppHeader>() {
+
+                @Override
+                public void onSuccess(AppHeader header) {
+                    if (header.getDatacluster() == null || header.getDatamodel() == null) {
+                        MessageBox.alert(MessagesFactory.getMessages().error_title(), MessagesFactory.getMessages()
+                                .data_model_not_specified(), null);
+                        return;
+                    }
+                    BrowseRecords.getSession().put(UserSession.APP_HEADER, header);
+                    Dispatcher dispatcher = Dispatcher.get();
+                    dispatcher.dispatch(BrowseRecordsEvents.DefaultView);
+                }
+            });
         }
     }
 
