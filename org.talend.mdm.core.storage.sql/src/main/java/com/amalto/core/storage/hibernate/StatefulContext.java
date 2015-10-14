0c7ea2ea9f336b8d60f378be7f94e220d4c19bca
qcui <qcui@talend.com>
2015-10-14 15:33:51 +0800

    TMDM-8792 : Can't delete a record of entity 'Country' using Customer's datamodel

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StatefulContext.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StatefulContext.java
@@ -1,22 +1,29 @@
 /*
  * Copyright (C) 2006-2015 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package com.amalto.core.storage.hibernate;
 
-import com.amalto.core.storage.datasource.RDBMSDataSource;
-import org.talend.mdm.commmon.metadata.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.ContainedTypeFieldMetadata;
+import org.talend.mdm.commmon.metadata.DefaultMetadataVisitor;
+import org.talend.mdm.commmon.metadata.EnumerationFieldMetadata;
+import org.talend.mdm.commmon.metadata.FieldMetadata;
+import org.talend.mdm.commmon.metadata.MetadataUtils;
+import org.talend.mdm.commmon.metadata.ReferenceFieldMetadata;
+import org.talend.mdm.commmon.metadata.SimpleTypeFieldMetadata;
+
+import com.amalto.core.storage.datasource.RDBMSDataSource;
 
 public class StatefulContext implements MappingCreatorContext {
 
@@ -30,6 +37,7 @@ public class StatefulContext implements MappingCreatorContext {
         this.dialect = dialect;
     }
 
+    @Override
     public String getFieldColumn(FieldMetadata field) {
         if (!field.getContainingType().getSuperTypes().isEmpty() && !field.getContainingType().isInstantiable()) {
             boolean isUnique = isUniqueWithinTypeHierarchy(field.getContainingType(), field.getName());
@@ -56,7 +64,7 @@ public class StatefulContext implements MappingCreatorContext {
     /**
      * Controls whether a field name is unique within type hierarchy accessible from <code>type</code> (i.e. go to the
      * top level type and recursively checks for field with name <code>name</code>).
-     *
+     * 
      * @param type A type part of an inheritance hierarchy
      * @param name A field name.
      * @return <code>true</code> if there's no other field named <code>name</code> in the type hierarchy accessible from
@@ -106,7 +114,9 @@ public class StatefulContext implements MappingCreatorContext {
 
             @Override
             public Integer visit(ComplexTypeMetadata complexType) {
-                super.visit(complexType);
+                if (complexType.getSubTypes().size() < 1) {
+                    super.visit(complexType);
+                }
                 for (ComplexTypeMetadata subType : complexType.getSubTypes()) {
                     subType.accept(this);
                 }
@@ -116,6 +126,7 @@ public class StatefulContext implements MappingCreatorContext {
         return occurrenceCount <= 1;
     }
 
+    @Override
     public String getFieldColumn(String name) {
         return "x_" + name.replace('-', '_').toLowerCase(); //$NON-NLS-1$
     }
--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/query/StorageIntegrityTest.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/query/StorageIntegrityTest.java
@@ -10,13 +10,23 @@
 
 package com.amalto.core.query;
 
+import static com.amalto.core.query.user.UserQueryBuilder.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
+
+import com.amalto.core.integrity.FKIntegrityCheckResult;
+import com.amalto.core.integrity.FKIntegrityChecker;
+import com.amalto.core.query.user.UserQueryBuilder;
 import com.amalto.core.server.MockServerLifecycle;
 import com.amalto.core.server.ServerContext;
 import com.amalto.core.storage.Storage;
+import com.amalto.core.storage.StorageResults;
 import com.amalto.core.storage.hibernate.HibernateStorage;
 import com.amalto.core.storage.record.DataRecord;
 import com.amalto.core.storage.record.DataRecordReader;
@@ -36,8 +46,7 @@ public class StorageIntegrityTest extends TestCase {
 
         try {
             DataRecordReader<String> factory = new XmlStringDataRecordReader();
-            DataRecord record = factory.read(repository, type,
-                    "<Metadata3_Main_1><Id>1</Id><ref>[999]</ref></Metadata3_Main_1>");
+            DataRecord record = factory.read(repository, type, "<Metadata3_Main_1><Id>1</Id><ref>[999]</ref></Metadata3_Main_1>");
             storage.begin();
             storage.update(record);
             try {
@@ -60,8 +69,7 @@ public class StorageIntegrityTest extends TestCase {
 
         try {
             DataRecordReader<String> factory = new XmlStringDataRecordReader();
-            DataRecord record = factory.read(repository, type,
-                    "<Metadata3_Main_2><Id>1</Id><ref>[999]</ref></Metadata3_Main_2>");
+            DataRecord record = factory.read(repository, type, "<Metadata3_Main_2><Id>1</Id><ref>[999]</ref></Metadata3_Main_2>");
             try {
                 storage.begin();
                 storage.update(record);
@@ -189,6 +197,41 @@ public class StorageIntegrityTest extends TestCase {
             storage.begin();
             storage.update(record);
             storage.commit();
+        } finally {
+            storage.close();
+        }
+    }
+
+    public void test8() throws Exception {
+        MetadataRepository repository = prepareMetadata("StorageIntegrityTest_7.xsd");
+        ComplexTypeMetadata TMDM_8792_Country = repository.getComplexType("TMDM_8792_Country");
+        ComplexTypeMetadata TMDM_8792_Entity = repository.getComplexType("TMDM_8792_Entity");
+        Storage storage = prepareStorage(repository);
+
+        try {
+            DataRecordReader<String> factory = new XmlStringDataRecordReader();
+            List<DataRecord> allRecords = new LinkedList<DataRecord>();
+            
+            allRecords.add(factory.read(repository, TMDM_8792_Country, "<TMDM_8792_Country><Code>CN</Code></TMDM_8792_Country>"));
+            allRecords.add(factory.read(repository, TMDM_8792_Entity, "<TMDM_8792_Entity><Id>Entity_Id1</Id><Type2_field1><Base_field1>[CN]</Base_field1></Type2_field1></TMDM_8792_Entity>"));
+            storage.begin();
+            try {
+                storage.update(allRecords);
+                storage.commit();
+            } catch (Exception e) {
+                storage.rollback();
+            }
+            
+            UserQueryBuilder qb = from(TMDM_8792_Entity).and(TMDM_8792_Country).selectId(TMDM_8792_Entity).select(TMDM_8792_Country.getField("Code")).where(eq(TMDM_8792_Country.getField("Code"), "CN"))
+                    .join(TMDM_8792_Entity.getField("Type2_field1/Base_field1"));
+            StorageResults results = storage.fetch(qb.getSelect());
+            
+            try {
+                assertEquals(results.getSize(), 1);
+            } finally {
+                results.close();
+            }
+            
         } finally {
             storage.close();
         }
