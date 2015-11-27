58ab4eb0c89c14c79e7ce940a695975ca0c739e6
yjli-talend <yjli@talend.com>
2015-11-27 17:44:56 +0800

    TMDM-9049 Start new import data thread after previous thread finished.

--- org.talend.mdm.bulkload.client/test/org/talend/mdm/bulkload/client/BulkloadClientTest.java
+++ org.talend.mdm.bulkload.client/test/org/talend/mdm/bulkload/client/BulkloadClientTest.java
@@ -1,18 +1,14 @@
 /*
  * Copyright (C) 2006-2015 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package org.talend.mdm.bulkload.client;
-
-import junit.framework.TestCase;
-import org.apache.commons.lang.math.RandomUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -21,6 +17,13 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import junit.framework.TestCase;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+/**
+ *
+ */
 public class BulkloadClientTest extends TestCase {
 
     public void testClient() throws Exception {
@@ -28,8 +31,7 @@ public class BulkloadClientTest extends TestCase {
 
         boolean isServerRunning = isServerRunning(serverURL);
         if (isServerRunning) {
-            BulkloadClient client = new BulkloadClient(serverURL, "admin",
-                    "talend", null, "Order", "Country", "Order");
+            BulkloadClient client = new BulkloadClient(serverURL, "admin", "talend", null, "Order", "Country", "Order");
             client.setOptions(new BulkloadOptions());
             InputStream bin = BulkloadClientTest.class.getResourceAsStream("test.xml");
             client.load(bin);
@@ -44,8 +46,7 @@ public class BulkloadClientTest extends TestCase {
             urlConnection.connect();
             isServerRunning = true;
         } catch (IOException e) {
-            System.out.println("Server is not running on '" + serverURL
-                    + "', skip this test.");
+            System.out.println("Server is not running on '" + serverURL + "', skip this test.");
         }
         return isServerRunning;
     }
@@ -80,24 +81,6 @@ public class BulkloadClientTest extends TestCase {
         }
     }
 
-    class InterruptedTestResult {
-        boolean success = false;
-        AtomicInteger successCount = new AtomicInteger();
-
-        public boolean isSuccess() {
-            return success;
-        }
-
-        public void setSuccess(boolean success) {
-            successCount.incrementAndGet();
-            this.success = success;
-        }
-
-        public int getSuccessCount() {
-            return successCount.get();
-        }
-    }
-
     public void testInterruptedBulkLoad() throws Exception {
         String serverURL = "http://localhost:8080/datamanager/loadServlet";
         boolean isServerRunning = isServerRunning(serverURL);
@@ -108,6 +91,7 @@ public class BulkloadClientTest extends TestCase {
 
             int count = 10;
             InputStream bin = new InputStream() {
+
                 @Override
                 public synchronized int read() throws IOException {
                     try {
@@ -165,6 +149,60 @@ public class BulkloadClientTest extends TestCase {
                 System.out.println("Excepted exception:" + e.getMessage());
             }
             
+        }
+    }
+
+    public void testInterruptedBulkLoadOrder() throws Exception {
+        String serverURL = "http://localhost:8180/service/bulkload";
+        boolean isServerRunning = isServerRunning(serverURL);
+        if (isServerRunning) {
+            BulkloadClient client = new BulkloadClient(serverURL, "admin", "talend", null, "Product", "Product", "Product");
+            client.setOptions(new BulkloadOptions());
+            int count = 5;
+            for (int i = 0; i < count; i++) {
+                InputStreamMerger manager = client.load();
+                synchronized (manager) {
+                    InputStream bin = new InputStream() {
+
+                        @Override
+                        public synchronized int read() throws IOException {
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            return -1;
+                        }
+                    };
+                    manager.push(bin);
+                    manager.close();
+                    while (!manager.isAlreadyProcessed()) {
+                        manager.wait(100);
+                    }
+                    assertEquals(true, manager.isAlreadyProcessed());
+                    manager.notify();
+                }
+            }
+        }
+    }
+
+    class InterruptedTestResult {
+
+        boolean success = false;
+
+        AtomicInteger successCount = new AtomicInteger();
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            successCount.incrementAndGet();
+            this.success = success;
+        }
+
+        public int getSuccessCount() {
+            return successCount.get();
         }
     }
 }
