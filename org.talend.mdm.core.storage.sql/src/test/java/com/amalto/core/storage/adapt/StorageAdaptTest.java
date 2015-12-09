dda1ae9207c5dfb77c7a6e6a91e2e2a7b7589382
Pingwen Lin <pwlin@talend.com>
2015-12-09 16:25:21 +0800

    TMDM-9094 : Issue when delete mandatory field from complex type: the field does not get deleted from the database

--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/adapt/StorageAdaptTest.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/adapt/StorageAdaptTest.java
@@ -11,11 +11,19 @@
 package com.amalto.core.storage.adapt;
 
 import java.io.File;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
 import org.h2.jdbc.JdbcSQLException;
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
 
 import com.amalto.core.server.MockServerLifecycle;
@@ -26,6 +34,9 @@ import com.amalto.core.storage.datasource.DataSource;
 import com.amalto.core.storage.datasource.DataSourceDefinition;
 import com.amalto.core.storage.datasource.RDBMSDataSource;
 import com.amalto.core.storage.hibernate.HibernateStorage;
+import com.amalto.core.storage.record.DataRecord;
+import com.amalto.core.storage.record.DataRecordReader;
+import com.amalto.core.storage.record.XmlStringDataRecordReader;
 
 public class StorageAdaptTest extends TestCase {
 
@@ -167,5 +178,255 @@ public class StorageAdaptTest extends TestCase {
             connection.close();
         }
     }
-
+    
+    public void test3_DeleteMandatoryField_Nested() throws Exception { 
+        // Test preparation
+        DataSourceDefinition dataSource = ServerContext.INSTANCE.get().getDefinition("H2-DS3", STORAGE_NAME);
+        Storage storage = new HibernateStorage("Test", StorageType.MASTER);
+        storage.init(dataSource);
+        String[] typeNames = { "TestEntity_Nested" };
+        String[] tables = { "testentity_nested", "x_nestedtype2", "x_nestedtype3" };
+        String[] columns = { "x_mandatory12", "x_mandatory22", "x_mandatory32" };
+        DataRecordReader<String> factory = new XmlStringDataRecordReader();
+        MetadataRepository repository1 = new MetadataRepository();
+        repository1.load(StorageAdaptTest.class.getResourceAsStream("schema3_1.xsd"));
+        storage.prepare(repository1, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {true, true, true});
+        String input1 = "<TestEntity_Nested>" +
+                       "  <id>1</id>"+
+                       "  <mandatory11>m11</mandatory11>"+
+                       "  <mandatory12>m12</mandatory12>"+
+                       "  <nestedType2>"+
+                       "    <mandatory21>m21</mandatory21>"+
+                       "    <mandatory22>m22</mandatory22>"+
+                       "    <nestedType3>"+
+                       "      <mandatory31>m31</mandatory31>"+
+                       "      <mandatory32>m32</mandatory32>"+
+                       "    </nestedType3>"+
+                       "  </nestedType2>"+
+                       "</TestEntity_Nested>";
+        createRecord(storage, factory, repository1, typeNames, new String[] { input1 });
+ 
+        MetadataRepository repository2 = new MetadataRepository();
+        repository2.load(StorageAdaptTest.class.getResourceAsStream("schema3_2.xsd"));
+        storage.adapt(repository2, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, true, true});
+        String input2 = "<TestEntity_Nested>" +
+                        "  <id>1</id>"+
+                        "  <mandatory11>m11</mandatory11>"+
+                        "  <nestedType2>"+
+                        "    <mandatory21>m21</mandatory21>"+
+                        "    <mandatory22>m22</mandatory22>"+
+                        "    <nestedType3>"+
+                        "      <mandatory31>m31</mandatory31>"+
+                        "      <mandatory32>m32</mandatory32>"+
+                        "    </nestedType3>"+
+                        "  </nestedType2>"+
+                        "</TestEntity_Nested>";
+        createRecord(storage, factory, repository2, typeNames, new String[] { input2 });
+             
+        MetadataRepository repository3 = new MetadataRepository();
+        repository3.load(StorageAdaptTest.class.getResourceAsStream("schema3_3.xsd"));
+        storage.adapt(repository3, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, true});
+        String input3 = "<TestEntity_Nested>" +
+                        "  <id>1</id>"+
+                        "  <mandatory11>m11</mandatory11>"+
+                        "  <nestedType2>"+
+                        "    <mandatory21>m21</mandatory21>"+
+                        "    <nestedType3>"+
+                        "      <mandatory31>m31</mandatory31>"+
+                        "      <mandatory32>m32</mandatory32>"+
+                        "    </nestedType3>"+
+                        "  </nestedType2>"+
+                        "</TestEntity_Nested>";
+        createRecord(storage, factory, repository3, typeNames, new String[] { input3 });
+        
+        MetadataRepository repository4 = new MetadataRepository();
+        repository4.load(StorageAdaptTest.class.getResourceAsStream("schema3_4.xsd"));
+        storage.adapt(repository4, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, false});
+        String input4 = "<TestEntity_Nested>" +
+                        "  <id>1</id>"+
+                        "  <mandatory11>m11</mandatory11>"+
+                        "  <nestedType2>"+
+                        "    <mandatory21>m21</mandatory21>"+
+                        "    <nestedType3>"+
+                        "      <mandatory31>m31</mandatory31>"+
+                        "    </nestedType3>"+
+                        "  </nestedType2>"+
+                        "</TestEntity_Nested>";
+        createRecord(storage, factory, repository4, typeNames, new String[] { input4 });
+        
+        storage.close(true);
+    }
+    
+    public void test4_DeleteMandatoryField_Extend() throws Exception { 
+        DataSourceDefinition dataSource = ServerContext.INSTANCE.get().getDefinition("H2-DS3", STORAGE_NAME);
+        Storage storage = new HibernateStorage("Test", StorageType.MASTER);
+        storage.init(dataSource);
+        String[] typeNames = { "TestEntity1_Extend", "TestEntity2_Extend", "TestEntity3_Extend" };
+        String[] tables = { "TestEntity1_Extend", "TestEntity2_Extend", "TestEntity3_Extend" };
+        String[] columns = { "x_mandatory12", "x_mandatory22", "x_mandatory32" };
+        DataRecordReader<String> factory = new XmlStringDataRecordReader();
+        MetadataRepository repository1 = new MetadataRepository();
+        repository1.load(StorageAdaptTest.class.getResourceAsStream("schema4_1.xsd"));
+        storage.prepare(repository1, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {true, true, true});
+        String input1_1 = "<TestEntity1_Extend><id>a</id><mandatory11>a</mandatory11><mandatory12>a</mandatory12></TestEntity1_Extend>";
+        String input1_2 = "<TestEntity2_Extend><id>b</id><mandatory11>b</mandatory11><mandatory12>b</mandatory12><mandatory21>b</mandatory21><mandatory22>b</mandatory22></TestEntity2_Extend>";
+        String input1_3 = "<TestEntity3_Extend><id>c</id><mandatory11>c</mandatory11><mandatory12>c</mandatory12><mandatory21>c</mandatory21><mandatory22>b</mandatory22><mandatory31>c</mandatory31><mandatory32>c</mandatory32></TestEntity3_Extend>";
+        createRecord(storage, factory, repository1, typeNames, new String[] { input1_1, input1_2, input1_3 });
+        
+        MetadataRepository repository2 = new MetadataRepository();
+        repository2.load(StorageAdaptTest.class.getResourceAsStream("schema4_2.xsd"));
+        storage.adapt(repository2, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, true, true});
+        String input2_1 = "<TestEntity1_Extend><id>a</id><mandatory11>a</mandatory11></TestEntity1_Extend>";
+        String input2_2 = "<TestEntity2_Extend><id>b</id><mandatory11>b</mandatory11><mandatory21>b</mandatory21><mandatory22>b</mandatory22></TestEntity2_Extend>";
+        String input2_3 = "<TestEntity3_Extend><id>c</id><mandatory11>c</mandatory11><mandatory21>c</mandatory21><mandatory22>b</mandatory22><mandatory31>c</mandatory31><mandatory32>c</mandatory32></TestEntity3_Extend>";
+        createRecord(storage, factory, repository2, typeNames, new String[] { input2_1, input2_2, input2_3 });
+        
+        MetadataRepository repository3 = new MetadataRepository();
+        repository3.load(StorageAdaptTest.class.getResourceAsStream("schema4_3.xsd"));
+        storage.adapt(repository3, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, true});
+        String input3_1 = "<TestEntity1_Extend><id>a</id><mandatory11>a</mandatory11></TestEntity1_Extend>";
+        String input3_2 = "<TestEntity2_Extend><id>b</id><mandatory11>b</mandatory11><mandatory21>b</mandatory21></TestEntity2_Extend>";
+        String input3_3 = "<TestEntity3_Extend><id>c</id><mandatory11>c</mandatory11><mandatory21>c</mandatory21><mandatory31>c</mandatory31><mandatory32>c</mandatory32></TestEntity3_Extend>";
+        createRecord(storage, factory, repository3, typeNames, new String[] { input3_1, input3_2, input3_3 });
+        
+        MetadataRepository repository4 = new MetadataRepository();
+        repository4.load(StorageAdaptTest.class.getResourceAsStream("schema4_4.xsd"));
+        storage.adapt(repository4, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, false});
+        String input4_1 = "<TestEntity1_Extend><id>a</id><mandatory11>a</mandatory11></TestEntity1_Extend>";
+        String input4_2 = "<TestEntity2_Extend><id>b</id><mandatory11>b</mandatory11><mandatory21>b</mandatory21></TestEntity2_Extend>";
+        String input4_3 = "<TestEntity3_Extend><id>c</id><mandatory11>c</mandatory11><mandatory21>c</mandatory21><mandatory31>c</mandatory31></TestEntity3_Extend>";
+        createRecord(storage, factory, repository3, typeNames, new String[] { input4_1, input4_2, input4_3 });
+        
+        storage.close(true);
+    }
+    
+    public void test5_DeleteMandatoryField_Reference() throws Exception { 
+        DataSourceDefinition dataSource = ServerContext.INSTANCE.get().getDefinition("H2-DS3", STORAGE_NAME);
+        Storage storage = new HibernateStorage("Test", StorageType.MASTER);
+        storage.init(dataSource);
+        String[] typeNames = { "TestEntity5_Reference", "TestEntity4_Reference", "TestEntity3_Reference", "TestEntity2_Reference", "TestEntity1_Reference" };
+        String[] tables = { "testentity1_reference", "testentity2_reference", "testentity3_reference", "testentity4_reference", "testentity5_reference" };
+        String[] columns = { "x_mandatory12", "x_mandatory22", "x_mandatory32", "x_mandatory42", "x_mandatory52" };
+        DataRecordReader<String> factory = new XmlStringDataRecordReader();
+        MetadataRepository repository1 = new MetadataRepository();
+        repository1.load(StorageAdaptTest.class.getResourceAsStream("schema5_1.xsd"));
+        storage.prepare(repository1, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {true, true, true, true, true});
+        String input_5 = "<TestEntity5_Reference><id>5</id><mandatory51>m51</mandatory51><mandatory52>m52</mandatory52></TestEntity5_Reference>";
+        String input_4 = "<TestEntity4_Reference><id>4</id><mandatory41>m41</mandatory41><mandatory42>m42</mandatory42></TestEntity4_Reference>";
+        String input_3 = "<TestEntity3_Reference><id>3</id><mandatory31>m31</mandatory31><mandatory32>m32</mandatory32><testEntity4>[4]</testEntity4><testEntity5>[5]</testEntity5></TestEntity3_Reference>";
+        String input_2 = "<TestEntity2_Reference><id>2</id><mandatory21>m21</mandatory21><mandatory22>m22</mandatory22><testEntity4>[4]</testEntity4><testEntity5>[5]</testEntity5></TestEntity2_Reference>";
+        String input_1 = "<TestEntity1_Reference><id>1</id><mandatory11>m11</mandatory11><mandatory12>m12</mandatory12><testEntity2>[2]</testEntity2><testEntity3>[3]</testEntity3></TestEntity1_Reference>";
+        createRecord(storage, factory, repository1, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+               
+        MetadataRepository repository2 = new MetadataRepository();
+        repository2.load(StorageAdaptTest.class.getResourceAsStream("schema5_2.xsd"));
+        storage.adapt(repository2, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, true, true, true, true});
+        input_1 = "<TestEntity1_Reference><id>1</id><mandatory11>m11</mandatory11><testEntity2>[2]</testEntity2><testEntity3>[3]</testEntity3></TestEntity1_Reference>";
+        createRecord(storage, factory, repository2, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+        
+        MetadataRepository repository3 = new MetadataRepository();
+        repository3.load(StorageAdaptTest.class.getResourceAsStream("schema5_3.xsd"));
+        storage.adapt(repository3, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, true, true, true});
+        input_2 = "<TestEntity2_Reference><id>2</id><mandatory21>m21</mandatory21><testEntity4>[4]</testEntity4><testEntity5>[5]</testEntity5></TestEntity2_Reference>";
+        createRecord(storage, factory, repository3, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+        
+        MetadataRepository repository4 = new MetadataRepository();
+        repository4.load(StorageAdaptTest.class.getResourceAsStream("schema5_4.xsd"));
+        storage.adapt(repository4, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, false, true, true});
+        input_3 = "<TestEntity3_Reference><id>3</id><mandatory31>m31</mandatory31><testEntity4>[4]</testEntity4><testEntity5>[5]</testEntity5></TestEntity3_Reference>";
+        createRecord(storage, factory, repository4, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+        
+        MetadataRepository repository5 = new MetadataRepository();
+        repository5.load(StorageAdaptTest.class.getResourceAsStream("schema5_5.xsd"));
+        storage.adapt(repository5, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, false, false, true});
+        input_4 = "<TestEntity4_Reference><id>4</id><mandatory41>m41</mandatory41></TestEntity4_Reference>";
+        createRecord(storage, factory, repository5, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+        
+        MetadataRepository repository6 = new MetadataRepository();
+        repository6.load(StorageAdaptTest.class.getResourceAsStream("schema5_6.xsd"));
+        storage.adapt(repository6, true);
+        assertDatabaseChange(dataSource, tables, columns, new boolean[] {false, false, false, false, false});
+        input_5 = "<TestEntity5_Reference><id>5</id><mandatory51>m51</mandatory51></TestEntity5_Reference>";
+        createRecord(storage, factory, repository6, typeNames, new String[] { input_5, input_4, input_3, input_2, input_1 });
+        
+        storage.close(true);
+    }
+    
+    private void assertDatabaseChange(DataSourceDefinition dataSource, String[] tables, String[] columns, boolean[] exists)
+            throws SQLException {
+        DataSource master = dataSource.getMaster();
+        assertTrue(master instanceof RDBMSDataSource);
+        RDBMSDataSource rdbmsDataSource = (RDBMSDataSource) master;
+        assertEquals(RDBMSDataSource.DataSourceDialect.H2, rdbmsDataSource.getDialectName());
+        Connection connection = DriverManager.getConnection(rdbmsDataSource.getConnectionURL());
+        Statement statement = connection.createStatement();
+        try {
+            for (int i = 0; i < tables.length; i++) {
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tables[i]);
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                boolean hasField = false;
+                for (int j = 1; j <= metaData.getColumnCount(); j++) {
+                    hasField |= columns[i].equalsIgnoreCase(metaData.getColumnName(j));
+                }
+                assertSame(exists[i], hasField);
+            }
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    
+    private void assertExistTables(DataSourceDefinition dataSource, String[] tables, boolean[] exists)
+            throws SQLException {
+        DataSource master = dataSource.getMaster();
+        assertTrue(master instanceof RDBMSDataSource);
+        RDBMSDataSource rdbmsDataSource = (RDBMSDataSource) master;
+        assertEquals(RDBMSDataSource.DataSourceDialect.H2, rdbmsDataSource.getDialectName());
+        Connection connection = DriverManager.getConnection(rdbmsDataSource.getConnectionURL());
+        Statement statement = connection.createStatement();
+        try {
+            for (int i = 0; i < tables.length; i++) {
+                try {
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tables[i]);
+                    if(exists[i] == false) {
+                        fail("Table : '" + tables[i] + "' should not exist anymore.");
+                    }
+                } catch (Exception e) {
+                    if(exists[i]) {
+                        fail("Table : '" + tables[i] + "' should still exist.");
+                    }
+                }
+            }
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    
+    private void createRecord(Storage storage, DataRecordReader<String> factory, MetadataRepository repository,  String[] typeNames, String[] inputs){
+        List<DataRecord> records = new ArrayList<DataRecord>();
+        for (int i = 0; i < typeNames.length; i++) {
+            records.add(factory.read(repository, repository.getComplexType(typeNames[i]), inputs[i]));
+        }
+        try {
+            storage.begin();
+            storage.update(records);
+            storage.commit();
+        } finally {
+            storage.end();
+        }
+    }
 }
