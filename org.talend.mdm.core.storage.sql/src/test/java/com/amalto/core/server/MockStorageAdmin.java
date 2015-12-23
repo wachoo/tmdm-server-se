ca9cd9a07db7e8493d2f6cc37b5dc536b0f200d8
Pingwen Lin <pwlin@talend.com>
2015-12-23 15:06:07 +0800

    TMDM-9192 : issue with deletion of complex optional field (for FieldUpdateAction, new EMPTY value should considered as NULL, else in some Contained  Field scenarios will have issue, see also StorageDocument#clean)

---
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/server/MockStorageAdmin.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+ * 
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
+ */
+
+package com.amalto.core.server;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amalto.core.storage.Storage;
+import com.amalto.core.storage.StorageType;
+
+public class MockStorageAdmin extends StorageAdminImpl {
+
+    private Set<Storage> storages = new HashSet<Storage>();
+
+    @Override
+    public Storage get(String storageName, StorageType type) {
+        for (Storage s : storages) {
+            if (s.getName().equals(storageName) && s.getType() == type) {
+                return s;
+            }
+        }
+        return super.get(storageName, type);
+    }
+
+    public void register(Storage storage) {
+        storages.add(storage);
+    }
+
+    public void forget(Storage storage) {
+        storages.remove(storage);
+    }
+
+    @Override
+    public boolean exist(String storageName, StorageType storageType) {
+        return true;
+    }
+
+}
