62223664593ea1c3a3a855a6939f1f06fca32fdf
Pingwen Lin <pwlin@talend.com>
2016-05-09 15:05:01 +0800

    TMDM-5253 Default Value Rule doesn't work in One to Many relationships. Add Junit Test
    Conflicts:
    	org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/actions/BrowseRecordsActionTest.java
    	org.talend.mdm.webapp.browserecords/src/test/resources/org/talend/mdm/webapp/browserecords/ProductDemo.xsd

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/actions/BrowseRecordsActionTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/actions/BrowseRecordsActionTest.java
@@ -855,7 +855,25 @@ public class BrowseRecordsActionTest extends TestCase {
             }
         }
     }
-    
+
+    public void testSetDefaultValueByExpression() throws Exception {
+        EntityModel entityModel = getViewBean("Product", "ProductDemo.xsd").getBindingEntityModel();
+        entityModel = action.setDefaultValueByExpression(entityModel, "Product", "en");
+
+        TypeModel name = entityModel.getTypeModel("Product/Name");
+        assertEquals("\"MDM Product\"", name.getDefaultValueExpression());
+        assertEquals("MDM Product", name.getDefaultValue());
+
+        TypeModel price = entityModel.getTypeModel("Product/Price");
+        assertEquals("fn:round(100.15)", price.getDefaultValueExpression());
+        assertEquals("100", price.getDefaultValue());
+
+        TypeModel color = entityModel.getTypeModel("Product/Features/Colors/Color");
+        assertEquals("\"White\"", color.getDefaultValueExpression());
+        assertEquals("White", color.getDefaultValue());
+
+    }
+
     private String parsingNodeValue(Document docXml, String xpath, String conceptName) throws Exception {
         NodeList nodes = Util.getNodeList(docXml, xpath.replaceFirst(conceptName + "/", "./"));
         if (nodes.getLength() > 0) {
