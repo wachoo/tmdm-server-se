4fef4b31ed911d03a9edc17d2260295996c4cb2c
Haiting Yin <htyin@talend.com>
2016-07-19 13:54:31 +0800

    TPS-1520 [6.1.1] tMDMInput with one filter return different records size by different batch size(TMDM-9703)
    TMDM-9703 tMDMInput with one filter return different records size by different batch size

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/query/optimization/ImplicitOrderBy.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/query/optimization/ImplicitOrderBy.java
@@ -40,8 +40,6 @@ public class ImplicitOrderBy implements Optimizer {
         case H2:        
         case MYSQL:
         case DB2:
-            // Nothing to do for those databases
-            return;
         case ORACLE_10G:
         case SQL_SERVER:
         case POSTGRES:
@@ -50,7 +48,7 @@ public class ImplicitOrderBy implements Optimizer {
                 Set<TypedExpression> missingOrderByExpressions = getMissingOrderByExpressions(select);
                 if (!missingOrderByExpressions.isEmpty()) {
                     if (LOGGER.isDebugEnabled()) {
-                        LOGGER.debug("Adding implicit order by id to keep consistent order trough pages."); //$NON-NLS-1$
+                        LOGGER.debug("Adding implicit order by id to keep consistent order through pages."); //$NON-NLS-1$
                     }
                     // Check if projection allow use of implicit order by.
                     boolean enableAddOrder = true;
