a14bbe149be68288d295eee7c653d9ec69f14a1b
qcui <qcui@talend.com>
2016-10-28 11:44:40 +0800

    TMDM-10154 : Excel import: display glitch

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemsToolBar.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/client/widget/ItemsToolBar.java
@@ -492,7 +492,7 @@ public class ItemsToolBar extends ToolBar {
             @Override
             public void componentSelected(MenuEvent ce) {
                 final Window window = new Window();
-                window.setSize(480, 280);
+                window.setSize(480, 300);
                 window.setPlain(true);
                 window.setModal(true);
                 window.setBlinkModal(true);
