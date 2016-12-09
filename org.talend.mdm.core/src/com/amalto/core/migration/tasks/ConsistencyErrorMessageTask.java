c71964565d9ab0389bf07aae7aa767b66b59454a
yjli-talend <yjli@talend.com>
2016-12-09 15:29:44 +0800

    TMDM-8795 Add copyright.

--- org.talend.mdm.core/src/com/amalto/core/migration/tasks/ConsistencyErrorMessageTask.java
+++ org.talend.mdm.core/src/com/amalto/core/migration/tasks/ConsistencyErrorMessageTask.java
@@ -14,18 +14,15 @@ import com.amalto.core.objects.transformers.util.TransformerProcessStep;
 import com.amalto.core.objects.transformers.util.TransformerVariablesMapping;
 import com.amalto.core.util.Util;
 
-// ============================================================================
-//
-// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
+/*
+ * Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+ * 
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
+ */
 
 /**
  * see 0020008 Consistency of process results
