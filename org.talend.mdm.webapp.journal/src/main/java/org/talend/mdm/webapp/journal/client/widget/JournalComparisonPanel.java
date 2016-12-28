468127b292615e0e9f516160dc9f7c997220cb5b
zfkang-talend <zfkang@talend.com>
2016-12-28 16:20:35 +0800

    TMDM-7410: Journal previous and next change buttons do not work when change is linked to a reusable type. (#156)

--- org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/widget/JournalComparisonPanel.java
+++ org.talend.mdm.webapp.journal/src/main/java/org/talend/mdm/webapp/journal/client/widget/JournalComparisonPanel.java
@@ -371,10 +371,12 @@ public class JournalComparisonPanel extends ContentPanel {
     }
 
     public void selectTreeNodeByPath(String path) {
-        JournalTreeModel model = modelMap.get(path);
-        if (path.endsWith("[1]") && model == null) { //$NON-NLS-1$
-            model = modelMap.get(path.replace("[1]", "")); //$NON-NLS-1$ //$NON-NLS-2$
+        if (path.contains("[") && path.contains("]")) { //$NON-NLS-1$ //$NON-NLS-2$
+            path = path.replaceAll("\\[\\d+\\]", ""); //$NON-NLS-1$ //$NON-NLS-2$
         }
+
+        JournalTreeModel model = modelMap.get(path);
+
         tree.getSelectionModel().select(false, model);
         tree.scrollIntoView(model);
     }
