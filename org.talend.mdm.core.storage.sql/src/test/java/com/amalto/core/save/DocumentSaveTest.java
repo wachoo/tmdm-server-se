0866484aaca7a9ac49065c2487270e5cce70d00f
zfkang-talend <zfkang@talend.com>
2017-05-03 17:52:01 +0800

    TMDM-10606: [Journal] wrong text value null in Item/newValue after remove value (#251)

--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/save/DocumentSaveTest.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/save/DocumentSaveTest.java
@@ -809,54 +809,54 @@ public class DocumentSaveTest extends TestCase {
                 put("report1", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Houses/House[3]", "[3]", "null" });
-                        add(new String[] { "Houses/House[2]", "[2]", "null" });
+                        add(new String[] { "Houses/House[3]", "[3]", "" });
+                        add(new String[] { "Houses/House[2]", "[2]", "" });
                         add(new String[] { "Houses/House[1]", "[1]", "[3]" });
                     }
                 });
                 put("report2", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Kids/Kid[3]/Name", "k3", "null" });
-                        add(new String[] { "Kids/Kid[3]/Age", "3", "null" });
-                        add(new String[] { "Kids/Kid[2]/Name", "k2", "null" });
-                        add(new String[] { "Kids/Kid[2]/Age", "2", "null" });
-                        add(new String[] { "Kids/Kid[2]/Habits/Habit[3]", "Boxing", "null" });
-                        add(new String[] { "Kids/Kid[2]/Habits/Habit[2]", "Football", "null" });
-                        add(new String[] { "Kids/Kid[2]/Habits/Habit[1]", "Basketball", "null" });
-                        add(new String[] { "Kids/Kid[2]/Habits", "", "null" });
+                        add(new String[] { "Kids/Kid[3]/Name", "k3", "" });
+                        add(new String[] { "Kids/Kid[3]/Age", "3", "" });
+                        add(new String[] { "Kids/Kid[2]/Name", "k2", "" });
+                        add(new String[] { "Kids/Kid[2]/Age", "2", "" });
+                        add(new String[] { "Kids/Kid[2]/Habits/Habit[3]", "Boxing", "" });
+                        add(new String[] { "Kids/Kid[2]/Habits/Habit[2]", "Football", "" });
+                        add(new String[] { "Kids/Kid[2]/Habits/Habit[1]", "Basketball", "" });
+                        add(new String[] { "Kids/Kid[2]/Habits", "", "" });
                     }
                 });
                 put("report3", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Habits/Habit[4]", "Boxing", "null" });
-                        add(new String[] { "Habits/Habit[3]", "Tennis", "null" });
-                        add(new String[] { "Habits/Habit[2]", "Football", "null" });
+                        add(new String[] { "Habits/Habit[4]", "Boxing", "" });
+                        add(new String[] { "Habits/Habit[3]", "Tennis", "" });
+                        add(new String[] { "Habits/Habit[2]", "Football", "" });
                         add(new String[] { "Habits/Habit[1]", "Basketball", "Tennis" });
                     }
                 });
                 put("report4", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Kids/Kid[1]/Habits/Habit[3]", "Tennis", "null" });
-                        add(new String[] { "Kids/Kid[1]/Habits/Habit[2]", "Football", "null" });
+                        add(new String[] { "Kids/Kid[1]/Habits/Habit[3]", "Tennis", "" });
+                        add(new String[] { "Kids/Kid[1]/Habits/Habit[2]", "Football", "" });
                         add(new String[] { "Kids/Kid[1]/Habits/Habit[1]", "Basketball", "Tennis" });
                     }
                 });
                 put("report5", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Kids/Kid[2]/Habits/Habit[3]", "Boxing", "null" });
-                        add(new String[] { "Kids/Kid[2]/Habits/Habit[2]", "Football", "null" });
+                        add(new String[] { "Kids/Kid[2]/Habits/Habit[3]", "Boxing", "" });
+                        add(new String[] { "Kids/Kid[2]/Habits/Habit[2]", "Football", "" });
                         add(new String[] { "Kids/Kid[2]/Habits/Habit[1]", "Basketball", "Boxing" });
                     }
                 });
                 put("report6", new ArrayList<String[]>() {
 
                     {
-                        add(new String[] { "Pets[4]/Pet", "Cow", "null" });
-                        add(new String[] { "Pets[3]/Pet", "Pig", "null" });
+                        add(new String[] { "Pets[4]/Pet", "Cow", "" });
+                        add(new String[] { "Pets[3]/Pet", "Pig", "" });
                         add(new String[] { "Pets[2]/Pet", "Dog", "Cow" });
                         add(new String[] { "Pets[1]/Pet", "Cat", "Dog" });
                     }
@@ -1523,42 +1523,44 @@ public class DocumentSaveTest extends TestCase {
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[2]/newValue");
         assertEquals("detail[1]/features/vendor", path);
         assertEquals("[vendor-original]", oldValue);
-        assertEquals("null", newValue);
+        assertEquals("", newValue);
 
         path = (String) evaluate(doc.getDocumentElement(), "Item[3]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[3]/oldValue");
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[3]/newValue");
         assertEquals("detail[1]/features/boolValue", path);
         assertEquals("true", oldValue);
-        assertEquals("null", newValue);
+        assertEquals("", newValue);
 
         path = (String) evaluate(doc.getDocumentElement(), "Item[4]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[4]/oldValue");
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[4]/newValue");
         assertEquals("detail[1]/features/actor", path);
         assertEquals("actor-original", oldValue);
-        assertEquals("null", newValue);
+        assertEquals("", newValue);
 
         path = (String) evaluate(doc.getDocumentElement(), "Item[5]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[5]/oldValue");
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[5]/newValue");
+
         assertEquals("detail[1]/features", path);
         assertEquals("", oldValue);
-        assertEquals("null", newValue);
+        assertEquals("", newValue);
 
         path = (String) evaluate(doc.getDocumentElement(), "Item[6]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[6]/oldValue");
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[6]/newValue");
+
         assertEquals("detail[1]/boolTest", path);
         assertEquals("true", oldValue);
-        assertEquals("null", newValue);
-
+        assertEquals("", newValue);
+        
         path = (String) evaluate(doc.getDocumentElement(), "Item[7]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[7]/oldValue");
         newValue = (String) evaluate(doc.getDocumentElement(), "Item[7]/newValue");
         assertEquals("detail[1]/ReadOnlyEle", path);
         assertEquals("[readOnlyEle-original]", oldValue);
-        assertEquals("null", newValue);
+        assertEquals("", newValue);
 
         path = (String) evaluate(doc.getDocumentElement(), "Item[8]/path");
         oldValue = (String) evaluate(doc.getDocumentElement(), "Item[8]/oldValue");
