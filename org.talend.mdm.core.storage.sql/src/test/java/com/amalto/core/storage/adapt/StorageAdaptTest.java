c6b4beeca64ba2af0fb83fe2a968b63ef3ecc79d
Haiting Yin <htyin@talend.com>
2018-05-18 10:15:07 +0800

    TMDM-12028 Deleting a foreign key causes MDM to raise an exception - POC!! (#537)

--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/adapt/StorageAdaptTest.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/adapt/StorageAdaptTest.java
@@ -27,8 +27,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import junit.framework.TestCase;
-
 import org.apache.log4j.Logger;
 import org.h2.jdbc.JdbcSQLException;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
@@ -51,6 +49,8 @@ import com.amalto.core.storage.hibernate.LiquibaseSchemaAdapter;
 import com.amalto.core.storage.record.DataRecord;
 import com.amalto.core.storage.record.DataRecordReader;
 import com.amalto.core.storage.record.XmlStringDataRecordReader;
+
+import junit.framework.TestCase;
 
 @SuppressWarnings("nls")
 public class StorageAdaptTest extends TestCase {
@@ -1919,9 +1919,10 @@ public class StorageAdaptTest extends TestCase {
         storage.init(dataSource);
         String[] typeNames = { "Person" };
         String[] tables = { "Person" };
-        String[] columns = { "", "X_ID", "X_BB_X_TALEND_ID", "X_EE", "X_UU_X_TALEND_ID", "X_TALEND_TIMESTAMP", "X_TALEND_TASK_ID" };
+        String[] columns = { "", "X_ID", "X_BB_X_TALEND_ID", "X_EE", "X_UU_X_TALEND_ID", "X_ADDRESS_X_ADDRESSID",
+                "X_TALEND_TIMESTAMP", "X_TALEND_TASK_ID" };
 
-        int[] isNullable = { 0, 0, 0, 0, 0, 0, 1 };
+        int[] isNullable = { 0, 0, 0, 0, 0, 1, 0, 1 };
         DataRecordReader<String> factory = new XmlStringDataRecordReader();
         MetadataRepository repository1 = new MetadataRepository();
         repository1.load(StorageAdaptTest.class.getResourceAsStream("../hibernate/schema2_1.xsd"));
@@ -1941,9 +1942,11 @@ public class StorageAdaptTest extends TestCase {
             assertNull(e2);
         }
 
+        String[] columnsUpdated = { "", "X_ID", "X_BB_X_TALEND_ID", "X_EE", "X_UU_X_TALEND_ID", "X_TALEND_TIMESTAMP",
+                "X_TALEND_TASK_ID" };
         int[] isNullableUpdated = { 0, 0, 1, 1, 1, 0, 1 };
         try {
-            assertColumnNullAble(dataSource, tables, columns, isNullableUpdated);
+            assertColumnNullAble(dataSource, tables, columnsUpdated, isNullableUpdated);
         } catch (SQLException e) {
             assertNull(e);
         }
