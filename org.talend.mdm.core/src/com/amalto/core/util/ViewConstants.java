810108044303571c5014f2e25f62930e87324c93
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2018-11-13 15:11:37 +0800

    TMDM-12670 [REST API] Get a view (#709)
    * TMDM-12670 [REST API] Get a view

---
+++ org.talend.mdm.core/src/com/amalto/core/util/ViewConstants.java
@@ -0,0 +1,54 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.core.util;
+
+public final class ViewConstants {
+
+    public static final String NODE_NAME = "name";
+
+    public static final String NODE_DESCRIPTION = "description";
+
+    public static final String NODE_DATA_MODEL_ID = "dataModelId";
+
+    public static final String VIEW_OBJECT_NAME = "View";
+
+    public static final String NODE_ROLES = "roles";
+
+    public static final String DEFAULT_VIEW_PREFIX = "Browse_items";
+
+    public static final String DEFAULT_VIEW_ALL = "Browse_items_.*";
+
+    public static final String SEARCHABLE_BUSINESS_ELEMENTS = "searchableBusinessElements";
+
+    public static final String VIEWABLE_BUSINESS_ELEMENTS = "viewableBusinessElements";
+
+    public static final String WHERE_CONDITIONS = "whereConditions";
+
+    public static final String SORT_FIELD = "sortField";
+
+    public static final String CUSTOM_FORM = "customForm";
+
+    public static final String IS_ASC = "isAsc";
+
+    public static final String IS_TRANSFORMER_ACTIVE = "isTransformerActive";
+
+    public static final String LEFT_PATH = "leftPath";
+
+    public static final String OPERATOR = "operator";
+
+    public static final String RIGHT_VALUE_OR_PATH = "rightValueOrPath";
+
+    public static final String SPELL_CHECK = "spellCheck";
+
+    public static final String STRING_PREDICATE = "stringPredicate";
+}
