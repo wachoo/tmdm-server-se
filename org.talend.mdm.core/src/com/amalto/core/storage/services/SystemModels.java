37e7a56a99ca0420830faac71545a99e1bde44ff
Haiting Yin <htyin@talend.com>
2018-11-20 15:54:58 +0800

    TMDM-12625 [REST API] HTTP code 200 if data model doesn't exist

--- org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
@@ -220,7 +220,7 @@ public class SystemModels {
         StorageAdmin storageAdmin = ServerContext.INSTANCE.get().getStorageAdmin();
         Storage storage = storageAdmin.get(modelName, StorageType.MASTER);
         if (storage == null) {
-            return getErrorResponse(new IllegalArgumentException(), "Container '" + modelName + "' does not exist."); //$NON-NLS-1$//$NON-NLS-2$
+            return getErrorResponse(new NotFoundException(), StringUtils.EMPTY);
         }
         // Parses new data model version for comparison with existing
         MetadataRepository previousRepository = storage.getMetadataRepository();
