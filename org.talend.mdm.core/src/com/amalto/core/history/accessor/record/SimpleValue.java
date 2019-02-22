8cf40c944cf053ae73874fa64d1899df6876d01d
zfkang-talend <zfkang@talend.com>
2019-02-22 10:23:59 +0800

    TPS-2910 [6.4.1] [RestAPI] GET /data/{containerName}/{type}/{id} fails with HTTP 500 (TMDM-13124)

--- org.talend.mdm.core/src/com/amalto/core/history/accessor/record/SimpleValue.java
+++ org.talend.mdm.core/src/com/amalto/core/history/accessor/record/SimpleValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
  *
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -28,11 +28,19 @@ class SimpleValue implements Setter, Getter {
             record.set(element.field, null);
         }
         if (element.field instanceof ReferenceFieldMetadata) {
-            ComplexTypeMetadata referencedType = ((ReferenceFieldMetadata) element.field).getReferencedType();
-            DataRecord referencedRecord = (DataRecord) StorageMetadataUtils.convert(String.valueOf(value), element.field, referencedType);
-            record.set(element.field, referencedRecord);
+            ReferenceFieldMetadata fieldMetadata = (ReferenceFieldMetadata) element.field;
+            boolean needResetValue = true;
+            if (record.get(element.field) != null) {
+                String oldValue = String.valueOf(record.get(fieldMetadata.getReferencedField()));
+                needResetValue = !value.equals('[' + oldValue + ']');
+            }
+            if (needResetValue) {
+                ComplexTypeMetadata referencedType = fieldMetadata.getReferencedType();
+                DataRecord referencedRecord = (DataRecord) StorageMetadataUtils.convert(value, element.field, referencedType);
+                record.set(element.field, referencedRecord);
+            }
         } else {
-            record.set(element.field, StorageMetadataUtils.convert(String.valueOf(value), element.field));
+            record.set(element.field, StorageMetadataUtils.convert(value, element.field));
         }
     }
 
