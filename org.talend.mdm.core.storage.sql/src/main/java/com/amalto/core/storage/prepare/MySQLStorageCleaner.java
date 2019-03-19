3760421047ea0b3558ea84423af68b495281f643
hw-zhu <hwzhu@talend.com>
2019-03-19 15:07:28 +0800

    TMDM-13179 [Postgres] It's not possible to activate SSL communication between MDM Server and Database

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/prepare/MySQLStorageCleaner.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/prepare/MySQLStorageCleaner.java
@@ -43,7 +43,7 @@ class MySQLStorageCleaner implements StorageCleaner {
                 throw new IllegalArgumentException("Data source '" + dataSource.getName()
                         + "' does not define initialization information.");
             }
-            Connection connection = RDBMSDataSource.getInitedConnection(dataSource);
+            Connection connection = RDBMSDataSource.getConnectionToInit(dataSource);
 
             try {
                 Statement statement = connection.createStatement();
