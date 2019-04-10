1eb0b080d457a8f70d942effe46827caa5533bb3
zfkang-talend <zfkang@talend.com>
2019-04-10 15:09:42 +0800

    TMDM-13229 java.lang.RuntimeException: Unable to load the item after items deletion by batch job (#787)

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/ReferenceEntityBridge.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/ReferenceEntityBridge.java
@@ -246,7 +246,9 @@ public class ReferenceEntityBridge implements TwoWayFieldBridge {
                 name += "." + field.getName(); // x_stores.store.name
                 for (Iterator<?> it = valList.iterator(); it.hasNext();) {
                     Object itemObj = it.next();
-                    createEvaluationClassInternal(name, itemObj, itemObj.getClass(), document, luceneOptions);
+                    if (itemObj != null) {
+                        createEvaluationClassInternal(name, itemObj, itemObj.getClass(), document, luceneOptions);
+                    }
                 }
             } catch (Exception e) {
                 throw new UnsupportedOperationException(
